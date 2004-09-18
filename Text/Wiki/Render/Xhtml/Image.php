d6aea1d3c14c5898b2d38a4c0c32036c05e99f41
Paul M Jones <pmjones@ciaweb.net>
2004-09-18 00:06:15 +0000

    now guesses width and height, and adds a pseudo-center alignment option (thanks Stephane Solliec)
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@168738 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Xhtml/Image.php
+++ Text/Wiki/Render/Xhtml/Image.php
@@ -43,6 +43,46 @@ class Text_Wiki_Render_Xhtml_Image extends Text_Wiki_Render {
     		$href = null;
     	}
     	
+		// stephane@metacites.net -- 25/07/2004 
+		// we make up an align="center" value for the <img> tag
+		// ok, this value doesn't exist, but USERS often want it, 
+		// and as wiki syntax if for USERS who don't know much of 
+		// html and is here to make life easy for THEM, we create
+		// it. Proficient html coders will understand the trick.
+		if (isset($options['attr']['align']) &&
+			$options['attr']['align'] == 'center') {
+			
+	    	unset($options['attr']['align']);
+	    	
+	    	if (! isset($options['attr']['style'])) {
+	    		$options['attr']['style'] = '';
+	    	}
+	    	
+			$options['attr']['style'] .= ' display: block; margin-left: auto; margin-right: auto;';
+		}
+		
+		// stephane@metacites.net -- 25/07/2004 
+		// try to guess width and height
+		if (! isset($options['attr']['width']) &&
+			! isset($options['attr']['height'])) {
+			
+			$imageFile = trim($src,'"');
+			if (strpos($src,'://')) {
+				// discard protocol
+				$imageFile = substr($imageFile,strpos($src,'://')+3); 
+				
+				// discard hostname but keep the leading slash
+				$imageFile = substr($imageFile,strpos($imageFile,'/'));  
+			}
+			
+			$imageSize = @getimagesize($_SERVER['DOCUMENT_ROOT'] . $imageFile);
+			
+			if (is_array($imageSize)) {
+				$options['attr']['width'] = $imageSize[0];
+				$options['attr']['height'] = $imageSize[1];
+			}
+			
+		}
     	// unset these so they don't show up as attributes
     	unset($options['attr']['link']);
     	
