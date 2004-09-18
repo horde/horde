1f7ca2c2a5790fac0b034b3baf4aee538fb6b884
Paul M Jones <pmjones@ciaweb.net>
2004-09-18 21:30:32 +0000

    added 'css' conf key (thanks Bob Glamm)
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@168765 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Xhtml/Anchor.php
+++ Text/Wiki/Render/Xhtml/Anchor.php
@@ -14,12 +14,18 @@
 
 class Text_Wiki_Render_Xhtml_Anchor extends Text_Wiki_Render {
 	
+	var $conf = array(
+		'css' => null
+	);
+	
 	function token($options)
 	{
 		extract($options); // $type, $name
 		
 		if ($type == 'start') {
-			return sprintf('<a id="%s">',$name);
+			$css = $this->formatConf(' class="%s"', 'css');
+			$format = "<a$css id=\"%s\">'";
+			return sprintf($format ,$name);
 		}
 		
 		if ($type == 'end') {
