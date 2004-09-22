7560e88358f4dbae9ac1a437f9e97eefaed65b65
Paul M Jones <pmjones@ciaweb.net>
2004-09-22 22:30:32 +0000

    added HTML translation as part of the pre() method
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@169013 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Xhtml.php
+++ Text/Wiki/Render/Xhtml.php
@@ -2,9 +2,26 @@
 
 class Text_Wiki_Render_Xhtml extends Text_Wiki_Render {
     
+    var $conf = array('translate' => HTML_ENTITIES);
+    
     function pre()
     {
-    	return;
+    	// attempt to translate HTML entities in the source before continuing.
+    	$type = $this->getConf('translate', null);
+    	
+    	// are we translating html?
+    	if ($type) {
+    	
+    		// yes! get the translation table.
+			$xlate = get_html_translation_table($type);
+			
+			// remove the delimiter character it doesn't get translated
+			unset($xlate[$this->wiki->delim]);
+			
+			// translate!
+			$this->wiki->source = strtr($this->wiki->source, $xlate);
+		}
+    	
     }
     
     function post()
