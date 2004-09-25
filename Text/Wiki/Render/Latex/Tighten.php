7c4360b9fc41bf65998b6724cdfdb93523426f2e
Paul M Jones <pmjones@ciaweb.net>
2004-09-25 18:49:56 +0000

    Jeremy Cowgar has kindly submitted a prelminary LaTeX renderer.  Thanks, Jeremy!
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@169208 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Text/Wiki/Render/Latex/Tighten.php
@@ -0,0 +1,10 @@
+<?php
+class Text_Wiki_Render_Latex_Tighten extends Text_Wiki_Render {
+    
+    function token()
+    {
+        echo 'JEREMY';
+    	return '';
+    }
+}
+?>
---
+++ Text/Wiki/Render/Latex/Translatehtml.php
@@ -0,0 +1,9 @@
+<?php
+
+class Text_Wiki_Render_Latex_Translatehtml extends Text_Wiki_Render {
+    function token()
+    {
+    	return '';
+    }
+}
+?>
