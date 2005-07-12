9db0f7ea6ba18e0e5bdddbbc648930d50bc61329
bertrand Gugger <toggg@php.net>
2005-07-12 13:06:42 +0000

    So we can play with it (dev copy), that will constitute next test/*.phpt too,  BBtest.txt for starting development of Text_Wiki_BBCode (not yet CS conform)
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@190579 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ doc/test_Text_Wiki.php
@@ -0,0 +1,36 @@
+<?php
+$html = false;
+$parser = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : 'BBcode';
+$render = isset($_SERVER['argv'][2]) ? $_SERVER['argv'][2] : 'Xhtml';
+if (!isset($_SERVER['argv'][3]) or !is_readable($sou = $_SERVER['argv'][3])) {
+	die("Enter a text file to be processed as 3d argument\n First and second are parser and renderer\n");
+}
+$source = file_get_contents ( $sou, $html);
+
+// load the class file
+require_once('../Text/Wiki/'.$parser.'.php');
+$class = 'Text_Wiki_'.$parser;
+
+// instantiate a Text_Wiki object from the given class
+$wiki =& new $class();
+
+// when rendering XHTML, make sure wiki links point to a
+// specific base URL
+//$wiki->setRenderConf('xhtml', 'wikilink', 'view_url',
+// 'http://example.com/view.php?page=');
+
+// set an array of pages that exist in the wiki
+// and tell the XHTML renderer about them
+//$pages = array('HomePage', 'AnotherPage', 'SomeOtherPage');
+//$wiki->setRenderConf('xhtml', 'wikilink', 'pages', $pages);
+
+// transform the wiki text into given rendering
+$result = $wiki->transform($source, $render);
+
+// display the transformed text
+print( $result);
+
+
+
+
+?>
