738af6b847ef5d19b13b714b022b6dd03275906f
Ben Klang <ben@alkaloid.net>
2005-07-14 05:13:19 +0000

    Form to handle new users
    git-svn-id: https://svn.alkaloid.net/gpl/shout/trunk@55 06cd67b6-e706-0410-b29e-9de616bca6e9

---
+++ users/add.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * $Horde: shout/users/add.php,v 1.0 2005/07/14 01:06:48 ben Exp $
+ *
+ * Copyright 2005 Ben Klang <ben@alkaloid.net>
+ *
+ * See the enclosed file LICENSE for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.php.
+ */
+@define('SHOUT_BASE', dirname(__FILE__) . '/..');
+require_once SHOUT_BASE . '/lib/User.php';
+require_once 'Horde/Variables.php';
+
+$RENDERER = &new Horde_Form_Renderer();
+
+$empty = '';
+$wereerrors = 0;
+
+$vars = &Variables::getDefaultVariables($empty);
+$formname = $vars->get('formname');
+
+$title = _("System Settings");
+
+$UserDetailsForm = &Horde_Form::singleton('UserDetailsForm', $vars);
+$UserDetailsFormValid = $UserDetailsForm->validate($vars, true);
+
+$UserDetailsForm->open($RENDERER, $vars, 'index.php', 'post');
+$UserDetailsForm->preserveVarByPost($vars, "section");
+$UserDetailsForm->preserve($vars);
+$RENDERER->beginActive($ContextForm->getTitle());
+$RENDERER->renderFormActive($ContextForm, $vars);
+$RENDERER->submit();
+$RENDERER->end();
+$ContextForm->close($RENDERER);
