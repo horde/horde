725a2f4b344c6f08b32b76e45cb9b31bcea53772
bertrand Gugger <toggg@php.net>
2005-07-16 06:22:06 +0000

    Class header
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@190775 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/BBCode/Parse/Colortext.php
+++ Text/Wiki/BBCode/Parse/Colortext.php
@@ -18,6 +18,19 @@
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/Text_Wiki
  */
+
+/**
+ * Colored text rule parser class (with nesting) for BBCode.
+ *
+ * @category   Text
+ * @package    Text_Wiki
+ * @author     Bertrand Gugger <bertrand@toggg.com>
+ * @copyright  2005 bertrand Gugger
+ * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Text_Wiki
+ * @see        Text_Wiki_Parse_BBCode::Text_Wiki_Parse_BBCode()
+ */
 class Text_Wiki_Parse_Colortext extends Text_Wiki_Parse_BBCode {
     
     /**
@@ -27,8 +40,7 @@ class Text_Wiki_Parse_Colortext extends Text_Wiki_Parse_BBCode {
      * 
      * @access public
      * @var string
-     * @see parse()
-     * 
+     * @see Text_Wiki_Parse_BBCode::parse()
      */
     
     var $regex = "'(?:\[color=(aqua|black|blue|fuchsia|gray|green|lime|maroon|navy|olive|purple|red|silver|teal|white|yellow|\#[0-9a-f]{6})]|\[/color])'i";
@@ -37,7 +49,7 @@ class Text_Wiki_Parse_Colortext extends Text_Wiki_Parse_BBCode {
      * Generates a replacement for the matched text.  Token options are:
      * - 'type' => ['start'|'end'] The starting or ending point of the
      * colored text.  The text itself is left in the source.
-     * - 'color' => the color indicator
+     * - 'color' => the color indicator (by post synchro for end)
      * 
      * @access public
      * @param array &$matches The array of matches from parse().
