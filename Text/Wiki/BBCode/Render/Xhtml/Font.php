9f22051947d16fd772ca76c1165552858792ce8d
bertrand Gugger <toggg@php.net>
2005-07-16 05:57:10 +0000

    Headers
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@190774 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/BBCode/Render/Xhtml/Font.php
+++ Text/Wiki/BBCode/Render/Xhtml/Font.php
@@ -1,5 +1,31 @@
 <?php
+// vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+/**
+ * BBCode: exra Font rules renderer to size the text
+ *
+ * PHP versions 4 and 5
+ *
+ * @category   Text
+ * @package    Text_Wiki
+ * @author     Bertrand Gugger <bertrand@toggg.com>
+ * @copyright  2005 bertrand Gugger
+ * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Text_Wiki
+ */
 
+/**
+ * Font rule render class (used for BBCode)
+ * 
+ * @category   Text
+ * @package    Text_Wiki
+ * @author     Bertrand Gugger <bertrand@toggg.com>
+ * @copyright  2005 bertrand Gugger
+ * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Text_Wiki
+ * @see        Text_Wiki::Text_Wiki_Render()
+ */
 class Text_Wiki_Render_Xhtml_Font extends Text_Wiki_Render {
     
 /*    var $size = array(
@@ -26,18 +52,13 @@ class Text_Wiki_Render_Xhtml_Font extends Text_Wiki_Render {
 */    
     
     /**
-    * 
-    * Renders a token into text matching the requested format.
-    * 
-    * @access public
-    * 
-    * @param array $options The "options" portion of the token (second
-    * element).
-    * 
-    * @return string The text rendered from the token options.
-    * 
-    */
-    
+      * Renders a token into text matching the requested format.
+      * process the font size option 
+      *
+      * @access public
+      * @param array $options The "options" portion of the token (second element).
+      * @return string The text rendered from the token options.
+      */
     function token($options)
     {
         if ($options['type'] == 'end') {
