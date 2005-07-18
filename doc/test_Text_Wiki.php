68c6bdf6c2f8c1d0c21f7a0d616fac3200587ccc
bertrand Gugger <toggg@php.net>
2005-07-18 16:35:50 +0000

    Test data completed , the test script works now not only as cli but also from HTTP
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@190923 c90b9560-bf6c-de11-be94-00142212c4b1

--- doc/test_Text_Wiki.php
+++ doc/test_Text_Wiki.php
@@ -1,15 +1,42 @@
 <?php
-$html = false;
-$parser = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : 'BBcode';
-$render = isset($_SERVER['argv'][2]) ? $_SERVER['argv'][2] : 'Xhtml';
-if (!isset($_SERVER['argv'][3]) or !is_readable($sou = $_SERVER['argv'][3])) {
-	die("Enter a text file to be processed as 3d argument\n First and second are parser and renderer\n");
+set_include_path( realpath(dirname(__FILE__).'/../'). PATH_SEPARATOR . get_include_path() );
+$parser = $render = $source = '';
+$plist = array('Default', 'BBCode');
+$rlist = array('Xhtml', 'Plain', 'Latex');
+if ($_SERVER['PHP_SELF']) {
+    $html = true;
+    if (isset($_REQUEST['example'])) {
+        $_REQUEST['source'] = file_get_contents ($_REQUEST['exchoice']);
+        if (preg_match('#(\b'.implode('\b|\b', $plist).'\b)#i',
+                         $_REQUEST['source'], $match)) {
+            $_REQUEST['parser'] = $match[1];
+        }
+        $_REQUEST['translate'] = true;
+    }
+    $parser = isset($_REQUEST['parser']) ? $_REQUEST['parser'] : 'BBCode';
+    $render = isset($_REQUEST['render']) ? $_REQUEST['render'] : 'Xhtml';
+    $source = isset($_REQUEST['source']) ? $_REQUEST['source'] : '';
+    if (!isset($_REQUEST['translate'])) {
+        echo bldHtml($parser, $render, $source, '', $plist, $rlist);
+        die();
+    }
+} else {
+    $html = false;
+    $parser = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : 'BBCode';
+    $render = isset($_SERVER['argv'][2]) ? $_SERVER['argv'][2] : 'Xhtml';
+    if (!isset($_SERVER['argv'][3]) or !is_readable($sou = $_SERVER['argv'][3])) {
+    	die("Enter a text file to be processed as 3d argument\n First and second are parser and renderer\n");
+    }
+    $source = file_get_contents ($sou);
 }
-$source = file_get_contents ( $sou, $html);
-
 // load the class file
-require_once('../Text/Wiki/'.$parser.'.php');
-$class = 'Text_Wiki_'.$parser;
+if ($parser != 'Default') {
+    require_once 'Text/Wiki/'.$parser.'.php';
+    $class = 'Text_Wiki_'.$parser;
+} else  {
+    require_once 'Text/Wiki.php';
+    $class = 'Text_Wiki';
+}
 
 // instantiate a Text_Wiki object from the given class
 $wiki =& new $class();
@@ -29,9 +56,70 @@ $wiki->setRenderConf('xhtml', 'code', 'css_filename', 'codefilename');
 $result = $wiki->transform($source, $render);
 
 // display the transformed text
-print( $result);
+if ($html) {
+    echo bldHtml($parser, $render, $source, $result, $plist, $rlist);
+} else {
+    echo $result;
+}
+function bldHtml($parser, $render, $source, $result, $plist, $rlist) {
+    $optparser = $optrender = $optexample = '';
+    foreach($plist as $opt) {
+          $optparser .= "<option value='{$opt}'".
+            ($opt == $parser? " selected" : "").
+            ">{$opt}</option>\n";
+    }
+    foreach($rlist as $opt) {
+          $optrender .= "<option value='{$opt}'".
+            ($opt == $render? " selected" : "").
+            ">{$opt}</option>\n";
+    }
+    $hresult = htmlentities($result);
+    if ($render != 'Xhtml') {
+        $result = '';
+    }
+    return <<<EOT
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 
-
-
-
+<head>
+  <title>PEAR::Text_Wiki Demo</title>
+  <meta name="AUTHOR" content="bertrand Gugger / Toggg">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="KEYWORDS" content="PEAR, Wiki, Parse, Render, Convert, PHP, BBCode, Xhtml, Plain, Latex">
+  <!--
+  <link rel="stylesheet" type="text/css" href="test_Text_Wiki.css">
+  -->
+  <script language="javascript" type="text/javascript">
+  // <!--
+  
+  // -->
+  </script>
+</head>
+<body>
+<h3>PEAR::Text_Wiki Demo</h3>
+<div style="float: left;">
+<FORM method="post">
+Translate from
+<SELECT name="parser">{$optparser}</SELECT>
+to
+<SELECT name="render">{$optrender}</SELECT>
+<br />
+<textarea name="source" cols="80" rows="25">{$source}</textarea>
+<br />
+<INPUT type="submit" name="translate" value="translate"> or choose
+<SELECT name="exchoice">{$optexample}</SELECT>
+and
+<INPUT type="submit" name="example" value="example">
+</FORM>
+</div>
+<div style="float: down;">
+{$hresult}
+</div>
+<div>
+{$result}
+</div>
+</body>
+</html>
+EOT;
+}
 ?>
