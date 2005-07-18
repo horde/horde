37abc6313a5878f419b66fa5abb8d1893815f566
bertrand Gugger <toggg@php.net>
2005-07-18 14:43:19 +0000

    Sorry for the last empty line and . without spaces Note: headers will be done as a whole
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@190900 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Latex/Bold.php
+++ Text/Wiki/Render/Latex/Bold.php
@@ -1,4 +1,4 @@
 <?php
-include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'Strong.php';
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Strong.php';
 class Text_Wiki_Render_Latex_Bold extends Text_Wiki_Render_Latex_Strong {}
 ?>
--- Text/Wiki/Render/Latex/Italic.php
+++ Text/Wiki/Render/Latex/Italic.php
@@ -1,5 +1,5 @@
 <?php
-include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'Emphasis.php';
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Emphasis.php';
 class Text_Wiki_Render_Latex_Italic extends Text_Wiki_Render_Latex_Emphasis {
 }
 ?>
