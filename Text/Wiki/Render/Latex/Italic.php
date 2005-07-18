a0569051acaef9cc422e6f0d41b2406b6b26ef76
bertrand Gugger <toggg@php.net>
2005-07-18 15:16:48 +0000

    No more ugly include, actually rendering latex, other classes for Latex renderer to be done (currently Xhtml) ....
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@190906 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Latex/Italic.php
+++ Text/Wiki/Render/Latex/Italic.php
@@ -1,5 +1,28 @@
 <?php
-include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Emphasis.php';
 class Text_Wiki_Render_Latex_Italic extends Text_Wiki_Render_Latex_Emphasis {
+    
+    /**
+    * 
+    * Renders a token into text matching the requested format.
+    * 
+    * @access public
+    * 
+    * @param array $options The "options" portion of the token (second
+    * element).
+    * 
+    * @return string The text rendered from the token options.
+    * 
+    */
+    
+    function token($options)
+    {
+        if ($options['type'] == 'start') {
+            return '\textit{';
+        }
+        
+        if ($options['type'] == 'end') {
+            return '}';
+        }
+    }
 }
 ?>
