2873d7e3faa99e1c5649e4bac2c4204193737c1e
Justin Patrin <justinpatrin@php.net>
2005-07-23 20:25:31 +0000

    Checking in missing Render classes (I thought I'd added these before..)
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@191293 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Text/Wiki/Render/Cowiki.php
@@ -0,0 +1,16 @@
+<?php
+
+class Text_Wiki_Render_CoWiki extends Text_Wiki_Render {
+    
+    function pre()
+    {
+        return;
+    }
+    
+    function post()
+    {
+        return;
+    }
+    
+}
+?>
---
+++ Text/Wiki/Render/Doku.php
@@ -0,0 +1,16 @@
+<?php
+
+class Text_Wiki_Render_Doku extends Text_Wiki_Render {
+    
+    function pre()
+    {
+        return;
+    }
+    
+    function post()
+    {
+        return;
+    }
+    
+}
+?>
