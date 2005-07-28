cc02bdb354b27ebbea58ba2e4ca08325dac21b37
bertrand Gugger <toggg@php.net>
2005-07-28 10:59:23 +0000

    A true test === for translate format conf is enough and works now !
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@191693 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Xhtml.php
+++ Text/Wiki/Render/Xhtml.php
@@ -1,13 +1,13 @@
 <?php
 
 class Text_Wiki_Render_Xhtml extends Text_Wiki_Render {
-    
+
     var $conf = array(
     	'translate' => HTML_ENTITIES,
     	'quotes'    => ENT_COMPAT,
     	'charset'   => 'ISO-8859-1'
     );
-    
+
     function pre()
     {
         // attempt to translate HTML entities in the source.
@@ -15,55 +15,55 @@ class Text_Wiki_Render_Xhtml extends Text_Wiki_Render {
         $type = $this->getConf('translate', HTML_ENTITIES);
         $quotes = $this->getConf('quotes', ENT_COMPAT);
         $charset = $this->getConf('charset', 'ISO-8859-1');
-        
+
         // have to check null and false because HTML_ENTITIES is a zero
-        if ($type !== null && $type !== false && $type == HTML_ENTITIES) {
-        
+        if ($type === HTML_ENTITIES) {
+
 			// keep a copy of the translated version of the delimiter
 			// so we can convert it back.
 			$new_delim = htmlentities($this->wiki->delim, $quotes, $charset);
-			
+
 			// convert the entities.  we silence the call here so that
-			// errors about charsets don't pop up, per counsel from 
+			// errors about charsets don't pop up, per counsel from
 			// Jan at Horde.  (http://pear.php.net/bugs/bug.php?id=4474)
 			$this->wiki->source = @htmlentities(
 				$this->wiki->source,
 				$quotes,
 				$charset
 			);
-			
+
 			// re-convert the delimiter
 			$this->wiki->source = str_replace(
 				$new_delim, $this->wiki->delim, $this->wiki->source
 			);
-			
-		} elseif ($type == HTML_SPECIALCHARS) {
-			
+
+		} elseif ($type === HTML_SPECIALCHARS) {
+
 			// keep a copy of the translated version of the delimiter
 			// so we can convert it back.
 			$new_delim = htmlspecialchars($this->wiki->delim, $quotes,
 			    $charset);
-			
+
 			// convert the entities.  we silence the call here so that
-			// errors about charsets don't pop up, per counsel from 
+			// errors about charsets don't pop up, per counsel from
 			// Jan at Horde.  (http://pear.php.net/bugs/bug.php?id=4474)
 			$this->wiki->source = @htmlspecialchars(
 				$this->wiki->source,
 				$quotes,
 				$charset
 			);
-			
+
 			// re-convert the delimiter
 			$this->wiki->source = str_replace(
 				$new_delim, $this->wiki->delim, $this->wiki->source
 			);
 		}
     }
-    
+
     function post()
     {
         return;
     }
-    
+
 }
 ?>
