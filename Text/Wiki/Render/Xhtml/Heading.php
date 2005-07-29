f393df588a79eb66c66a429b2bfdbad362ad6f92
bertrand Gugger <toggg@php.net>
2005-07-29 13:45:46 +0000

    Introduces tikiwiki/justin's collapsing in headers, needs some refactoting though
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@191801 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Xhtml/Heading.php
+++ Text/Wiki/Render/Xhtml/Heading.php
@@ -1,7 +1,7 @@
 <?php
 
 class Text_Wiki_Render_Xhtml_Heading extends Text_Wiki_Render {
-    
+
     var $conf = array(
         'css_h1' => null,
         'css_h2' => null,
@@ -10,19 +10,54 @@ class Text_Wiki_Render_Xhtml_Heading extends Text_Wiki_Render {
         'css_h5' => null,
         'css_h6' => null
     );
-    
+
     function token($options)
     {
+        static $jsOutput = false;
         // get nice variable names (id, type, level)
         extract($options);
-        
-        if ($type == 'start') {
+
+        switch($type) {
+        case 'start':
             $css = $this->formatConf(' class="%s"', "css_h$level");
-            return "<h$level$css id=\"$id\">";
-        }
-        
-        if ($type == 'end') {
-            return "</h$level>\n";
+            return '
+<h'.$level.$css.' id="'.$id.'"'.($collapse !== null ? ' onclick="hideTOC(\''.$id.'\');"' : '').'>';
+
+        case 'end':
+            return '</h'.$level.'>
+'.($collapse !== null ? '<a id="'.$id.'__link" href="javascript:void();" onclick="hideTOC(\''.$id.'\')">['.($collapse ? '+' : '-').']</a>
+' : '');
+        case 'startContent':
+            if ($collapse !== null) {
+                if ($jsOutput) {
+                    $js = '';
+                } else {
+                    $js = '
+<script language="javascript">
+function hideTOC(id) {
+    div = document.getElementById(id+"__content");
+    link = document.getElementById(id+"__link");
+    if (div.style.display == "none") {
+        div.style.display = "";
+        link.innerHTML = "[-]";
+    } else {
+        div.style.display = "none";
+        link.innerHTML = "[+]";
+    }
+}
+</script>
+';
+                }
+            } else {
+                $js = '';
+            }
+            return  $js.'
+<div style="'.($collapse === true ? 'display: none; ' : '').'padding: 0px; margin: 0px; border: none;" id="'.$id.'__content">
+';
+        case 'endContent':
+            return '
+</div>
+';
         }
     }
 }
