befa7d6df4e85d509e2515bf80493f1f82231b56
bertrand Gugger <toggg@php.net>
2005-08-05 19:42:59 +0000

    Making the rule recursive
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@192528 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/BBCode/Parse/Font.php
+++ Text/Wiki/BBCode/Parse/Font.php
@@ -4,7 +4,7 @@
  * BBCode: Parses for font size tag.
  *
  * This class implements a Text_Wiki_Rule to find source text with size
- * as defined by text surrounded by [size=...] ... [/size]
+ * as defined by text surrounded by [size=...] ... [/size] (nesting)
  * On parsing, the text itself is left in place, but the starting and ending
  * tags are replaced with tokens.
  *
@@ -20,7 +20,7 @@
  */
 
 /**
- * Font rule parser class (with nesting) for BBCode. ([size=...]...)
+ * Font rule parser class (with nesting) for BBCode. ([size=...]...[/size])
  *
  * @category   Text
  * @package    Text_Wiki
@@ -29,9 +29,9 @@
  * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
  * @version    Release: @package_version@
  * @link       http://pear.php.net/package/Text_Wiki
- * @see        Text_Wiki_Parse_BBCode::Text_Wiki_Parse_BBCode()
+ * @see        Text_Wiki_Parse_BBCode::Text_Wiki_Parse()
  */
-class Text_Wiki_Parse_Font extends Text_Wiki_Parse_BBCode {
+class Text_Wiki_Parse_Font extends Text_Wiki_Parse {
 
     /**
      * The regular expression used to parse the source text and find
@@ -41,36 +41,48 @@ class Text_Wiki_Parse_Font extends Text_Wiki_Parse_BBCode {
      * @var string
      * @see parse()
      */
-    var $regex = "#\[size=(\d+)]|\[/size]#i";
+    var $regex = "#\[size=(\d+)]((?:((?R))|.)*?)\[/size]#msi";
+
+    /**
+     * The current font nesting depth, starts by zero
+     *
+     * @access private
+     * @var int
+     */
+    var $_level = 0;
 
     /**
      * Generates a replacement for the matched text.  Token options are:
-     * - 'type' => ['start'|'end'] The starting or ending point of the
-     * sized text.  The text itself is left in the source.
-     * - 'size' => the size indicator (by post synchro for end)
+     * - 'type' => ['start'|'end'] The starting or ending point of the sized text.
+     * The text itself is left in the source but may content bested blocks
+     * - 'level' => the level of nesting (starting 0)
+     * - 'size' => the size indicator
      *
      * @access public
      * @param array &$matches The array of matches from parse().
-     * @return string A delimited token to be used as a start/end
-     * placeholder in the source text surrounding the text to be
-     * sized.
+     * @return string Delimited by start/end tokens to be used as
+     * placeholder in the source text surrounding the text to be sized.
      */
     function process(&$matches)
     {
-        // end tag ?
-        if (!isset($matches[1])) {
-            if (!($start = $this->getStart())) {
-                return $matches;
-            }
-            return $this->wiki->addToken(
-                $this->rule,
-                array(
-                    'type' => 'end',
-                    'size' => $this->wiki->tokens[$start['tok']][1]['size']
-                )
+        // nested block ?
+        if (array_key_exists(3, $matches)) {
+            $this->_level++;
+            $expsub = preg_replace_callback(
+                $this->regex,
+                array(&$this, 'process'),
+                $matches[2]
             );
+            $this->_level--;
+        } else {
+            $expsub = $matches[2];
         }
-        return $this->addStart(array('size' => $matches[1]), $matches);
+
+        // builds the option array
+        $options = array('type' => 'start', 'level' => $this->_level, 'size' => $matches[1]);
+        $statok = $this->wiki->addToken($this->rule, $options);
+        $options['type'] = 'end';
+        return $statok . $expsub . $this->wiki->addToken($this->rule, $options);
     }
 }
 ?>
