9affb004125c9a4c32b1bba839fa725f3d07e9ac
bertrand Gugger <toggg@php.net>
2005-08-06 18:43:19 +0000

    Base class for Mediawiki parsing
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@192618 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Text/Wiki/Mediawiki.php
@@ -0,0 +1,108 @@
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+/**
+ * Parse structured wiki text and render into arbitrary formats such as XHTML.
+ * This is the Text_Wiki extension for Mediawiki markup
+ *
+ * PHP versions 4 and 5
+ *
+ * @category   Text
+ * @package    Text_Wiki
+ * @author     Bertrand Gugger <bertrand@toggg.com>
+ * @author     Paul M. Jones <pmjones@php.net>
+ * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Text_Wiki
+ */
+
+/**
+ * "master" class for handling the management and convenience
+ */
+require_once('Text/Wiki.php');
+
+/**
+ * Base Text_Wiki handler class extension for Mediawiki markup
+ *
+ * @category   Text
+ * @package    Text_Wiki
+ * @author     Bertrand Gugger <bertrand@toggg.com>
+ * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Text_Wiki
+ * @see        Text_Wiki::Text_Wiki()
+ */
+class Text_Wiki_Mediawiki extends Text_Wiki {
+    var $rules = array(
+        'Prefilter',
+        'Delimiter',
+        'Code',
+        'Plugin',
+        'Function',
+        'Html',
+        'Raw',
+        'Preformatted',
+        'Include',
+        'Embed',
+        'Page',
+        'Anchor',
+        'Heading',
+        'Toc',
+        'Titlebar',
+        'Horiz',
+        'Break',
+        'Blockquote',
+        'List',
+        'Deflist',
+        'Table',
+        'Box',
+        'Image',
+        'Phplookup',
+        'Center',
+        'Newline',
+        'Paragraph',
+        'Url',
+        //'Freelink',
+        'Colortext',
+        'Wikilink',
+        'Strong',
+        'Bold',
+        'Emphasis',
+        'Italic',
+        'Underline',
+        'Tt',
+        'Superscript',
+        'Subscript',
+        'Specialchar',
+        'Revise',
+        'Interwiki',
+        'Tighten'
+    );
+
+    function Text_Wiki_Mediawiki($rules = null) {
+        parent::Text_Wiki($rules);
+        $this->addPath('parse', $this->fixPath(dirname(__FILE__)).'Parse/Mediawiki');
+        $this->addPath('render', $this->fixPath(dirname(__FILE__)).'Render');
+    }
+
+    function getTokens($rules = null, $originalIndex = false)
+    {
+        if (is_null($rules)) {
+            return $this->tokens;
+        } else {
+            settype($rules, 'array');
+            $result = array();
+            foreach ($this->tokens as $key => $val) {
+                if (in_array($val[0], $rules)) {
+                    if ($originalIndex) {
+                        $result[$key] = $val;
+                    } else {
+                        $result[] = $val;
+                    }
+                }
+            }
+            return $result;
+        }
+    }
+}
+
+?>
