826f5e9f600c453fa51503b7a8a20c3ffba5dfff
bertrand Gugger <toggg@php.net>
2005-09-14 07:33:22 +0000

    Output the source of the image
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@196023 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Tiki/Image.php
+++ Text/Wiki/Render/Tiki/Image.php
@@ -1,22 +1,22 @@
 <?php
 class Text_Wiki_Render_Tiki_Image extends Text_Wiki_Render {
-    
+
     /**
-    * 
+    *
     * Renders a token into text matching the requested format.
-    * 
+    *
     * @access public
-    * 
+    *
     * @param array $options The "options" portion of the token (second
     * element).
-    * 
+    *
     * @return string The text rendered from the token options.
-    * 
+    *
     */
-    
+
     function token($options)
     {
-        $img = '{img';
+        $img = '{img src=' . $options['src'];
         foreach ($options['attr'] as $var => $val) {
             $img .= ' '.$var.'='.$val;
         }
