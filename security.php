dc5b83baa04c5a0418a033c6d15a5be5bf0786c6
Ben Klang <ben@alkaloid.net>
2005-11-10 05:45:49 +0000

    XML test
    git-svn-id: https://svn.alkaloid.net/gpl/shout/trunk@85 06cd67b6-e706-0410-b29e-9de616bca6e9

---
+++ security.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * $Horde: shout/users.php,v 0.1 2005/07/13 10:01:01 ben Exp $
+ *
+ * Copyright 2005 Ben Klang <ben@alkaloid.net>
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ */
+
+@define('SHOUT_BASE', dirname(__FILE__));
+$shout_configured = (@is_readable(SHOUT_BASE . '/config/conf.php'));# &&
+    #@is_readable(SHOUT_BASE . '/config/prefs.php'));
+if (!$shout_configured) {
+    require SHOUT_BASE . '/../lib/Test.php';
+    Horde_Test::configFilesMissing('Shout', SHOUT_BASE,
+    array('conf.php', 'prefs.php'));
+}
+
+require_once SHOUT_BASE . '/lib/base.php';
+require_once SHOUT_BASE . '/lib/Shout.php';
+
+Shout::getApplist();
