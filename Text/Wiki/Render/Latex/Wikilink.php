355284ca10763308467efaba1828e9b430abd1b6
bertrand Gugger <toggg@php.net>
2005-11-18 19:53:11 +0000

    Only variables should be assigned by reference (bug #6010)
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@200807 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Latex/Wikilink.php
+++ Text/Wiki/Render/Latex/Wikilink.php
@@ -7,27 +7,27 @@ class Text_Wiki_Render_Latex_Wikilink extends Text_Wiki_Render {
         'new_url'  => 'http://example.com/new.php?page=%s',
         'new_text' => '?'
     );
-    
+
     /**
-    * 
+    *
     * Renders a token into XHTML.
-    * 
+    *
     * @access public
-    * 
+    *
     * @param array $options The "options" portion of the token (second
     * element).
-    * 
+    *
     * @return string The text rendered from the token options.
-    * 
+    *
     */
-    
+
     function token($options)
     {
         // make nice variable names (page, anchor, text)
         extract($options);
-        
+
         // are we checking page existence?
-        $list =& $this->getConf('pages');
+        $list = $this->getConf('pages');
         if (is_array($list)) {
             // yes, check against the page list
             $exists = in_array($page, $list);
@@ -35,15 +35,15 @@ class Text_Wiki_Render_Latex_Wikilink extends Text_Wiki_Render {
             // no, assume it exists
             $exists = true;
         }
-        
+
         // convert *after* checking against page names so as not to mess
         // up what the user typed and what we're checking.
         $page = htmlspecialchars($page);
         $anchor = htmlspecialchars($anchor);
         $text = htmlspecialchars($text);
-        
+
         $href = $this->getConf('view_url');
-            
+
         if (strpos($href, '%s') === false) {
             // use the old form (page-at-end)
             $href = $href . $page . $anchor;
@@ -51,7 +51,7 @@ class Text_Wiki_Render_Latex_Wikilink extends Text_Wiki_Render {
             // use the new form (sprintf format string)
             $href = sprintf($href, $page . $anchor);
         }
-        
+
         // get the CSS class and generate output
         $css = $this->formatConf(' class="%s"', 'css');
         return "$text\\footnote\{$href}";
