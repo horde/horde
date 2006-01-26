9641e97f729baf015443edd4b407dd1e2005e811
Ben Klang <ben@alkaloid.net>
2006-01-26 22:51:11 +0000

    Adding comments
    git-svn-id: https://svn.alkaloid.net/gpl/shout/trunk@261 06cd67b6-e706-0410-b29e-9de616bca6e9

--- lib/defines.php
+++ lib/defines.php
@@ -9,10 +9,30 @@
  *
  * @package Shout
  */
+# Branches
 @define('SHOUT_ASTERISK_BRANCH', "ou=Asterisk");
 @define('SHOUT_USERS_BRANCH', "ou=Customers");
+
+# Object Classes - Users
 @define('SHOUT_USER_OBJECTCLASS', "asteriskUser");
 
+# Object Classes - Contexts
+@define('SHOUT_CONTEXT_CUSTOMERS_OBJECTCLASS', 'vofficeCustomer');
+@define('SHOUT_CONTEXT_EXTENSIONS_OBJECTCLASS', 'asteriskExtensions');
+@define('SHOUT_CONTEXT_MOH_OBJECTCLASS', 'asteriskMusiconHold');
+@define('SHOUT_CONTEXT_CONFERENCE_OBJECTCLASS', 'asteriskMeetMe');
+@define('SHOUT_CONTEXT_VOICEMAIL_OBJECTCLASS', 'asteriskVoicemail');
+
+# Attributes - Dialplans
+@define('SHOUT_DIALPLAN_EXTENSIONLINE_ATTRIBUTE', 'asteriskExtensionLine');
+@define('SHOUT_DIALPLAN_INCLUDE_ATTRIBUTE', 'asteriskIncludeLine');
+@define('SHOUT_DIALPLAN_IGNOREPAT_ATTRIBUTE', 'asteriskIgnorePat');
+@define('SHOUT_DIALPLAN_BARELINE_ATTRIBUTE', 'asteriskExtensionBareLine');
+
+# Attributes - Accounts
+@define('SHOUT_ACCOUNT_ID_ATTRIBUTE', 'vofficeCustomerNumber');
+
+# Perms Fields
 @define('SHOUT_CONTEXT_ALL', 0xFFF);
 @define('SHOUT_CONTEXT_NONE', 0);
 @define('SHOUT_CONTEXT_CUSTOMERS', 1 << 0);
@@ -20,18 +40,3 @@
 @define('SHOUT_CONTEXT_MOH', 1 << 2);
 @define('SHOUT_CONTEXT_CONFERENCE', 1 << 3);
 @define('SHOUT_CONTEXT_VOICEMAIL', 1 << 4);
-
-# FIXME Maybe these should be params instead?
-@define('SHOUT_CONTEXT_CUSTOMERS_OBJECTCLASS', 'vofficeCustomer');
-@define('SHOUT_CONTEXT_EXTENSIONS_OBJECTCLASS', 'asteriskExtensions');
-@define('SHOUT_CONTEXT_MOH_OBJECTCLASS', 'asteriskMusiconHold');
-@define('SHOUT_CONTEXT_CONFERENCE_OBJECTCLASS', 'asteriskMeetMe');
-@define('SHOUT_CONTEXT_VOICEMAIL_OBJECTCLASS', 'asteriskVoicemail');
-
-@define('SHOUT_DIALPLAN_EXTENSIONLINE_ATTRIBUTE', 'asteriskExtensionLine');
-@define('SHOUT_DIALPLAN_INCLUDE_ATTRIBUTE', 'asteriskIncludeLine');
-@define('SHOUT_DIALPLAN_IGNOREPAT_ATTRIBUTE', 'asteriskIgnorePat');
-@define('SHOUT_DIALPLAN_BARELINE_ATTRIBUTE', 'asteriskExtensionBareLine');
-
-# FIXME Maybe these should be params instead?
-@define('SHOUT_ACCOUNT_ID_ATTRIBUTE', 'vofficeCustomerNumber');
