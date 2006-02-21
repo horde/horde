8f17da40d1e0d7f1dc6c9fdc9126bafc8d193216
Del Elson <delatbabel@php.net>
2006-02-21 08:10:59 +0000

    Whitespace changes only.
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@207653 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Parse/Default/Code.php
+++ Text/Wiki/Parse/Default/Code.php
@@ -1,25 +1,25 @@
 <?php
 
 /**
-* 
+*
 * Parses for text marked as a code example block.
-* 
+*
 * @category Text
-* 
+*
 * @package Text_Wiki
-* 
+*
 * @author Paul M. Jones <pmjones@php.net>
-* 
+*
 * @license LGPL
-* 
+*
 * @version $Id$
-* 
+*
 */
 
 /**
-* 
+*
 * Parses for text marked as a code example block.
-* 
+*
 * This class implements a Text_Wiki_Parse to find sections marked as code
 * examples.  Blocks are marked as the string <code> on a line by itself,
 * followed by the inline code example, and terminated with the string
@@ -28,36 +28,35 @@
 * with <pre>...</pre> tags when rendered as XHTML.
 *
 * @category Text
-* 
+*
 * @package Text_Wiki
-* 
+*
 * @author Paul M. Jones <pmjones@php.net>
-* 
+*
 */
 
 class Text_Wiki_Parse_Code extends Text_Wiki_Parse {
-    
-    
+
     /**
-    * 
+    *
     * The regular expression used to find source text matching this
     * rule.
-    * 
+    *
     * @access public
-    * 
+    *
     * @var string
-    * 
+    *
     */
-    
+
 /*    var $regex = '/^(\<code( .+)?\>)\n(.+)\n(\<\/code\>)(\s|$)/Umsi';*/
     var $regex = ';^<code(\s[^>]*)?>((?:(?R)|.)*?)\n</code>(\s|$);msi';
-    
+
     /**
-    * 
+    *
     * Generates a token entry for the matched text.  Token options are:
-    * 
+    *
     * 'text' => The full matched text, not including the <code></code> tags.
-    * 
+    *
     * @access public
     *
     * @param array &$matches The array of matches from parse().
@@ -66,33 +65,33 @@ class Text_Wiki_Parse_Code extends Text_Wiki_Parse {
     * the source text.
     *
     */
-    
+
     function process(&$matches)
     {
         // are there additional attribute arguments?
         $args = trim($matches[1]);
-        
+
         if ($args == '') {
             $options = array(
                 'text' => $matches[2],
                 'attr' => array('type' => '')
             );
         } else {
-        	// get the attributes...
-        	$attr = $this->getAttrs($args);
-        	
-        	// ... and make sure we have a 'type'
-        	if (! isset($attr['type'])) {
-        		$attr['type'] = '';
-        	}
-        	
-        	// retain the options
+            // get the attributes...
+            $attr = $this->getAttrs($args);
+
+            // ... and make sure we have a 'type'
+            if (! isset($attr['type'])) {
+                $attr['type'] = '';
+            }
+
+            // retain the options
             $options = array(
                 'text' => $matches[2],
                 'attr' => $attr
             );
         }
-        
+
         return $this->wiki->addToken($this->rule, $options) . $matches[3];
     }
 }
