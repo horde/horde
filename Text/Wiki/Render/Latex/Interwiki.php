6a4645bf0a5aaac7a511ead548761cb2f9a4e143
bertrand Gugger <toggg@php.net>
2006-02-25 05:10:10 +0000

    Allow parsers to give the full url in token options instead of the site + wiki page Urlencode the page name Removed unused configuration option 'target'
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@207915 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Latex/Interwiki.php
+++ Text/Wiki/Render/Latex/Interwiki.php
@@ -7,8 +7,7 @@ class Text_Wiki_Render_Latex_Interwiki extends Text_Wiki_Render {
             'MeatBall' => 'http://www.usemod.com/cgi-bin/mb.pl?%s',
             'Advogato' => 'http://advogato.org/%s',
             'Wiki'       => 'http://c2.com/cgi/wiki?%s'
-        ),
-        'target' => '_blank'
+        )
     );
     
     
@@ -27,31 +26,30 @@ class Text_Wiki_Render_Latex_Interwiki extends Text_Wiki_Render {
     
     function token($options)
     {
-        $site = $options['site'];
-        $page = $options['page'];
         $text = $options['text'];
-        
-        if (isset($this->conf['sites'][$site])) {
-            $href = $this->conf['sites'][$site];
+        if (isset($options['url'])) {
+            // calculated by the parser (e.g. Mediawiki)
+            $href = $options['url'];
         } else {
-            return $text;
-        }
-        
-        // old form where page is at end,
-        // or new form with %s placeholder for sprintf()?
-        if (strpos($href, '%s') === false) {
-            // use the old form
-            $href = $href . $page;
-        } else {
-            // use the new form
-            $href = sprintf($href, $page);
-        }
-        
-        // allow for alternative targets
-        $target = $this->getConf('target', '');
-        
-        if ($target && trim($target) != '') {
-            $target = " target=\"$target\"";
+            $site = $options['site'];
+            // toggg 2006/02/05 page name must be url encoded (e.g. may contain spaces)
+            $page = $this->urlEncode($options['page']);
+
+            if (isset($this->conf['sites'][$site])) {
+                $href = $this->conf['sites'][$site];
+            } else {
+                return $text;
+            }
+
+            // old form where page is at end,
+            // or new form with %s placeholder for sprintf()?
+            if (strpos($href, '%s') === false) {
+                // use the old form
+                $href = $href . $page;
+            } else {
+                // use the new form
+                $href = sprintf($href, $page);
+            }
         }
         
         return "$text\\footnote\{$href}";
