36d1c731625df67b8098d0206ebb2c9099c40b23
Moritz Venn <ritzmo@php.net>
2006-12-05 14:46:59 +0000

    dunno if someone ever used this, but has been broken for quite some time.
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@224398 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Render/Docbook/Wikilink.php
+++ Text/Wiki/Render/Docbook/Wikilink.php
@@ -82,8 +82,8 @@ class Text_Wiki_Render_Docbook_Wikilink extends Text_Wiki_Render {
 
         // convert *after* checking against page names so as not to mess
         // up what the user typed and what we're checking.
-        $page = $this->urlEncode(($page);
-        $anchor = $this->urlEncode(($anchor);
+        $page = $this->urlEncode($page);
+        $anchor = $this->urlEncode($anchor);
         $text = $this->textEncode($text);
 
         // does the page exist?
