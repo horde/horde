d1bfcec37a434fb9a66e9f1f2686ae7b034de2df
Moritz Venn <ritzmo@php.net>
2007-07-12 20:25:50 +0000

    Implement #8503, thanks Brian
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@239632 c90b9560-bf6c-de11-be94-00142212c4b1

--- Text/Wiki/Mediawiki.php
+++ Text/Wiki/Mediawiki.php
@@ -36,6 +36,7 @@ class Text_Wiki_Mediawiki extends Text_Wiki {
         'Prefilter',
         'Delimiter',
         'Code',
+        'Comment',
         'Preformatted',
 //        'Plugin',
 //        'Function',
