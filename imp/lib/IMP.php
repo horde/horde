93a1983c5b753f5407eaaced3cdb6f2d82cb7c05
Michael M Slusarz <slusarz@curecanti.org>
2008-11-23 11:07:22 -0700

    Merge from HEAD: 1.733

--- imp/lib/IMP.php
+++ imp/lib/IMP.php
@@ -124,11 +124,11 @@ class IMP
     static public function checkRequestToken($slug, $token)
     {
         if (empty($_SESSION['horde_form_secrets'][$token])) {
-            return PEAR::raiseError(_("We cannot verify that this request was really sent by you. It could be a malicious request."));
+            return PEAR::raiseError(_("We cannot verify that this request was really sent by you. It could be a malicious request. If you intended to perform this action, you can retry it now."));
         }
 
         if ($_SESSION['horde_form_secrets'][$token] + $GLOBALS['conf']['server']['token_lifetime'] < time()) {
-            return PEAR::raiseError(sprintf(_("This request cannot be completed because the link you followed or the form you submitted was only valid for %d minutes"), round($GLOBALS['conf']['server']['token_lifetime'] / 60)));
+            return PEAR::raiseError(sprintf(_("This request cannot be completed because the link you followed or the form you submitted was only valid for %d minutes. Please try again now."), round($GLOBALS['conf']['server']['token_lifetime'] / 60)));
         }
 
         return true;
