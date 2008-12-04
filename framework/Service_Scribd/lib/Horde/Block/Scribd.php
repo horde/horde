4747b51acb3590313722d94d454301f593cb9b22
Chuck Hagenbuch <chuck@horde.org>
2008-12-04 11:56:55 -0500

    store the block I was working on here also

---
+++ framework/Service_Scribd/lib/Horde/Block/Scribd.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package Horde_Block
+ */
+class Horde_Block_Horde_Scribd extends Horde_Block {
+
+    var $_app = 'horde';
+
+    function _params()
+    {
+    }
+
+    /**
+     * The title to go in this block.
+     *
+     * @return string   The title text.
+     */
+    function _title()
+    {
+        return _("Scribd Documents");
+    }
+
+    /**
+     * The content to go in this block.
+     *
+     * @return string   The content
+     */
+    function _content()
+    {
+        $docs = $this->_list();
+        if (is_array($this->_docs)) {
+            $html = '';
+            $count = 0;
+            foreach ($this->_feed as $entry) {
+                if ($count++ > $this->_params['limit']) {
+                    break;
+                }
+                $html .= '<a href="' . $entry->link. '"';
+                if (empty($this->_params['details'])) {
+                    $html .= ' title="' . htmlspecialchars(strip_tags($entry->description())) . '"';
+                }
+                $html .= '>' . htmlspecialchars($entry->title) . '</a>';
+                if (!empty($this->_params['details'])) {
+                    $html .= '<br />' .  htmlspecialchars(strip_tags($entry->description())). "<br />\n";
+                }
+                $html .= '<br />';
+            }
+            return $html;
+        } elseif (is_string($docs)) {
+            return $docs;
+        } else {
+            return '';
+        }
+    }
+
+    function _list()
+    {
+    }
+
+}
