1326b8c744395a1a9fc7dd0e02054da195ff2873
Chuck Hagenbuch <chuck@horde.org>
2008-12-24 14:19:52 -0500

    routes can be relative to the app now, since we set the mapper prefix appropriately in rampage.php

--- content/config/routes.php
+++ content/config/routes.php
@@ -1,7 +1,4 @@
 <?php
-
-$_root = ltrim(dirname($_SERVER['PHP_SELF']), '/');
-
 /**
  * References:
  * http://code.google.com/apis/gdata/docs/2.0/reference.html#Queries
@@ -13,30 +10,30 @@ $_root = ltrim(dirname($_SERVER['PHP_SELF']), '/');
 //   typeId:    restrict matches to tags that have been applied to objects with type $typeId
 //   userId:    restrict matches to tags that have been applied by $userId
 //   objectId:  restrict matches to tags that have been applied to $objectId
-$mapper->connect($_root . '/tags', array('controller' => 'tag', 'action' => 'searchTags'));
-$mapper->connect($_root . '/tags.:(format)', array('controller' => 'tag', 'action' => 'searchTags'));
+$mapper->connect('tags', array('controller' => 'tag', 'action' => 'searchTags'));
+$mapper->connect('tags.:(format)', array('controller' => 'tag', 'action' => 'searchTags'));
 
 // List objects. At least a content type, or more specific parameters, are
 // required; listing all objects is not allowed.
-$mapper->connect($_root . '/objects', array('controller' => 'tag', 'action' => 'searchObjects'));
-$mapper->connect($_root . '/objects.:(format)', array('controller' => 'tag', 'action' => 'searchObjects'));
+$mapper->connect('objects', array('controller' => 'tag', 'action' => 'searchObjects'));
+$mapper->connect('objects.:(format)', array('controller' => 'tag', 'action' => 'searchObjects'));
 
 // List users. Specific parameters are required as listing all users is not
 // allowed.
-$mapper->connect($_root . '/users', array('controller' => 'tag', 'action' => 'searchUsers'));
-$mapper->connect($_root . '/users.:(format)', array('controller' => 'tag', 'action' => 'searchUsers'));
+$mapper->connect('users', array('controller' => 'tag', 'action' => 'searchUsers'));
+$mapper->connect('users.:(format)', array('controller' => 'tag', 'action' => 'searchUsers'));
 
 
 // Tag an object. Required POST parameters are: tags (array or string list) and
 // objectId. userId is inferred from the authenticated user:
-$mapper->connect($_root . '/tag', array('controller' => 'tag', 'action' => 'tag'));
+$mapper->connect('tag', array('controller' => 'tag', 'action' => 'tag'));
 
 // Untag an object. Required POST parameters are: tags (array or string list)
 // and objectId. userId is inferred from the authenticated user:
-$mapper->connect($_root . '/untag', array('controller' => 'tag', 'action' => 'untag'));
+$mapper->connect('untag', array('controller' => 'tag', 'action' => 'untag'));
 
 
 // Local route overrides
-if (file_exists($CONTENT_DIR . '/config/routes.local.php')) {
-    include $CONTENT_DIR . '/config/routes.local.php';
+if (file_exists(dirname(__FILE__) . '/routes.local.php')) {
+    include dirname(__FILE__) . '/routes.local.php';
 }
