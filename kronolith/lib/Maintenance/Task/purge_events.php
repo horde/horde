73e4b103d66c8a8e9a42e088d1b0f2ec70faf78c
Michael J. Rubinsky <mrubinsk@horde.org>
2009-01-15 10:17:04 -0500

    This is a Horde_Date now, not an integer

--- kronolith/lib/Maintenance/Task/purge_events.php
+++ kronolith/lib/Maintenance/Task/purge_events.php
@@ -27,7 +27,7 @@ class Maintenance_Task_purge_events extends Maintenance_Task {
          * 'purge_events_keep'.  An event will be deleted if it has an end
          * time prior to this time. */
         $del_time = new Horde_Date($_SERVER['REQUEST_TIME']);
-        $del_time -= $prefs->getValue('purge_events_keep');
+        $del_time->mday -= $prefs->getValue('purge_events_keep');
         $del_time->correct();
 
         /* Need to have PERMS_DELETE on a calendar to delete events from it */
