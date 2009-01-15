c446abf8c64a0ed6408cdb98bde1bea117c553d0
Chuck Hagenbuch <chuck@horde.org>
2009-01-14 21:20:39 -0500

    fix ngettext parameters

--- imp/lib/Views/ShowMessage.php
+++ imp/lib/Views/ShowMessage.php
@@ -301,7 +301,7 @@ class IMP_Views_ShowMessage
         if (count($atc_parts) || (count($display_ids) > 2)) {
             $result['atc_label'] = ($show_parts == 'all')
                 ? _("Parts")
-                : sprintf(ngettext("%d Attachment", "%d Attachments", $atc_parts), $atc_parts);
+                : sprintf(ngettext("%d Attachment", "%d Attachments", count($atc_parts)), count($atc_parts));
             $result['atc_download'] = (count($display_ids) > 2)
                 ? Horde::link($imp_contents->urlView($imp_contents->getMIMEMessage(), 'download_all')) . '[' . _("Save All") . ']</a>'
                 : '';
