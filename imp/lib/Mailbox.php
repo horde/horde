4da5cec41bc20263880c4c19d7b81f5cbc9f39a9
Chuck Hagenbuch <chuck@horde.org>
2009-01-18 23:25:17 -0500

    i had empty arrays in the cache, so this check wasn't sufficient to fetch and store the info

--- imp/lib/Mailbox.php
+++ imp/lib/Mailbox.php
@@ -189,7 +189,7 @@ class IMP_Mailbox
                         (($preview === 2) ||
                          !$GLOBALS['prefs']->getValue('preview_show_unread') ||
                          !in_array('\\seen', $v['flags']))) {
-                        if (!isset($preview_info[$k])) {
+                        if (empty($preview_info[$k])) {
                             $imp_contents = IMP_Contents::singleton($k . IMP::IDX_SEP . $mbox);
                             if (is_a($imp_contents, 'PEAR_Error')) {
                                 $preview_info[$k] = array('IMPpreview' => '', 'IMPpreviewc' => false);
