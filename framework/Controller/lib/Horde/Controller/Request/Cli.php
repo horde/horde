d0f4253bf4358852e741658fc98174cb103afa04
Chuck Hagenbuch <chuck@horde.org>
2009-01-24 15:49:26 -0500

    initial command-line request and response classes

---
+++ framework/Controller/lib/Horde/Controller/Request/Cli.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright 2007 Maintainable Software, LLC
+ * Copyright 2008-2009 The Horde Project (http://www.horde.org/)
+ *
+ * @author     Mike Naberezny <mike@maintainable.com>
+ * @author     Derek DeVries <derek@maintainable.com>
+ * @author     Chuck Hagenbuch <chuck@horde.org>
+ * @license    http://opensource.org/licenses/bsd-license.php
+ * @category   Horde
+ * @package    Horde_Controller
+ * @subpackage Request
+ */
+
+/**
+ * Represents a command line invocation.
+ *
+ * @author     Mike Naberezny <mike@maintainable.com>
+ * @author     Derek DeVries <derek@maintainable.com>
+ * @author     Chuck Hagenbuch <chuck@horde.org>
+ * @license    http://opensource.org/licenses/bsd-license.php
+ * @category   Horde
+ * @package    Horde_Controller
+ * @subpackage Request
+ */
+class Horde_Controller_Request_Cli extends Horde_Controller_Request_Base
+{
+    /**
+     * Command line arguments
+     */
+    protected $_argv;
+
+    /**
+     */
+    public function __construct($options = array())
+    {
+        parent::__construct($options);
+
+        $parser = new Horde_Argv_Parser(array('allowUnknownArgs' => true));
+        list($this->_argv, $args) = $parser->parseArgs();
+        if (count($args) != 1) {
+            throw new Horde_Controller_Exception("unknown command: " . implode(' ', $args));
+        }
+        $this->_path = $args[0];
+    }
+
+    public function getUri()
+    {
+        return $this->getPath();
+    }
+
+    public function getPath()
+    {
+        return $this->_path;
+    }
+
+    /**
+     * Get all command line parameters.
+     * some wacky loops to make sure that nested values in one
+     * param list don't overwrite other nested values
+     *
+     * @return  array
+     */
+    public function getParameters()
+    {
+        $allParams = array();
+        $paramArrays = array($this->_pathParams, $this->_argv);
+
+        foreach ($paramArrays as $params) {
+            foreach ((array)$params as $key => $value) {
+                if (!is_array($value) || !isset($allParams[$key])) {
+                    $allParams[$key] = $value;
+                } else {
+                    $allParams[$key] = array_merge($allParams[$key], $value);
+                }
+            }
+        }
+        return $allParams;
+    }
+
+}
