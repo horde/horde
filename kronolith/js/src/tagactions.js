322354d7b63c7fe13dd0685ac11352eaa6bd6d48
Michael J. Rubinsky <mrubinsk@horde.org>
2009-01-31 11:16:21 -0500

    Move the toggle function to panel.inc since we won't necessarily have any tags (so we wouldn't have attached the tagactions).
    Compress the tagactions.js script

---
+++ kronolith/js/src/tagactions.js
@@ -0,0 +1,32 @@
+function addTag(resource, type, endpoint)
+{
+    if (!$('newtags-input_' + resource).value.blank()) {
+        var params = new Object();
+        params.imple="TagActions/action=add/resource=" + resource + "/type=" + type + "/tags=" + $('newtags-input_' + resource).value;
+        new Ajax.Updater({success:'tags_' + resource},
+                         endpoint,
+                         {
+                             method: 'post',
+                             parameters: params,
+                             onComplete: function() {$('newtags-input_' + resource).value = "";}
+                         }
+        );
+    }
+
+    return true;
+}
+
+function removeTag(resource, type, tagid, endpoint)
+{
+    var params = new Object();
+    params.imple = "TagActions/action=remove/resource=" + resource + "/type=" + type + "/tags=" + tagid;
+    new Ajax.Updater({success:'tags_' + resource},
+                     endpoint,
+                     {
+                         method: 'post',
+                         parameters: params
+                     }
+    );
+    
+    return true;
+}
