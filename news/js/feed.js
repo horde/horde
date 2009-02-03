d38dac42a97bdf6512ca14c06ab1ac776b804812
Duck (Jakob Munih) <duck@obala.net>
2009-02-03 16:50:05 +0100

    Add js copressed file

--- news/js/feed.js
+++ news/js/feed.js
@@ -1,24 +1,2 @@
 
-function getFeed() {
-    RedBox.loading();
-    new Ajax.Updater('feed_content',
-                    document.feed_select.action,
-                    {
-                        onComplete: function(){ RedBox.close();},
-                        parameters: { feed_id: $F('feed_id') },
-                        onFailure: function(){ RedBox.close(); alert('Error'); },
-                        asynchronous: true
-                    });
-}
-
-function send_news_mail() {
-    RedBox.loading();
-    new Ajax.Updater('mail_send',
-                    document.mail_send.action,
-                    {
-                        onComplete: function(){ RedBox.close();},
-                        onFailure: function(){ RedBox.close(); alert('Error'); },
-                        asynchronous: true,
-                        parameters: { fee: $F('email') }
-                    });
-}
+function getFeed(){RedBox.loading();new Ajax.Updater('feed_content',document.feed_select.action,{onComplete:function(){RedBox.close();},parameters:{feed_id:$F('feed_id')},onFailure:function(){RedBox.close();alert('Error');},asynchronous:true});}
