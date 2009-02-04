ebaa57f73e90aac971a778800f86ee601e12be84
Chuck Hagenbuch <chuck@horde.org>
2009-02-04 17:07:12 -0500

    copy repeater tests from chronic

--- framework/Date_Parser/test/Horde/Date/Repeater/DayTest.php
+++ framework/Date_Parser/test/Horde/Date/Repeater/DayTest.php
@@ -17,6 +17,7 @@ class Horde_Date_Repeater_DayTest extends PHPUnit_Framework_TestCase
         $repeater = new Horde_Date_Repeater_Day();
         $repeater->now = new Horde_Date('2009-01-01');
         $this->assertEquals('(2009-01-02 00:00:00..2009-01-03 00:00:00)', (string)$repeater->next('future'));
+        $this->assertEquals('(2008-12-31 00:00:00..2009-01-01 00:00:00)', (string)$repeater->next('past'));
     }
 
 }
