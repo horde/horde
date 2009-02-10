79176634bcec327bf1cf6e76c10cc35779671bc2
Mike Naberezny <mike@naberezny.com>
2009-02-09 20:54:19 -0800

    Added Horde_Log_Filter_Suppress.

---
+++ framework/Log/lib/Horde/Log/Filter/Suppress.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Horde Log package
+ *
+ * This package is based on Zend_Log from the Zend Framework
+ * (http://framework.zend.com).  Both that package and this
+ * one were written by Mike Naberezny and Chuck Hagenbuch.
+ *
+ * @category Horde
+ * @package  Horde_Log
+ * @subpackage Filters
+ * @author   Mike Naberezny <mike@maintainable.com>
+ * @author   Chuck Hagenbuch <chuck@horde.org>
+ * @license  http://opensource.org/licenses/bsd-license.php BSD
+ */
+
+/**
+ * @category Horde
+ * @package  Horde_Log
+ * @subpackage Filters
+ * @author   Mike Naberezny <mike@maintainable.com>
+ * @author   Chuck Hagenbuch <chuck@horde.org>
+ * @license  http://opensource.org/licenses/bsd-license.php BSD
+ */
+class Horde_Log_Filter_Suppress implements Horde_Log_Filter_Interface
+{
+    /**
+     * @var boolean
+     */
+    protected $_accept = true;
+
+    /**
+     * This is a simple boolean filter.
+     *
+     * Call suppress(true) to suppress all log events.
+     * Call suppress(false) to accept all log events.
+     *
+     * @param  boolean  $suppress  Should all log events be suppressed?
+     * @return  void
+     */
+    public function suppress($suppress)
+    {
+        $this->_accept = (! $suppress);
+    }
+
+    /**
+     * Returns TRUE to accept the message, FALSE to block it.
+     *
+     * @param  array    $event    event data
+     * @return boolean            accepted?
+     */
+    public function accept($event)
+    {
+        return $this->_accept;
+    }
+
+}
---
+++ framework/Log/test/Horde/Log/Filter/SuppressTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Horde Log package
+ *
+ * This package is based on Zend_Log from the Zend Framework
+ * (http://framework.zend.com).  Both that package and this
+ * one were written by Mike Naberezny and Chuck Hagenbuch.
+ *
+ * @category Horde
+ * @package  Horde_Log
+ * @subpackage UnitTests
+ * @author   Mike Naberezny <mike@maintainable.com>
+ * @author   Chuck Hagenbuch <chuck@horde.org>
+ * @license  http://opensource.org/licenses/bsd-license.php BSD
+ */   
+
+/**
+ * @category Horde
+ * @package  Horde_Log
+ * @subpackage UnitTests
+ * @author   Mike Naberezny <mike@maintainable.com>
+ * @author   Chuck Hagenbuch <chuck@horde.org>
+ * @license  http://opensource.org/licenses/bsd-license.php BSD
+ */ 
+class Horde_Log_Filter_SuppressTest extends PHPUnit_Framework_TestCase 
+{
+    public function setUp()
+    {
+        $this->filter = new Horde_Log_Filter_Suppress();
+    }
+    
+    public function testSuppressIsInitiallyOff()
+    {
+        $this->assertTrue($this->filter->accept(array()));
+    }
+
+    public function testSuppressOn()
+    {
+        $this->filter->suppress(true);
+        $this->assertFalse($this->filter->accept(array()));
+        $this->assertFalse($this->filter->accept(array()));
+    }
+
+    public function testSuppressOff()
+    {
+        $this->filter->suppress(false);
+        $this->assertTrue($this->filter->accept(array()));
+        $this->assertTrue($this->filter->accept(array()));
+    }
+    
+    public function testSuppressCanBeReset()
+    {
+        $this->filter->suppress(true);
+        $this->assertFalse($this->filter->accept(array()));
+        $this->filter->suppress(false);
+        $this->assertTrue($this->filter->accept(array()));
+        $this->filter->suppress(true);
+        $this->assertFalse($this->filter->accept(array()));
+    }
+}
