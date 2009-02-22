d1c22b2bdbe60773c6945daca2cf78c0098e4028
Michael M Slusarz <slusarz@curecanti.org>
2009-02-22 02:17:40 -0700

    Import Horde_Token from CVS HEAD

---
+++ framework/Token/lib/Horde/Token.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * The Horde_Token:: class provides a common abstracted interface into the
+ * various token generation mediums. It also includes all of the
+ * functions for retrieving, storing, and checking tokens.
+ *
+ * Copyright 1999-2009 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author  Max Kalika <max@horde.org>
+ * @author  Chuck Hagenbuch <chuck@horde.org>
+ * @package Horde_Token
+ */
+class Horde_Token
+{
+    /**
+     * Singleton instances.
+     *
+     * @var array
+     */
+    static protected $_instances = array();
+
+    /**
+     * Hash of parameters necessary to use the chosen backend.
+     *
+     * @var array
+     */
+    protected $_params = array();
+
+    /**
+     * Attempts to return a concrete Horde_Token instance based on $driver.
+     *
+     * @param mixed $driver  The type of concrete Horde_Token subclass to
+     *                       return. If $driver is an array, then we will look
+     *                       in $driver[0]/lib/Token/ for the subclass
+     *                       implementation named $driver[1].php.
+     * @param array $params  A hash containing any additional configuration or
+     *                       connection parameters a subclass might need.
+     *
+     * @return Horde_Token  The newly created concrete Horde_Token instance, or
+     *                      false an error.
+     */
+    static public function getInstance($driver, $params = array())
+    {
+        if (is_array($driver)) {
+            list($app, $driver) = $driver;
+        }
+
+        $driver = basename($driver);
+        $class = ($driver == 'none')
+            ? 'Horde_Token';
+            : 'Horde_Token_' . ucfirst($driver);
+
+        if (!class_exists($class)) {
+            /* If driver class doesn't exist or the driver is not
+             * available just default to the parent class, and it is
+             * not necessary to warn about degraded service. */
+            $class = 'Horde_Token';
+        }
+
+        return new $class($params);
+    }
+
+    /**
+     * Attempts to return a reference to a concrete Horde_Token instance based
+     * on $driver.
+     *
+     * It will only create a new instance if no Horde_Token instance with the
+     * same parameters currently exists.
+     *
+     * This should be used if multiple types of token generators (and, thus,
+     * multiple Horde_Token instances) are required.
+     *
+     * This method must be invoked as:
+     * <code>$var = Horde_Token::singleton();</code>
+     *
+     * @param mixed $driver  The type of concrete Horde_Token subclass to
+     *                       return. If $driver is an array, then we will look
+     *                       in $driver[0]/lib/Token/ for the subclass
+     *                       implementation named $driver[1].php.
+     * @param array $params  A hash containing any additional configuration or
+     *                       connection parameters a subclass might need.
+     *
+     * @return Horde_Token  The concrete Horde_Token reference, or false on
+     *                      error.
+     */
+    static public function singleton($driver, $params = array())
+    {
+        ksort($params);
+        $sig = hash('md5', serialize(array($driver, $params)));
+
+        if (!isset(self::$_instances[$sig])) {
+            self::$_instances[$sig] = Horde_Token::getInstance($driver, $params);
+        }
+
+        return self::$_instances[$sig];
+    }
+
+    /**
+     * Constructor.
+     */
+    protected function __construct($params)
+    {
+        $this->_params = $params;
+    }
+
+    /**
+     * TODO
+     */
+    public function encodeRemoteAddress()
+    {
+        return isset($_SERVER['REMOTE_ADDR'])
+            ? base64_encode($_SERVER['REMOTE_ADDR'])
+            : '';
+    }
+
+    /**
+     * Generates a connection id and returns it.
+     *
+     * @param string $seed  A unique ID to be included in the token.
+     *
+     * @return string  The generated id string.
+     */
+    public function generateId($seed = '')
+    {
+        require_once 'Horde/Util.php';
+        return Util::uriB64Encode(pack('H*', sha1(uniqid(mt_rand(), true) . $seed . (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : ''))));
+    }
+
+    /**
+     * Checks if the given token has been previously used. First
+     * purges all expired tokens. Then retrieves current tokens for
+     * the given ip address. If the specified token was not found,
+     * adds it.
+     *
+     * @param string $token  The value of the token to check.
+     *
+     * @return boolean  True if the token has not been used, false otherwise.
+     */
+    public function verify($token)
+    {
+        $this->purge();
+        $exists = $this->exists($token);
+        if (is_a($exists, 'PEAR_Error')) {
+            return $exists;
+        } elseif ($exists) {
+            return false;
+        } else {
+            return $this->add($token);
+        }
+    }
+
+    /**
+     * This is an abstract method that should be overridden by a
+     * subclass implementation. The base implementation allows all
+     * token values.
+     */
+    public function exists()
+    {
+        return false;
+    }
+
+    /**
+     * This is an abstract method that should be overridden by a
+     * subclass implementation. The base implementation allows all
+     * token values.
+     */
+    public function add()
+    {
+        return true;
+    }
+
+    /**
+     * This is an abstract method that should be overridden by a
+     * subclass implementation. The base implementation allows all
+     * token values.
+     */
+    public function purge()
+    {
+        return true;
+    }
+
+}
---
+++ framework/Token/lib/Horde/Token/File.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Token tracking implementation for local files.
+ *
+ * Optional parameters:<pre>
+ *   'token_dir'  The directory where to keep token files.
+ *   'timeout'    The period (in seconds) after which an id is purged.
+ *                Defaults to 86400 (i.e. 24 hours).</pre>
+ *
+ * Copyright 1999-2009 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author  Max Kalika <max@horde.org>
+ * @package Horde_Token
+ */
+class Horde_Token_file extends Horde_Token
+{
+    /**
+     * Handle for the open file descriptor.
+     *
+     * @var resource
+     */
+    protected $_fd = false;
+
+    /**
+     * Boolean indicating whether or not we have an open file descriptor.
+     *
+     * @var boolean
+     */
+    protected $_connected = false;
+
+    /**
+     * Create a new file based token-tracking container.
+     *
+     * @param array $params  A hash containing storage parameters.
+     */
+    protected function __construct($params = array())
+    {
+        parent::__construct($params);
+
+        /* Choose the directory to save the stub files. */
+        if (!isset($this->_params['token_dir'])) {
+            $this->_params['token_dir'] = Util::getTempDir();
+        }
+
+        /* Set timeout to 24 hours if not specified. */
+        if (!isset($this->_params['timeout'])) {
+            $this->_params['timeout'] = 86400;
+        }
+    }
+
+    /**
+     * Destructor.
+     */
+    protected function __destruct()
+    {
+        $this->_disconnect();
+    }
+
+    /**
+     * Deletes all expired connection id's from the SQL server.
+     *
+     * @return boolean  True on success, a PEAR_Error object on failure.
+     */
+    public function purge()
+    {
+        // Make sure we have no open file descriptors before unlinking
+        // files.
+        if (!$this->_disconnect()) {
+            return PEAR::raiseError('Unable to close file descriptors');
+        }
+
+        /* Build stub file list. */
+        if (!($dir = opendir($this->_params['token_dir']))) {
+            return PEAR::raiseError('Unable to open token directory');
+        }
+
+        /* Find expired stub files */
+        while (($dirEntry = readdir($dir)) != '') {
+            if (preg_match('|^conn_\w{8}$|', $dirEntry) && (time() - filemtime($this->_params['token_dir'] . '/' . $dirEntry) >= $this->_params['timeout'])) {
+                if (!@unlink($this->_params['token_dir'] . '/' . $dirEntry)) {
+                    return PEAR::raiseError('Unable to purge token file.');
+                }
+            }
+        }
+
+        closedir($dir);
+        return true;
+    }
+
+    /**
+     * TODO
+     */
+    public function exists($tokenID)
+    {
+        if (is_a(($result = $this->_connect($tokenID)), 'PEAR_Error')) {
+            return $result;
+        }
+
+        /* Find already used IDs. */
+        $fileContents = file($this->_params['token_dir'] . '/conn_' . $this->encodeRemoteAddress());
+        if ($fileContents) {
+            $iMax = count($fileContents);
+            for ($i = 0; $i < $iMax; $i++) {
+                if (chop($fileContents[$i]) == $tokenID) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * TODO
+     */
+    public function add($tokenID)
+    {
+        if (is_a(($result = $this->_connect($tokenID)), 'PEAR_Error')) {
+            return $result;
+        }
+
+        /* Write the entry. */
+        fwrite($this->_fd, "$tokenID\n");
+
+        /* Return an error if the update fails, too. */
+        if (!$this->_disconnect()) {
+            return PEAR::raiseError('Failed to close token file cleanly.');
+        }
+
+        return true;
+    }
+
+    /**
+     * Opens a file descriptor to a new or existing file.
+     *
+     * @return boolean  True on success, a PEAR_Error object on failure.
+     */
+    protected function _connect($tokenID)
+    {
+        if (!$this->_connected) {
+
+            // Open a file descriptor to the token stub file.
+            $this->_fd = @fopen($this->_params['token_dir'] . '/conn_' . $this->encodeRemoteAddress(), 'a');
+            if (!$this->_fd) {
+                return PEAR::raiseError('Failed to open token file.');
+            }
+
+            $this->_connected = true;
+        }
+
+        return true;
+    }
+
+    /**
+     * Closes the file descriptor.
+     *
+     * @return boolean  True on success, false on failure.
+     */
+    protected function _disconnect()
+    {
+        if ($this->_connected) {
+            $this->_connected = false;
+            return fclose($this->_fd);
+        }
+
+        return true;
+    }
+
+}
