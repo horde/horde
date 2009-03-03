20e8526a93be1743232fb5d387a467f3599db091
Michael M Slusarz <slusarz@curecanti.org>
2009-03-02 21:34:35 -0700

    Update e-mail address.

--- framework/Imap_Client/lib/Horde/Imap/Client/Exception.php
+++ framework/Imap_Client/lib/Horde/Imap/Client/Exception.php
@@ -7,7 +7,7 @@
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
  *
- * @author   Michael Slusarz <slusarz@curecanti.org>
+ * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
  * @package  Horde_Imap_Client
  */
