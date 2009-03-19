555a74591d10029987573de8005490ca5e89d785
Jan Schneider <jan@horde.org>
2009-03-19 10:53:37 +0100

    ws

--- kronolith/templates/calendar_info.php
+++ kronolith/templates/calendar_info.php
@@ -16,20 +16,20 @@
 <?php endif; ?>
 <p>
  <?php printf(_("Local calendar owned by %s."), Kronolith::getUserName($calendar->get('owner'))) ?>
- <?php echo _("To subscribe to this calendar from another calendar program, use this URL: ") ?>
+ <?php echo _("To subscribe to this calendar from another calendar program, use this URL:") ?>
 </p>
 <p class="calendar-info-url">
  <?php echo htmlspecialchars($subscribe_url) ?>
 </p>
 <p>
- <?php echo _("To subscribe to this calendar from a feed reader, use this URL: ") ?>
+ <?php echo _("To subscribe to this calendar from a feed reader, use this URL:") ?>
 </p>
 <p class="calendar-info-url">
  <?php echo htmlspecialchars(Kronolith::feedUrl($calendar->getName())) ?>
 </p>
 
 <p>
- <?php echo _("To embed this calendar in another website, use this code: ") ?>
+ <?php echo _("To embed this calendar in another website, use this code:") ?>
 </p>
 <p class="calendar-info-url">
 <?php echo htmlspecialchars(Kronolith::embedCode($calendar->getName())); ?>
