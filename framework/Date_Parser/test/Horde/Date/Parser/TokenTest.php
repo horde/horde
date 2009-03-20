49bb5c8873378bd94dfa4dbbc72c4cd90426b336
Chuck Hagenbuch <chuck@horde.org>
2009-03-20 10:34:08 -0400

    incremental test work

--- framework/Date_Parser/test/Horde/Date/Parser/TokenTest.php
+++ framework/Date_Parser/test/Horde/Date/Parser/TokenTest.php
@@ -34,3 +34,14 @@ class Horde_Date_Parser_TokenTest extends PHPUnit_Framework_TestCase
     }
 
 }
+  def test_match
+    token = Chronic::Token.new('saturday')
+    repeater = Chronic::Repeater.scan_for_day_names(token)
+    assert_equal Chronic::RepeaterDayName, repeater.class
+    assert_equal :saturday, repeater.type
+
+    token = Chronic::Token.new('sunday')
+    repeater = Chronic::Repeater.scan_for_day_names(token)
+    assert_equal Chronic::RepeaterDayName, repeater.class
+    assert_equal :sunday, repeater.type
+  end
