d5037a1d4d2cae8bacb3e24b093b5d28d3865403
Gunnar Wrobel <p@rdus.de>
2009-04-28 22:55:58 +0200

    Completed object deletion.

--- koward/app/views/Object/delete.html.php
+++ koward/app/views/Object/delete.html.php
@@ -1,2 +1,15 @@
 <?= $this->renderPartial('header'); ?>
 <?= $this->renderPartial('menu'); ?>
+
+<form action="<?= $this->submit_url ?>" method="post" name="delete">
+<?php echo Util::formInput() ?>
+
+<div class="headerbox">
+
+ <p><?php echo _("Permanently delete this object?") ?></p>
+
+ <input type="submit" class="button" name="delete" value="<?php echo _("Delete") ?>" />
+ <a class="button" href="<?= $this->return_url ?>"><?php echo _("Cancel") ?></a>
+</div>
+
+</form>
