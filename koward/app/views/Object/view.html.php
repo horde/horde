9a22711e505b5e8117afd3450c9ad366bb3e2544
Gunnar Wrobel <p@rdus.de>
2009-04-28 22:56:08 +0200

    Enable object actions.

--- koward/app/views/Object/view.html.php
+++ koward/app/views/Object/view.html.php
@@ -1,6 +1,11 @@
 <?= $this->renderPartial('header'); ?>
 <?= $this->renderPartial('menu'); ?>
 <?php
+if (isset($this->actions)) {
+    echo $this->actions->renderActive(new Horde_Form_Renderer(), $this->vars,
+                                      $this->post, 'post');
+}
+
 if (isset($this->form)) {
     echo $this->form->renderInactive(new Horde_Form_Renderer(), $this->vars);
 
