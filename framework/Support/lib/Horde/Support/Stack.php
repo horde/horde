80ddb0a4a936611c84eaf657d36e893ced5d6dd7
Chuck Hagenbuch <chuck@horde.org>
2009-06-08 10:48:35 -0400

    add Horde_Support_Stack

---
+++ framework/Support/lib/Horde/Support/Stack.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @category   Horde
+ * @package    Support
+ * @copyright  2008-2009 The Horde Project (http://www.horde.org/)
+ * @license    http://opensource.org/licenses/bsd-license.php
+ */
+
+/**
+ * Simple class for using an array as a stack.
+ *
+ * @category   Horde
+ * @package    Support
+ * @copyright  2008-2009 The Horde Project (http://www.horde.org/)
+ * @license    http://opensource.org/licenses/bsd-license.php
+ */
+class Horde_Support_Stack
+{
+    /**
+     * @var array
+     */
+    protected $_stack = array();
+
+    public function __construct($stack = array())
+    {
+        $this->_a = $stack;
+    }
+
+    public function push($value)
+    {
+        $this->_stack[] = $value;
+    }
+
+    public function pop()
+    {
+        return array_pop($this->_stack);
+    }
+
+    public function peek($offset = 1)
+    {
+        if (isset($this->_stack[count($this->_stack) - $offset])) {
+            return $this->_stack[count($this->_stack) - $offset];
+        } else {
+            return null;
+        }
+    }
+
+}
