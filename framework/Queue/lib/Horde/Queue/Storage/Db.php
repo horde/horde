7c9ee8bcf6597695db2bf0349044c847dd7a6b00
Chuck Hagenbuch <chuck@horde.org>
2009-06-12 23:41:00 -0400

    stubs for other backends

---
+++ framework/Queue/lib/Horde/Queue/Storage/Db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Stores queue tasks in a database table.
+ */
+class Horde_Queue_Storage_Db extends Horde_Queue_Storage_Base
+{
+    protected $_db;
+
+    public function add($task)
+    {
+    }
+
+    public function getMany($num = 50)
+    {
+    }
+
+}
---
+++ framework/Queue/lib/Horde/Queue/Storage/Sqs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Stores queue tasks in Amazon's SQS.
+ */
+class Horde_Queue_Storage_Sqs extends Horde_Queue_Storage_Base
+{
+    public function add($task)
+    {
+    }
+
+    public function getMany($num = 50)
+    {
+    }
+
+}
