e2b6513306aad13e15151b9af02f5eb544cf7af6
Chuck Hagenbuch <chuck@horde.org>
2009-06-14 00:14:42 -0400

    check 'status' against Vcs_Patchset constants

--- chora/patchsets.php
+++ chora/patchsets.php
@@ -67,7 +67,7 @@ while (list($id, $patchset) = each($patchsets)) {
 
         $file['file'] = Horde::link(Chora::url('patchsets', $member['file'])) . htmlspecialchars($member['file']) . '</a>';
 
-        if ($member['from'] == Horde_Vcs_Patchset::INITIAL) {
+        if ($member['status'] == Horde_Vcs_Patchset::INITIAL) {
             $file['from'] = '<ins>' . _("New File") . '</ins>';
             $file['diff'] = '';
         } else {
@@ -75,7 +75,7 @@ while (list($id, $patchset) = each($patchsets)) {
             $file['diff'] = Horde::link(Chora::url('diff', $member['file'], array('r1' => $member['from'], 'r2' => $member['to']))) . ' ' . $diff_img . '</a>';
         }
 
-        if ($member['from'] == Horde_Vcs_Patchset::DEAD) {
+        if ($member['status'] == Horde_Vcs_Patchset::DEAD) {
             $file['to'] = '<del>' . _("Deleted") . '</del>';
             $file['diff'] = '';
         } else {
