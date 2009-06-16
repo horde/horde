372937b74dbb75fead174549440848ac107f60d5
Rodrigo Sampaio Primo <rodrigosprimo@php.net>
2009-06-16 04:56:54 +0000

    Added tests for all Tiki Render rules
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@282204 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/tests.php
+++ tests/tests.php
@@ -9,7 +9,7 @@ class Framework_AllTests
 {
     public static function suite()
     {
-        $suite = new PHPUnit_Framework_TestSuite('PHPUnit Framework');
+        $suite = new PHPUnit_Framework_TestSuite('Text_Wiki_TestSuite');
  
         /* almost all phpt tests are failling and need to be fixed
            before uncommenting the code below
@@ -17,7 +17,7 @@ class Framework_AllTests
         $suite->addTestSuite($phptTests); */
 
         $suite->addTestSuite('Text_Wiki_Parser_Tiki_Test');
-        $suite->addTestSuite('Text_Wiki_Render_Tiki_Test');
+        $suite->addTestSuite('Text_Wiki_Render_Tiki_AllTests');
  
         return $suite;
     }
