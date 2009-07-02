b83535b1050b513fe7395e7d26187e4cdd3db9c5
Michael M Slusarz <slusarz@curecanti.org>
2009-07-02 12:30:32 -0600

    Add Horde_Util_Filter_Eol

---
+++ framework/Util/lib/Horde/Util/Filter/Eol.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Stream filter class to convert WOL characters.
+ *
+ * Usage:
+ *   stream_filter_register('horde_eol', 'Horde_Util_Filter_Eol');
+ *   stream_filter_[app|pre]pend($stream, 'horde_eol', $params);
+ *
+ * $params can contain the following:
+ * <pre>
+ * 'eol' - The EOL string to use.
+ *         DEFAULT: <CR><LF> ("\r\n")
+ * </pre>
+ *
+ * Copyright 2009 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author  Michael Slusarz <slusarz@horde.org>
+ * @package Horde_Util
+ */
+class Horde_Util_Filter_Eol extends php_user_filter
+{
+    protected $_search;
+    protected $_replace;
+
+    public function onCreate()
+    {
+        $eol = isset($this->params['eol']) ? $this->params['eol'] : "\r\n";
+        if (!strlen($eol)) {
+            $this->_search = array("\r", "\n");
+            $this->_replace = '';
+        } elseif (in_array($eol, array("\r", "\n"))) {
+            $this->_search = array("\r\n", ($eol == "\r") ? "\n" : "\r");
+            $this->_replace = $eol;
+        } else {
+            $this->_search = array("\r\n", "\r", "\n");
+            $this->_replace = array("\n", "\n", $eol);
+        }
+
+        return true;
+    }
+
+    public function filter($in, $out, &$consumed, $closing)
+    {
+        while ($bucket = stream_bucket_make_writeable($in)) {
+            $bucket->data = str_replace($this->_search, $this->_replace, $bucket->data);
+            $consumed += $bucket->datalen;
+            stream_bucket_append($out, $bucket);
+        }
+
+        return PSFS_PASS_ON;
+    }
+
+}
---
+++ framework/Util/test/Horde/Util/eol_filter.phpt
@@ -0,0 +1,62 @@
+--TEST--
+Horde_Util_Filter_Eol:: tests
+--FILE--
+<?php
+
+require_once dirname(__FILE__) . '/../../../lib/Horde/Util/Filter/Eol.php';
+
+stream_filter_register('horde_eol', 'Horde_Util_Filter_Eol');
+
+$test = fopen('php://temp', 'r+');
+fwrite($test, "A\r\nB\rC\nD\r\n\r\nE\r\rF\n\nG\r\n\n\r\nH\r\n\r\r\nI");
+
+foreach (array("\r", "\n", "\r\n", "") as $val) {
+    $filter = stream_filter_prepend($test, 'horde_eol', STREAM_FILTER_READ, array('eol' => $val));
+    rewind($test);
+    fpassthru($test);
+    stream_filter_remove($filter);
+
+    echo "\n---\n";
+}
+
+fclose($test);
+?>
+--EXPECT--
+ABCDEFGHI
+---
+A
+B
+C
+D
+
+E
+
+F
+
+G
+
+
+H
+
+
+I
+---
+A
+B
+C
+D
+
+E
+
+F
+
+G
+
+
+H
+
+
+I
+---
+ABCDEFGHI
+---
