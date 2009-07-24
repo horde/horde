3af973eac7768261f67c406190a62d02bc1000ef
Chuck Hagenbuch <chuck@bluestatedigital.com>
2009-07-24 17:30:12 -0400

    no reference

--- chora/lib/base.php
+++ chora/lib/base.php
@@ -40,7 +40,7 @@ $conf = &$GLOBALS['conf'];
 define('CHORA_TEMPLATES', $registry->get('templates'));
 
 // Notification system.
-$notification = &Horde_Notification::singleton();
+$notification = Horde_Notification::singleton();
 $notification->attach('status');
 
 // Initialize objects, path, etc.
