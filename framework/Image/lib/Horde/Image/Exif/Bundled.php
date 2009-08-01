2a2882be947ac1bde9d36d48d0d7e76c02fb111e
Michael J. Rubinsky <mrubinsk@horde.org>
2009-08-01 14:52:03 -0400

    Remove what looks like some cruft left over from Exifer development

--- framework/Image/lib/Horde/Image/Exif/Bundled.php
+++ framework/Image/lib/Horde/Image/Exif/Bundled.php
@@ -455,7 +455,7 @@ class Horde_Image_Exif_Bundled extends Horde_Image_Exif_Base
             case '8298': $tag = 'Copyright'; break;                // text string up to 999 bytes long
             case '8649': $tag = 'PhotoshopSettings'; break;        // ??
             case '8825': $tag = 'GPSInfoOffset'; break;
-            case '9286': $tag = 'UserCommentOld'; break;           // ??
+            //case '9286': $tag = 'UserCommentOld'; break;           // ??
             case '8769': $tag = 'ExifOffset'; break;               // positive integer
             // used by Exif SubIFD 'Image Tags'
             case '829a': $tag = 'ExposureTime'; break;             // seconds or fraction of seconds 1/x
