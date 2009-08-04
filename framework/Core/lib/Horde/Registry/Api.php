048e7057cdd7a402b3c0efd62d8ca4cc3eb3b16c
Michael M Slusarz <slusarz@curecanti.org>
2009-08-03 22:23:24 -0600

    Add a bit of API documentation
    This should eventually be done elsewhere, but keep the info here for
    now so it doesn't get lost.

--- framework/Core/lib/Horde/Registry/Api.php
+++ framework/Core/lib/Horde/Registry/Api.php
@@ -2,6 +2,73 @@
 /**
  * Template class for application API files.
  *
+ * Other Horde-defined API calls
+ * =============================
+ * Horde_Auth_Application::
+ * ------------------------
+ *   'authLoginParams' => array(
+ *       'args' => array(),
+ *       'checkperms' => false,
+ *       'type' => '{urn:horde}hashHash'
+ *   ),
+ *   'authAuthenticate' => array(
+ *       'args' => array(
+ *           'userID' => 'string',
+ *           'credentials' => '{urn:horde}hash',
+ *           'params' => '{urn:horde}hash'
+ *       ),
+ *       'checkperms' => false,
+ *       'type' => 'boolean'
+ *   ),
+ *   'authAuthenticateCallback' => array(
+ *       'args' => array(),
+ *       'checkperms' => false
+ *   ),
+ *   'authTransparent' => array(
+ *       'args' => array(),
+ *       'checkperms' => false,
+ *       'type' => 'boolean'
+ *   ),
+ *   'authAddUser' => array(
+ *       'args' => array(
+ *           'userId' => 'string',
+ *           'credentials' => '{urn:horde}stringArray'
+ *       )
+ *   ),
+ *   'authRemoveUser' => array(
+ *       'args' => array(
+ *           'userId' => 'string'
+ *       )
+ *   ),
+ *   'authUserList' => array(
+ *       'type' => '{urn:horde}stringArray'
+ *   )
+ *
+ * Prefs_UI::
+ * ----------
+ *   'prefsInit' => array(
+ *       'args' => array()
+ *   ),
+ *   'prefsHandle' => array(
+ *       'args' => array(
+ *           'item' => 'string',
+ *           'updated' => 'boolean'
+ *       ),
+ *       'type' => 'boolean'
+ *   ),
+ *   'prefsCallback' => array(
+ *       'args' => array()
+ *   )
+ *
+ * TODO:
+ * -----
+ *   'cacheOutput' => array(
+ *       'args' => array(
+ *           '{urn:horde}hashHash'
+ *       ),
+ *       'type' => '{urn:horde}hashHash'
+ *   )
+ *
  * Copyright 2009 The Horde Project (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
@@ -36,6 +103,11 @@ class Horde_Registry_Api
         'perms' => array(
             'args' => array(),
             'type' => '{urn:horde}hashHash'
+        ),
+
+        'changeLanguage' => array(
+            'args' => array(),
+            'type' => 'boolean'
         )
     );
 
@@ -59,4 +131,13 @@ class Horde_Registry_Api
     {
         return array();
     }
+
+    /**
+     * Called when the language is changed.
+     */
+    public function changeLanguage()
+    {
+        return array();
+    }
+
 }
