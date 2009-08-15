c3b649fc8ec141acb751f1dd5431cf3e3ac42bde
Michael M Slusarz <slusarz@curecanti.org>
2009-08-15 12:22:02 -0600

    Make sure imap_ob exists

--- imp/lib/Notification/Listener/Status.php
+++ imp/lib/Notification/Listener/Status.php
@@ -75,8 +75,10 @@ class IMP_Notification_Listener_Status extends Horde_Notification_Listener_Statu
         }
 
         /* Display IMAP alerts. */
-        foreach ($GLOBALS['imp_imap']->ob->alerts() as $alert) {
-            $GLOBALS['notification']->push($alert, 'horde.warning');
+        if (isset($GLOBALS['imp_imap']->ob)) {
+            foreach ($GLOBALS['imp_imap']->ob->alerts() as $alert) {
+                $GLOBALS['notification']->push($alert, 'horde.warning');
+            }
         }
 
         parent::notify($messageStack, $options);
