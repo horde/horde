7461fa94927538b226cbde7e2d4041812305afe6
Jan Schneider <jan@horde.org>
2009-09-01 14:57:15 +0200

    This is no longer necessary with Chuck's change to Horde::prepareResponse().

--- kronolith/lib/Notification/Listener/Status.php
+++ kronolith/lib/Notification/Listener/Status.php
@@ -27,21 +27,4 @@ class Kronolith_Notification_Listener_Status extends Horde_Notification_Listener
         return (substr($type, 0, 10) == 'kronolith.') || parent::handles($type);
     }
 
-    /**
-     * Returns all status message if there are any on the 'status' message
-     * stack.
-     *
-     * @param array &$messageStack  The stack of messages.
-     * @param array $options        An array of options.
-     */
-    public function notify(&$messageStack, $options = array())
-    {
-        /* Don't capture notification messages if we are logging out are
-         * accessing the options pages. */
-        if (Horde_Auth::getAuth() && !strstr($_SERVER['PHP_SELF'], '/prefs.php')) {
-            $options['store'] = true;
-        }
-        parent::notify($messageStack, $options);
-    }
-
 }
