a2362bc321848e5a3191a07b039eda951d8ac268
Jan Schneider <jan@horde.org>
2009-09-08 23:55:17 +0200

    Add Horde_Support_Guid to generate the Horde-style GUIDs.

---
+++ framework/Support/lib/Horde/Support/Guid.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @category   Horde
+ * @package    Support
+ * @copyright  2009 The Horde Project (http://www.horde.org/)
+ * @license    http://opensource.org/licenses/bsd-license.php
+ */
+
+/**
+ * Class for generating GUIDs. Usage:
+ *
+ * <code>
+ *  <?php
+ *
+ *  $uid = (string)new Horde_Support_Guid;
+ *
+ *  ?>
+ * </code>
+ *
+ * @category   Horde
+ * @package    Support
+ * @copyright  2009 The Horde Project (http://www.horde.org/)
+ * @license    http://opensource.org/licenses/bsd-license.php
+ */
+class Horde_Support_Guid
+{
+    /**
+     * Generated GUID
+     * @var string
+     */
+    private $_guid;
+
+    /**
+     * New GUID
+     */
+    public function __construct()
+    {
+        $this->generate();
+    }
+
+    /**
+     * Generates a GUID.
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        $this->_guid = date('YmdHis')
+            . '.'
+            . substr(str_pad(base_convert(microtime(), 10, 36), 16, uniqid(mt_rand()), STR_PAD_LEFT), -16)
+            . '@'
+            . (isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost');
+    }
+
+    /**
+     * Cooerce to string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->_guid;
+    }
+
+}
