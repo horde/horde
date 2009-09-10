6a60820325518dc08379fc33aa4edb814e1defed
Chuck Hagenbuch <chuck@horde.org>
2009-09-09 23:24:47 -0400

    add an initial Horde_View_Json object that takes the variables assigned to it and returns a JSON object when rendered

---
+++ framework/View/lib/Horde/View/Json.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @category Horde
+ * @package Horde_View
+ */
+
+/**
+ * Concrete class for handling views.
+ *
+ * @category Horde
+ * @package Horde_View
+ */
+class Horde_View_Json extends Horde_View_Base
+{
+    /**
+     * Processes a template and returns the output.
+     *
+     * @param string $name The template to process.
+     *
+     * @return string The template output.
+     */
+    public function render($name = '', $locals = array())
+    {
+        return json_encode((object)(array)$this);
+    }
+
+    /**
+     * Satisfy the abstract _run function in Horde_View_Base.
+     */
+    protected function _run()
+    {
+    }
+}
