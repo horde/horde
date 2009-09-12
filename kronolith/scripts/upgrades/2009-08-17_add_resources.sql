638ca83cbc856d0456d23ce5e00f58cbd94ea81d
Michael J. Rubinsky <mrubinsk@horde.org>
2009-09-12 11:06:36 -0400

    More additions based on some discussion with Gunnar in IRC:  - Allow specifying if a resource is required or optional  - Add a response_type property to resources. Specifies if REQUESTS are    either always accepted, always denied, automatically accepted/denied    based on actual availability, handled manually (still todo), or simply    ignored (set response to NONE).
    Also, try to clean up the implementation - trying to make it more likely
    to be able to be extened for kolab. No idea if this helps of not though ;)

--- kronolith/scripts/upgrades/2009-08-17_add_resources.sql
+++ kronolith/scripts/upgrades/2009-08-17_add_resources.sql
@@ -6,6 +6,8 @@ CREATE TABLE kronolith_resources (
     resource_calendar VARCHAR(255),
     resource_description TEXT,
     resource_category VARCHAR(255),
+    resource_response_type INT DEFAULT 0,
+    resource_max_reservations INT DEFAULT 1,
     
     PRIMARY KEY (resource_id)
 );
