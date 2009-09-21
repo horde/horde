63f36293393a679e442153ef0546df4cd566f566
Chuck Hagenbuch <chuck@horde.org>
2009-09-20 23:42:51 -0400

    shorthand for mocking a class with a private constructor

--- framework/Test/lib/Horde/Test/Case.php
+++ framework/Test/lib/Horde/Test/Case.php
@@ -1,4 +1,11 @@
 <?php
 class Horde_Test_Case extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Useful shorthand if you are mocking a class with a private constructor
+     */
+    public function getMockSkipConstructor($className, array $methods = array(), array $arguments = array(), $mockClassName = '')
+    {
+        return $this->getMock($className, $methods, $arguments, $mockClassName, /* $callOriginalConstructor */ false);
+    }
 }
