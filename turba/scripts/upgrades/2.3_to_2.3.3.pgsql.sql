f32cdb41522af0536afc5ae308a9c5e4ddc96189
Jan Schneider <jan@horde.org>
2009-09-25 17:19:59 +0200

    Add upgrade script (Bug #8584).

---
+++ turba/scripts/upgrades/2.3_to_2.3.3.pgsql.sql
@@ -0,0 +1,4 @@
+ALTER TABLE turba_shares_users ALTER share_id TYPE integer;       
+ALTER TABLE turba_shares_groups ALTER share_id TYPE integer; 
+ALTER TABLE turba_shares ALTER share_id TYPE integer;
+
