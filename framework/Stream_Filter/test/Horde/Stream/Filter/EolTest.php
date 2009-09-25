c1ac5a117bdf9e426912fcb31d41f0426a1d1377
Chuck Hagenbuch <chuck@horde.org>
2009-09-25 13:50:32 -0400

    make this a working phpunit test

--- framework/Stream_Filter/test/Horde/Stream/Filter/EolTest.php
+++ framework/Stream_Filter/test/Horde/Stream/Filter/EolTest.php
@@ -1,66 +1,43 @@
+<?php
 /**
+ * @category   Horde
  * @package    Horde_Stream_Filter
  * @subpackage UnitTests
  */
---TEST--
-Horde_Stream_Filter_Eol:: tests
---FILE--
-<?php
 
-require_once dirname(__FILE__) . '/../../../lib/Horde/Stream/Filter/Eol.php';
+/**
+ * @category   Horde
+ * @package    Horde_Stream_Filter
+ * @subpackage UnitTests
+ */
+class Horde_Stream_Filter_EolTest extends Horde_Test_Case
+{
+    public function setup()
+    {
+        stream_filter_register('horde_eol', 'Horde_Stream_Filter_Eol');
+        $this->fp = fopen('php://temp', 'r+');
+        fwrite($this->fp, "A\r\nB\rC\nD\r\n\r\nE\r\rF\n\nG\r\n\n\r\nH\r\n\r\r\nI");
+    }
 
-stream_filter_register('horde_eol', 'Horde_Stream_Filter_Eol');
+    public static function lineEndingProvider()
+    {
+        return array(
+            array("\r", "ABCDEFGHI"),
+            array("\n", "A\nB\nC\nD\n\nE\n\nF\n\nG\n\n\nH\n\n\nI"),
+            array("\r\n", "A\nB\nC\nD\n\nE\n\nF\n\nG\n\n\nH\n\n\nI"),
+            array("", "ABCDEFGHI"),
+        );
+    }
 
-$test = fopen('php://temp', 'r+');
-fwrite($test, "A\r\nB\rC\nD\r\n\r\nE\r\rF\n\nG\r\n\n\r\nH\r\n\r\r\nI");
-
-foreach (array("\r", "\n", "\r\n", "") as $val) {
-    $filter = stream_filter_prepend($test, 'horde_eol', STREAM_FILTER_READ, array('eol' => $val));
-    rewind($test);
-    fpassthru($test);
-    stream_filter_remove($filter);
-
-    echo "\n---\n";
+    /**
+     * @dataProvider lineEndingProvider
+     */
+    public function testFilterLineEndings($eol, $expected)
+    {
+        $filter = stream_filter_prepend($this->fp, 'horde_eol', STREAM_FILTER_READ, array('eol' => $eol));
+        rewind($this->fp);
+        $this->assertEquals($expected, stream_get_contents($this->fp));
+        stream_filter_remove($filter);
+        fclose($this->fp);
+    }
 }
-
-fclose($test);
-?>
---EXPECT--
-ABCDEFGHI
----
-A
-B
-C
-D
-
-E
-
-F
-
-G
-
-
-H
-
-
-I
----
-A
-B
-C
-D
-
-E
-
-F
-
-G
-
-
-H
-
-
-I
----
-ABCDEFGHI
----
