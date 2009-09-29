fbccac8b24325928838802685f0f9bb13f3a7256
Chuck Hagenbuch <chuck@horde.org>
2009-09-29 09:27:05 -0400

    Figured out why I was getting errors; include PHPUnit/Framework.php

--- framework/Db/test/Horde/Db/AllTests.php
+++ framework/Db/test/Horde/Db/AllTests.php
@@ -16,7 +16,7 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Horde_Db_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once dirname(__FILE__) . '/Adapter/MissingTest.php';
 
@@ -44,7 +44,7 @@ class Horde_Db_AllTests {
         // Ensure a default timezone is set.
         date_default_timezone_set('America/New_York');
 
-        // Set up autoload
+        // Set up include_path and autoloading
         set_include_path(dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR . get_include_path());
         if (!spl_autoload_functions()) {
             spl_autoload_register(create_function('$class', '$filename = str_replace(array(\'::\', \'_\'), \'/\', $class); @include_once "$filename.php";'));
