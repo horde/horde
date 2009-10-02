84eac74cb6f6dd11149da8d8a96fae9bd2c2ff2a
Gunnar Wrobel <p@rdus.de>
2009-10-02 15:40:09 +0200

    Phpdoc.

--- framework/Injector/lib/Horde/Injector/Binder.php
+++ framework/Injector/lib/Horde/Injector/Binder.php
@@ -1,12 +1,44 @@
 <?php
+/**
+ * Binder interface definition.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Injector
+ * @author   Bob Mckee <bmckee@bywires.com>
+ * @author   James Pepin <james@jamespepin.com>
+ * @license  http://opensource.org/licenses/bsd-license.php BSD
+ * @link     http://pear.horde.org/index.php?package=Injector
+ */
+
+/**
+ * Describes a binding class that is able to create concrete object instances.
+ *
+ * @category Horde
+ * @package  Injector
+ * @author   Bob Mckee <bmckee@bywires.com>
+ * @author   James Pepin <james@jamespepin.com>
+ * @license  http://opensource.org/licenses/bsd-license.php BSD
+ * @link     http://pear.horde.org/index.php?package=Injector
+ */
 interface Horde_Injector_Binder
 {
+    /**
+     * Create an instance.
+     *
+     * @param Horde_Injector $injector The injector should provide all required
+     *                                 dependencies for creating the instance.
+     *
+     * @return mixed The concrete instance.
+     */
     public function create(Horde_Injector $injector);
 
     /**
-     * determine if one binder equals another binder
+     * Determine if one binder equals another binder
      *
      * @param Horde_Injector_Binder $binder The binder to compare against $this
+     *
      * @return bool true if they are equal, or false if they are not equal
      */
     public function equals(Horde_Injector_Binder $binder);
