0b53e6f3524a07dad71ec4e11b9b09997fd49ba6
Chuck Hagenbuch <chuck@horde.org>
2009-10-12 23:04:14 -0400

    Verify in a test that StringStream doesn't duplicate the string being streamed in memory.

---
+++ framework/Support/test/Horde/Support/StringStreamTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @category   Horde
+ * @package    Support
+ * @subpackage UnitTests
+ * @copyright  2008-2009 The Horde Project (http://www.horde.org/)
+ * @license    http://opensource.org/licenses/bsd-license.php
+ */
+
+/**
+ * @group      support
+ * @category   Horde
+ * @package    Support
+ * @subpackage UnitTests
+ * @copyright  2008-2009 The Horde Project (http://www.horde.org/)
+ * @license    http://opensource.org/licenses/bsd-license.php
+ */
+class Horde_Support_StringStreamTest extends PHPUnit_Framework_TestCase
+{
+    public function testMemoryUsage()
+    {
+        $dummy = '';
+        $dummy = new Horde_Support_StringStream($dummy);
+
+        $bytes = 1024 * 1024;
+        $string = str_repeat('*', $bytes);
+        $memoryUsage = memory_get_usage();
+
+        $stream = new Horde_Support_StringStream($string);
+        $memoryUsage2 = memory_get_usage();
+        $this->assertLessThan($memoryUsage + $bytes, $memoryUsage2);
+
+        $fp = $stream->fopen();
+        $memoryUsage3 = memory_get_usage();
+        $this->assertLessThan($memoryUsage + $bytes, $memoryUsage3);
+
+        while (!feof($fp)) { fread($fp, 1024); }
+        $memoryUsage4 = memory_get_usage();
+        $this->assertLessThan($memoryUsage + $bytes, $memoryUsage4);
+    }
+}
