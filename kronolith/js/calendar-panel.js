d3ad60793bf4e8c0a5fd5a8bff97f2d450338c7d
Michael J. Rubinsky <mrubinsk@horde.org>
2009-10-28 18:16:34 -0400

    remove stray, empty function

--- kronolith/js/calendar-panel.js
+++ kronolith/js/calendar-panel.js
@@ -13,10 +13,6 @@ function sbarToggle()
     new Ajax.Request(KronolithVar.pref_api_url, { parameters: { pref: 'show_panel', value: pref_value } });
 }
 
-function removeTag(tagid)
-{
-
-}
 document.observe('dom:loaded', function() {
     $$('#pageControlsInner .checkbox').invoke('observe', 'click', function() {
         Views.invalidate();
