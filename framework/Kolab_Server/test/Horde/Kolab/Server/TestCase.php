e6d859d71ef14287cd4ff0fd9e4f8f08acfc1e73
Gunnar Wrobel <p@rdus.de>
2009-10-29 16:54:34 +0100

    Add basic test case definition.

---
+++ framework/Kolab_Server/test/Horde/Kolab/Server/TestCase.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Provides functions required by several Kolab_Server tests.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Server
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Server
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/Autoload.php';
+
+/**
+ * Skip LDAP based tests if we don't have ldap or Net_LDAP2.
+ *
+ * Copyright 2008-2009 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Kolab
+ * @package  Kolab_Server
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Server
+ */
+class Horde_Kolab_Server_TestCase extends PHPUnit_Framework_TestCase
+{
+    protected function getComposite()
+    {
+        return $this->getMock(
+            'Horde_Kolab_Server_Composite', array(), array(), '', false
+        );
+    }
+
+    protected function getMockedComposite()
+    {
+        return new Horde_Kolab_Server_Composite(
+            $this->getMock(
+                'Horde_Kolab_Server', array(), array(), '', false
+            ),
+            $this->getMock(
+                'Horde_Kolab_Server_Objects', array(), array(), '', false
+            ),
+            $this->getMock(
+                'Horde_Kolab_Server_Structure', array(), array(), '', false
+            ),
+            $this->getMock(
+                'Horde_Kolab_Server_Search', array(), array(), '', false
+            ),
+            $this->getMock(
+                'Horde_Kolab_Server_Schema', array(), array(), '', false
+            )
+        );
+    }
+}
