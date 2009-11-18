9501c16b39c6c5c4ac47a8bd6c873adab0c3e670
Jan Schneider <jan@horde.org>
2009-11-19 00:46:24 +0100

    Spacing

--- kronolith/lib/Block/summary.php
+++ kronolith/lib/Block/summary.php
@@ -201,7 +201,7 @@ class Horde_Block_Kronolith_summary extends Horde_Block {
                 if ($event_active) {
                     $html .= '<strong>';
                 }
-                $html .= $event->getLink(null, true, null, true);
+                $html .= ' ' . $event->getLink(null, true, null, true);
                 if ($event_active) {
                     $html .= '</strong>';
                 }
