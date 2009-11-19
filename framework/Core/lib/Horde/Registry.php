a145b86e29f411a582164556eec5f6291714eebc
Jan Schneider <jan@horde.org>
2009-11-20 00:28:45 +0100

    Don't start a session on CLI.

--- framework/Core/lib/Horde/Registry.php
+++ framework/Core/lib/Horde/Registry.php
@@ -139,7 +139,10 @@ class Horde_Registry
         }
 
         /* Start a session. */
-        if ($session_flags & self::SESSION_NONE) {
+        if ($session_flags & self::SESSION_NONE ||
+            (PHP_SAPI == 'cli') ||
+            (((PHP_SAPI == 'cgi') || (PHP_SAPI == 'cgi-fcgi')) &&
+             empty($_SERVER['SERVER_NAME']))) {
             /* Never start a session if the session flags include
                SESSION_NONE. */
             $_SESSION = array();
