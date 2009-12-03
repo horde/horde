68e1addcf3287581e5e02e77577639e2d87d78fb
Jan Schneider <jan@horde.org>
2009-12-03 13:07:38 +0100

    One test suite to rule them all.

--- framework/Icalendar/test/Horde/Icalendar/AllTests.php
+++ framework/Icalendar/test/Horde/Icalendar/AllTests.php
@@ -7,48 +7,26 @@
  * @license    http://www.fsf.org/copyleft/lgpl.html
  */
 
+/**
+ * Define the main method
+ */
 if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Horde_Icalendar_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+/**
+ * Prepare the test setup.
+ */
+require_once 'Horde/Test/AllTests.php';
 
-class Horde_Icalendar_AllTests {
-
-    public static function main()
-    {
-        PHPUnit_TextUI_TestRunner::run(self::suite());
-    }
-
-    public static function suite()
-    {
-        set_include_path(dirname(__FILE__) . '/../../../lib' . PATH_SEPARATOR . get_include_path());
-        if (!spl_autoload_functions()) {
-            spl_autoload_register(create_function('$class', '$filename = str_replace(array(\'\\\', \'_\'), \'/\', $class); include "$filename.php";'));
-        }
-
-        $suite = new PHPUnit_Framework_TestSuite('Horde Framework - Horde_Icalendar');
-
-        $basedir = dirname(__FILE__);
-        $baseregexp = preg_quote($basedir . DIRECTORY_SEPARATOR, '/');
-
-        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($basedir)) as $file) {
-            if ($file->isFile() && preg_match('/Test.php$/', $file->getFilename())) {
-                $pathname = $file->getPathname();
-                require $pathname;
-
-                $class = str_replace(DIRECTORY_SEPARATOR, '_',
-                                     preg_replace("/^$baseregexp(.*)\.php/", '\\1', $pathname));
-                $suite->addTestSuite('Horde_Icalendar_' . $class);
-            }
-        }
-
-        return $suite;
-    }
-
+/**
+ * @package    Horde_Icalendar
+ * @subpackage UnitTests
+ */
+class Horde_Icalendar_AllTests extends Horde_Test_AllTests
+{
 }
 
 if (PHPUnit_MAIN_METHOD == 'Horde_Icalendar_AllTests::main') {
-    Horde_Icalendar_AllTests::main();
+    Horde_Icalendar_AllTests::main('Horde_Icalendar', __FILE__);
 }
--- framework/Rdo/test/Horde/Rdo/AllTests.php
+++ framework/Rdo/test/Horde/Rdo/AllTests.php
@@ -4,43 +4,26 @@
  * @subpackage UnitTests
  */
 
+/**
+ * Define the main method
+ */
 if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Horde_Rdo_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+/**
+ * Prepare the test setup.
+ */
+require_once 'Horde/Test/AllTests.php';
 
-class Horde_Rdo_AllTests {
-
-    public static function main()
-    {
-        PHPUnit_TextUI_TestRunner::run(self::suite());
-    }
-
-    public static function suite()
-    {
-        $suite = new PHPUnit_Framework_TestSuite('Horde Framework - Horde_Rdo');
-
-        $basedir = dirname(__FILE__);
-        $baseregexp = preg_quote($basedir . DIRECTORY_SEPARATOR, '/');
-
-        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($basedir)) as $file) {
-            if ($file->isFile() && preg_match('/Test.php$/', $file->getFilename())) {
-                $pathname = $file->getPathname();
-                require $pathname;
-
-                $class = str_replace(DIRECTORY_SEPARATOR, '_',
-                                     preg_replace("/^$baseregexp(.*)\.php/", '\\1', $pathname));
-                $suite->addTestSuite('Horde_Rdo_' . $class);
-            }
-        }
-
-        return $suite;
-    }
-
+/**
+ * @package    Horde_Rdo
+ * @subpackage UnitTests
+ */
+class Horde_Rdo_AllTests extends Horde_Test_AllTests
+{
 }
 
 if (PHPUnit_MAIN_METHOD == 'Horde_Rdo_AllTests::main') {
-    Horde_Rdo_AllTests::main();
+    Horde_Rdo_AllTests::main('Horde_Rdo', __FILE__);
 }
