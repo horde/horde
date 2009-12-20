50a187e57d69da8e5fdebb1f65dbfdc50f6e0102
Chuck Hagenbuch <chuck@horde.org>
2009-12-20 03:15:14 -0500

    Add assertHtmlDomEquals for less-strict parsing

--- framework/Test/lib/Horde/Test/Functional.php
+++ framework/Test/lib/Horde/Test/Functional.php
@@ -2,7 +2,8 @@
 class Horde_Test_Functional extends Horde_Test_Case
 {
     /**
-     * Test two XML strings for equivalency (e.g., identical up to reordering of attributes).
+     * Test two XML strings for equivalency (e.g., identical up to reordering of
+     * attributes).
      */
     public function assertDomEquals($expected, $actual, $message = null)
     {
@@ -15,4 +16,18 @@ class Horde_Test_Functional extends Horde_Test_Case
         $this->assertEquals($expectedDom->saveXML(), $actualDom->saveXML(), $message);
     }
 
+    /**
+     * Test two HTML strings for equivalency (e.g., identical up to reordering
+     * of attributes).
+     */
+    public function assertHtmlDomEquals($expected, $actual, $message = null)
+    {
+        $expectedDom = new DOMDocument();
+        $expectedDom->loadHTML($expected);
+
+        $actualDom = new DOMDocument();
+        $actualDom->loadHTML($actual);
+
+        $this->assertEquals($expectedDom->saveHTML(), $actualDom->saveHTML(), $message);
+    }
 }
