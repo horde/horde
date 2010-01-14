146958422ed9cbfdab46c1d241e01755a64a157f
Chuck Hagenbuch <chuck@horde.org>
2010-01-14 17:35:23 -0500

    It's not yet clear how these apps will manager their databse connection, so just remove the line for now.

--- hydra/app/controllers/ApplicationController.php
+++ hydra/app/controllers/ApplicationController.php
@@ -14,8 +14,5 @@ class Hydra_ApplicationController extends Horde_Controller_Base
      */
     protected function _initializeApplication()
     {
-        $GLOBALS['conf']['sql']['adapter'] = $GLOBALS['conf']['sql']['phptype'] == 'mysqli' ? 'mysqli' : 'pdo_' . $GLOBALS['conf']['sql']['phptype'];
-        $this->db = Horde_Db_Adapter::factory($GLOBALS['conf']['sql']);
     }
-
 }
