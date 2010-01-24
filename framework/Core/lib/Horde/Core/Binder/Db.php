9b07f7676f7534e256962fbabcbc9ba4812edf1a
Chuck Hagenbuch <chuck@horde.org>
2010-01-23 20:59:32 -0500

    remove "kind" concept from the Db binder

--- framework/Core/lib/Horde/Core/Binder/Db.php
+++ framework/Core/lib/Horde/Core/Binder/Db.php
@@ -2,20 +2,6 @@
 class Horde_Core_Binder_Db implements Horde_Injector_Binder
 {
     /**
-     * If there are multiple db instances, which one should be returned?
-     * @var string
-     */
-    protected $_kind = 'manager';
-
-    /**
-     * @param string $kind  The kind of database connection to return - reader, writer, manager.
-     */
-    public function __construct($kind = null)
-    {
-        $this->_kind = $kind;
-    }
-
-    /**
      * Handle Horde-style configuration arrays, PEAR DB/MDB2 arrays or DSNs, or
      * PDO DSNS.
      */
@@ -39,9 +25,6 @@ class Horde_Core_Binder_Db implements Horde_Injector_Binder
         if (!isset($config['cache'])) {
             $config['cache'] = $injector->getInstance('Horde_Cache');
         }
-
-        /* @TODO Based on $this->_kind and the splitread configuration, return a
-         * reader, writer, or manager connection. */
 
         $adapter = str_replace(' ', '_' , ucwords(str_replace('_', ' ', basename($config['adapter']))));
         $class = 'Horde_Db_Adapter_' . $adapter;
