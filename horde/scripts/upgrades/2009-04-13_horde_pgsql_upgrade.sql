189bd65251fb62dab17d60786dbdf810fba55c1b
Jan Schneider <jan@horde.org>
2010-01-29 15:45:24 +0100

    Make this actually work.

--- horde/scripts/upgrades/2009-04-13_horde_pgsql_upgrade.sql
+++ horde/scripts/upgrades/2009-04-13_horde_pgsql_upgrade.sql
@@ -1,3 +1,3 @@
 -- Replace 'UTF8' with the encoding used in your database.
 -- This script is tested with Postgres 8.3+.
-ALTER TABLE horde_prefs ALTER COLUMN pref_value TYPE BYTEA USING encode(convert_to(pref_value, 'UTF8'), 'escape');
+ALTER TABLE horde_prefs ALTER COLUMN pref_value TYPE BYTEA USING CAST(ENCODE(CONVERT_TO(pref_value, 'UTF8'), 'escape') AS BYTEA);
