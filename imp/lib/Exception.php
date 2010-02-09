27c87db4dd9aa17ae94c980c3e48dedaefbb79d3
Michael M Slusarz <slusarz@curecanti.org>
2010-02-09 10:17:53 -0700

    me != jan

--- imp/lib/Exception.php
+++ imp/lib/Exception.php
@@ -7,7 +7,7 @@
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
- * @author  Michael Slusarz <jan@horde.org>
+ * @author  Michael Slusarz <slusarz@horde.org>
  * @package IMP
  */
 class IMP_Exception extends Horde_Exception
