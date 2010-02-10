b507ee46750b26a7e209d15926985cd52dd2a110
Gunnar Wrobel <wrobel@temple.(none)>
2010-02-10 17:45:19 +0100

    Use the main method from the parent class to avoid strict warnings.

--- framework/Db/test/Horde/Db/AllTests.php
+++ framework/Db/test/Horde/Db/AllTests.php
@@ -34,11 +34,6 @@ date_default_timezone_set('America/New_York');
  */
 class Horde_Db_AllTests extends Horde_Test_AllTests
 {
-    public static function main()
-    {
-        PHPUnit_TextUI_TestRunner::run(self::suite());
-    }
-
     public static function suite()
     {
         // Catch strict standards
