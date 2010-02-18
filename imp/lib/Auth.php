1e394986ba8dd83a6f2854f51c361f72fce9e7b5
Michael M Slusarz <slusarz@curecanti.org>
2010-02-18 00:43:32 -0700

    Fix license language

--- imp/lib/Auth.php
+++ imp/lib/Auth.php
@@ -4,8 +4,8 @@
  *
  * Copyright 1999-2010 The Horde Project (http://www.horde.org/)
  *
- * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
  * @author  Chuck Hagenbuch <chuck@horde.org>
  * @author  Jon Parise <jon@horde.org>
--- imp/lib/Imap/Acl.php
+++ imp/lib/Imap/Acl.php
@@ -4,8 +4,8 @@
  *
  * Copyright 2003-2010 The Horde Project (http://www.horde.org/)
  *
- * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
  * @author  Chris Hastie <imp@oak-wood.co.uk>
  * @package IMP
