0651e95847d51fc62c5ea75d1db649b89ef0cb8f
Ben Klang <ben@alkaloid.net>
2010-02-28 12:01:22 -0500

    Shout: Begin new dialplan edit UI

---
+++ shout/dialplan.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright 2010 Alkaloid Networks LLC (http://projects.alkaloid.net)
+ *
+ * See the enclosed file COPYING for license information (BSD). If you
+ * did not receive this file, see
+ * http://www.opensource.org/licenses/bsd-license.php.
+ *
+ * @author  Ben Klang <ben@alkaloid.net>
+ */
+
+require_once dirname(__FILE__) . '/lib/Application.php';
+$shout = Horde_Registry::appInit('shout');
+
+require_once SHOUT_BASE . '/lib/Forms/ExtensionForm.php';
+
+//$action = Horde_Util::getFormData('action');
+$action = 'show';
+$context = $_SESSION['shout']['context'];
+
+Horde::addScriptFile('prototype.js', 'horde');
+
+require SHOUT_TEMPLATES . '/common-header.inc';
+require SHOUT_TEMPLATES . '/menu.inc';
+
+$notification->notify();
+
+require SHOUT_TEMPLATES . '/dialplan/' . $action . '.inc';
+
+require $registry->get('templates', 'horde') . '/common-footer.inc';
