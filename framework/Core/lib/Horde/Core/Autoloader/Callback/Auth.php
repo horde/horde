d1a378203863dd947c0133e1ec36b1a9e7e65070
Michael J. Rubinsky <mrubinsk@horde.org>
2010-03-17 17:29:34 -0400

    These are called statically

--- framework/Core/lib/Horde/Core/Autoloader/Callback/Auth.php
+++ framework/Core/lib/Horde/Core/Autoloader/Callback/Auth.php
@@ -7,7 +7,7 @@ class Horde_Core_Autoloader_Callback_Auth
     /**
      * TODO
      */
-    public function callback()
+        static public function callback()
     {
         Horde_Auth::$dnsResolver = $GLOBALS['injector']->getInstance('Net_DNS_Resolver');
     }
--- framework/Core/lib/Horde/Core/Autoloader/Callback/Mime.php
+++ framework/Core/lib/Horde/Core/Autoloader/Callback/Mime.php
@@ -7,7 +7,7 @@ class Horde_Core_Autoloader_Callback_Mime
     /**
      * TODO
      */
-    public function callback()
+    static public function callback()
     {
         Horde_Mime::$brokenRFC2231 = !empty($GLOBALS['conf']['mailformat']['brokenrfc2231']);
     }
