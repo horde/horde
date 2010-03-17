07f2b4a41ed1573f23fd9f21434c55fb401ba154
Gunnar Wrobel <p@rdus.de>
2010-03-17 08:49:13 +0100

    Obsolete.

--- framework/Auth/lib/Horde/Auth/Kolab.php
+++ framework/Auth/lib/Horde/Auth/Kolab.php
@@ -17,20 +17,6 @@
 class Horde_Auth_Kolab extends Horde_Auth_Base
 {
     /**
-     * The session handler.
-     *
-     * @var Horde_Kolab_Session
-     */
-    private $_session;
-
-    /**
-     * The session factory.
-     *
-     * @var Horde_Kolab_Session_Factory_Interface
-     */
-    private $_factory;
-
-    /**
      * An array of capabilities, so that the driver can report which
      * operations it supports and which it doesn't.
      *
@@ -44,51 +30,6 @@ class Horde_Auth_Kolab extends Horde_Auth_Base
         'remove'        => false,
         'update'        => false
     );
-
-    /**
-     * Set the session handler.
-     *
-     * @param Horde_Kolab_Session $session The session handler.
-     *
-     * @return NULL
-     */
-    public function setSession(Horde_Kolab_Session $session)
-    {
-        $this->_session = $session;
-    }
-
-    /**
-     * Set the session factory.
-     *
-     * @param Horde_Kolab_Session_Factory_Interface $factory The session factory.
-     *
-     * @return NULL
-     */
-    public function setSessionFactory(Horde_Kolab_Session_Factory_Interface $factory)
-    {
-        $this->_factory = $factory;
-    }
-
-    /**
-     * Retrieve a connected kolab session.
-     *
-     * @return Horde_Kolab_Session The connected session.
-     *
-     * @throws Horde_Kolab_Session_Exception
-     */
-    public function getSession($userId = null, $credentials = null)
-    {
-        if (!isset($this->_session)) {
-            if (!isset($this->_factory)) {
-                $this->_session = Horde_Kolab_Session_Singleton::singleton(
-                    $userId, $credentials
-                );
-            } else {
-                $this->_session = $this->_factory->getSession($userId, $credentials);
-            }
-        }
-        return $this->_session;
-    }
 
     /**
      * Find out if a set of login credentials are valid.
