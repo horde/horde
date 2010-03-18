3dbc711eaef366ac743945d1957293e6240facea
Jan Schneider <jan@horde.org>
2010-03-18 17:16:25 +0100

    Fix setting logger.

--- framework/Core/lib/Horde/Core/Binder/Token.php
+++ framework/Core/lib/Horde/Core/Binder/Token.php
@@ -3,12 +3,14 @@ class Horde_Core_Binder_Token implements Horde_Injector_Binder
 {
     public function create(Horde_Injector $injector)
     {
-        $token = isset($GLOBALS['conf']['token'])
-            ? Horde_Token::singleton($GLOBALS['conf']['token']['driver'], Horde::getDriverConfig('token', $GLOBALS['conf']['token']['driver']))
-            : Horde_Token::singleton('file');
-        $token->setLogger($injector->getInstance('Horde_Log_Logger'));
-
-        return $token;
+        $driver = isset($GLOBALS['conf']['token'])
+            ? $GLOBALS['conf']['token']['driver']
+            : 'file';
+        $params = isset($GLOBALS['conf']['token'])
+            ? Horde::getDriverConfig('token', $GLOBALS['conf']['token']['driver'])
+            : array();
+        $params['logger'] = $injector->getInstance('Horde_Log_Logger');
+        return Horde_Token::singleton($driver, $params);
     }
 
     public function equals(Horde_Injector_Binder $binder)
