f5be4f1c8e3af9e80abced40dc381fcc97043e2e
Jan Schneider <jan@horde.org>
2010-03-18 17:12:05 +0100

    Fix scope.

--- framework/Core/lib/Horde/Core/Binder/Token.php
+++ framework/Core/lib/Horde/Core/Binder/Token.php
@@ -4,7 +4,7 @@ class Horde_Core_Binder_Token implements Horde_Injector_Binder
     public function create(Horde_Injector $injector)
     {
         $token = isset($GLOBALS['conf']['token'])
-            ? Horde_Token::singleton($conf['token']['driver'], Horde::getDriverConfig('token', $conf['token']['driver']))
+            ? Horde_Token::singleton($GLOBALS['conf']['token']['driver'], Horde::getDriverConfig('token', $GLOBALS['conf']['token']['driver']))
             : Horde_Token::singleton('file');
         $token->setLogger($injector->getInstance('Horde_Log_Logger'));
 
