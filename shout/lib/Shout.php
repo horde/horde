8aeece4e3cfab5ff8daf7ff925a470339b177e66
Ben Klang <ben@alkaloid.net>
2010-03-28 12:17:46 -0400

    Shout: hide inactive "help" and "options" menu items

--- shout/lib/Shout.php
+++ shout/lib/Shout.php
@@ -24,12 +24,13 @@ class Shout
      */
     static public function getMenu($returnType = 'object')
     {
-        $menu = new Horde_Menu(Horde_Menu::MASK_ALL);
+        $mask = Horde_Menu::MASK_PROBLEM | Horde_Menu::MASK_LOGIN;
+        $menu = new Horde_Menu($mask);
 
         $menu->add(Horde::applicationUrl('dialplan.php'), _("Incoming Calls"), "dialplan.png");
         $menu->add(Horde::applicationUrl('extensions.php'), _("Extensions"), "extension.png");
         $menu->add(Horde::applicationUrl('devices.php'), _("Devices"), "shout.png");
-        $menu->add(Horde::applicationUrl('conferences.pngp'), _("Conferences"), "conference.png");
+        $menu->add(Horde::applicationUrl('conferences.png'), _("Conferences"), "conference.png");
         //$menu->add(Horde::applicationUrl('recordings.php'), _("Recordings"), "recordings.png");
 
         /* Administration. */
