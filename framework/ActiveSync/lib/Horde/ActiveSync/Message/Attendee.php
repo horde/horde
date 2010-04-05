7b2368fb50a9d44626622866a19822efd6fff12a
Michael J. Rubinsky <mrubinsk@horde.org>
2010-04-05 17:57:25 -0400

    Default value

--- framework/ActiveSync/lib/Horde/ActiveSync/Message/Attendee.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Message/Attendee.php
@@ -27,7 +27,7 @@ class Horde_ActiveSync_Message_Attendee extends Horde_ActiveSync_Message_Base
      *
      * @param array $params
      */
-    function __construct($params) {
+    function __construct($params = array()) {
         $mapping = array(
             SYNC_POOMCAL_EMAIL => array (Horde_ActiveSync_Message_Base::KEY_ATTRIBUTE => 'email'),
             SYNC_POOMCAL_NAME => array (Horde_ActiveSync_Message_Base::KEY_ATTRIBUTE => 'name'),
