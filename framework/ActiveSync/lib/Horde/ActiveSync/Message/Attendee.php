0c7199b02d5bc5ca1aac04eb9f87f056918fd8f0
Michael J. Rubinsky <mrubinsk@horde.org>
2010-04-05 11:52:35 -0400

    add type and status to the Attendee message object

--- framework/ActiveSync/lib/Horde/ActiveSync/Message/Attendee.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Message/Attendee.php
@@ -10,6 +10,18 @@
  */
 class Horde_ActiveSync_Message_Attendee extends Horde_ActiveSync_Message_Base
 {
+    /* Attendee Type Constants */
+    const TYPE_REQUIRED = 1;
+    const TYPE_OPTIONAL = 2;
+    const TYPE_RESOURCE = 3;
+
+    /* Attendee Status */
+    const STATUS_UNKNOWN = 0;
+    const STATUS_TENATIVE = 2;
+    const STATUS_ACCEPT = 3;
+    const STATUS_DECLINE = 4;
+    const STATUS_NORESPONSE = 5;
+
     /**
      * Const'r
      *
@@ -18,7 +30,9 @@ class Horde_ActiveSync_Message_Attendee extends Horde_ActiveSync_Message_Base
     function __construct($params) {
         $mapping = array(
             SYNC_POOMCAL_EMAIL => array (Horde_ActiveSync_Message_Base::KEY_ATTRIBUTE => 'email'),
-            SYNC_POOMCAL_NAME => array (Horde_ActiveSync_Message_Base::KEY_ATTRIBUTE => 'name' )
+            SYNC_POOMCAL_NAME => array (Horde_ActiveSync_Message_Base::KEY_ATTRIBUTE => 'name'),
+            SYNC_POOMCAL_ATTENDEETYPE => array(Horde_ActiveSync_Message_Base::KEY_ATTRIBUTE => 'type'),
+            SYNC_POOMCAL_ATTENDEESTATUS => array(Horde_ActiveSync_Message_Base::KEY_ATTRIBUTE => 'status')
         );
 
         parent::__construct($mapping, $params);
