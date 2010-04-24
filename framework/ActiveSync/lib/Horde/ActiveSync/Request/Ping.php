e1a191acc64bb7abcda533007facc1a4f94c5ea9
Michael J. Rubinsky <mrubinsk@horde.org>
2010-04-24 15:30:51 -0400

    Add configuration switch for forcing the server heartbeat interval.
    For those of us who don't like devices telling us what we have to do...allow
    overriding the heartbeat interval so we can force it to be shorter then most
    devices would request...useful for managing load on the server.

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/Ping.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/Ping.php
@@ -42,6 +42,9 @@ class Horde_ActiveSync_Request_Ping extends Horde_ActiveSync_Request_Base
 
     protected function _checkHeartbeat($lifetime)
     {
+        if ($this->_ping_settings['forcedheartbeat']) {
+            return $this->_ping_settings['forcedheartbeat'];
+        }
         if ($lifetime !== 0 && $lifetime < $this->_ping_settings['heartbeatmin']) {
             $this->_statusCode = self::STATUS_HBOUTOFBOUNDS;
             $lifetime = $this->_ping_settings['heartbeatmin'];
