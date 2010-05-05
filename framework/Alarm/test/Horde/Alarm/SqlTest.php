5f95f75e8b94f50e09abf0bb4e068a64e5ba00d8
Jan Schneider <jan@horde.org>
2010-05-05 18:43:34 +0200

    Complete Horde_Alarm tests.

--- framework/Alarm/test/Horde/Alarm/SqlTest.php
+++ framework/Alarm/test/Horde/Alarm/SqlTest.php
@@ -12,6 +12,8 @@ class Horde_Alarm_SqlTest extends PHPUnit_Framework_TestCase
     protected static $db;
     protected static $migrator;
     protected static $alarm;
+    protected static $date;
+    protected static $end;
 
     public static function setUpBeforeClass()
     {
@@ -52,12 +54,12 @@ class Horde_Alarm_SqlTest extends PHPUnit_Framework_TestCase
     public function testSet()
     {
         $now = time();
-        $date = new Horde_Date($now);
-        $end = new Horde_Date($now + 3600);
+        self::$date = new Horde_Date($now);
+        self::$end = new Horde_Date($now + 3600);
         $hash = array('id' => 'personalalarm',
                       'user' => 'john',
-                      'start' => $date,
-                      'end' => $end,
+                      'start' => self::$date,
+                      'end' => self::$end,
                       'methods' => array(),
                       'params' => array(),
                       'title' => 'This is a personal alarm.');
@@ -70,5 +72,113 @@ class Horde_Alarm_SqlTest extends PHPUnit_Framework_TestCase
     public function testExists()
     {
         $this->assertTrue(self::$alarm->exists('personalalarm', 'john'));
+    }
+
+    /**
+     * @depends testExists
+     */
+    public function testGet()
+    {
+        $alarm = self::$alarm->get('personalalarm', 'john');
+        $this->assertType('array', $alarm);
+        $this->assertEquals('personalalarm', $alarm['id']);
+        $this->assertEquals('john', $alarm['user']);
+        $this->assertEquals(array(), $alarm['methods']);
+        $this->assertEquals(array(), $alarm['params']);
+        $this->assertEquals('This is a personal alarm.', $alarm['title']);
+        $this->assertNull($alarm['text']);
+        $this->assertNull($alarm['snooze']);
+        $this->assertNull($alarm['internal']);
+        $this->assertTrue($alarm['start'] instanceof Horde_Date);
+        $this->assertTrue($alarm['end'] instanceof Horde_Date);
+        $this->assertEquals(0, $alarm['start']->compareDateTime(self::$date));
+        return $alarm;
+    }
+
+    /**
+     * @depends testGet
+     */
+    public function testUpdate($alarm)
+    {
+        $alarm['title'] = 'Changed alarm text';
+        self::$alarm->set($alarm);
+    }
+
+    /**
+     * @depends testUpdate
+     */
+    public function testListAlarms()
+    {
+        self::$date->min--;
+        self::$alarm->set(array('id' => 'publicalarm',
+                                'start' => self::$date,
+                                'end' => self::$end,
+                                'methods' => array(),
+                                'params' => array(),
+                                'title' => 'This is a public alarm.'));
+        $list = self::$alarm->listAlarms('john');
+        $this->assertEquals(2, count($list));
+        $this->assertEquals('publicalarm', $list[0]['id']);
+        $this->assertEquals('personalalarm', $list[1]['id']);
+    }
+
+    /**
+     * @depends testListAlarms
+     */
+    public function testDelete()
+    {
+        self::$alarm->delete('publicalarm', '');
+        $list = self::$alarm->listAlarms('john');
+        $this->assertEquals(1, count($list));
+        $this->assertEquals('personalalarm', $list[0]['id']);
+    }
+
+    /**
+     * @depends testDelete
+     * @expectedException Horde_Alarm_Exception
+     */
+    public function testSnoozeException()
+    {
+        self::$alarm->snooze('personalalarm', 'jane', 30);
+    }
+
+    /**
+     * @depends testDelete
+     */
+    public function testSnooze()
+    {
+        self::$alarm->snooze('personalalarm', 'john', 30);
+        $this->assertTrue(self::$alarm->isSnoozed('personalalarm', 'john'));
+        $list = self::$alarm->listAlarms('john');
+        $this->assertEquals(0, count($list));
+        $list = self::$alarm->listAlarms('john', self::$end);
+        $this->assertEquals(1, count($list));
+        $this->assertEquals('personalalarm', $list[0]['id']);
+    }
+
+    /**
+     * @depends testSnooze
+     */
+    public function testAlarmWithoutEnd()
+    {
+        self::$alarm->set(array('id' => 'noend',
+                                'user' => 'john',
+                                'start' => self::$date,
+                                'methods' => array('notify'),
+                                'params' => array(),
+                                'title' => 'This is an alarm without end.'));
+        $list = self::$alarm->listAlarms('john', self::$end);
+        $this->assertEquals(2, count($list));
+        $this->assertEquals('noend', $list[0]['id']);
+        $this->assertEquals('personalalarm', $list[1]['id']);
+    }
+
+    /**
+     * @depends testAlarmWithoutEnd
+     */
+    public function testCleanUp()
+    {
+        self::$alarm->delete('noend', 'john');
+        self::$alarm->delete('personalalarm', 'john');
     }
 }
