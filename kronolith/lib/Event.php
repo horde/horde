73ab4748c3f4f7ca5df1986d87400d1b0fc69220
Michael J. Rubinsky <mrubinsk@horde.org>
2010-05-13 10:14:16 -0400

    Sort exception list before display.
    Ticket: 7688

--- kronolith/lib/Event.php
+++ kronolith/lib/Event.php
@@ -1723,8 +1723,9 @@ abstract class Kronolith_Event
      * @return string  List of exception dates and delete links.
      */
     public function exceptionsList()
-    {
-        return implode(', ', array_map(array($this, 'exceptionLink'), $this->recurrence->getExceptions()));
+    {   $exceptions = $this->recurrence->getExceptions();
+        asort($exceptions);
+        return implode(', ', array_map(array($this, 'exceptionLink'), $exceptions));
     }
 
     /**
