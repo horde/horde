f60700fd8ca4685e02a31fa6ab8fc0cf2258de0a
Michael M Slusarz <slusarz@curecanti.org>
2010-05-13 14:30:42 -0600

    ws

--- framework/Core/lib/Horde/Core/Autoloader/Callback/Auth.php
+++ framework/Core/lib/Horde/Core/Autoloader/Callback/Auth.php
@@ -8,7 +8,7 @@ class Horde_Core_Autoloader_Callback_Auth
     /**
      * TODO
      */
-        static public function callback()
+    static public function callback()
     {
         Horde_Auth::$dnsResolver = $GLOBALS['injector']->getInstance('Net_DNS_Resolver');
     }
