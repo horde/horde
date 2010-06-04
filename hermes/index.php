5b1912386e561f9df0016acb99fe5e5b25a3b569
Ben Klang <ben@alkaloid.net>
2010-06-04 13:46:29 -0400

    Hermes: H4 index.php

--- hermes/index.php
+++ hermes/index.php
@@ -1,18 +1,12 @@
 <?php
 /**
- * Copyright 2002-2009 The Horde Project (http://www.horde.org/)
+ * Copyright 2002-2010 The Horde Project (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (BSD). If you
  * did not receive this file, see http://www.horde.org/licenses/bsdl.php.
  */
 
-@define('HERMES_BASE', dirname(__FILE__));
-$hermes_configured = (is_readable(HERMES_BASE . '/config/conf.php'));
-
-if (!$hermes_configured) {
-    require HERMES_BASE . '/../lib/Test.php';
-    Horde_Test::configFilesMissing('Hermes', HERMES_BASE,
-        array('conf.php'));
-}
-
+// Will redirect to login page if not authenticated.
+require_once dirname(__FILE__) . '/lib/Application.php';
+$hermes = Horde_Registry::appInit('hermes');
 require HERMES_BASE . '/time.php';
