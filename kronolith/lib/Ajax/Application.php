be4b9450c139ead14412f8d2a0407c8611b4ca36
Jan Schneider <jan@horde.org>
2010-06-15 21:30:29 +0200

    Fix saving copy of an event to a different calendar.

--- kronolith/lib/Ajax/Application.php
+++ kronolith/lib/Ajax/Application.php
@@ -110,7 +110,8 @@ class Kronolith_Ajax_Application extends Horde_Ajax_Application_Base
             }
         }
 
-        if ($this->_vars->cal &&
+        if ($this->_vars->event &&
+            $this->_vars->cal &&
             $this->_vars->cal != $this->_vars->targetcalendar) {
             if (strpos($kronolith_driver->calendar, ':')) {
                 list($target, $user) = explode(':', $kronolith_driver->calendar, 2);
