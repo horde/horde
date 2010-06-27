5c0e015e170775e836f52833daedbd24a8959e85
Gunnar Wrobel <p@rdus.de>
2010-06-27 11:44:17 +0200

    Initial package structure.

---
+++ framework/Kolab_Config/test/Horde/Kolab/Config/Autoload.php
@@ -0,0 +1,29 @@
+
+<?php
+/**
+ * Setup autoloading for the tests.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Config
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Config
+ */
+
+if (!spl_autoload_functions()) {
+    spl_autoload_register(
+        create_function(
+            '$class', 
+            '$filename = str_replace(array(\'::\', \'_\'), \'/\', $class);'
+            . '$err_mask = E_ALL ^ E_WARNING;'
+            . '$oldErrorReporting = error_reporting($err_mask);'
+            . 'include "$filename.php";'
+            . 'error_reporting($oldErrorReporting);'
+        )
+    );
+}
+
+/** Catch strict standards */
+error_reporting(E_ALL | E_STRICT);
---
+++ framework/Kolab_Config/test/Horde/Kolab/Config/Integration/ConfigTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Test the Kolab configuration handler.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Config
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Config
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../Autoload.php';
+
+/**
+ * Test the Kolab session handler base implementation.
+ *
+ * Copyright 2009-2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Kolab
+ * @package  Kolab_Config
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Config
+ */
+class Horde_Kolab_Config_Integration_ConfigTest
+extends PHPUnit_Framework_TestCase
+{
+
+    public function testSomething()
+    {
+    }
+}
