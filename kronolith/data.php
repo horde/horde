e649acf414796c7b4dbd3b2e79b1169c9f1603f9
Michael J. Rubinsky <mrubinsk@horde.org>
2010-07-07 18:13:09 -0400

    Don't show errors before we've actually done anything.

--- kronolith/data.php
+++ kronolith/data.php
@@ -182,7 +182,7 @@ case Horde_Data::IMPORT_FILE:
     break;
 }
 
-if (!$error) {
+if (!$error && $import_format) {
     $data = null;
     try {
         $data = $injector->getInstance('Horde_Data')->getData($import_format, array('cleanup' => '_cleanupData'));
