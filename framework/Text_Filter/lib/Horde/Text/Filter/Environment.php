627a1c64d6a13ae37e40c1abca767377e0c98054
Michael M Slusarz <slusarz@curecanti.org>
2010-07-09 16:02:43 -0600

    Use callback rather than preg e modifier

--- framework/Text_Filter/lib/Horde/Text/Filter/Environment.php
+++ framework/Text_Filter/lib/Horde/Text/Filter/Environment.php
@@ -23,10 +23,31 @@ class Horde_Text_Filter_Environment extends Horde_Text_Filter_Base
      */
     public function getPatterns()
     {
-        $regexp = array('/^#.*$\n/m' => '',
-                        '/^([^#]*)#.*$/m' => '$1',
-                        '/%([A-Za-z_]+)%/e' => 'getenv("$1")');
-        return array('regexp' => $regexp);
+        $regexp = array(
+            '/^#.*$\n/m' => '',
+            '/^([^#]*)#.*$/m' => '$1'
+        );
+
+        $regexp_callback = array(
+            '/%([A-Za-z_]+)%/e' => array($this, 'regexCallback')
+        );
+
+        return array(
+            'regexp' => $regexp,
+            'regexp_callback' => $regexp_callback
+        );
+    }
+
+    /**
+     * Preg callback.
+     *
+     * @param array $matches  preg_replace_callback() matches.
+     *
+     * @return string  The replacement string.
+     */
+    public function regexCallback($matches)
+    {
+        return getenv($matches[1]);
     }
 
 }
