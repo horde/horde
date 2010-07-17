af3cdd744d0fa8f538baaa19bde12053e9f07667
Michael J. Rubinsky <mrubinsk@horde.org>
2010-07-16 20:51:22 -0400

    looks like the function signature changed, fix it.

--- kronolith/lib/Ajax/Application.php
+++ kronolith/lib/Ajax/Application.php
@@ -27,9 +27,9 @@ class Kronolith_Ajax_Application extends Horde_Core_Ajax_Application
      * @param string $app     The application name.
      * @param string $action  The AJAX action to perform.
      */
-    public function __construct($app, $action = null)
+    public function __construct($app, $vars, $action = null)
     {
-        parent::__construct($app, $action);
+        parent::__construct($app, $vars, $action);
         $this->_defaultDomain = empty($GLOBALS['conf']['storage']['default_domain']) ? null : $GLOBALS['conf']['storage']['default_domain'];
     }
 
