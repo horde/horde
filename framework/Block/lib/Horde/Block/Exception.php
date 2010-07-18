2e60bcd854e2a7b9456cc49dc39754fa4ac5b8f0
Chuck Hagenbuch <chuck@horde.org>
2010-07-18 16:17:03 -0400

    Add Horde_Block_Exception

---
+++ framework/Block/lib/Horde/Block/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Horde_Block exceptions
+ *
+ * Copyright 2003-2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author   Chuck Hagenbuch <chuck@horde.org>
+ * @category Horde
+ * @package  Horde_Block
+ */
+class Horde_Block_Exception extends Horde_Exception
+{}
