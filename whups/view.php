04a605a61c597b7d7b993300638d1a5c770c5f34
Jan Schneider <jan@horde.org>
2010-08-13 14:57:30 +0200

    Fix VFS name.

--- whups/view.php
+++ whups/view.php
@@ -38,7 +38,7 @@ if (!count(Whups::permissionsFilter($whups_driver->getHistory($id), 'comment', H
 }
 
 try {
-    $injector->getInstance('Horde_Vfs')->getVfs('whups');
+    $injector->getInstance('Horde_Vfs')->getVfs();
 } catch (Horde_Exception $e) {
     throw new Horde_Exception(_("The VFS backend needs to be configured to enable attachment uploads."));
 }
