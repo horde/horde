7115a22bef9ead59732b473875f5cd38efee3b38
Michael J. Rubinsky <mrubinsk@horde.org>
2010-08-12 20:20:22 -0400

    Implement unsharpmask filtering in Ansel.

--- ansel/lib/ImageGenerator/Mini.php
+++ ansel/lib/ImageGenerator/Mini.php
@@ -16,6 +16,17 @@ class Ansel_ImageGenerator_Mini extends Ansel_ImageGenerator
         $this->_image->resize(min(50, $this->_dimensions['width']),
                                       min(50, $this->_dimensions['height']),
                                       true);
+        if ($GLOBALS['conf']['thumbnail']['unsharp'] && Ansel::isAvailable('Unsharpmask')) {
+            try {
+                $this->_image->addEffect('Unsharpmask',
+                                         array('radius' => $GLOBALS['conf']['thumbnail']['radius'],
+                                               'threshold' => $GLOBALS['conf']['thumbnail']['threshold'],
+                                               'amount' => $GLOBALS['conf']['thumbnail']['amount']));
+                $this->_image->applyEffects();
+            } catch (Horde_Image_Exception $e) {
+                throw new Ansel_Exception($e);
+            }
+        }
 
         return $this->_image->getHordeImage();
     }
--- ansel/lib/ImageGenerator/Screen.php
+++ ansel/lib/ImageGenerator/Screen.php
@@ -16,6 +16,17 @@ class Ansel_ImageGenerator_Screen extends Ansel_ImageGenerator
         $this->_image->resize(min($GLOBALS['conf']['screen']['width'], $this->_dimensions['width']),
                               min($GLOBALS['conf']['screen']['height'], $this->_dimensions['height']),
                               true);
+        if ($GLOBALS['conf']['screen']['unsharp'] && Ansel::isAvailable('Unsharpmask')) {
+            try {
+                $this->_image->addEffect('Unsharpmask',
+                                         array('radius' => $GLOBALS['conf']['screen']['radius'],
+                                               'threshold' => $GLOBALS['conf']['screen']['threshold'],
+                                               'amount' => $GLOBALS['conf']['screen']['amount']));
+                $this->_image->applyEffects();
+            } catch (Horde_Image $e) {
+                throw new Ansel_Exception($e);
+            }
+        }
 
         return $this->_image->getHordeImage();
     }
