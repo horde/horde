3cceb2d86d85c370aba3baa16d94d40f34bf9f43
Michael Rubinsky <mrubinsk@simon.horde.org>
2010-08-15 15:27:06 +0000

    Avoid adding redundant slashes when building selfUrl.
    This fixes broken url parameters when using pretty urls. For example,
    the url parameter to the login link.

--- framework/Core/lib/Horde.php
+++ framework/Core/lib/Horde.php
@@ -1285,7 +1285,7 @@ HTML;
 
         if ($script_params) {
             if ($pathInfo = Horde_Util::getPathInfo()) {
-                $url .= '/' . $pathInfo;
+                $url .= (strrpos($url, '/') !== (strlen($url) - 1) ?  '/' : '')  . $pathInfo;
             }
             if (!empty($_SERVER['QUERY_STRING'])) {
                 $url .= '?' . $_SERVER['QUERY_STRING'];
