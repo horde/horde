243c4ec7141cd7ba2dd87d51ceed4e335228ce32
Michael J. Rubinsky <mrubinsk@horde.org>
2010-09-11 11:09:49 -0400

    set the app_name value

--- horde/lib/Prefs/Ui.php
+++ horde/lib/Prefs/Ui.php
@@ -460,6 +460,7 @@ class Horde_Prefs_Ui
         $t = $GLOBALS['injector']->createInstance('Horde_Template');
         $t->setOption('gettext', true);
         $t->set('css_link', $GLOBALS['registry']->get('themesuri', 'horde') . '/facebook.css');
+        $t->set('app_name', $GLOBALS['registry']->get('name', 'horde'));
 
         /* Check for facebook session */
         $fbp = unserialize($prefs->getValue('facebook'));
