5283b8cd2f1e4eadfea79fc33cc494cf070efcbb
Jan Schneider <jan@horde.org>
2010-09-13 16:17:23 +0200

    Don't halt, just skip, if db hasn't been configured.

--- framework/Db/test/Horde/Db/Adapter/MysqlSuite.php
+++ framework/Db/test/Horde/Db/Adapter/MysqlSuite.php
@@ -34,7 +34,9 @@ class Horde_Db_Adapter_MysqlSuite extends PHPUnit_Framework_TestSuite
                 list($conn,) = $suite->getConnection();
                 $skip = false;
                 $conn->disconnect();
-            } catch (Exception $e) {var_dump($e); exit;}
+            } catch (Exception $e) {
+                echo $e->getMessage() . "\n";
+            }
         }
 
         if ($skip) {
