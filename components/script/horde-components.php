7be8e19425a74eccad86dc25bebbeda433a75c3f
Gunnar Wrobel <p@rdus.de>
2010-09-28 15:03:10 +0200

    Remove debug statement.

--- components/script/horde-components.php
+++ components/script/horde-components.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 if (strpos('@php_dir@', '@php_dir') === 0) {
-    set_include_path(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR . get_include_path());var_dump(get_include_path());
+    set_include_path(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR . get_include_path());
 }
 
 require_once 'Horde/Autoloader/Default.php';
