8e22d4a72eaa8dc62f9fb905b1db295d380c33c3
Chuck Hagenbuch <chuck@horde.org>
2010-10-03 17:18:05 -0400

    Fix loading PHPUnit for PHPUnit 3.5+

--- framework/Test/lib/Horde/Test/AllTests.php
+++ framework/Test/lib/Horde/Test/AllTests.php
@@ -13,8 +13,7 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Horde_Test_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once 'PHPUnit/Autoload.php';
 
 /**
  * @package    Horde_Test
