5fc65304ee2d516aecb2744bed8e316e8e98d212
Gunnar Wrobel <p@rdus.de>
2010-10-03 19:41:07 +0200

    Turn of E_STRICT which doesn't make sense with the current state of PEAR.

--- components/script/horde-components.php
+++ components/script/horde-components.php
@@ -4,5 +4,11 @@ if (strpos('@php_dir@', '@php_dir') === 0) {
     set_include_path(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR . get_include_path());
 }
 
+/**
+ * We are heavily relying on the PEAR libraries which are not clean with regard
+ * to E_STRICT.
+ */
+error_reporting(E_ALL & ~E_STRICT);
+
 require_once 'Horde/Autoloader/Default.php';
 Components::main();
