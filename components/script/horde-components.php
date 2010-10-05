b228b990fba80421d21e0418e6169fd2ac58b377
Gunnar Wrobel <p@rdus.de>
2010-10-05 21:05:28 +0200

    Respect that E_DEPRECATED is 5.3.0 only.

--- components/script/horde-components.php
+++ components/script/horde-components.php
@@ -8,7 +8,11 @@ if (strpos('@php_dir@', '@php_dir') === 0) {
  * We are heavily relying on the PEAR libraries which are not clean with regard
  * to E_STRICT.
  */
-error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+    error_reporting(E_ALL & ~E_STRICT);
+} else {
+    error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
+}
 
 require_once 'Horde/Autoloader/Default.php';
 Components::main();
