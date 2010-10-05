767c95750d02360bfd40b9e71ce183338f7e4c33
Jan Schneider <jan@horde.org>
2010-10-05 13:14:56 +0200

    Add Translation package.

---
+++ framework/Translation/lib/Horde/Translation.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Translation
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ */
+
+/**
+ * The Horde_Translation interface defines the interface for any classes
+ * providing translations.
+ *
+ * @author  Jan Schneider <jan@horde.org>
+ * @package Translation
+ */
+interface Horde_Translation
+{
+    /**
+     * Returns the translation of a message.
+     *
+     * @var string $message  The string to translate.
+     *
+     * @return string  The string translation, or the original string if no
+     *                 translation exists.
+     */
+    public function t($message);
+}
