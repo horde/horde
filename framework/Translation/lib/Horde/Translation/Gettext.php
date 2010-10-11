2f2ef3417712bbf2c0ce46c58c901ee84796655f
Jan Schneider <jan@horde.org>
2010-10-11 15:23:20 +0200

    Add n().

--- framework/Translation/lib/Horde/Translation/Gettext.php
+++ framework/Translation/lib/Horde/Translation/Gettext.php
@@ -50,7 +50,7 @@ class Horde_Translation_Gettext implements Horde_Translation
     /**
      * Returns the translation of a message.
      *
-     * @var string $message  The string to translate.
+     * @param string $message  The string to translate.
      *
      * @return string  The string translation, or the original string if no
      *                 translation exists.
@@ -58,5 +58,22 @@ class Horde_Translation_Gettext implements Horde_Translation
     public function t($message)
     {
         return $this->_gettext ? dgettext($this->_domain, $message) : $message;
+    }
+
+    /**
+     * Returns the plural translation of a message.
+     *
+     * @param string $singular  The singular version to translate.
+     * @param string $plural    The plural version to translate.
+     * @param integer $number   The number that determines singular vs. plural.
+     *
+     * @return string  The string translation, or the original string if no
+     *                 translation exists.
+     */
+    public function n($singular, $plural, $number)
+    {
+        return $this->_gettext
+          ? dngettext($this->_domain, $singular, $plural, $number)
+          : ($number == 1 ? $singular : $plural);
     }
 }
