1010e163600fb627ed1ce04fe81641b1ed325bc8
Jan Schneider <jan@horde.org>
2010-10-13 00:52:15 +0200

    One more test.

--- framework/Translation/test/Horde/Translation/GettextTest.php
+++ framework/Translation/test/Horde/Translation/GettextTest.php
@@ -10,6 +10,7 @@
 class Horde_Translation_GettextTest extends PHPUnit_Framework_TestCase
 {
     private $_dict;
+    private $_otherDict;
 
     public function setUp()
     {
@@ -17,6 +18,7 @@ class Horde_Translation_GettextTest extends PHPUnit_Framework_TestCase
         putenv('LANG=de_DE.UTF-8');
         putenv('LANGUAGE=de_DE.UTF-8');
         $this->_dict = new Horde_Translation_Gettext('Horde_Translation', dirname(__FILE__) . '/locale');
+        $this->_otherDict = new Horde_Translation_Gettext('Horde_Other', dirname(__FILE__) . '/locale');
     }
 
     public function testGettext()
@@ -24,6 +26,7 @@ class Horde_Translation_GettextTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Heute', $this->_dict->t('Today'));
         $this->assertEquals('Schön', $this->_dict->t('Beautiful'));
         $this->assertEquals('2 Tage', sprintf($this->_dict->t('%d days'), 2));
+        $this->assertEquals('Morgen', $this->_otherDict->t('Tomorrow'));
     }
 
     public function testNgettext()
