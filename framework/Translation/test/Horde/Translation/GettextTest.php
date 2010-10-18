facaa53b69d3499289a445f083a8339f09d2878c
Gunnar Wrobel <p@rdus.de>
2010-10-18 07:06:13 +0200

    Use setlocale. Catch errors when setting locale. Restore environment after test.
    For me the test didn't run without also calling setlocale(). Added
    this based on the PHPUnit setLocale() call which resets to the
    old locale after testing.
    
    In case setting the locale fails the tests will be skipped.
    
    I'm not certain how environment variables will be handled by PHPUnit
    but to be on the safe side I rather reset them after the test.

--- framework/Translation/test/Horde/Translation/GettextTest.php
+++ framework/Translation/test/Horde/Translation/GettextTest.php
@@ -11,14 +11,23 @@ class Horde_Translation_GettextTest extends PHPUnit_Framework_TestCase
 {
     private $_dict;
     private $_otherDict;
+    private $_env;
 
     public function setUp()
     {
-        putenv('LC_ALL=de_DE.UTF-8');
-        putenv('LANG=de_DE.UTF-8');
-        putenv('LANGUAGE=de_DE.UTF-8');
+        try {
+            $this->setLocale(LC_ALL, 'de_DE.UTF-8');
+        } catch (PHPUnit_Framework_Exception $e) {
+            $this->markTestSkipped('Setting the locale failed. de_DE.UTF-8 might not be supported.');
+        }
+        $this->_setEnv('de_DE.UTF-8');
         $this->_dict = new Horde_Translation_Gettext('Horde_Translation', dirname(__FILE__) . '/locale');
         $this->_otherDict = new Horde_Translation_Gettext('Horde_Other', dirname(__FILE__) . '/locale');
+    }
+
+    public function tearDown()
+    {
+        $this->_restoreEnv();
     }
 
     public function testGettext()
@@ -33,5 +42,20 @@ class Horde_Translation_GettextTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals('1 Woche', sprintf($this->_dict->ngettext('%d week', '%d weeks', 1), 1));
         $this->assertEquals('2 Wochen', sprintf($this->_dict->ngettext('%d week', '%d weeks', 2), 2));
+    }
+
+    private function _setEnv($value)
+    {
+        foreach (array('LC_ALL', 'LANG', 'LANGUAGE') as $env) {
+            $this->_env[$env] = getenv($env);
+            putenv($env . '=' . $value);
+        }
+    }
+
+    private function _restoreEnv()
+    {
+        foreach (array('LC_ALL', 'LANG', 'LANGUAGE') as $env) {
+            putenv($env . '=' . $this->_env[$env]);
+        }
     }
 }
