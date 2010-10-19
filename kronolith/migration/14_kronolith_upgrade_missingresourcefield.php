9c7a04add75efcdda37a71dd2a7d384d52f0a8a4
Michael J. Rubinsky <mrubinsk@horde.org>
2010-10-18 23:44:26 -0400

    Missed this field when upgrade scripts refactored to migrations.

---
+++ kronolith/migration/14_kronolith_upgrade_missingresourcefield.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Adds event_resource field
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author   Michael J. Rubinsky <mrubinsk@horde.org>
+ * @category Horde
+ * @license  http://www.fsf.org/copyleft/gpl.html GPL
+ * @package  Kronolith
+ */
+class KronolithUpgradeMissingResourceField extends Horde_Db_Migration_Base
+{
+    /**
+     * Upgrade.
+     */
+    public function up()
+    {
+        $t = $this->_connection->table('kronolith_events');
+        $cols = $t->getColumns();
+        if (!in_array('event_resources', array_keys($cols))) {
+            $this->addColumn('kronolith_events', 'event_resources', 'text');
+        }
+    }
+
+    /**
+     * Downgrade
+     */
+    public function down()
+    {
+        $this->removeColumn('kronolith_events', 'event_resources');
+    }
+
+}
