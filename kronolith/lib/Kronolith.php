ea92f1128c91567ad11a8ad1df58cc59ddb1b2d1
Michael J. Rubinsky <mrubinsk@horde.org>
2010-10-20 11:39:11 -0400

    Catch errors from remote calendars, they shouldn't cause fatal errors.

--- kronolith/lib/Kronolith.php
+++ kronolith/lib/Kronolith.php
@@ -1561,11 +1561,15 @@ class Kronolith
         }
 
         foreach ($GLOBALS['all_remote_calendars'] as $id => $calendar) {
-            if ($calendar->hasPermission($permission) &&
-                (!$display || $calendar->display())) {
-                if ($flat) {
-                    $calendars['remote_' . $id] = $calendar;
+            try {
+                if ($calendar->hasPermission($permission) &&
+                    (!$display || $calendar->display())) {
+                    if ($flat) {
+                        $calendars['remote_' . $id] = $calendar;
+                    }
                 }
+            } catch (Kronolith_Exception $e) {
+                $GLOBALS['notification']->push(sprintf(_("The calendar %s returned the error: %s"), $calendar->name(), $e->getMessage()), 'horde.error');
             }
         }
 
