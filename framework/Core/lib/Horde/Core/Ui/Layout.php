44d61f2c0e458c633805e1d9c94629982fdda3e4
Chuck Hagenbuch <chuck@horde.org>
2010-10-24 23:20:29 -0400

    Experiment with passing variable access and method calls to the underlying view.

--- framework/Core/lib/Horde/Core/Ui/Layout.php
+++ framework/Core/lib/Horde/Core/Ui/Layout.php
@@ -9,6 +9,16 @@ class Horde_Core_Ui_Layout
         $this->_view = $view;
     }
 
+    public function __get($name)
+    {
+        return $this->_view->$name;
+    }
+
+    public function __call($method, $args)
+    {
+        return call_user_func_array(array($this->_view, $method), $args);
+    }
+
     public function setLayoutName($layoutName)
     {
         $this->_layoutName = $layoutName;
