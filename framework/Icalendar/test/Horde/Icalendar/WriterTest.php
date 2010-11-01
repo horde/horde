b6742b9c49112ccc8c6ae395fa8718e85bf93072
Jan Schneider <jan@horde.org>
2010-11-01 17:45:12 +0100

    Fix tests.

--- framework/Icalendar/test/Horde/Icalendar/WriterTest.php
+++ framework/Icalendar/test/Horde/Icalendar/WriterTest.php
@@ -45,16 +45,16 @@ UID:20041120-8550-innerjoin-org
 DTSTART;VALUE=DATE:20050503
 DTSTAMP:20041120T000000Z
 SUMMARY:Escaped Comma in Description Field
-DESCRIPTION:There is a comma (escaped with a baskslash) in this sentence an
- d some important words after it\, see anything here?
+DESCRIPTION:There is a comma (escaped with a baskslash) in this sentence
+  and some important words after it\, see anything here?
 END:VEVENT
 BEGIN:VEVENT
 UID:20041120-8549-innerjoin-org
 DTSTART;VALUE=DATE:20050504
 DTSTAMP:20041120T000000Z
 SUMMARY:Dash (rather than Comma) in the Description Field
-DESCRIPTION:There are important words after this dash - see anything here o
- r have the words gone?
+DESCRIPTION:There are important words after this dash - see anything here
+  or have the words gone?
 END:VEVENT
 END:VCALENDAR
 ',
@@ -77,8 +77,8 @@ BEGIN:VEVENT
 UID:XXX
 DTSTART;VALUE=DATE:20080101
 DTSTAMP:20080101T000000Z
-DESCRIPTION:Lorem ipsum dolor sit amet\, consectetuer adipiscing elit. Aliq
- uam sollicitudin faucibus mauris amet.
+DESCRIPTION:Lorem ipsum dolor sit amet\, consectetuer adipiscing elit.
+  Aliquam sollicitudin faucibus mauris amet.
 END:VEVENT
 END:VCALENDAR
 ',
@@ -96,9 +96,9 @@ PRODID:-//The Horde Project//Horde_Icalendar Library//EN
 VERSION:1.0
 BEGIN:VEVENT
 UID:XXX
-DTSTART:20080101T000000
-DTSTAMP:20080101T000000Z
-DESCRIPTION:Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam sollicitudin faucibus mauris amet.
+DTSTART;VALUE=DATE:20080101
+DESCRIPTION:Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+ Aliquam sollicitudin faucibus mauris amet.
 END:VEVENT
 END:VCALENDAR
 ',
@@ -118,8 +118,8 @@ BEGIN:VEVENT
 UID:XXX
 DTSTART;VALUE=DATE:20080101
 DTSTAMP:20080101T000000Z
-DESCRIPTION:Lörem ipsüm dölör sit ämet\, cönsectetüer ädipiscing elit.
-  Aliqüäm söllicitüdin fäücibüs mäüris ämet.
+DESCRIPTION:Lörem ipsüm dölör sit ämet\, cönsectetüer ädipiscing
+  elit. Aliqüäm söllicitüdin fäücibüs mäüris ämet.
 END:VEVENT
 END:VCALENDAR
 ',
@@ -137,9 +137,8 @@ PRODID:-//The Horde Project//Horde_Icalendar Library//EN
 VERSION:1.0
 BEGIN:VEVENT
 UID:XXX
-DTSTART:20080101T000000
-DTSTAMP:20080101T000000Z
-DESCRIPTION;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:L=C3=B6rem ips=C3=BCm d=C3=B6l=C3=B6r sit =C3=A4met,=
+DTSTART;VALUE=DATE:20080101
+DESCRIPTION;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8:L=C3=B6rem ips=C3=BCm d=C3=B6l=C3=B6r sit =C3=A4met,=
  c=C3=B6nsectet=C3=BCer =C3=A4dipiscing elit. Aliq=C3=BC=C3=A4m=
  s=C3=B6llicit=C3=BCdin f=C3=A4=C3=BCcib=C3=BCs m=C3=A4=C3=BCris =C3=A4met.
 END:VEVENT
@@ -159,9 +158,8 @@ PRODID:-//The Horde Project//Horde_Icalendar Library//EN
 VERSION:1.0
 BEGIN:VEVENT
 UID:XXX
-DTSTART:20080101T000000
-DTSTAMP:20080101T000000Z
-DESCRIPTION;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:L=C3=B6remips=C3=BCmd=C3=B6l=C3=B6rsit=C3=A4met,c=C3=B6nsectet=C3=BCer=C3=A=
+DTSTART;VALUE=DATE:20080101
+DESCRIPTION;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8:L=C3=B6remips=C3=BCmd=C3=B6l=C3=B6rsit=C3=A4met,c=C3=B6nsectet=C3=BCer=C3=A=
 4dipiscingelit.Aliq=C3=BC=C3=A4ms=C3=B6llicit=C3=BCdinf=C3=A4=C3=BCcib=C3=B=
 Csm=C3=A4=C3=BCris=C3=A4met.=
  L=C3=B6remips=C3=BCmd=C3=B6l=C3=B6rsit=C3=A4met,c=C3=B6nsectet=C3=BCer=C3==
@@ -198,20 +196,20 @@ UID:XXX
 DTSTART;VALUE=DATE:20080101
 DTSTAMP:20080101T000000Z
 DESCRIPTION:SYLVIE DAGORNE a écrit :\n\nBonjour\,\n\nsuite à mon appel
-  téléphonique auprès de Jacques Benzerara\, il m\'a renvoyé vers vous. En
-  effet\, je souhaiterais vous rencontrer car:\n1°) au niveau de
+  téléphonique auprès de Jacques Benzerara\, il m\'a renvoyé vers vous.
+  En effet\, je souhaiterais vous rencontrer car:\n1°) au niveau de
   l\'observatoire local nous devons lancer une enquête sur un suivi de
-  cohorte à la rentrée prochaine qui concernera tous les étudiants de L1.
-  Nous souhaiterons faire un questionnaire en ligne ce questionnaire devra
-  être hébergé sur un serveur.\n\n2°) dans le cadre de l\'observatoire
-  régional\, nos partenaires nous demande également de faire des
-  questionnaires en ligne. Nous disposons du logiciel Modalisa qui permet
-  de le réaliser mais du point de vu technique\, nous avons besoin de voir
-  avec vous\,  les difficultés et les limites d\'un tel dispositif afin de
-  voir les démarches à suivre et pouvoir évoquer tous ces problèmes
-  techniques\, je souhaiterais vous rencontrer. Merci de me précisez vos
-  disponibilités?\n...\nJe serai accompagné d\'un collègue pour
-  l\'observatoire local (David Le foll) et de la chargée d\'études de
+  cohorte à la rentrée prochaine qui concernera tous les étudiants de
+  L1. Nous souhaiterons faire un questionnaire en ligne ce questionnaire
+  devra être hébergé sur un serveur.\n\n2°) dans le cadre de
+  l\'observatoire régional\, nos partenaires nous demande également de
+  faire des questionnaires en ligne. Nous disposons du logiciel Modalisa
+  qui permet de le réaliser mais du point de vu technique\, nous avons
+  besoin de voir avec vous\,  les difficultés et les limites d\'un tel
+  dispositif afin de voir les démarches à suivre et pouvoir évoquer tous
+  ces problèmes techniques\, je souhaiterais vous rencontrer. Merci de me
+  précisez vos disponibilités?\n...\nJe serai accompagné d\'un collègue
+  pour l\'observatoire local (David Le foll) et de la chargée d\'études de
   l\'observatoire régional (Amélie Gicquel) pour la partie régionale.
 END:VEVENT
 END:VCALENDAR
@@ -221,7 +219,7 @@ END:VCALENDAR
 
     public function testQuotedParams()
     {
-        $ical = new Horde_Icalendar_Vcalendar(array('version' => '1.0'));
+        $ical = new Horde_Icalendar_Vcalendar(array('version' => '2.0'));
         $event = new Horde_Icalendar_Vevent();
         $event->uid = '20041120-8550-innerjoin-org';
         $event->startDate = new Horde_Date(array('year' => 2005, 'month' => 5, 'mday' => 3));
