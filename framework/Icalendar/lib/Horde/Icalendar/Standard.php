0c1530b8e1769ad7f6416e7db4f224efc4a646bb
Jan Schneider <jan@horde.org>
2010-11-02 16:11:12 +0100

    Add class stubs for iCalendar 2.0 DAYLIGHT and STANDARD components.

---
+++ framework/Icalendar/lib/Horde/Icalendar/Standard.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright 2009-2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @package  Icalendar
+ */
+
+/**
+ * This class represents a STANDARD component as part of a VTIMEZONE component.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @package  Icalendar
+ */
+class Horde_Icalendar_Standard extends Horde_Icalendar_Base
+{
+}
