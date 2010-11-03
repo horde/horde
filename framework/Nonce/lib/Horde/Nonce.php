17c3c203f309f2d3170033708374d04eb77cb36b
Gunnar Wrobel <p@rdus.de>
2010-11-03 17:32:53 +0100

    Allow to create nonces.

--- framework/Nonce/lib/Horde/Nonce.php
+++ framework/Nonce/lib/Horde/Nonce.php
@@ -1,1 +1,39 @@
 <?php
+/**
+ * The nonce handler.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Nonce
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Nonce
+ */
+
+/**
+ * The nonce handler.
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Horde
+ * @package  Nonce
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Nonce
+ */
+class Horde_Nonce
+{
+    /**
+     * Return a nonce.
+     *
+     * @return string The nonce.
+     */
+    public function get()
+    {
+        return pack('Nn2', time(), mt_rand(), mt_rand());
+    }
+}
--- framework/Nonce/test/Horde/Nonce/Autoload.php
+++ framework/Nonce/test/Horde/Nonce/Autoload.php
@@ -21,3 +21,6 @@ require_once 'Horde/Test/Autoload.php';
 
 /** Catch strict standards */
 error_reporting(E_ALL | E_STRICT);
+
+/** Load the basic test definition */
+require_once dirname(__FILE__) . '/StoryTestCase.php';
