45105e6f5f78f621741ebc0188d59437828a9b8c
Jan Schneider <jan@horde.org>
2010-11-11 17:50:30 +0100

    Use Horde_String.

--- kronolith/templates/agenda/notification.plain.php
+++ kronolith/templates/agenda/notification.plain.php
@@ -3,9 +3,9 @@
 
 <?php foreach ($this->events as $event): ?>
 <?php if ($event->isAllDay()): ?>
-<?php echo str_pad(_("All day") . ':', $this->pad) . $event->title ?>
+<?php echo Horde_String::pad(_("All day") . ':', $this->pad) . $event->title ?>
 <?php else: ?>
-<?php echo str_pad($event->start->format($this->timeformat) . ':', $this->pad) . $event->title ?>
+<?php echo Horde_String::pad($event->start->format($this->timeformat) . ':', $this->pad) . $event->title ?>
 <?php endif ?>
 
 <?php endforeach ?>
