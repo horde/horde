4fb5d376b8d8056f271d02314510a8fa4fcbd3b8
Michael J. Rubinsky <mrubinsk@horde.org>
2010-11-12 14:02:35 -0500

    phpdoc

--- framework/Core/lib/Horde/Core/Share/FactoryCallback.php
+++ framework/Core/lib/Horde/Core/Share/FactoryCallback.php
@@ -1,13 +1,18 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of FactoryCallback
+ * Small factory class for wrapping the injection of Horde_Share obejcts into
+ * the Horde_Share_Object objects. We must use the GLOBAL here instead of
+ * keeping a reference to the injector since this object will be serialized
+ * along with the Horde_Share_Object object.
  *
- * @author mrubinsk
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Horde
+ * @package  Core
+ * @author   Michael J. Rubinsky <mrubinsk@horde.org>
  */
 class Horde_Core_Share_FactoryCallback
 {
