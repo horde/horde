b28396bed70cbb866dd485ddacf1722cfef0f6d8
Jan Schneider <jan@horde.org>
2010-11-16 16:03:42 +0100

    Remove name redundency.

--- kronolith/templates/mobile/head.html.php
+++ kronolith/templates/mobile/head.html.php
@@ -1,7 +1,7 @@
   <?php Horde::addInlineScript(Kronolith::includeJSVars());?>
   <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'horde') ?>/date/en-US.js"></script>
   <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'horde') ?>/date/date.js"></script>
-  <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'kronolith') ?>/kronolithmobile.js"></script>
+  <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'kronolith') ?>/mobile.js"></script>
   <link href="<?php echo $this->registry->get('themesuri');?>/mobile.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
