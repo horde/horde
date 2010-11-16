44ab5b9e6393f47283017ac491d9add6fd865ea8
Chuck Hagenbuch <chuck@horde.org>
2010-11-16 11:28:16 -0500

    Fix gradients and size of "empty" added/removed blocks

--- chora/app/views/diff/added.html.php
+++ chora/app/views/diff/added.html.php
@@ -1,4 +1,4 @@
-<div class="diff-container">
- <div class="diff-left diff-added-empty">&nbsp;</div>
+<div class="diff-container diff-added-empty">
+ <div class="diff-left"></div>
  <div class="diff-right diff-added"><pre><?php foreach ($lines as $l) echo $this->escape($l) . '<br>' ?></pre></div>
 </div>
--- chora/app/views/diff/removed.html.php
+++ chora/app/views/diff/removed.html.php
@@ -1,4 +1,4 @@
-<div class="diff-container">
+<div class="diff-container diff-removed-empty">
  <div class="diff-left diff-removed"><pre><?php foreach ($lines as $l) echo $this->escape($l) . '<br>' ?></pre></div>
- <div class="diff-right diff-removed-empty">&nbsp;</div>
+ <div class="diff-right"></div>
 </div>
