cc288c7f6f05533efbb741f180a2e982200e933f
Michael J. Rubinsky <mrubinsk@horde.org>
2010-11-17 15:46:33 -0500

    Move general jquery-safe javascript to it's own file

--- kronolith/templates/mobile/head.html.php
+++ kronolith/templates/mobile/head.html.php
@@ -1,4 +1,5 @@
   <?php Horde::addInlineScript(Kronolith::includeJSVars());?>
+  <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'horde')?>/horde-jquery.js"></script>
   <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'horde') ?>/date/en-US.js"></script>
   <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'horde') ?>/date/date.js"></script>
   <script type="text/javascript" src="<?php echo $this->registry->get('jsuri', 'kronolith') ?>/mobile.js"></script>
