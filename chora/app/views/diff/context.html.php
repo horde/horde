9d4b6832532c6df8df3d7bb551ef8532966f12c6
Chuck Hagenbuch <chuck@horde.org>
2010-11-28 21:39:57 -0500

    Add line numbers to side-by-side diffs

--- chora/app/views/diff/context.html.php
+++
@@ -1,4 +0,0 @@
-<div class="diff-container">
- <div class="diff-left diff-unmodified"><pre><?php echo $context ?></pre></div>
- <div class="diff-right diff-unmodified"><pre><?php echo $context ?></pre></div>
-</div>
