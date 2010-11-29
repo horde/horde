fcd303f80d0639c208e409e5dd66bf0a5e5f43ce
Jan Schneider <jan@horde.org>
2010-11-29 17:56:08 +0100

    Initial content.

--- imp/templates/mobile/mailbox.html.php
+++ imp/templates/mobile/mailbox.html.php
@@ -3,7 +3,7 @@
     <?php if (!$this->allowFolders): ?>
     <a rel="external" href="<?php echo $this->portal ?>"><?php echo _("Portal")?></a>
     <?php endif ?>
-    <h1 id="imp-mailbox-header"></h1>
+    <h1 id="imp-mailbox-header">&nbsp;</h1>
     <?php if ($this->logout): ?>
     <a href="<?php echo $this->logout ?>" rel="external" data-theme="e" data-icon="delete" class="ui-btn-right"><?php echo _("Log out") ?></a>
     <?php endif ?>
