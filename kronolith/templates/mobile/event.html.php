bc5d7ce25d4d68c10007622dfd8bfb49e7dc9a37
Jan Schneider <jan@horde.org>
2010-11-30 13:31:11 +0100

    Use dialog view for events.

--- kronolith/templates/mobile/event.html.php
+++ kronolith/templates/mobile/event.html.php
@@ -1,4 +1,4 @@
-<div data-role="page" data-theme="b" id="eventview">
-  <div data-role="header" data-theme="b"><h1><?php echo ("Event")?></h1></div>
+<div data-role="dialog" id="eventview">
+  <div data-role="header"><h1><?php echo ("Event")?></h1></div>
   <div data-role="content" class="ui-body"></div>
 </div>
