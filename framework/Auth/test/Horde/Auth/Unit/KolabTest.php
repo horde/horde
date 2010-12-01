452bb93042bb90d44874dcfc34d0ea3d4d251c59
Gunnar Wrobel <p@rdus.de>
2010-12-01 21:41:26 +0100

    Replace the Kolab tests with a simpler unit test.

---
+++ framework/Auth/test/Horde/Auth/Unit/KolabTest.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Test the Horde_Auth_Kolab:: class.
+ *
+ * PHP version 5
+ *
+ * @category   Horde
+ * @package    Auth
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Auth
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../Autoload.php';
+
+/**
+ * Test the Horde_Auth_Kolab:: class.
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Horde
+ * @package    Auth
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Auth
+ */
+class Horde_Auth_Unit_KolabTest extends Horde_Auth_TestCase
+{
+    public function setUp()
+    {
+        if (!interface_exists('Horde_Kolab_Session')) {
+            $this->markTestSkipped('The Kolab_Session package is apparently not installed (Interface Horde_Kolab_Session is unavailable).');
+        }
+        $this->kolab = $this->getMock('Horde_Kolab_Session');
+        $this->driver = new Horde_Auth_Kolab(array('kolab' => $this->kolab));
+    }
+
+    public function testAuthenticate()
+    {
+        $this->kolab->expects($this->once())
+            ->method('connect')
+            ->with('user', array('password' => 'password'))
+            ->will($this->returnValue(null));
+        $this->assertTrue($this->driver->authenticate('user', array('password' => 'password')));
+    }
+
+    public function testBadLogin()
+    {
+        $this->kolab->expects($this->once())
+            ->method('connect')
+            ->with('user', array('password' => 'incorrect'))
+            ->will($this->throwException(new Horde_Kolab_Session_Exception_Badlogin()));
+        try {
+            $this->driver->authenticate('user', array('password' => 'incorrect'));
+        } catch (Horde_Auth_Exception $e) {
+            $this->assertEquals(Horde_Auth::REASON_BADLOGIN, $e->getCode());
+        }
+    }
+
+    public function testFailure()
+    {
+        $this->kolab->expects($this->once())
+            ->method('connect')
+            ->with('user', array('password' => ''))
+            ->will($this->throwException(new Horde_Kolab_Session_Exception()));
+        try {
+            $this->driver->authenticate('user', array('password' => ''));
+        } catch (Horde_Auth_Exception $e) {
+            $this->assertEquals(Horde_Auth::REASON_FAILED, $e->getCode());
+        }
+    }
+}
