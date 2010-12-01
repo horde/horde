f0b736e75506479a072524fc3d84626768a1aec1
Michael M Slusarz <slusarz@curecanti.org>
2010-11-30 23:22:04 -0700

    Add link to file from commits page

--- chora/app/views/diff/diff.html.php
+++ chora/app/views/diff/diff.html.php
@@ -1,5 +1,5 @@
 <div class="diff"<?php if ($id) echo ' id="' . $id . '"' ?>>
-<div class="diff-header"><h4><?php echo $file->queryModulePath() ?></h4></div>
+<div class="diff-header"><h4><a href="<?php echo Chora::url('co', $file->queryModulePath(), array('onb' => $r1)) ?>"><?php echo $file->queryModulePath() ?></a></h4></div>
 <?php foreach ($diff as $section): ?>
 <div class="diff-container diff-section">
  <div class="diff-left"><h3><?php printf(_("Line %s"), $section['oldline']) ?></h3></div>
