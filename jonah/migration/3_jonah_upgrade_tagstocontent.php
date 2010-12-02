b7a0f1a6a22c6bceceb9721ed5e4d445908be346
Ian Roth <ian@web.hot-rain.com>
2010-12-02 13:24:34 -0500

    Migration scripts for story tags and jonah shares

---
+++ jonah/migration/3_jonah_upgrade_tagstocontent.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Move tags from jonah to content storage.
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author   Michael J. Rubinsky <mrubinsk@horde.org>
+ * @author   Ian Roth <iron_hat@hotmail.com>
+ * @category Horde
+ * @license  http://www.fsf.org/copyleft/gpl.html GPL
+ * @package  Jonah
+ */
+class JonahUpgradeTagsToContent extends Horde_Db_Migration_Base
+{
+    public function up()
+    {
+        $tableList = $this->tables();
+        if (in_array('jonah_tags', $tableList)) {
+            $GLOBALS['registry']->pushApp('jonah');
+
+            /* Story tags */
+            $sql = 'SELECT jonah_stories.story_id, tag_name, story_author FROM jonah_stories INNER JOIN '
+                . 'jonah_stories_tags ON jonah_stories.story_id = jonah_stories_tags.story_id '
+                . 'INNER JOIN jonah_tags ON jonah_tags.tag_id = jonah_stories_tags.tag_id';
+
+            $this->announce('Migrating story tags. This may take a while.');
+            $rows = $this->_connection->selectAll($sql);
+            foreach ($rows as $row) {
+                $GLOBALS['injector']->getInstance('Jonah_Tagger')->tag($row['story_id'], $row['tag_name'], $row['story_id'], 'story');
+            }
+            $this->announce('Story tags finished.');
+
+            $this->announce('Dropping jonah tag tables');
+            $this->dropTable('jonah_stories_tags');
+            $this->dropTable('jonah_tags');
+        } else {
+            $this->announce('Tags ALREADY migrated to content system.');
+        }
+    }
+
+    public function down()
+    {
+        // Not supported. One way upgrade.
+    }
+
+}
---
+++ jonah/migration/4_jonah_share_tables.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Create jonah share tables
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author   Ian Roth <iron_hat@hotmail.com>
+ * @category Horde
+ * @license  http://www.fsf.org/copyleft/gpl.html GPL
+ * @package  Jonah
+ */
+class JonahShareTables extends Horde_Db_Migration_Base
+{
+    /**
+     * Upgrade.
+     */
+    public function up()
+    {
+        $tableList = $this->tables();
+
+        if (!in_array('jonah_shares', $tableList)) {
+            $t = $this->createTable('jonah_shares', array('primaryKey' => false));
+            $t->column('share_id', 'integer', array('null' => false));
+            $t->column('share_name', 'string', array('limit' => 255, 'null' => false));
+            $t->column('share_owner', 'string', array('limit' => 255, 'null' => false));
+            $t->column('share_flags', 'integer', array('default' => 0, 'null' => false));
+            $t->column('perm_creator', 'integer', array('default' => 0, 'null' => false));
+            $t->column('perm_default', 'integer', array('default' => 0, 'null' => false));
+            $t->column('perm_guest', 'integer', array('default' => 0, 'null' => false));
+            $t->column('attribute_name', 'string', array('limit' => 255, 'null' => false));
+            $t->column('attribute_desc', 'string', array('limit' => 255));
+            $t->column('attribute_slug', 'string', array('limit' => 64));
+            $t->column('attribute_type', 'integer', array('default' => 0));
+            $t->column('attribute_full_feed', 'integer', array('default' => 0, 'null' =>false));
+            $t->column('attribute_interval', 'integer');
+            $t->column('attribute_
+            $t->primaryKey(array('share_id'));
+            $t->end();
+
+            $this->addIndex('jonah_shares', array('share_name'));
+            $this->addIndex('jonah_shares', array('share_owner'));
+            $this->addIndex('jonah_shares', array('perm_creator'));
+            $this->addIndex('jonah_shares', array('perm_default'));
+            $this->addIndex('jonah_shares', array('perm_guest'));
+        }
+
+        if (!in_array('jonah_shares_groups', $tableList)) {
+            $t = $this->createTable('jonah_shares_groups');
+            $t->column('share_id', 'integer', array('null' => false));
+            $t->column('group_uid', 'string', array('limit' => 255, 'null' => false));
+            $t->column('perm', 'integer', array('null' => false));
+            $t->end();
+
+            $this->addIndex('jonah_shares_groups', array('share_id'));
+            $this->addIndex('jonah_shares_groups', array('group_uid'));
+            $this->addIndex('jonah_shares_groups', array('perm'));
+        }
+
+        if (!in_array('jonah_shares_users', $tableList)) {
+            $t = $this->createTable('jonah_shares_users');
+
+            $t->column('share_id', 'integer', array('null' => false));
+            $t->column('user_uid', 'string', array('limit' => 255));
+            $t->column('perm', 'integer', array('null' => false));
+            $t->end();
+
+            $this->addIndex('jonah_shares_users', array('share_id'));
+            $this->addIndex('jonah_shares_users', array('user_uid'));
+            $this->addIndex('jonah_shares_users', array('perm'));
+        }
+
+        //convert from channel table
+        if (in_array('jonah_channels')) {
+            $this->dropTable('jonah_channels');
+        }
+    }
+
+    /**
+     * Downgrade to 3
+     */
+    public function down()
+    {
+    //forward only
+    }
+}
