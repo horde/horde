4d77c15d2838c03de0e04dfed4de49e8c234fa61
Jan Schneider <jan@horde.org>
2010-12-08 19:10:15 +0100

    Fix bootstrapping.

--- vilma/lib/Application.php
+++ vilma/lib/Application.php
@@ -15,7 +15,25 @@
  * @author  Ben Klang <ben@alkaloid.net>
  * @package Vilma
  */
-define('VILMA_BASE', dirname(__FILE__) . '/..');
+
+/* Determine the base directories. */
+if (!defined('VILMA_BASE')) {
+    define('VILMA_BASE', dirname(__FILE__) . '/..');
+}
+
+if (!defined('HORDE_BASE')) {
+    /* If Horde does not live directly under the app directory, the HORDE_BASE
+     * constant should be defined in config/horde.local.php. */
+    if (file_exists(VILMA_BASE . '/config/horde.local.php')) {
+        include VILMA_BASE . '/config/horde.local.php';
+    } else {
+        define('HORDE_BASE', VILMA_BASE . '/..');
+    }
+}
+
+/* Load the Horde Framework core (needed to autoload
+ * Horde_Registry_Application::). */
+require_once HORDE_BASE . '/lib/core.php';
 
 class Vilma_Application extends Horde_Registry_Application
 {
