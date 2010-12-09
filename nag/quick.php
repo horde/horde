20b3857b605e9696abdab419e316768ee27f93c4
Jan Schneider <jan@horde.org>
2010-12-09 16:02:41 +0100

    Improve quick task adding.

--- nag/quick.php
+++ nag/quick.php
@@ -4,10 +4,15 @@ require_once dirname(__FILE__) . '/lib/Application.php';
 Horde_Registry::appInit('nag');
 
 if ($quickText = Horde_Util::getPost('quickText')) {
-    $result = $registry->tasks->quickAdd($quickText);
-    if (!$result) {
-        throw new Nag_Exception($result);
+    $result = Nag::createTasksFromText($quickText, Nag::getDefaultTasklist(Horde_Perms::EDIT));
+    if (is_a($result, 'PEAR_Error')) {
+        $notification->push($result);
+    } elseif ($result) {
+        $notification->push(sprintf(ngettext("Added %d task", "Added %d tasks", count($result)), count($result)), 'horde.success');
+    } else {
+        $notification->push(_("No tasks have been added."), 'horde.warning');
     }
-    $notification->push(sprintf(ngettext("Added %d task", "Added %d tasks", count($result)), count($result)), 'horde.success');
-    Horde::url('list.php', true)->redirect();
+} else {
+    $notification->push(_("No tasks have been added."), 'horde.warning');
 }
+Horde::url('list.php', true)->redirect();
