09801cd4e9fb951d00bebba191b33eb7b701428b
Gunnar Wrobel <p@rdus.de>
2010-12-14 10:51:36 +0100

    Start adding a factory.

---
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/Factory.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * A generic factory for the various Kolab_Storage classes.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Storage
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+
+/**
+ * A generic factory for the various Kolab_Storage classes.
+ *
+ * Copyright 2004-2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Kolab
+ * @package  Kolab_Storage
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+class Horde_Kolab_Storage_Factory
+{
+    /**
+     * Create the storage handler.
+     *
+     * @return Horde_Kolab_Storage The storage handler.
+     */
+    public function create(Horde_Kolab_Storage_Driver $driver)
+    {
+        return new Horde_Kolab_Storage_Base($driver);
+    }
+
+}
---
+++ framework/Kolab_Storage/test/Horde/Kolab/Storage/Unit/FactoryTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Test the factory.
+ *
+ * PHP version 5
+ *
+ * @category   Kolab
+ * @package    Kolab_Storage
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../Autoload.php';
+
+/**
+ * Test the factory.
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Kolab
+ * @package    Kolab_Storage
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+class Horde_Kolab_Storage_Unit_FactoryTest
+extends Horde_Kolab_Storage_TestCase
+{
+    public function testConstruction()
+    {
+        $factory = new Horde_Kolab_Storage_Factory();
+        $this->assertType(
+            'Horde_Kolab_Storage_Base',
+            $factory->create(new Horde_Kolab_Storage_Driver_Mock())
+        );
+    }
+}
