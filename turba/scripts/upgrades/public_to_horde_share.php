c78b8f9928ca4c87f5260a17737b2226962c00da
Jan Schneider <jan@horde.org>
2010-12-16 23:12:27 +0100

    Horde_Share throws exceptions.

--- turba/scripts/upgrades/public_to_horde_share.php
+++ turba/scripts/upgrades/public_to_horde_share.php
@@ -59,11 +59,7 @@ if (!$sourceKey) {
 
 // Create the new share.
 $owner_uid = strval(new Horde_Support_Randomid());
-$share = &$turba_shares->newShare($GLOBALS['registry']->getAuth(), $sourceKey . ':' . $owner_uid);
-if ($share instanceof Horde_Share_Exception) {
-    var_dump($share);
-    exit;
-}
+$share = $turba_shares->newShare($GLOBALS['registry']->getAuth(), $sourceKey . ':' . $owner_uid);
 $share->set('owner', $owner);
 $share->set('name', $title);
 $share->set('perm_default', Horde_Perms::SHOW | Horde_Perms::READ);
