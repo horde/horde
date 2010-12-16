f241bea0acaa1b2d32a94242980a43813d10e4fa
Jan Schneider <jan@horde.org>
2010-12-17 00:16:00 +0100

    Work around broken sqlite primary key generation.

--- framework/Share/test/Horde/Share/SqlTest.php
+++ framework/Share/test/Horde/Share/SqlTest.php
@@ -19,19 +19,19 @@ class Horde_Share_SqlTest extends PHPUnit_Framework_TestCase
         }
 
         self::$db = new Horde_Db_Adapter_Pdo_Sqlite(array('dbname' => ':memory:'));
+        //self::$db->setLogger(new Horde_Log_Logger(new Horde_Log_Handler_Stream(STDOUT)));
         $migration = new Horde_Db_Migration_Base(self::$db);
 
-        $t = $migration->createTable('test_shares', array('primaryKey' => false));
-        $t->column('share_id', 'integer', array('null' => false));
+        $t = $migration->createTable('test_shares', array('primaryKey' => 'share_id'));
+        //$t->column('share_id', 'integer', array('null' => false, 'autoincrement' => true));
         $t->column('share_name', 'string', array('limit' => 255, 'null' => false));
-        $t->column('share_owner', 'string', array('limit' => 255, 'null' => false));
+        $t->column('share_owner', 'string', array('limit' => 255));
         $t->column('share_flags', 'integer', array('default' => 0, 'null' => false));
         $t->column('perm_creator', 'integer', array('default' => 0, 'null' => false));
         $t->column('perm_default', 'integer', array('default' => 0, 'null' => false));
         $t->column('perm_guest', 'integer', array('default' => 0, 'null' => false));
-        $t->column('attribute_name', 'string', array('limit' => 255, 'null' => false));
+        $t->column('attribute_name', 'string', array('limit' => 255));
         $t->column('attribute_desc', 'string', array('limit' => 255));
-        $t->primaryKey(array('share_id'));
         $t->end();
 
         $migration->addIndex('test_shares', array('share_name'));
