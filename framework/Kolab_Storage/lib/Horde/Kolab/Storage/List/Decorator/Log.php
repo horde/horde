a497f63f4f95dc6ef4af9540ffd8ea2ed026bd87
Gunnar Wrobel <p@rdus.de>
2011-01-02 07:41:07 +0100

    Fix copy error.

--- framework/Kolab_Storage/lib/Horde/Kolab/Storage/List/Decorator/Log.php
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/List/Decorator/Log.php
@@ -14,7 +14,7 @@
 /**
  * The log decorator for folder lists from Kolab storage.
  *
- * Copyright 2004-2010 The Horde Project (http://www.horde.org/)
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
