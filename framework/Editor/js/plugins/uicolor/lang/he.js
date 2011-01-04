76a1898f4439ac88dcd9eb78ecc4014f100946a4
Michael M Slusarz <slusarz@curecanti.org>
2011-01-04 15:05:14 -0700

    Update to ckeditor v3.4.3
    Bug #9489: Fix encoding of language files

---
+++ framework/Editor/js/plugins/uicolor/lang/he.js
@@ -0,0 +1,1 @@
+CKEDITOR.plugins.setLang("uicolor","he",{uicolor:{title:"בחירת צבע ממשק משתמש",preview:"תצוגה מקדימה",config:"הדבק את הטקסט הבא לתוך הקובץ config.js",predefined:"קבוצות צבעים מוגדרות מראש"}});
