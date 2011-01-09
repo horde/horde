b3d5d47de8328e54735cfb5b4450d58efdb4b0cd
Chuck Hagenbuch <chuck@horde.org>
2011-01-09 16:47:53 -0500

    Update to the controller base class for the new MVC system

---
+++ hippo/app/controllers/Base.php
@@ -0,0 +1,9 @@
+<?php
+/**
+  */
+class Hippo_Base_Controller extends Horde_Controller_Base
+{
+    public function processRequest(Horde_Controller_Request $request, Horde_Controller_Response $response)
+    {
+    }
+}
