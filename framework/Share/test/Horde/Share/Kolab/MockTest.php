8db7c5195b08aa91be6d7933282637d3dede578f
Gunnar Wrobel <p@rdus.de>
2011-01-13 16:25:11 +0100

    Stubs for the Kolab tests.

---
+++ framework/Share/test/Horde/Share/Kolab/MockTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Integration test for the Kolab driver based on the in-memory mock driver.
+ *
+ * PHP version 5
+ *
+ * @category   Horde
+ * @package    Share
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Share
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../Base.php';
+
+/**
+ * Integration test for the Kolab driver based on the in-memory mock driver.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Horde
+ * @package    Share
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Share
+ */
+class Horde_Share_Kolab_MockTest extends Horde_Share_Test_Base
+{
+    public static function setUpBeforeClass()
+    {
+        $group = new Horde_Group_Test();
+        self::$share = new Horde_Share_Kolab('test', 'john', new Horde_Perms(), $group);
+    }
+
+
+}
---
+++ framework/Share/test/Horde/Share/Kolab/UnitTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Unit testing for the Kolab driver.
+ *
+ * PHP version 5
+ *
+ * @category   Horde
+ * @package    Share
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Share
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../Autoload.php';
+
+/**
+ * Unit testing for the Kolab driver.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Horde
+ * @package    Share
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Share
+ */
+class Horde_Share_Kolab_UnitTest
+extends PHPUnit_Framework_TestCase
+{
+
+    public function test()
+    {
+    }
+
+}
