67a100e094d894016ba60133de8aac96bf5981b4
Michael M Slusarz <slusarz@curecanti.org>
2011-01-20 11:05:21 -0700

    Fix URL determination when scanning for URLs
    Don't search at beginning of string - URL can appear anywhere in text.

--- framework/Text_Filter/lib/Horde/Text/Filter/Linkurls.php
+++ framework/Text_Filter/lib/Horde/Text/Filter/Linkurls.php
@@ -83,7 +83,7 @@ class Horde_Text_Filter_Linkurls extends Horde_Text_Filter_Base
 
         return array(
             'regexp' => array(
-                '|^([a-zA-Z][a-zA-Z0-9+.-]{0,19})://([^\s"<]*[\w+#?/&=])|e' => $replacement
+                '|([a-zA-Z][a-zA-Z0-9+.-]{0,19})://([^\s"<]*[\w+#?/&=])|e' => $replacement
             )
         );
     }
