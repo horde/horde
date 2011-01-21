fb63e6294a71ac37353ca96999486f90fa25d81b
Jan Schneider <jan@horde.org>
2011-01-21 20:03:06 +0100

    Check for backend-specific configuration.

--- framework/Share/test/Horde/Share/Sql/MysqlTest.php
+++ framework/Share/test/Horde/Share/Sql/MysqlTest.php
@@ -20,7 +20,7 @@ class Horde_Share_Sql_MysqlTest extends Horde_Share_Test_Sql_Base
             return;
         }
         $config = self::getConfig('SHARE_SQL_MYSQL_TEST_CONFIG');
-        if ($config) {
+        if ($config && !empty($config['share']['sql']['mysql'])) {
             self::$db = new Horde_Db_Adapter_Mysql($config['share']['sql']['mysql']);
             parent::setUpBeforeClass();
         }
--- framework/Share/test/Horde/Share/Sql/Pdo/PgsqlTest.php
+++ framework/Share/test/Horde/Share/Sql/Pdo/PgsqlTest.php
@@ -21,7 +21,7 @@ class Horde_Share_Sql_Pdo_PgsqlTest extends Horde_Share_Test_Sql_Base
             return;
         }
         $config = self::getConfig('SHARE_SQL_PDO_PGSQL_TEST_CONFIG');
-        if ($config) {
+        if ($config && !empty($config['share']['sql']['pdo_pgsql'])) {
             try {
                 self::$db = new Horde_Db_Adapter_Pdo_Pgsql($config['share']['sql']['pdo_pgsql']);
             } catch (Horde_Db_Exception $e) {
