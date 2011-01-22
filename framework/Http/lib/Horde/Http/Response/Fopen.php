4e44faf8092fe079379bec349b2a021a67fe130d
Chuck Hagenbuch <chuck@horde.org>
2011-01-21 22:36:57 -0500

    Make getBody() repeatable

--- framework/Http/lib/Horde/Http/Response/Fopen.php
+++ framework/Http/lib/Horde/Http/Response/Fopen.php
@@ -24,6 +24,11 @@ class Horde_Http_Response_Fopen extends Horde_Http_Response_Base
     protected $_stream;
 
     /**
+     * Response content
+     */
+    protected $_content;
+
+    /**
      * Constructor.
      */
     public function __construct($uri, $stream, $headers = array())
@@ -41,17 +46,21 @@ class Horde_Http_Response_Fopen extends Horde_Http_Response_Base
      */
     public function getBody()
     {
-        $oldTrackErrors = ini_set('track_errors', 1);
-        $content = @stream_get_contents($this->_stream);
-        ini_set('track_errors', $oldTrackErrors);
-        if ($content === false) {
-            $msg = 'Problem reading data from ' . $this->uri;
-            if (isset($php_errormsg)) {
-                $msg .= ': ' . $php_errormsg;
+        if (is_null($this->_content)) {
+            $oldTrackErrors = ini_set('track_errors', 1);
+            $content = @stream_get_contents($this->_stream);
+            ini_set('track_errors', $oldTrackErrors);
+            if ($content === false) {
+                $msg = 'Problem reading data from ' . $this->uri;
+                if (isset($php_errormsg)) {
+                    $msg .= ': ' . $php_errormsg;
+                }
+                throw new Horde_Http_Exception($msg);
             }
-            throw new Horde_Http_Exception($msg);
+            $this->_content = $content;
         }
-        return $content;
+
+        return $this->_content;
     }
 
     /**
