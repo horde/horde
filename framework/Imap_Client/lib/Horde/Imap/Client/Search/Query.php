42783a2db517cf548083bc27a88956d030852d47
Michael M Slusarz <slusarz@curecanti.org>
2011-02-03 12:06:19 -0700

    fix license

--- framework/Imap_Client/lib/Horde/Imap/Client/Search/Query.php
+++ framework/Imap_Client/lib/Horde/Imap/Client/Search/Query.php
@@ -7,8 +7,8 @@
  *
  * Copyright 2008-2011 The Horde Project (http://www.horde.org/)
  *
- * See the enclosed file COPYING for license information (GPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
  *
  * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
