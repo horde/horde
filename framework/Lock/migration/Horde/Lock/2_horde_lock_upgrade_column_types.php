395a6ec41304312840190d8e9a45eea3cbc81d93
Jan Schneider <jan@horde.org>
2011-02-03 14:55:41 +0100

    This column change is not portable and not necessary either.

--- framework/Lock/migration/Horde/Lock/2_horde_lock_upgrade_column_types.php
+++ framework/Lock/migration/Horde/Lock/2_horde_lock_upgrade_column_types.php
@@ -1,13 +1,14 @@
 <?php
+/**
+ * Intentionally left empty.
+ */
 class HordeLockUpgradeColumnTypes extends Horde_Db_Migration_Base
 {
     public function up()
     {
-        $this->changeColumn('horde_locks', 'lock_type', 'tinyint', array('null' => false));
     }
 
     public function down()
     {
-        $this->changeColumn('horde_locks', 'lock_type', 'smallint', array('null' => false, 'unsigned' => true));
     }
 }
