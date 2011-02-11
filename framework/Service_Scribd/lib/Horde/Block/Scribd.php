f39575008479d4fd318a77829f4f85d718bf1e81
Michael M Slusarz <slusarz@curecanti.org>
2011-02-11 11:12:04 -0700

    Another Horde_Block conversion

--- framework/Service_Scribd/lib/Horde/Block/Scribd.php
+++ framework/Service_Scribd/lib/Horde/Block/Scribd.php
@@ -1,56 +1,53 @@
 <?php
 /**
- * @package Horde_Block
  */
-class Horde_Block_Horde_Scribd extends Horde_Block {
-
-    var $_app = 'horde';
-
+class Horde_Scribd_Block_Scribd extends Horde_Block
+{
     /**
-     * The title to go in this block.
-     *
-     * @return string   The title text.
      */
-    function _title()
+    public function getName()
     {
         return Horde_Service_Scribd_Translation::t("Scribd Documents");
     }
 
     /**
-     * The content to go in this block.
-     *
-     * @return string   The content
      */
-    function _content()
+    public function _title()
     {
-        $docs = $this->_list();
-        if (is_array($this->_docs)) {
-            $html = '';
-            $count = 0;
-            foreach ($this->_feed as $entry) {
-                if ($count++ > $this->_params['limit']) {
-                    break;
-                }
-                $html .= '<a href="' . $entry->link. '"';
-                if (empty($this->_params['details'])) {
-                    $html .= ' title="' . htmlspecialchars(strip_tags($entry->description())) . '"';
-                }
-                $html .= '>' . htmlspecialchars($entry->title) . '</a>';
-                if (!empty($this->_params['details'])) {
-                    $html .= '<br />' .  htmlspecialchars(strip_tags($entry->description())). "<br />\n";
-                }
-                $html .= '<br />';
-            }
-            return $html;
-        } elseif (is_string($docs)) {
-            return $docs;
-        } else {
-            return '';
-        }
+        return $this->getName();
     }
 
-    function _list()
+    /**
+     */
+    public function _content()
     {
+        if (!is_array($this->_docs)) {
+            return '';
+        }
+
+        $html = '';
+        $count = 0;
+
+        foreach ($this->_feed as $entry) {
+            if ($count++ > $this->_params['limit']) {
+                break;
+            }
+
+            $html .= '<a href="' . $entry->link. '"';
+
+            if (empty($this->_params['details'])) {
+                $html .= ' title="' . htmlspecialchars(strip_tags($entry->description())) . '"';
+            }
+
+                $html .= '>' . htmlspecialchars($entry->title) . '</a>';
+
+            if (!empty($this->_params['details'])) {
+                $html .= '<br />' .  htmlspecialchars(strip_tags($entry->description())). "<br />\n";
+            }
+            $html .= '<br />';
+        }
+
+        return $html;
     }
 
 }
