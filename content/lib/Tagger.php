9f3d9f11ca7d0eb5931b5edb598d54484354af34
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-04 11:35:50 -0500

    Revert "Use Horde_String::lower"
    This reverts commit ae5714cbb917c43e184f942db0e1b5f3197f679f.
    
    We actually *do* want locale dependent conversions

--- content/lib/Tagger.php
+++ content/lib/Tagger.php
@@ -698,7 +698,7 @@ class Content_Tagger
             foreach ($this->_db->selectAll('SELECT tag_id, tag_name FROM ' . $this->_t('tags') . ' WHERE tag_name IN ('.implode(',', array_map(array($this->_db, 'quote'), array_keys($tagText))).')') as $row) {
                 $tagTextCopy = $tagText;
                 foreach ($tagTextCopy as $tag => $tagIndex) {
-                    if (Horde_String::lower($row['tag_name']) == Horde_String::lower($tag)) {
+                    if (strtolower($row['tag_name']) == strtolower($tag)) {
                         unset($tagText[$tag]);
                         break;
                     }
