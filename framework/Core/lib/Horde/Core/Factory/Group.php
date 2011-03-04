f7f1faad6f67f2b720625d9c5d36e55c11d7474a
Jan Schneider <jan@horde.org>
2011-03-05 00:12:33 +0100

    Fix class building.

--- framework/Core/lib/Horde/Core/Factory/Group.php
+++ framework/Core/lib/Horde/Core/Factory/Group.php
@@ -8,15 +8,16 @@ class Horde_Core_Factory_Group extends Horde_Core_Factory_Injector
     public function create(Horde_Injector $injector)
     {
         $driver = Horde_String::ucfirst($GLOBALS['conf']['group']['driver']);
-        $class = 'Horde_Core_Group_' . basename($driver);
         $params = Horde::getDriverConfig('group', $driver);
         switch ($driver) {
         case 'Ldap':
+            $class = 'Horde_Core_Group_Ldap';
             $params['ldap'] = $injector
                 ->getInstance('Horde_Core_Factory_Ldap')
                 ->create('horde', 'group');
             break;
         case 'Sql':
+            $class = 'Horde_Group_Sql';
             $params['db'] = $injector
                 ->getInstance('Horde_Core_Factory_Db')
                 ->create('horde', 'group');
