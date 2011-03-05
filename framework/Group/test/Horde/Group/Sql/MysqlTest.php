215056627d8ca7210851c8eef99634a0f094af5e
Jan Schneider <jan@horde.org>
2011-03-05 18:41:07 +0100

    Add MySQL tests without PDO.

---
+++ framework/Group/test/Horde/Group/Sql/MysqlTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/Base.php';
+
+/**
+ * @author     Jan Schneider <jan@horde.org>
+ * @category   Horde
+ * @package    Group
+ * @subpackage UnitTests
+ * @copyright  2010 The Horde Project (http://www.horde.org/)
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ */
+class Horde_Group_Sql_MysqlTest extends Horde_Group_Test_Sql_Base
+{
+    public static function setUpBeforeClass()
+    {
+        if (!extension_loaded('mysql')) {
+            self::$reason = 'No mysql extension';
+            return;
+        }
+        $config = self::getConfig('GROUP_SQL_MYSQL_TEST_CONFIG');
+        if ($config && !empty($config['group']['sql']['mysql'])) {
+            self::$db = new Horde_Db_Adapter_Mysql($config['group']['sql']['mysql']);
+            parent::setUpBeforeClass();
+        }
+    }
+}
