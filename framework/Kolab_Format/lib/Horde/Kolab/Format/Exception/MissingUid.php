b41034d80e6eeb22f4545dc197c37cfa5e081da3
Gunnar Wrobel <p@rdus.de>
2011-03-08 23:42:49 +0100

    MissingUid exception.

---
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Exception/MissingUid.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Indicates a missing UID value when reading or writing a Kolab Format object.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Format
+ */
+
+/**
+ * Indicates a missing UID value when reading or writing a Kolab Format object.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Format
+ */
+class Horde_Kolab_Format_Exception_MissingUid
+extends Horde_Kolab_Format_Exception
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct('No UID in the Kolab XML object!');
+    }
+}
