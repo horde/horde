1a52a360ca41b49a802770ab8212a76fd9e1ea4b
Gunnar Wrobel <p@rdus.de>
2011-03-08 23:54:05 +0100

    Simple test for saving notes.

---
+++ framework/Kolab_Format/test/Horde/Kolab/Format/Unit/XmlTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Test the DOM based XML handler.
+ *
+ * PHP version 5
+ *
+ * @category   Kolab
+ * @package    Kolab_Format
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Format
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../Autoload.php';
+
+/**
+ * Test the factory.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Kolab
+ * @package    Kolab_Format
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Format
+ */
+class Horde_Kolab_Format_Unit_XmlTest
+extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Horde_Kolab_Format_Exception_MissingValue
+     */
+    public function testMissingUid()
+    {
+        $factory = new Horde_Kolab_Format_Factory();
+        $note = $factory->create('Xml', 'Note');
+        $note->save(array());
+    }
+
+    public function testSave()
+    {
+        $factory = new Horde_Kolab_Format_Factory();
+        $note = $factory->create('Xml', 'Note');
+        $note->save(array('uid' => 'test'));
+    }
+}
