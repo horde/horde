90a5ed60b8e12e26142122ccb8d0ccc4d3c3b54f
Gunnar Wrobel <p@rdus.de>
2011-03-09 17:32:47 +0100

    Add an object type that provides an XML envelope around arbitrary data.

---
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Envelope.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * A Kolab XML envelope for arbitrary XML.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Format
+ */
+
+/**
+ * A Kolab XML envelope for arbitrary XML.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you did not
+ * receive this file, see
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Format
+ */
+class Horde_Kolab_Format_Xml_Envelope extends Horde_Kolab_Format_Xml
+{
+}
---
+++ framework/Kolab_Format/test/Horde/Kolab/Format/Unit/Xml/EnvelopeTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Test the XML envelope handler.
+ *
+ * PHP version 5
+ *
+ * @category   Kolab
+ * @package    Kolab_Format
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Format
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../../Autoload.php';
+
+/**
+ * Test the XML envelope handler.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Kolab
+ * @package    Kolab_Format
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Format
+ */
+class Horde_Kolab_Format_Unit_Xml_EnvelopeTest
+extends PHPUnit_Framework_TestCase
+{
+    public function testSave()
+    {
+        $factory = new Horde_Kolab_Format_Factory();
+        $note = $factory->create('Xml', 'Raw');
+        $this->assertContains(
+            '<uid>test</uid>',
+            $note->save(array('uid' => 'test'))
+        );
+    }
+}
