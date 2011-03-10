75076e06b2b63aa17349237366d30dff598ec0b8
Gunnar Wrobel <p@rdus.de>
2011-03-10 13:39:31 +0100

    Add a ledger module.

---
+++ framework/Kolab_Cli/lib/Horde/Kolab/Cli/Module/Ledger.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * The Horde_Kolab_Cli_Module_Ledger:: provides tools to deal with ledger data
+ * stored in a Kolab backend.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Kolab_Cli
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Cli
+ */
+
+/**
+ * The Horde_Kolab_Cli_Module_Ledger:: provides tools to deal with ledger data
+ * stored in a Kolab backend.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Horde
+ * @package  Kolab_Cli
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Cli
+ */
+class Horde_Kolab_Cli_Module_Ledger
+implements Horde_Kolab_Cli_Module
+{
+    /**
+     * Get the usage description for this module.
+     *
+     * @return string The description.
+     */
+    public function getUsage()
+    {
+        return Horde_Kolab_Cli_Translation::t("  ledger - Handle ledger data in a Kolab backend (the default action is \"display\").
+
+  - display            : Display all ledgers.
+  - import FOLDER FILE : Import ledger XML data stored in FILE into Kolab folder FOLDER.
+
+");
+    }
+
+    /**
+     * Get a set of base options that this module adds to the CLI argument
+     * parser.
+     *
+     * @return array The options.
+     */
+    public function getBaseOptions()
+    {
+        return array();
+    }
+
+    /**
+     * Indicate if the module provides an option group.
+     *
+     * @return boolean True if an option group should be added.
+     */
+    public function hasOptionGroup()
+    {
+        return false;
+    }
+
+    /**
+     * Return the title for the option group representing this module.
+     *
+     * @return string The group title.
+     */
+    public function getOptionGroupTitle()
+    {
+        return '';
+    }
+
+    /**
+     * Return the description for the option group representing this module.
+     *
+     * @return string The group description.
+     */
+    public function getOptionGroupDescription()
+    {
+        return '';
+    }
+
+    /**
+     * Return the options for this module.
+     *
+     * @return array The group options.
+     */
+    public function getOptionGroupOptions()
+    {
+        return array();
+    }
+
+    /**
+     * Handle the options and arguments.
+     *
+     * @param mixed &$options   An array of options.
+     * @param mixed &$arguments An array of arguments.
+     * @param array &$world     A list of initialized dependencies.
+     *
+     * @return NULL
+     */
+    public function handleArguments(&$options, &$arguments, &$world)
+    {
+    }
+
+    /**
+     * Run the module.
+     *
+     * @param Horde_Cli $cli       The CLI handler.
+     * @param mixed     $options   An array of options.
+     * @param mixed     $arguments An array of arguments.
+     * @param array     &$world    A list of initialized dependencies.
+     *
+     * @return NULL
+     */
+    public function run($cli, $options, $arguments, &$world)
+    {
+        if (!isset($arguments[1])) {
+            $action = 'display';
+        } else {
+            $action = $arguments[1];
+        }
+        switch ($action) {
+        case 'display':
+            $folders = $world['storage']->getList()
+                ->getQuery()
+                ->listByType('ledger');
+            foreach ($folders as $folder) {
+                $cli->writeln($folder);
+            }
+            break;
+            break;
+        case 'import':
+            break;
+        default:
+            $cli->message(
+                sprintf(
+                    Horde_Kolab_Cli_Translation::t('Action %s not supported!'),
+                    $action
+                ),
+                'cli.error'
+            );
+            break;
+        }
+    }
+}
