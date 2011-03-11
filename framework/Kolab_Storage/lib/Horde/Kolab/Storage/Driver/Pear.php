f313701101b05b61cf536f7d02c180ead2c6915f
Gunnar Wrobel <p@rdus.de>
2011-03-11 23:49:34 +0100

    Support moving messages in the PEAR backend.

--- framework/Kolab_Storage/lib/Horde/Kolab/Storage/Driver/Pear.php
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/Driver/Pear.php
@@ -421,15 +421,20 @@ extends Horde_Kolab_Storage_Driver_Base
     /**
      * Moves a message to a new folder.
      *
-     * @param integer $uid        IMAP message id.
-     * @param string $new_folder  Target folder.
+     * @param integer $uid         IMAP message id.
+     * @param string  $old_folder  Source folder.
+     * @param string  $new_folder  Target folder.
      *
-     * @return mixed  True or a PEAR error in case of an error.
+     * @return NULL
      */
-    public function moveMessage($old_folder, $uid, $new_folder)
+    public function moveMessage($uid, $old_folder, $new_folder)
     {
-        $options = array('ids' => array($uid), 'move' => true);
-        return $this->getBackend()->copy($old_folder, $new_folder, $options);
+        $this->select($old_folder);
+        Horde_Kolab_Storage_Exception_Pear::catchError(
+            $this->getBackend()->copyMessage($uid, $new_folder)
+        );
+        $this->deleteMessages($old_folder, array($uid));
+        $this->expunge($old_folder);
     }
 
     /**
