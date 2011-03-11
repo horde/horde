9106b8b1bad69f3df28e8c9293fd351aec1d9da9
Gunnar Wrobel <p@rdus.de>
2011-03-11 23:46:47 +0100

    Support moving messages in the cclient driver.

--- framework/Kolab_Storage/lib/Horde/Kolab/Storage/Driver/Cclient.php
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/Driver/Cclient.php
@@ -707,15 +707,35 @@ extends Horde_Kolab_Storage_Driver_Base
     /**
      * Moves a message to a new folder.
      *
-     * @param integer $uid        IMAP message id.
-     * @param string $new_folder  Target folder.
+     * @param integer $uid         IMAP message id.
+     * @param string  $old_folder  Source folder.
+     * @param string  $new_folder  Target folder.
      *
-     * @return mixed  True or a PEAR error in case of an error.
+     * @return NULL
      */
-    public function moveMessage($old_folder, $uid, $new_folder)
+    public function moveMessage($uid, $old_folder, $new_folder)
     {
-        $options = array('ids' => array($uid), 'move' => true);
-        return $this->_imap->copy($old_folder, $new_folder, $options);
+        $this->select($old_folder);
+
+        $result = @imap_mail_move(
+            $this->getBackend(), 
+            $uid, 
+            $this->encodePath($new_folder),
+            CP_UID
+        );
+        if (!$result) {
+            throw new Horde_Kolab_Storage_Exception(
+                sprintf(
+                    Horde_Kolab_Storage_Translation::t(
+                        "Failed message %s from folder %s to folder %s. Error: %s"
+                    ),
+                    $uid,
+                    $old_folder,
+                    $new_folder,
+                    imap_last_error()
+                )
+            );
+        }
     }
 
     /**
