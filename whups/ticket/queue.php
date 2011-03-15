ba3ad3b8a8e3cc9055be567744f0775ff4b118c7
Jan Schneider <jan@horde.org>
2011-03-15 13:14:42 +0000

    Fix groups.

--- whups/ticket/queue.php
+++ whups/ticket/queue.php
@@ -33,9 +33,8 @@ class SetQueueStep1Form extends Horde_Form {
         /* Group restrictions. */
         if ($GLOBALS['registry']->isAdmin(array('permission' => 'whups:admin', 'permlevel' => Horde_Perms::EDIT)) ||
             $GLOBALS['injector']->getInstance('Horde_Perms')->hasPermission('whups:hiddenComments', $GLOBALS['registry']->getAuth(), Horde_Perms::EDIT)) {
-            $mygroups = $GLOBALS['injector']
-                ->getInstance('Horde_Group')
-                ->listGroups($GLOBALS['registry']->getAuth());
+            $groups = $GLOBALS['injector']->getInstance('Horde_Group');
+            $mygroups = $groups->listGroups($GLOBALS['registry']->getAuth());
             if ($mygroups) {
                 foreach (array_keys($mygroups) as $gid) {
                     $grouplist[$gid] = $groups->getName($gid, true);
