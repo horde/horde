93b73049c02daeb5205d304e82da8afb6b98bdfc
Jan Schneider <jan@horde.org>
2011-03-15 12:49:52 +0100

    Bug #9153: Add test case.

--- framework/Db/test/Horde/Db/Adapter/Pdo/PgsqlTest.php
+++ framework/Db/test/Horde/Db/Adapter/Pdo/PgsqlTest.php
@@ -300,8 +300,8 @@ class Horde_Db_Adapter_Pdo_PgsqlTest extends PHPUnit_Framework_TestCase
 
     public function testQuoteBinary()
     {
-        // Test string is foo\0bar - should be 7 bytes long
-        $original = base64_decode('Zm9vAGJhcg==');
+        // Test string is foo\0bar\baz'boo\'bee - should be 20 bytes long
+        $original = base64_decode('Zm9vAGJhclxiYXonYm9vXCdiZWU=');
 
         $table = $this->_conn->createTable('binary_testings');
             $table->column('data', 'binary', array('null' => false));
