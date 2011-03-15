fffc7646ac074bf7253f7d1148a24c9f11cd9171
Gunnar Wrobel <p@rdus.de>
2011-03-15 21:38:42 +0100

    PEAR to Horde version converter.

---
+++ components/lib/Components/Helper/Version.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Convert the PEAR package version to the Horde version scheme.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Components
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Components
+ */
+
+/**
+ * Convert the PEAR package version to the Horde version scheme.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Horde
+ * @package  Components
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Components
+ */
+class Components_Helper_Version
+{
+    /**
+     * Convert the PEAR package version number to Horde style.
+     *
+     * @param string $version The PEAR package version.
+     *
+     * @return string The Horde style version.
+     */
+    static public function pearToHorde($version)
+    {
+        preg_match('/([.\d]+)(.*)/', $version, $matches);
+        if (!empty($matches[2])) {
+            $post = '-' . strtoupper($matches[2]);
+        } else {
+            $post = '';
+        }
+        $vcomp = explode('.', $matches[1]);
+        if ($vcomp[2] === '0') {
+            $main = $vcomp[0] . '.' . $vcomp[1];
+        } else {
+            $main = $matches[1];
+        }
+        return $main . $post;
+    }
+}
---
+++ components/test/Components/Unit/Components/Helper/VersionTest.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Test the version helper.
+ *
+ * PHP version 5
+ *
+ * @category   Horde
+ * @package    Components
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Components
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../../../Autoload.php';
+
+/**
+ * Test the version helper.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Horde
+ * @package    Components
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Components
+ */
+class Components_Unit_Components_Helper_VersionTest
+extends Components_TestCase
+{
+    public function testAlpha()
+    {
+        $this->assertEquals(
+            '4.0-ALPHA1',
+            Components_Helper_Version::pearToHorde('4.0.0alpha1')
+        );
+    }
+
+    public function testBeta()
+    {
+        $this->assertEquals(
+            '4.0-BETA1',
+            Components_Helper_Version::pearToHorde('4.0.0beta1')
+        );
+    }
+
+    public function testRc1()
+    {
+        $this->assertEquals(
+            '4.0-RC1',
+            Components_Helper_Version::pearToHorde('4.0.0rc1')
+        );
+    }
+
+    public function testRc2()
+    {
+        $this->assertEquals(
+            '4.0-RC2',
+            Components_Helper_Version::pearToHorde('4.0.0rc2')
+        );
+    }
+
+    public function testFourOh()
+    {
+        $this->assertEquals(
+            '4.0',
+            Components_Helper_Version::pearToHorde('4.0.0')
+        );
+    }
+
+    public function testFourOneOh()
+    {
+        $this->assertEquals(
+            '4.1',
+            Components_Helper_Version::pearToHorde('4.1.0')
+        );
+    }
+
+    public function testFourOneOhBeta1()
+    {
+        $this->assertEquals(
+            '4.1-BETA1',
+            Components_Helper_Version::pearToHorde('4.1.0beta1')
+        );
+    }
+
+    public function testFiveOh()
+    {
+        $this->assertEquals(
+            '5.0',
+            Components_Helper_Version::pearToHorde('5.0.0')
+        );
+    }
+
+    public function testFiveTwoOhRc2()
+    {
+        $this->assertEquals(
+            '5.2-RC2',
+            Components_Helper_Version::pearToHorde('5.2.0rc2')
+        );
+    }
+
+}
