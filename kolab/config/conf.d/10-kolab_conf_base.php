d89909f744c364c457748a4b8972debcab4b3918
Gunnar Wrobel <p@rdus.de>
2011-03-17 22:17:07 +0100

    First configuration file for horde.

---
+++ kolab/config/conf.d/10-kolab_conf_base.php
@@ -0,0 +1,23 @@
+<?php
+
+$conf['auth']['driver'] = 'kolab';
+$conf['auth']['admins'] = array('manager');
+
+$conf['group']['driver'] = 'kolab';
+
+$conf['perms']['driverconfig'] = 'horde';
+$conf['perms']['driver'] = 'Null';
+
+$conf['share']['driver'] = 'kolab';
+
+$conf['mailer']['type'] = 'smtp';
+$conf['mailer']['params']['auth'] = true;
+$conf['mailer']['params']['port'] = 25;
+// @todo: Reactivate for Kolab Server 2.3.
+//$conf['mailer']['params']['port'] = 587;
+
+$conf['accounts']['driver'] = 'kolab';
+$conf['accounts']['params']['attr'] = 'mail';
+$conf['accounts']['params']['strip'] = false;
+
+$conf['kolab']['enabled'] = true;
