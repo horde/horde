a722380f4c051fc098fd57ec36929e67cb38e1e2
Till Klampaeckel <till@php.net>
2011-03-19 18:58:28 +0000

    * test seems to work standalone only  * switched to factory(), apparently __construct is deprecated
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@309446 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Text_Wiki_BugTests.php
+++ tests/Text_Wiki_BugTests.php
@@ -6,26 +6,37 @@ require_once 'Text/Wiki.php';
 // class to test the Text_Wiki::transform() with different wiki markups
 class Text_Wiki_BugTests extends PHPUnit_Framework_TestCase
 {
+    protected $wiki;
+
+    protected function setUp()
+    {
+        $this->wiki = Text_Wiki::factory('Default');
+    }
+
+    protected function tearDown()
+    {
+        unset($this->wiki);
+    }
+
     /**
      * @see http://pear.php.net/bugs/bug.php?id=18289
      */
     public function test18289()
     {
         $text = <<<EOT
-* level one
- * level two
-* level one
- * level two
+* level1
+ * level2
+* level1
+ * level2
 EOT;
 
-        $wiki = new Text_Wiki();
-        $html = $wiki->transform($text);
+        $html = $this->wiki->transform($text);
 
         // strip all whitespace to make assertEquals() easier
         $html = preg_replace('/\s+/','',$html);
 
-        $assertion  = '<ul><li>level one<ul><li>level two</li></ul></li>';
-        $assertion .= '<li>level one<ul><li>level two</li></ul></li></ul>';
+        $assertion  = '<ul><li>level1<ul><li>level2</li></ul></li>';
+        $assertion .= '<li>level1<ul><li>level2</li></ul></li></ul>';
         $this->assertEquals($assertion, $html);
     }
 }
