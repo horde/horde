3349762824e692bec7b7ff10e4359b1c9500b78c
Till Klampaeckel <till@php.net>
2011-03-19 22:41:42 +0000

    fixture and test for #11649
    git-svn-id: https://svn.php.net/repository/pear/packages/Text_Wiki/trunk@309455 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Text_Wiki_BugTests.php
+++ tests/Text_Wiki_BugTests.php
@@ -2,6 +2,7 @@
 
 require_once 'PHPUnit/Framework/TestCase.php';
 require_once 'Text/Wiki.php';
+require_once 'Text/Wiki/Parse/Default/List.php';
 
 // class to test the Text_Wiki::transform() with different wiki markups
 class Text_Wiki_BugTests extends PHPUnit_Framework_TestCase
@@ -38,5 +39,19 @@ EOT;
         $assertion  = '<ul><li>level1<ul><li>level2</li></ul></li>';
         $assertion .= '<li>level1<ul><li>level2</li></ul></li></ul>';
         $this->assertEquals($assertion, $html);
+    }
+
+    /**
+     * <code> parsing fails ("blank page") for large data. Let's make sure it works.
+     *
+     * @uses fixtures/bug11649.txt
+     * @see  http://pear.php.net/bugs/bug11649
+     */
+    public function testbug11649()
+    {
+        $data = file_get_contents(dirname(__FILE__) . '/fixtures/bug11649.txt');
+        $html = $this->wiki->transform($data);
+        var_dump($html);
+        $this->assertTrue(is_string($html));
     }
 }
