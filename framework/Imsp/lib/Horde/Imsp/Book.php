2c249ed0390170b3f473c83a86c2f2603b459a6d
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-19 13:14:07 -0400

    phpdoc

--- framework/Imsp/lib/Horde/Imsp/Book.php
+++ framework/Imsp/lib/Horde/Imsp/Book.php
@@ -3,19 +3,12 @@
  * Horde_Imsp_Book Class - provides api for dealing with IMSP
  * address books.
  *
- * Required parameters:<pre>
- *   'username'     Username to logon to IMSP server as.
- *   'password'     Password for current user.
- *   'auth_method'  The authentication method to use to login.
- *   'server'       The hostname of the IMSP server.
- *   'port'         The port of the IMSP server.</pre>
- *
  * Copyright 2002-2011 The Horde Project (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
  *
- * @author  Michael Rubinsky <mrubinsk@horde.org>
+ * @author  Michael J Rubinsky <mrubinsk@horde.org>
  * @package Horde_Imsp
  */
 class Horde_Imsp_Book
