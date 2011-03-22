71e342fa097e33c07f66ce8c2094d69687aaf267
Jan Schneider <jan@horde.org>
2011-03-23 00:16:50 +0100

    Released imp-5.0.0RC1

--- imp/lib/Application.php
+++ imp/lib/Application.php
@@ -57,7 +57,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H4 (5.0-BETA1)';
+    public $version = 'H4 (5.0-RC1)';
 
     /**
      * Cached values to add to the session after authentication.
