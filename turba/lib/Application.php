b53f8eedd5259471ecba8a1860d48d7c7fa99f7f
Jan Schneider <jan@horde.org>
2011-03-23 00:20:39 +0100

    Development mode for turba-3.0.0RC2

--- turba/lib/Application.php
+++ turba/lib/Application.php
@@ -38,7 +38,7 @@ class Turba_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H4 (3.0-RC1)';
+    public $version = 'H4 (3.0-RC2)';
 
     /**
      * Global variables defined:
