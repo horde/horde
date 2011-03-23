cdd39f8b3f73a6c7a7865a6470b0c256753f227a
Gunnar Wrobel <p@rdus.de>
2011-03-23 20:28:53 +0100

    Ignore handler for "." and "..".

---
+++ framework/Pear/test/Horde/Pear/Unit/Package/Contents/Ignore/DotTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Test the dot file ignore handler for package contents.
+ *
+ * PHP version 5
+ *
+ * @category   Horde
+ * @package    Pear
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Pear
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../../../../Autoload.php';
+
+/**
+ * Test the dot file ignore handler for package contents.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Horde
+ * @package    Pear
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Pear
+ */
+class Horde_Pear_Unit_Package_Contents_Ignore_DotTest
+extends Horde_Pear_TestCase
+{
+    public function testAny()
+    {
+        $this->_checkNotIgnored('ANY');
+    }
+
+    public function testDot()
+    {
+        $this->_checkIgnored('.');
+    }
+
+    public function testDotDot()
+    {
+        $this->_checkIgnored('..');
+    }
+
+    private function _checkIgnored($file)
+    {
+        $this->assertTrue(
+            $this->_getIgnore()->isIgnored(new SplFileInfo($file))
+        );
+    }
+
+    private function _checkNotIgnored($file)
+    {
+        $this->assertFalse(
+            $this->_getIgnore()->isIgnored(new SplFileInfo($file))
+        );
+    }
+
+    private function _getIgnore()
+    {
+        return new Horde_Pear_Package_Contents_Ignore_Dot();
+    }
+}
