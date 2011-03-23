e739d04fc6d2b95879cc26bdc43f49bb351e0dcd
Gunnar Wrobel <p@rdus.de>
2011-03-23 17:33:47 +0100

    Stub for application testing.

---
+++ framework/Test/lib/Horde/Test/Application.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * A test helper for unit testing Horde applications.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Test
+ */
+
+/**
+ * A test helper for unit testing Horde applications.
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Test
+ */
+class Horde_Test_Application extends Horde_Test_Case
+{
+}
