254fc7c330138041cd08e5b5b008bcb0e3b7fb1f
Gunnar Wrobel <p@rdus.de>
2011-03-23 17:33:09 +0100

    Clearly indicate which suite could not be added.

---
+++ framework/Test/lib/Horde/Test/Exception.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The exception specific to this package.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Test
+ */
+
+/**
+ * The exception specific to this package.
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Test
+ */
+class Horde_Test_Exception extends Horde_Exception_Prior
+{
+}
