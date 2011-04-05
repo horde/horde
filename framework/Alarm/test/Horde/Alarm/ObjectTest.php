01d44d3c3daedee1e0a5931864ec26777d08574e
Jan Schneider <jan@horde.org>
2011-04-05 09:57:49 +0200

    Update deprecated usage.

--- framework/Alarm/test/Horde/Alarm/ObjectTest.php
+++ framework/Alarm/test/Horde/Alarm/ObjectTest.php
@@ -50,7 +50,7 @@ class Horde_Alarm_ObjectTest extends PHPUnit_Framework_TestCase
     public function testGet()
     {
         $alarm = self::$alarm->get('personalalarm', 'john');
-        $this->assertType('array', $alarm);
+        $this->assertInternalType('array', $alarm);
         $this->assertEquals('personalalarm', $alarm['id']);
         $this->assertEquals('john', $alarm['user']);
         $this->assertEquals(array(), $alarm['methods']);
