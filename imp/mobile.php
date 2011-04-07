9555b0cee71d2b36c9144d15d19346688e38005f
Michael M Slusarz <slusarz@curecanti.org>
2011-04-07 01:55:07 -0600

    Allow mobile view to work with POP3/disabled folders

--- imp/mobile.php
+++ imp/mobile.php
@@ -22,28 +22,20 @@ require $registry->get('templates', 'horde') . '/common-header-mobile.inc';
 $view = new Horde_View(array('templatePath' => IMP_TEMPLATES . '/mobile'));
 new Horde_View_Helper_Text($view);
 
-if (!$injector->getInstance('IMP_Factory_Imap')->create()->allowFolders()) {
-    $view->allowFolders = false;
-} else {
-    $view->allowFolders = true;
-
-    /* Initialize the IMP_Imap_Tree object. */
-    $imptree = $injector->getInstance('IMP_Imap_Tree');
-    $imptree->setIteratorFilter(IMP_Imap_Tree::FLIST_NOCONTAINER);
-    $tree = $imptree->createTree('mobile_folders', array(
-        'poll_info' => true,
-        'render_type' => 'Jquerymobile'
-    ));
-    $view->tree = $tree->getTree(true);
-}
+/* Initialize the IMP_Imap_Tree object. */
+$imptree = $injector->getInstance('IMP_Imap_Tree');
+$imptree->setIteratorFilter(IMP_Imap_Tree::FLIST_NOCONTAINER);
+$tree = $imptree->createTree('mobile_folders', array(
+    'poll_info' => true,
+    'render_type' => 'Jquerymobile'
+));
+$view->tree = $tree->getTree(true);
 
 $view->portal = Horde::getServiceLink('portal', 'horde')->setRaw(false);
 $view->logout = Horde::getServiceLink('logout')->setRaw(false);
 
 echo $view->render('head.html.php');
-if ($view->allowFolders) {
-    echo $view->render('folders.html.php');
-}
+echo $view->render('folders.html.php');
 echo $view->render('mailbox.html.php');
 echo $view->render('message.html.php');
 echo $view->render('notice.html.php');
