a9b1cbd21e99e2483ea3a088380b89fb775dfc4c
Jan Schneider <jan@horde.org>
2011-04-07 19:28:17 +0200

    If the request is an internal redirect from Apache, the correct URL is in REDIRECT_URL, not REQUEST_URI.

--- framework/Core/lib/Horde/Core/Factory/Request.php
+++ framework/Core/lib/Horde/Core/Factory/Request.php
@@ -8,7 +8,7 @@ class Horde_Core_Factory_Request extends Horde_Core_Factory_Injector
     public function create(Horde_Injector $injector)
     {
         $request = new Horde_Controller_Request_Http();
-        $request->setPath($_SERVER['REQUEST_URI']);
+        $request->setPath(isset($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : $_SERVER['REQUEST_URI']);
         return $request;
     }
 }
