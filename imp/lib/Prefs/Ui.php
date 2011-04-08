71e933ef1f87efc5563674b1271204b91a484ded
Michael M Slusarz <slusarz@curecanti.org>
2011-04-08 15:40:32 -0600

    Fix importing PGP private keys

--- imp/lib/Prefs/Ui.php
+++ imp/lib/Prefs/Ui.php
@@ -1117,6 +1117,7 @@ class IMP_Prefs_Ui
                 ), 'dom');
 
                 if ($GLOBALS['session']->get('imp', 'file_upload')) {
+                    $t->set('import_pgp_private', true);
                     Horde::addInlineScript(array(
                         '$("import_pgp_personal").observe("click", function(e) { ' . Horde::popupJs($pgp_url, array('params' => array('actionID' => 'import_personal_public_key', 'reload' => $GLOBALS['session']->store($ui->selfUrl()->setRaw(true), false)), 'height' => 275, 'width' => 750, 'urlencode' => true)) . '; e.stop(); })'
                     ), 'dom');
