5394aeb46011e471ddcb5a660b0c1c0e84707ebc
Jan Schneider <jan@horde.org>
2011-04-12 16:00:14 +0200

    Fix displaying the Facebook preference group.

--- horde/lib/Prefs/Ui.php
+++ horde/lib/Prefs/Ui.php
@@ -30,7 +30,6 @@ class Horde_Prefs_Ui
 
         if (empty($conf['facebook']['enabled']) ||
             empty($conf['facebook']['id']) ||
-            empty($conf['facebook']['key']) ||
             empty($conf['facebook']['secret'])) {
             $ui->suppressGroups[] = 'facebook';
         }
