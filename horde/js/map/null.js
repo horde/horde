39c7241a50a2f6aa15bcc2962d3437524704d88b
Michael J. Rubinsky <mrubinsk@horde.org>
2011-04-12 23:47:13 -0400

    Check for a lon/lat pair when using the null geocoder.
    Parially fixes Bug: 9883

--- horde/js/map/null.js
+++ horde/js/map/null.js
@@ -6,12 +6,18 @@ HordeMap.Geocoder.Null = Class.create({
 
     geocode: function(address, callback, onErrorCallback)
     {
-        return onErrorCallback('No geocoding support');
+        // Try to get a lon/lat out of this.
+        var ll = address.match(/(-?\d+\.\d+)\ (-?\d+\.\d+)/);
+        if (ll) {
+            return callback([{ lat: ll[2], lon: ll[1], 'address': address, precision: 1 }]);
+        }
+
+        return onErrorCallback('No geocoding support. Try entering a longitude latitude pair.');
     },
 
     reverseGeocode: function(lonlat, completeCallback, errorCallback)
     {
-        var ll = { lon: lonlat.lon, lat: lonlat.lat, address: lonlat.lon + ' ' + lonlat.lat};
+        var ll = { lon: lonlat.lon, lat: lonlat.lat, address: lonlat.lon + ' ' + lonlat.lat };
         return completeCallback([ll]);
     }
 });
