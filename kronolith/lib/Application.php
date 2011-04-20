337ad61b06f4c4285c5c585b76a8dbbbbef5faa7
Jan Schneider <jan@horde.org>
2011-04-20 22:33:04 +0200

    Development mode for kronolith-3.0.2-git

--- kronolith/lib/Application.php
+++ kronolith/lib/Application.php
@@ -44,7 +44,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H4 (3.0.1)';
+    public $version = 'H4 (3.0.2-git)';
 
     /**
      * Global variables defined:
