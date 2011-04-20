c8657ac04b71005179238b0ecf4ecd1214869bbc
Jan Schneider <jan@horde.org>
2011-04-20 22:24:58 +0200

    Development mode for imp-5.0.2-git

--- imp/lib/Application.php
+++ imp/lib/Application.php
@@ -57,7 +57,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H4 (5.0.1)';
+    public $version = 'H4 (5.0.2-git)';
 
     /**
      * Cached values to add to the session after authentication.
