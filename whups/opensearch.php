fd07429c7f8036e44001c07585dae18a022bdd94
Vilius Šumskas <vilius@lnk.lt>
2011-04-23 13:18:51 +0300

    Fix opensearch charset and icon path (Bug #9959)
    Signed-off-by: Chuck Hagenbuch <chuck@horde.org>

--- whups/opensearch.php
+++ whups/opensearch.php
@@ -17,7 +17,7 @@ $url = Horde::url('', true);
 $name = $registry->get('name', 'whups') . ' (' . $url . ')';
 
 // Icon.
-$icon = base64_encode(file_get_contents($registry->get('themesfs', 'whups') . '/graphics/whups.png'));
+$icon = base64_encode(file_get_contents($registry->get('themesfs', 'whups') . '/default/graphics/whups.png'));
 
 // Charset.
 header('Content-Type: text/xml; charset=UTF-8');
@@ -31,7 +31,7 @@ echo <<<PAYLOAD
     <Param name="id" value="{searchTerms}"/>
   </Url>
   <Image height="16" width="16">data:image/png;base64,$icon</Image>
-  <InputEncoding>$charset</InputEncoding>
+  <InputEncoding>UTF-8</InputEncoding>
 
 </OpenSearchDescription>
 PAYLOAD;
