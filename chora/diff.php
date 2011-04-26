e08868fffae1e334a61d7177379378e682f95374
Michael M Slusarz <slusarz@curecanti.org>
2011-04-25 18:11:46 -0600

    Re-add ability to link to diff from previous version in log entries - since it is arguably the most important feature of a VCS display

--- chora/diff.php
+++ chora/diff.php
@@ -1,12 +1,22 @@
 <?php
 /**
+ * Diff display.
+ *
+ * Parameters in:
+ *   - r1: Revision 1
+ *   - r2: Revision 2 (if empty, will diff to previous revision of r1)
+ *   - t: 'colored'
+ *   - ty: Diff type
+ *
  * Copyright 2000-2011 The Horde Project (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
- * @author  Anil Madhavapeddy <avsm@horde.org>
- * @package Chora
+ * @author   Anil Madhavapeddy <avsm@horde.org>
+ * @category Horde
+ * @license  http://www.fsf.org/copyleft/gpl.html GPL
+ * @package  Chora
  */
 
 require_once dirname(__FILE__) . '/lib/Application.php';
@@ -19,18 +29,21 @@ try {
     Chora::fatal($e);
 }
 
-/* Initialise the form variables correctly. */
-$r1 = Horde_Util::getFormData('r1');
-$r2 = Horde_Util::getFormData('r2');
+$vars = Horde_Variables::getDefaultVariables();
+
+if (!isset($vars->r2)) {
+    $vars->r2 = $vars->r1;
+    $vars->r1 = $fl->queryPreviousRevision($vars->r1);
+}
 
 /* Ensure that we have valid revision numbers. */
-if (!$VC->isValidRevision($r1) || !$VC->isValidRevision($r2)) {
+if (!$VC->isValidRevision($vars->r1) || !$VC->isValidRevision($vars->r2)) {
     Chora::fatal(_("Malformed Query"), '500 Internal Server Error');
 }
 
 /* If no type has been specified, then default to human readable. */
-$type = Horde_Util::getFormData('t', 'colored');
-if (Horde_Util::getFormData('ty') == 'u') {
+$type = $vars->get('t', 'colored');
+if ($vara->ty == 'u') {
     $type = 'unified';
 }
 
@@ -42,19 +55,19 @@ header('Cache-Control: max-age=604800');
  * the end of the file - patch requires it. */
 if ($type != 'colored') {
     header('Content-Type: text/plain');
-    echo implode("\n", $VC->diff($fl, $r1, $r2, array('num' => $num, 'type' => $type))) . "\n";
+    echo implode("\n", $VC->diff($fl, $vars->r1, $vars->r2, array('num' => $num, 'type' => $type))) . "\n";
     exit;
 }
 
 /* Human-Readable diff. */
-$abbrev_r1 = $VC->abbrev($r1);
-$abbrev_r2 = $VC->abbrev($r2);
+$abbrev_r1 = $VC->abbrev($vars->r1);
+$abbrev_r2 = $VC->abbrev($vars->r2);
 $title = sprintf(_("Diff for %s between version %s and %s"),
                  $injector->getInstance('Horde_Core_Factory_TextFilter')->filter($where, 'space2html', array('encode' => true, 'encode_all' => true)), $abbrev_r1, $abbrev_r2);
 
 /* Format log entries. */
 $log_messages = array();
-foreach ($VC->getRevisionRange($fl, $r1, $r2) as $val) {
+foreach ($VC->getRevisionRange($fl, $vars->r1, $vars->r2) as $val) {
     $clog = $fl->queryLogs($val);
     if (!is_null($clog)) {
         $log_messages[] = $clog;
@@ -70,15 +83,15 @@ require CHORA_TEMPLATES . '/diff/header.inc';
 $mime_type = Horde_Mime_Magic::filenameToMIME($fullname);
 if (substr($mime_type, 0, 6) == 'image/') {
     /* Check for images. */
-    $url1 = Chora::url('co', $where, array('r' => $r1, 'p' => 1));
-    $url2 = Chora::url('co', $where, array('r' => $r2, 'p' => 1));
+    $url1 = Chora::url('co', $where, array('r' => $vars->r1, 'p' => 1));
+    $url2 = Chora::url('co', $where, array('r' => $vars->r2, 'p' => 1));
 
-    echo "<tr><td><img src=\"$url1\" alt=\"" . htmlspecialchars($r1) . '" /></td>' .
-        "<td><img src=\"$url2\" alt=\"" . htmlspecialchars($r2) . '" /></td></tr>';
+    echo "<tr><td><img src=\"$url1\" alt=\"" . htmlspecialchars($vars->r1) . '" /></td>' .
+        "<td><img src=\"$url2\" alt=\"" . htmlspecialchars($vars->r2) . '" /></td></tr>';
 } else {
     $view = $injector->createInstance('Horde_View');
     $view->addHelper('Chora_Diff_Helper');
-    echo $view->diff($fl, $r1, $r2);
+    echo $view->diff($fl, $vars->r1, $vars->r2);
     echo $view->diffCaption();
 }
 
