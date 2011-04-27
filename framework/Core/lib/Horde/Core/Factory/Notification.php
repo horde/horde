6e9a0b7c052e69921dfcad86175ccb5d1e32db44
Jan Schneider <jan@horde.org>
2011-04-27 16:20:38 +0200

    Make the notification handler a singleton. Fixes alarm notifications.

--- framework/Core/lib/Horde/Core/Factory/Notification.php
+++ framework/Core/lib/Horde/Core/Factory/Notification.php
@@ -5,28 +5,34 @@
  */
 class Horde_Core_Factory_Notification extends Horde_Core_Factory_Base
 {
+    protected $_notify;
+
     public function create()
     {
-        $notify = new Horde_Notification_Handler(
+        if (isset($this->_notify)) {
+            return $this->_notify;
+        }
+
+        $this->_notify = new Horde_Notification_Handler(
             new Horde_Core_Notification_Storage_Session()
         );
 
-        $notify->addType('default', '*', 'Horde_Core_Notification_Event_Status');
-        $notify->addType('status', 'horde.*', 'Horde_Core_Notification_Event_Status');
+        $this->_notify->addType('default', '*', 'Horde_Core_Notification_Event_Status');
+        $this->_notify->addType('status', 'horde.*', 'Horde_Core_Notification_Event_Status');
 
-        $notify->addDecorator(new Horde_Notification_Handler_Decorator_Alarm($this->_injector->getInstance('Horde_Core_Factory_Alarm'), $GLOBALS['registry']->getAuth()));
-        $notify->addDecorator(new Horde_Core_Notification_Handler_Decorator_Hordelog());
+        $this->_notify->addDecorator(new Horde_Notification_Handler_Decorator_Alarm($this->_injector->getInstance('Horde_Core_Factory_Alarm'), $GLOBALS['registry']->getAuth()));
+        $this->_notify->addDecorator(new Horde_Core_Notification_Handler_Decorator_Hordelog());
 
         foreach ($GLOBALS['registry']->listApps(null, false, Horde_Perms::READ) as $app) {
             if ($GLOBALS['registry']->isAuthenticated(array('app' => $app, 'notransparent' => true))) {
                 try {
-                    $GLOBALS['registry']->callAppMethod($app, 'setupNotification', array('args' => array($notify), 'noperms' => true));
+                    $GLOBALS['registry']->callAppMethod($app, 'setupNotification', array('args' => array($this->_notify), 'noperms' => true));
                 } catch (Exception $e) {
                     continue;
                 }
             }
         }
 
-        return $notify;
+        return $this->_notify;
     }
 }
