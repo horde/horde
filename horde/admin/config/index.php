df0add699b5b06ba8bd9ab614ef595e9d86ebf28
Jan Schneider <jan@horde.org>
2011-05-11 17:36:14 +0200

    Add button to update all configurations with default values at once.
    Strictly this is a new feature, but it's required for painless
    Groupware installations.

--- horde/admin/config/index.php
+++ horde/admin/config/index.php
@@ -60,6 +60,7 @@ function _uploadFTP($params)
 $hconfig = new Horde_Config();
 $migration = new Horde_Core_Db_Migration(dirname(__FILE__) . '/../../..');
 $vars = Horde_Variables::getDefaultVariables();
+$a = $registry->listAllApps();
 
 /* Check for versions if requested. */
 $versions = array();
@@ -68,6 +69,29 @@ if ($vars->check_versions) {
         $versions = $hconfig->checkVersions();
     } catch (Horde_Exception $e) {
         $notification->push(_("Could not contact server. Try again later."), 'horde.error');
+    }
+}
+
+/* Update configurations if requested. */
+if ($vars->action == 'config') {
+    foreach ($a as $app) {
+        $path = $registry->get('fileroot', $app) . '/config';
+        if (!file_exists($path . '/conf.xml') ||
+            (file_exists($path . '/conf.php') &&
+             ($xml_ver = $hconfig->getVersion(@file_get_contents($path . '/conf.xml'))) !== false &&
+             ($php_ver = $hconfig->getVersion(@file_get_contents($path . '/conf.php'))) !== false &&
+             $xml_ver == $php_ver)) {
+            continue;
+        }
+        $vars = Horde_Variables::getDefaultVariables();
+        $form = new Horde_Config_Form($vars, $app, true);
+        $form->setSubmitted(true);
+        if ($form->validate($vars)) {
+            $config = new Horde_Config($app);
+            $config->writePHPConfig($vars);
+        } else {
+            $notification->push(sprintf(_("The configuration for %s cannot be updated automatically. Please update the configuration manually."), $app), 'horde.error');
+        }
     }
 }
 
@@ -95,10 +119,9 @@ $error = Horde::img('alerts/error.png');
 
 $self_url = Horde::url('admin/config/');
 $conf_url = Horde::url('admin/config/config.php');
-$a = $registry->listAllApps();
 $apps = $libraries = array();
 $i = -1;
-$schema_outdated = false;
+$config_outdated = $schema_outdated = false;
 if (file_exists(HORDE_BASE . '/lib/bundle.php')) {
     include HORDE_BASE . '/lib/bundle.php';
     $apps[0] = array('sort' => '00',
@@ -163,11 +186,13 @@ foreach ($a as $app) {
             /* No conf.php exists. */
             $apps[$i]['conf'] = $conf_link . $error . '</a>';
             $apps[$i]['status'] = $conf_link . _("Missing configuration.") . '</a>';
+            $config_outdated = true;
         } else {
             /* A conf.php exists, get the xml version. */
             if (($xml_ver = $hconfig->getVersion(@file_get_contents($path . '/conf.xml'))) === false) {
                 $apps[$i]['conf'] = $conf_link . $warning . '</a>';
                 $apps[$i]['status'] = $conf_link . _("No version found in original configuration. Regenerate configuration.") . '</a>';
+                $config_outdated = true;
                 continue;
             }
             /* Get the generated php version. */
@@ -176,6 +201,7 @@ foreach ($a as $app) {
                  * case. */
                 $apps[$i]['conf'] = $conf_link . $warning . '</a>';
                 $apps[$i]['status'] = $conf_link . _("No version found in your configuration. Regenerate configuration.") . '</a>';
+                $config_outdated = true;
                 continue;
             }
 
@@ -183,6 +209,7 @@ foreach ($a as $app) {
                 /* Versions are not the same, configuration is out of date. */
                 $apps[$i]['conf'] = $conf_link . $error . '</a>';
                 $apps[$i]['status'] = $conf_link . _("Configuration is out of date.") . '</a>';
+                $config_outdated = true;
             } else {
                 /* Configuration is ok. */
                 $apps[$i]['conf'] = $conf_link . $success . '</a>';
@@ -310,6 +337,7 @@ $template->setOption('gettext', true);
 $template->set('versions', !empty($versions), true);
 $template->set('version_action', Horde::url('admin/config/index.php'));
 $template->set('version_input', Horde_Util::formInput());
+$template->set('config_outdated', $config_outdated && method_exists('Horde_Config', 'writePHPConfig'));
 $template->set('schema_outdated', $schema_outdated);
 $template->set('apps', $apps);
 $template->set('actions', $actions, true);
