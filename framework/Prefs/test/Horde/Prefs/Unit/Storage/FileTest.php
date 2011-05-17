0529848214d2b657d7d8b1cfbefb9ac427d9fe76
Gunnar Wrobel <p@rdus.de>
2011-05-17 06:35:22 +0200

    Use Horde_Util::createTempDir()

--- framework/Prefs/test/Horde/Prefs/Unit/Storage/FileTest.php
+++ framework/Prefs/test/Horde/Prefs/Unit/Storage/FileTest.php
@@ -17,7 +17,7 @@
 require_once dirname(__FILE__) . '/../../Autoload.php';
 
 /**
- * Base for session testing.
+ * Test the file based preferences storage backend.
  *
  * Copyright 2010-2011 The Horde Project (http://www.horde.org/)
  *
@@ -32,13 +32,6 @@ require_once dirname(__FILE__) . '/../../Autoload.php';
  */
 class Horde_Prefs_Unit_Storage_FileTest extends PHPUnit_Framework_TestCase
 {
-    public function tearDown()
-    {
-        if (!empty($this->_temp_dir)) {
-            $this->_rrmdir($this->_temp_dir);
-        }
-    }
-
     /**
      * @expectedException InvalidArgumentException
      */
@@ -57,32 +50,6 @@ class Horde_Prefs_Unit_Storage_FileTest extends PHPUnit_Framework_TestCase
 
     public function testConstruction()
     {
-        $b = new Horde_Prefs_Storage_File('nobody', array('directory' => $this->_getTemporaryDirectory()));
-    }
-
-    private function _getTemporaryDirectory()
-    {
-        $this->_temp_dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR
-            . 'Horde_Prefs_' . mt_rand();
-        mkdir($this->_temp_dir);
-        return $this->_temp_dir;
-    }
-
-    private function _rrmdir($dir)
-    {
-        if (is_dir($dir)) {
-            $objects = scandir($dir);
-            foreach ($objects as $object) {
-                if ($object != '.' && $object != '..') {
-                    if (filetype($dir . DIRECTORY_SEPARATOR . $object) == 'dir') {
-                        $this->_rrmdir($dir . DIRECTORY_SEPARATOR . $object);
-                    } else {
-                        unlink($dir . DIRECTORY_SEPARATOR . $object);
-                    }
-                }
-            }
-            reset($objects);
-            rmdir($dir);
-        }
+        $b = new Horde_Prefs_Storage_File('nobody', array('directory' => Horde_Util::createTempDir()));
     }
 }
