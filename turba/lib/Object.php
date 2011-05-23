horde/horde

commit 9fd6579fa7b1e1a920601c3f9517c1fd18a84c0e

Fix creating temporary files with open_basedir restrictions.
if tempnam() is called with an empty first parameter, open_basedir
restrictions set in. Use Horde_Util::getTempFile() consistently, and
make sure that it always uses a valid temporary directory.

turba/lib/Object.php
@@ -112,7 +112,7 @@ class Turba_Object
                 : array(
                       'load' => array(
                           'data' => $this->attributes[$attribute],
-                          'file' => basename(tempnam(Horde::getTempDir(), 'horde_form_'))
+                          'file' => basename(Horde::getTempFile('horde_form_'))
                       )
                   );
         }

framework/Text_Diff/lib/Horde/Text/Diff/Engine/Shell.php
@@ -36,8 +36,8 @@ class Horde_Text_Diff_Engine_Shell
         array_walk($to_lines, array('Horde_Text_Diff', 'trimNewlines'));
 
         // Execute gnu diff or similar to get a standard diff file.
-        $from_file = tempnam(null, 'Horde_Text_Diff');
-        $to_file = tempnam(null, 'Horde_Text_Diff');
+        $from_file = Horde_Util::getTempFile('Horde_Text_Diff');
+        $to_file = Horde_Util::getTempFile('Horde_Text_Diff');
         $fp = fopen($from_file, 'w');
         fwrite($fp, implode("\n", $from_lines));
         fclose($fp);

framework/Text_Diff/package.xml
@@ -29,7 +29,7 @@
  </stability>
  <license uri="http://opensource.org/licenses/lgpl-license.php">LGPL</license>
  <notes>
-* 
+* [jan] Fix creating temporary files with open_basedir restrictions.
  </notes>
  <contents>
   <dir baseinstalldir="/" name="/">
@@ -279,7 +279,7 @@
    <date>2011-04-06</date>
    <license uri="http://opensource.org/licenses/lgpl-license.php">LGPL</license>
    <notes>
-* 
+* [jan] Fix creating temporary files with open_basedir restrictions.
    </notes>
   </release>
  </changelog>

framework/Util/lib/Horde/Util.php
@@ -333,7 +333,7 @@ class Horde_Util
                                        $secure = false)
     {
         $tempDir = (empty($dir) || !is_dir($dir))
-            ? null
+            ? self::getTempDir()
             : $dir;
 
         $tempFile = tempnam($tempDir, $prefix);

framework/Util/package.xml
@@ -29,7 +29,7 @@
  </stability>
  <license uri="http://www.gnu.org/copyleft/lesser.html">LGPL</license>
  <notes>
-* 
+* [jan] Fix creating temporary files with open_basedir restrictions.
  </notes>
  <contents>
   <dir baseinstalldir="/" name="/">
@@ -341,7 +341,7 @@ Converted to package.xml 2.0 for pear.horde.org
    <date>2011-04-06</date>
    <license uri="http://www.gnu.org/copyleft/lesser.html">LGPL</license>
    <notes>
-* 
+* [jan] Fix creating temporary files with open_basedir restrictions.
    </notes>
   </release>
  </changelog>

framework/Vfs/lib/Horde/Vfs/Base.php
@@ -190,7 +190,7 @@ abstract class Horde_Vfs_Base
     {
         // Create a temporary file and register it for deletion at the
         // end of this request.
-        if (!($localFile = tempnam(null, 'vfs'))) {
+        if (!($localFile = Horde_Util::getTempFile('vfs'))) {
             throw new Horde_Vfs_Exception('Unable to create temporary file.');
         }
         register_shutdown_function(create_function('', 'unlink(\'' . addslashes($localFile) . '\');'));

framework/Vfs/lib/Horde/Vfs/Ftp.php
@@ -153,7 +153,7 @@ class Horde_Vfs_Ftp extends Horde_Vfs_Base
 
         // Create a temporary file and register it for deletion at the
         // end of this request.
-        if (!($localFile = tempnam(null, 'vfs'))) {
+        if (!($localFile = Horde_Util::getTempFile('vfs'))) {
             throw new Horde_Vfs_Exception('Unable to create temporary file.');
         }
         register_shutdown_function(create_function('', '@unlink(\'' . addslashes($localFile) . '\');'));
@@ -226,7 +226,7 @@ class Horde_Vfs_Ftp extends Horde_Vfs_Base
     public function writeData($path, $name, $data, $autocreate = false)
     {
         $this->_checkQuotaWrite('string', $data);
-        $tmpFile = tempnam(null, 'vfs');
+        $tmpFile = Horde_Util::getTempFile('vfs');
         file_put_contents($tmpFile, $data);
         try {
             $this->write($path, $name, $tmpFile, $autocreate);
@@ -679,7 +679,7 @@ class Horde_Vfs_Ftp extends Horde_Vfs_Base
         if ($this->isFolder($path, $name)) {
             $this->_copyRecursive($path, $name, $dest);
         } else {
-            $tmpFile = tempnam(null, 'vfs');
+            $tmpFile = Horde_Util::getTempFile('vfs');
             $fetch = @ftp_get($this->_stream, $tmpFile, $orig, FTP_BINARY);
             if (!$fetch) {
                 unlink($tmpFile);

framework/Vfs/lib/Horde/Vfs/Smb.php
@@ -89,7 +89,7 @@ class Horde_Vfs_Smb extends Horde_Vfs_Base
     {
         // Create a temporary file and register it for deletion at the
         // end of this request.
-        if (!($localFile = tempnam(null, 'vfs'))) {
+        if (!($localFile = Horde_Util::getTempFile('vfs'))) {
             throw new Horde_Vfs_Exception('Unable to create temporary file.');
         }
         register_shutdown_function(create_function('', '@unlink(\'' . addslashes($localFile) . '\');'));
@@ -155,7 +155,7 @@ class Horde_Vfs_Smb extends Horde_Vfs_Base
      */
     public function writeData($path, $name, $data, $autocreate = false)
     {
-        $tmpFile = tempnam(null, 'vfs');
+        $tmpFile = Horde_Util::getTempFile('vfs');
         file_put_contents($tmpFile, $data);
         try {
             $this->write($path, $name, $tmpFile, $autocreate);

commit 3a6dae7dab4c44fce7e283bd0c0ed4489808851a

Bug: 10050 Don't remove tmp image file at request shutdown.
Commit 9fd6579fa7b1e1a920601c3f9517c1fd18a84c0e broke the existing
behavior of not removing the temporary file. This restores that
functionality. Not sure if this is the final fix for this, as this
causes tmp image files to build up in the temporary directory. However,
this was the previous behavior and should be the admin's job to perform
gc on temp directory periodically anyway.

turba/lib/Object.php
@@ -112,7 +112,7 @@ class Turba_Object
                 : array(
                       'load' => array(
                           'data' => $this->attributes[$attribute],
-                          'file' => basename(Horde::getTempFile('horde_form_'))
+                          'file' => basename(Horde::getTempFile('horde_form_', false))
                       )
                   );
         }