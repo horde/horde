4967256db25bb0e0a40dfad8c508bcfb6e3eed95
Ralf Lang <lang@b1-systems.de>
2011-05-27 12:44:32 +0000

    [#8786] turba minisearch enhancement -- done. Greetings from B1 Hack Week.

--- turba/lib/Block/Minisearch.php
+++ turba/lib/Block/Minisearch.php
@@ -6,10 +6,14 @@ class Turba_Block_Minisearch extends Horde_Core_Block
 {
     /**
      */
+    protected $_options = array();
+
     public function __construct($app, $params = array())
     {
         parent::__construct($app, $params);
-
+        foreach (Turba::getAddressBooks(Horde_Perms::READ) as $key => $addressbook) {
+             $this->_options[$key] = $addressbook['title'];
+        }
         $this->_name = _("Contact Search");
     }
 
@@ -22,15 +26,37 @@ class Turba_Block_Minisearch extends Horde_Core_Block
     }
 
     /**
+     * Select the address books where to search
+     *
+     * @var string
+     */
+    protected function _params()
+    {
+        return array(
+            'addressbooks' => array(
+                'type' => 'multienum',
+                'name' => _("Address Books"),
+                'values' => $this->_options
+            )
+        );
+    }
+    /**
      */
     protected function _content()
     {
         if ($GLOBALS['browser']->hasFeature('iframes')) {
+            $imploded_calendars = '';
+            //<?php echo implode(';',$this->_params['addressbooks']);
+            if (count($this->_params['addressbooks'])) {
+                $imploded_calendars = implode(';', $this->_params['addressbooks']);
+            } else 
+            {
+                $imploded_calendars = implode(';', array_keys($this->_options));
+            }
             Horde::startBuffer();
             include TURBA_TEMPLATES . '/block/minisearch.inc';
             return Horde::endBuffer();
         }
-
         return '<em>' . _("A browser that supports iframes is required") . '</em>';
     }
 
