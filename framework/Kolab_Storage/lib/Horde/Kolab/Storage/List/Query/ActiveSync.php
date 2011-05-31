a625e6d575b5bce6790af906979f26a8576d2743
Gunnar Wrobel <p@rdus.de>
2011-05-31 05:08:24 +0200

    [gwr] Added a handler for active sync settings.

---
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/List/Query/ActiveSync.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The interface of the query for active sync settings.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Storage
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+
+/**
+ * The interface of the query for active sync settings.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @since Horde_Kolab_Storage 1.1.0
+ *
+ * @category Kolab
+ * @package  Kolab_Storage
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+interface Horde_Kolab_Storage_List_Query_ActiveSync
+extends Horde_Kolab_Storage_List_Query
+{
+    /**
+     * Returns the active sync settings.
+     *
+     * @param string $folder The folder name.
+     *
+     * @return array The folder active sync parameters.
+     */
+    public function getActiveSync($folder);
+
+    /**
+     * Set the active sync settings.
+     *
+     * @param string $folder The folder name.
+     * @param array  $data   The active sync settings.
+     *
+     * @return string The encoded share parameters.
+     */
+    public function setActiveSync($folder, array $data);
+}
---
+++ framework/Kolab_Storage/test/Horde/Kolab/Storage/Unit/List/Query/ActiveSync/BaseTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Test the handling of active sync data.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Storage
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../../../../Autoload.php';
+
+/**
+ * Test the handling of active sync data.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Kolab
+ * @package  Kolab_Storage
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Storage
+ */
+class Horde_Kolab_Storage_Unit_List_Query_Activesync_BaseTest
+extends Horde_Kolab_Storage_TestCase
+{
+    public function testGetEmptyActiveSync()
+    {
+        $activesync = $this->_getActivesync();
+        $this->driver->expects($this->once())
+            ->method('getAnnotation')
+            ->with('INBOX', '/priv/vendor/kolab/activesync')
+            ->will($this->returnValue(''));
+        $this->assertEquals(null, $activesync->getActiveSync('INBOX'));
+    }
+
+    public function testGetValidActiveSync()
+    {
+        $activesync = $this->_getActivesync();
+        $this->driver->expects($this->once())
+            ->method('getAnnotation')
+            ->with('INBOX', '/priv/vendor/kolab/activesync')
+            ->will($this->returnValue('eyJ4IjoieSJ9'));
+        $this->assertEquals(array('x' => 'y'), $activesync->getActiveSync('INBOX'));
+    }
+
+    public function testGetInvalidBase64ActiveSync()
+    {
+        $activesync = $this->_getActivesync();
+        $this->driver->expects($this->once())
+            ->method('getAnnotation')
+            ->with('INBOX', '/priv/vendor/kolab/activesync')
+            ->will($this->returnValue('#&&'));
+        $this->assertEquals(null, $activesync->getActiveSync('INBOX'));
+    }
+
+    public function testGetInvalidJsonActiveSync()
+    {
+        $activesync = $this->_getActivesync();
+        $this->driver->expects($this->once())
+            ->method('getAnnotation')
+            ->with('INBOX', '/priv/vendor/kolab/activesync')
+            ->will($this->returnValue('SGFsbG8K'));
+        $this->assertEquals(null, $activesync->getActiveSync('INBOX'));
+    }
+
+    public function testSetActiveSync()
+    {
+        $activesync = $this->_getActivesync();
+        $this->driver->expects($this->once())
+            ->method('setAnnotation')
+            ->with('INBOX', '/priv/vendor/kolab/activesync', 'eyJ4IjoieSJ9');
+        $activesync->setActiveSync('INBOX', array('x' => 'y'));
+    }
+
+    private function _getActivesync()
+    {
+        $this->driver = $this->getMock('Horde_Kolab_Storage_Driver');
+        $this->list = new Horde_Kolab_Storage_List_Base(
+            $this->driver,
+            new Horde_Kolab_Storage_Factory()
+        );
+        return new Horde_Kolab_Storage_List_Query_ActiveSync_Base(
+            $this->list, array()
+        );
+    }
+}
