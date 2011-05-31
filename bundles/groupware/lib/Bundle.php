285502a0fd3422545e5bfb360857cd0c19fc385b
Jan Schneider <jan@horde.org>
2011-05-31 19:29:25 +0200

    Extend Horde_Core_Bundle, implement _configAuth().

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -6,10 +6,10 @@
  *
  * Copyright 2011 The Horde Project (http://www.horde.org/)
  *
+ * @author  Jan Schneider <chuck@horde.org>
  * @package groupware
  */
-
-class Horde_Bundle
+class Horde_Bundle extends Horde_Core_Bundle
 {
     /**
      * The bundle name.
@@ -25,4 +25,51 @@ class Horde_Bundle
      * The bundle descriptive name.
      */
     const FULLNAME = 'Horde Groupware';
+
+    /**
+     * Asks for the administrator settings.
+     *
+     * @return string  The administrator name.
+     */
+    protected function _configAuth(Horde_Variables $vars)
+    {
+        $vars->auth__driver = 'sql';
+
+        while (true) {
+            $admin_user = $cli->prompt('Specify a user name for the administrator account:');
+            if (empty($admin_user)) {
+                $cli->writeln($cli->red('An administration user is required'));
+                continue;
+            }
+            $admin_pass = $cli->prompt('Specify a password for the adminstrator account:');
+            if (empty($admin_pass)) {
+                $cli->writeln($cli->red('An administrator password is required'));
+            } else {
+                $auth = &Auth::singleton($GLOBALS['conf']['auth']['driver']);
+                $exists = $auth->exists($admin_user);
+                if (is_a($exists, 'PEAR_Error')) {
+                    $cli->message('An error occured while trying to list the users. Error messages:', 'cli.error');
+                    $cli->writeln($exists->getMessage());
+                    $cli->writeln($exists->getUserInfo());
+                    return;
+                }
+                if ($exists) {
+                    if ($cli->prompt('This user exists already, do you want to update his password?', array('y' => 'Yes', 'n' => 'No'), 'y') == 'y') {
+                        $result = $auth->updateUser($admin_user, $admin_user, array('password' => $admin_pass));
+                    } else {
+                        break;
+                    }
+                } else {
+                    $result = $auth->addUser($admin_user, array('password' => $admin_pass));
+                }
+                if (is_a($result, 'PEAR_Error')) {
+                    $cli->message('An error occured while adding or updating the adminstrator. Error messages:', 'cli.error');
+                    $cli->writeln($result->getMessage());
+                    $cli->writeln($result->getUserInfo());
+                    return;
+                }
+                break;
+            }
+        }
+    }
 }
--- bundles/webmail/lib/Bundle.php
+++ bundles/webmail/lib/Bundle.php
@@ -6,10 +6,10 @@
  *
  * Copyright 2011 The Horde Project (http://www.horde.org/)
  *
+ * @author  Jan Schneider <chuck@horde.org>
  * @package webmail
  */
-
-class Horde_Bundle
+class Horde_Bundle extends Horde_Core_Bundle
 {
     /**
      * The bundle name.
@@ -25,4 +25,16 @@ class Horde_Bundle
      * The bundle descriptive name.
      */
     const FULLNAME = 'Horde Groupware Webmail Edition';
+
+    /**
+     * Asks for the administrator settings.
+     *
+     * @return string  The administrator name.
+     */
+    protected function _configAuth(Horde_Variables $vars)
+    {
+        $vars->auth__driver = 'application';
+        $vars->auth__params__app = 'imp';
+        return $this->_cli->prompt('Specify an ' . $this->_cli->bold('existing') . ' mail user who you want to give administrator permissions (optional):');
+    }
 }
