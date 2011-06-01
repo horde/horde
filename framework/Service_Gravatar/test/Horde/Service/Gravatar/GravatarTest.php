6565c6f00bca6cfa8d64dc47fa1894accaccd886
Gunnar Wrobel <p@rdus.de>
2011-06-01 08:33:15 +0200

    Start the test suite for "Horde_Service_Gravatar".
    This extends the default PHPUnit_Framework_TestCase for now. Running
    this suite with "phpunit GravatarTest.php" will still fail as no tests
    are present (yet).

---
+++ framework/Service_Gravatar/test/Horde/Service/Gravatar/GravatarTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Horde_Service_Gravatar abstracts communication with Services supporting the
+ * Gravatar API (http://www.gravatar.com/site/implement/).
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Service_Gravatar
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Service_Gravatar
+ */
+
+/**
+ * Horde_Service_Gravatar abstracts communication with Services supporting the
+ * Gravatar API (http://www.gravatar.com/site/implement/).
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Horde
+ * @package  Service_Gravatar
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Service_Gravatar
+ */
+class Horde_Service_Gravatar_GravatarTest
+extends PHPUnit_Framework_TestCase
+{
+}
