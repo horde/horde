c0623760f58348f34ea6be9308f24e5fcde5d47d
Michael M Slusarz <slusarz@curecanti.org>
2011-06-04 17:21:38 -0600

    Bug #10178: Fix undefined error

--- imp/lib/IMP.php
+++ imp/lib/IMP.php
@@ -243,6 +243,9 @@ class IMP
 
         $args = array_merge(self::_decodeMailto($args), $extra);
         $callback = $raw = false;
+        $uid = isset($args['uid'])
+            ? $args['uid']
+            : null;
         $view = self::getViewMode();
 
         if ($simplejs || ($view == 'dimp')) {
@@ -265,14 +268,14 @@ class IMP
         }
 
         if (isset($args['thismailbox'])) {
-            $url = IMP_Mailbox::get($args['thismailbox'])->url($url, $args['uid']);
-            unset($args['thismailbox'], $args['uid']);
+            $url = IMP_Mailbox::get($args['thismailbox'])->url($url, $uid);
         } elseif (isset($args['mailbox'])) {
-            $url = IMP_Mailbox::get($args['mailbox'])->url($url, $args['uid']);
-            unset($args['mailbox'], $args['uid']);
+            $url = IMP_Mailbox::get($args['mailbox'])->url($url, $uid);
         } else {
             $url = Horde::url($url);
         }
+
+        unset($args['mailbox'], $args['thismailbox'], $args['uid']);
 
         $url->setRaw($raw)->add($args);
         if ($callback) {
