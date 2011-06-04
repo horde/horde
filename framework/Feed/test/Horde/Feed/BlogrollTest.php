843243468d0fc4a2b29d7229aaff440859be7b3e
Gunnar Wrobel <p@rdus.de>
2011-06-04 13:00:27 -0400

    t/framework/Feed/Blogroll_Tests
    Tries to fixes the blogroll tests in the Feed package.
    
    It is strange though that the node attributes are handled
    case-insensitive. XML attributes are case-sensitive.
    
    Signed-off-by: Gunnar Wrobel <p@rdus.de>

--- framework/Feed/test/Horde/Feed/BlogrollTest.php
+++ framework/Feed/test/Horde/Feed/BlogrollTest.php
@@ -31,13 +31,13 @@ class Horde_Feed_BlogrollTest extends PHPUnit_Framework_TestCase
 
         $this->assertType('Horde_Feed_Entry_Blogroll', $entry);
         $this->assertGreaterThan(0, strlen($entry->text));
-        $this->assertGreaterThan(0, strlen($entry->xmlUrl));
+        $this->assertGreaterThan(0, strlen($entry->xmlurl));
 
         $this->assertEquals($entry->text, $entry['text']);
         $this->assertEquals($entry->description, $entry['description']);
         $this->assertEquals($entry->title, $entry['title']);
-        $this->assertEquals($entry->htmlUrl, $entry['htmlUrl']);
-        $this->assertEquals($entry->xmlUrl, $entry['xmlUrl']);
+        $this->assertEquals($entry->htmlurl, $entry['htmlurl']);
+        $this->assertEquals($entry->xmlurl, $entry['xmlurl']);
     }
 
     public function testGroupedBlogrolls()
