1ff7dac28d61e58bb7932d4aa545fe079714afbd
Gunnar Wrobel <p@rdus.de>
2011-06-06 08:07:17 +0200

    Typo.

--- components/lib/Components/Module/Release.php
+++ components/lib/Components/Module/Release.php
@@ -155,7 +155,7 @@ extends Components_Module_Base
      */
     public function getHelp($action)
     {
-        return 'Releases the component. This handles a number of automated steps usually required when releasing a package to pear.horde.org. In the most simple situation it will be sufficient to move to the directory of the component you with to release and run
+        return 'Releases the component. This handles a number of automated steps usually required when releasing a package to pear.horde.org. In the most simple situation it will be sufficient to move to the directory of the component you wish to release and run
 
   horde-components release
 
