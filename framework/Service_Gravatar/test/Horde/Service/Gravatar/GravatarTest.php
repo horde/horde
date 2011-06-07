2c99017b178f567dc68687791f5a208a84110dcd
Gunnar Wrobel <p@rdus.de>
2011-06-07 12:31:45 +0200

    Test different addresses with a data provider.
    "@dataProvider provideAddresses" identifies the data provider. The
    function "provideAddresses" is then expected to deliver the input
    arguments to our test function "testAddresses". As "testAddresses"
    requests two arguments ($mail and $id) "provideAddresses" has to
    deliver an array of arrays with two elements (the mail and the
    expected id).

--- framework/Service_Gravatar/test/Horde/Service/Gravatar/GravatarTest.php
+++ framework/Service_Gravatar/test/Horde/Service/Gravatar/GravatarTest.php
@@ -46,4 +46,22 @@ extends PHPUnit_Framework_TestCase
             $g->getId('test@example.org')
         );
     }
+
+    /**
+     * @dataProvider provideAddresses
+     */
+    public function testAddresses($mail, $id)
+    {
+        $g = new Horde_Service_Gravatar();
+        $this->assertEquals($id, $g->getId($mail));
+    }
+
+    public function provideAddresses()
+    {
+        return array(
+            array('test@example.org', '0c17bf66e649070167701d2d3cd71711'),
+            array('x@example.org', 'ae46d8cbbb834a85db7287f8342d0c42'),
+            array('test@example.com', '55502f40dc8b7c769880b10874abc9d0'),
+        );
+    }
 }
