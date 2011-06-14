027716fbbe9014a4883f7b84ff52f7617b0a542a
Jan Schneider <jan@horde.org>
2011-06-14 20:39:39 +0200

    Released groupware-4.0.0

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '4.0-RC1';
+    const VERSION = '4.0';
 
     /**
      * The bundle descriptive name.
