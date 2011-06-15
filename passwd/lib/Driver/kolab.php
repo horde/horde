b2073e967c3f69ea1ea14ce0a599a0d46c1f5034
Ralf Lang <lang@b1-systems.de>
2011-06-15 13:19:24 +0000

    Really formal conversion of the kolab driver @gunnar @gwr can you please try this?

--- passwd/lib/Driver/kolab.php
+++ passwd/lib/Driver/kolab.php
@@ -3,9 +3,13 @@
  * The Kolab class attempts to change a user's password on the designated
  * Kolab backend. Based off the LDAP passwd class.
  *
- * $Horde: passwd/lib/Driver/kolab.php,v 1.2.2.9 2009/01/06 15:25:23 jan Exp $
+ * Copyright 2005-2011 The Horde Project (http://www.horde.org/)
  *
- * Copyright 2005-2009 The Horde Project (http://www.horde.org/)
+ * WARNING: This driver has only formally been converted to Horde 4. 
+ * No testing has been done. If this doesn't work, please file bugs at
+ * bugs.horde.org
+ * If you really need this to work reliably, think about sponsoring development
+ * Please send a mail to lang -at- b1-systems.de if you can verify this driver to work
  *
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.horde.org/licenses/gpl.php.
@@ -35,6 +39,8 @@ class Passwd_Driver_kolab extends Passwd_Driver {
      *
      * @return boolean  True or false based on success of the change.
      */
+
+    // TODO: Shouldn't this extend on Passwd_Driver_ldap or at least be similar?
     function changePassword($username, $old_password, $new_password)
     {
         // Connect to the LDAP server.
@@ -43,7 +49,7 @@ class Passwd_Driver_kolab extends Passwd_Driver {
             $GLOBALS['conf']['kolab']['ldap']['port']
         );
         if (!$ds) {
-            return PEAR::raiseError(_("Could not connect to LDAP server"));
+            throw new Passwd_Exception(_("Could not connect to LDAP server"));
         }
 
         ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
@@ -57,7 +63,7 @@ class Passwd_Driver_kolab extends Passwd_Driver {
             $result = @ldap_bind($ds);
         }
         if (!$result) {
-            return PEAR::raiseError(_("Could not bind to LDAP server"));
+            throw new Passwd_Exception(_("Could not bind to LDAP server"));
         }
 
         // Make sure we're using the full user@domain format.
@@ -73,7 +79,7 @@ class Passwd_Driver_kolab extends Passwd_Driver {
         );
         $entry = ldap_first_entry($ds, $result);
         if ($entry === false) {
-            return PEAR::raiseError(_("User not found."));
+            throw new Passwd_Exception(_("User not found."));
         }
 
         $userdn = ldap_get_dn($ds, $entry);
@@ -81,7 +87,7 @@ class Passwd_Driver_kolab extends Passwd_Driver {
         // Connect as the user.
         $result = @ldap_bind($ds, $userdn, $old_password);
         if (!$result) {
-            return PEAR::raiseError(_("Incorrect old password."));
+            throw new Passwd_Exception(_("Incorrect old password."));
         }
 
         // And finally change the password.
@@ -89,7 +95,7 @@ class Passwd_Driver_kolab extends Passwd_Driver {
             base64_encode(pack('H*', sha1($new_password)));
 
         if (!ldap_mod_replace($ds, $userdn, $new_details)) {
-            return PEAR::raiseError(ldap_error($ds));
+            throw new Passwd_Exception(ldap_error($ds));
         }
 
         ldap_unbind($ds);
