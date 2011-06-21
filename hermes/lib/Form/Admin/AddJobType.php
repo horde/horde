ea4bd7549bb6f8b51e19625163ada2b374fb9adc
Michael J Rubinsky <mrubinsk@horde.org>
2011-06-20 23:51:05 -0400

    break out admin forms

---
+++ hermes/lib/Form/Admin/AddJobType.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright 2002-2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsdl.php.
+ *
+ * @author Chuck Hagenbuch <chuck@horde.org>
+ */
+
+/**
+ * @package Hermes
+ */
+class Hermes_Form_Admin_AddJobType extends Horde_Form
+{
+
+    public function __construct(&$vars)
+    {
+        parent::__construct($vars, 'addjobtypeform');
+
+        $this->addVariable(_("Job Type"), 'name', 'text', true);
+        $var = &$this->addVariable(_("Enabled?"), 'enabled', 'boolean', false);
+        $var->setDefault(true);
+        $var = &$this->addVariable(_("Billable?"), 'billable', 'boolean', false);
+        $var->setDefault(true);
+        $this->addVariable(_("Hourly Rate"), 'rate', 'number', false);
+    }
+
+}
---
+++ hermes/lib/Form/Admin/EditClientStepTwo.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright 2002-2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsdl.php.
+ *
+ * @author Chuck Hagenbuch <chuck@horde.org>
+ */
+
+/**
+ * @package Hermes
+ */
+class Hermes_Form_Admin_EditClientStepTwo extends Horde_Form
+{
+
+    public function __construct(&$vars)
+    {
+        parent::__construct($vars, 'editclientstep2form');
+
+        $client = $vars->get('client');
+        try {
+            $info = $GLOBALS['injector']
+                ->getInstance('Hermes_Driver')
+                ->getClientSettings($client);
+        } catch (Hermes_Exception $e) {}
+        if (!$info) {
+            $stype = 'invalid';
+            $type_params = array(_("This is not a valid client."));
+        } else {
+            $stype = 'text';
+            $type_params = array();
+        }
+
+        $this->addHidden('', 'client', 'text', true, true);
+        $name = &$this->addVariable(_("Client"), 'name', $stype, false, true, null, $type_params);
+        $name->setDefault($info['name']);
+
+        $enterdescription = &$this->addVariable(sprintf(_("Should users enter descriptions of their timeslices for this client? If not, the description will automatically be \"%s\"."), _("See Attached Timesheet")), 'enterdescription', 'boolean', true);
+        if (!empty($info['enterdescription'])) {
+            $enterdescription->setDefault($info['enterdescription']);
+        }
+
+        $exportid = &$this->addVariable(_("ID for this client when exporting data, if different from the name displayed above."), 'exportid', 'text', false);
+        if (!empty($info['exportid'])) {
+            $exportid->setDefault($info['exportid']);
+        }
+    }
+
+}
