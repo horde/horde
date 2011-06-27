136e667a74917b1e3be6889a909d7b91ca5c1f8e
Gunnar Wrobel <p@rdus.de>
2011-06-27 06:59:12 +0200

    Complete the conversion of "toicalendar.phpt".
    The tests do not error out anymore but there are still failures as the
    expected output changed. Recurrence exceptions seem to be handled
    differently in the newer Kronolith.

--- kronolith/test/Kronolith/Integration/ToIcalendarTest.php
+++ kronolith/test/Kronolith/Integration/ToIcalendarTest.php
@@ -36,12 +36,13 @@ class Kronolith_Integration_ToIcalendarTest extends Kronolith_TestCase
 {
     public function setUp()
     {
-        $this->markTestIncomplete('Too much boilerplate necessary.');
         $GLOBALS['registry'] = new Kronolith_Stub_Registry();
         $GLOBALS['injector'] = new Horde_Injector(new Horde_Injector_TopLevel());
         $GLOBALS['conf']['prefs']['driver'] = 'Null';
         $GLOBALS['injector']->bindFactory('Kronolith_Geo', 'Kronolith_Factory_Geo', 'create');
-        $GLOBALS['conf']['calendar']['driver'] = 'Ical';
+        $logger = new Horde_Log_Logger(new Horde_Log_Handler_Null());
+        $GLOBALS['injector']->setInstance('Horde_Log_Logger', $logger);
+        $GLOBALS['conf']['calendar']['driver'] = 'Mock';
     }
 
     public function tearDown()
@@ -51,15 +52,47 @@ class Kronolith_Integration_ToIcalendarTest extends Kronolith_TestCase
         unset($GLOBALS['conf']);
     }
 
-    public function testBasic()
+    public function testBasicVersion1()
     {
-        $event = $this->_getEvent();
-        $ical = new Horde_Icalendar('1.0');
+        $this->markTestIncomplete('Fails because of quoting and recurrence exceptions.');
+        $this->_testExport($this->_getEvent(), '1.0', 'export1.ics');
+    }
+
+    public function testBasicVersion2()
+    {
+        $this->markTestIncomplete('Fails because of recurrence exceptions.');
+        $this->_testExport($this->_getEvent(), '2.0', 'export2.ics');
+    }
+
+    public function testPrivateVersion1()
+    {
+        $this->markTestIncomplete('Fails because of recurrence exceptions.');
+        $this->_testExport($this->_getPrivateEvent(), '1.0', 'export3.ics');
+    }
+
+    public function testPrivateVersion2()
+    {
+        $this->markTestIncomplete('Fails because of recurrence exceptions.');
+        $this->_testExport($this->_getPrivateEvent(), '2.0', 'export4.ics');
+    }
+
+    private function _testExport($event, $export_version, $fixture)
+    {
+        $ical = new Horde_Icalendar($export_version);
         $cal = $event->toiCalendar($ical);
         $ical->addComponent($cal);
         $this->assertEquals(
-            $this->_getFixture('export1.ics'),
-            $ical->exportvCalendar()
+            $this->_removeDateStamp($this->_getFixture($fixture)),
+            $this->_removeDateStamp($ical->exportvCalendar())
+        );
+    }
+
+    private function _removeDateStamp($ics)
+    {
+        return preg_replace(
+            '/DTSTAMP:\d\d\d\d\d\d\d\dT\d\d\d\d\d\dZ/',
+            'DTSTAMP:--------T------Z',
+            $ics
         );
     }
 
@@ -80,6 +113,35 @@ class Kronolith_Integration_ToIcalendarTest extends Kronolith_TestCase
         $event->recurrence->setRecurInterval(2);
         $event->recurrence->addException(2007, 3, 19);
         $event->initialized = true;
+        return $event;
+    }
+
+    private function _getPrivateEvent()
+    {
+        $event = $this->_getEvent();
+        $event->private = true;
+        $event->status = Kronolith::STATUS_TENTATIVE;
+        $event->recurrence = new Horde_Date_Recurrence($event->start);
+        $event->recurrence->setRecurType(Horde_Date_Recurrence::RECUR_MONTHLY_DATE);
+        $event->recurrence->setRecurInterval(1);
+        $event->recurrence->addException(2007, 4, 15);
+        $event->recurrence->addException(2007, 6, 15);
+        $event->attendees =
+            array('juergen@example.com' =>
+                  array('attendance' => Kronolith::PART_REQUIRED,
+                        'response' => Kronolith::RESPONSE_NONE,
+                        'name' => 'Jürgen Doe'),
+                  0 =>
+                  array('attendance' => Kronolith::PART_OPTIONAL,
+                        'response' => Kronolith::RESPONSE_ACCEPTED,
+                        'name' => 'Jane Doe'),
+                  'jack@example.com' =>
+                  array('attendance' => Kronolith::PART_NONE,
+                        'response' => Kronolith::RESPONSE_DECLINED,
+                        'name' => 'Jack Doe'),
+                  'jenny@example.com' =>
+                  array('attendance' => Kronolith::PART_NONE,
+                        'response' => Kronolith::RESPONSE_TENTATIVE));
         return $event;
     }
 
