863ffe4f0fc55501f799c75b397c06f263a4b220
Gunnar Wrobel <p@rdus.de>
2011-06-28 12:47:20 +0200

    Integer handler.

---
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Type/Integer.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Handles a integer attribute.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Format
+ */
+
+/**
+ * Handles a integer attribute.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you did not
+ * receive this file, see
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * @since Horde_Kolab_Format 1.1.0
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_Format
+ */
+class Horde_Kolab_Format_Xml_Type_Integer
+extends Horde_Kolab_Format_Xml_Type_String
+{
+    /**
+     * Load the value of a node.
+     *
+     * @param string $query The query.
+     *
+     * @return DOMNode|false The named DOMNode or empty if no node value was
+     *                       found.
+     */
+    protected function loadNodeValue($name, &$attributes, $parent_node)
+    {
+        $result = parent::loadNodeValue($name, $attributes, $parent_node);
+        if (isset($attributes[$name]) && !is_int($attributes[$name])) {
+            $attributes[$name] = (int)$attributes[$name];
+        }
+        return $result;
+    }
+
+    /**
+     * Update the specified attribute.
+     *
+     * @param string  $name        The name of the the attribute
+     *                             to be updated.
+     * @param array   $attributes  The data array that holds all
+     *                             attribute values.
+     * @param DOMNode $parent_node The parent node of the node that
+     *                             should be updated.
+     *
+     * @return DOMNode|boolean The new/updated child node or false if this
+     *                         failed.
+     *
+     * @throws Horde_Kolab_Format_Exception If converting the data to XML failed.
+     */
+    public function save($name, $attributes, $parent_node)
+    {
+        if (isset($attributes[$name])) {
+            if (!is_string($attributes[$name])) {
+                $attributes[$name] = (string)$attributes[$name];
+            }
+            if (((string)((int)$attributes[$name]) !== $attributes[$name])
+                && !$this->isRelaxed()) {
+                throw new Horde_Kolab_Format_Exception(
+                    sprintf('Invalid integer input %s!', $attributes[$name])
+                );
+            }
+        }
+        return parent::save($name, $attributes, $parent_node);
+    }
+}
---
+++ framework/Kolab_Format/test/Horde/Kolab/Format/Unit/Xml/Type/IntegerTest.php
@@ -0,0 +1,228 @@
+<?php
+/**
+ * Test the integer attribute handler.
+ *
+ * PHP version 5
+ *
+ * @category   Kolab
+ * @package    Kolab_Format
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Format
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../../../Autoload.php';
+
+/**
+ * Test the integer attribute handler.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Kolab
+ * @package    Kolab_Format
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_Format
+ */
+class Horde_Kolab_Format_Unit_Xml_Type_IntegerTest
+extends PHPUnit_Framework_TestCase
+{
+    public function testLoadInteger()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0" a="b"><integer>1</integer>c</kolab>'
+        );
+        $attributes = array();
+        $result->load('integer', $attributes, $rootNode);
+        $this->assertSame(1, $attributes['integer']);
+    }
+
+    public function testLoadStrangeInteger()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0" a="b"><integer type="strange"><b/>false<a/></integer>c</kolab>'
+        );
+        $attributes = array();
+        $result->load('integer', $attributes, $rootNode);
+        $this->assertSame(0, $attributes['integer']);
+    }
+
+    public function testLoadMissingInteger()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"/>'
+        );
+        $attributes = array();
+        $result->load('integer', $attributes, $rootNode);
+        $this->assertFalse(isset($attributes['integer']));
+    }
+
+    public function testLoadDefault()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(
+                'value' => Horde_Kolab_Format_Xml::VALUE_DEFAULT,
+                'default' => 5
+            ),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"/>'
+        );
+        $attributes = array();
+        $result->load('integer', $attributes, $rootNode);
+        $this->assertSame(5, $attributes['integer']);
+    }
+
+    /**
+     * @expectedException Horde_Kolab_Format_Exception_MissingValue
+     */
+    public function testLoadNotEmpty()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(
+                'value' => Horde_Kolab_Format_Xml::VALUE_NOT_EMPTY,
+            ),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"/>'
+        );
+        $attributes = array();
+        $result->load('integer', $attributes, $rootNode);
+    }
+
+    public function testLoadNotEmptyRelaxed()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(
+                'value' => Horde_Kolab_Format_Xml::VALUE_NOT_EMPTY,
+                'relaxed' => true,
+            ),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"/>'
+        );
+        $attributes = array();
+        $result->load('integer', $attributes, $rootNode);
+        $this->assertFalse(isset($attributes['integer']));
+    }
+
+    public function testSave()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger();
+        $this->assertInstanceOf(
+            'DOMNode',
+            $result->save('integer', array(), $rootNode)
+        );
+    }
+
+    public function testSaveInteger()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger();
+        $result->save('integer', array('integer' => 7), $rootNode);
+        $this->assertEquals(
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"><integer>7</integer></kolab>
+',
+            $doc->saveXML()
+        );
+    }
+
+    public function testSaveOverwritesOldValue()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0" a="b"><integer type="strange"><b/>STRANGE<a/></integer>c</kolab>'
+        );
+        $result->save('integer', array('integer' => 7), $rootNode);
+        $this->assertEquals(
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0" a="b"><integer type="strange">7<b/><a/></integer>c</kolab>
+',
+            $doc->saveXML()
+        );
+    }
+
+    /**
+     * @expectedException Horde_Kolab_Format_Exception_MissingValue
+     */
+    public function testSaveNotEmpty()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(
+                'value' => Horde_Kolab_Format_Xml::VALUE_NOT_EMPTY,
+            ),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"/>'
+        );
+        $result->save('integer', array(), $rootNode);
+    }
+
+    /**
+     * @expectedException Horde_Kolab_Format_Exception
+     */
+    public function testSaveInvalidInteger()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(
+                'value' => Horde_Kolab_Format_Xml::VALUE_NOT_EMPTY,
+            ),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"/>'
+        );
+        $result->save('integer', array('integer' => 'INVALID'), $rootNode);
+    }
+
+    public function testSaveNotEmptyWithOldValue()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(
+                'value' => Horde_Kolab_Format_Xml::VALUE_NOT_EMPTY,
+            ),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0" a="b"><integer type="strange"><b/>STRANGE<a/></integer>c</kolab>'
+        );
+        $this->assertInstanceOf(
+            'DOMNode', 
+            $result->save('integer', array(), $rootNode)
+        );
+    }
+
+    public function testSaveNotEmptyRelaxed()
+    {
+        list($doc, $rootNode, $result) = $this->_getDefaultInteger(
+            array(
+                'value' => Horde_Kolab_Format_Xml::VALUE_NOT_EMPTY,
+                'relaxed' => true,
+            ),
+            '<?xml version="1.0" encoding="UTF-8"?>
+<kolab version="1.0"/>'
+        );
+        $this->assertFalse($result->save('integer', array(), $rootNode));
+    }
+
+    private function _getDefaultInteger($params = array(), $previous = null)
+    {
+        $doc = new DOMDocument('1.0', 'UTF-8');
+        if ($previous !== null) {
+            $doc->loadXML($previous);
+        }
+        $root = new Horde_Kolab_Format_Xml_Type_Root(
+            $doc, array('type' => 'kolab', 'version' => '1.0')
+        );
+        $rootNode = $root->save();
+        $result = new Horde_Kolab_Format_Xml_Type_Integer($doc, $params);
+        return array($doc, $rootNode, $result);
+    }
+}
