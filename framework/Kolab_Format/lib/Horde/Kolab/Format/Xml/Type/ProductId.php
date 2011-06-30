5f5981ef4519ebf20253833f87e0e30e7c69d265
Gunnar Wrobel <p@rdus.de>
2011-06-30 09:30:13 +0200

    Adapt to new base.

--- framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Type/ProductId.php
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Type/ProductId.php
@@ -32,6 +32,11 @@ class Horde_Kolab_Format_Xml_Type_ProductId
 extends Horde_Kolab_Format_Xml_Type_Base
 {
     /**
+     * The parameters required for the parsing operation.
+     */
+    protected $_required_parameters = array('helper', 'api-version');
+
+    /**
      * Load the node value from the Kolab object.
      *
      * @param string  $name        The name of the the attribute
@@ -39,13 +44,14 @@ extends Horde_Kolab_Format_Xml_Type_Base
      * @param array   &$attributes The data array that holds all
      *                             attribute values.
      * @param DOMNode $parent_node The parent node of the node to be loaded.
+     * @param array   $params      The parameters for this parse operation.
      *
      * @return DOMNode|boolean The named DOMNode or false if no node value was
      *                         found.
      */
-    public function load($name, &$attributes, $parent_node)
+    public function load($name, &$attributes, $parent_node, $params = array())
     {
-        $result = parent::load($name, $attributes, $parent_node);
+        $result = parent::load($name, $attributes, $parent_node, $params);
         if ($result !== false) {
             return $result;
         } else {
@@ -54,48 +60,22 @@ extends Horde_Kolab_Format_Xml_Type_Base
     }
 
     /**
-     * Update the specified attribute.
+     * Generate the value that should be written to the node. Override in the
+     * extending classes.
      *
      * @param string  $name        The name of the the attribute
      *                             to be updated.
      * @param array   $attributes  The data array that holds all
      *                             attribute values.
-     * @param DOMNode $parent_node The parent node of the node that
-     *                             should be updated.
+     * @param array   $params      The parameters for this write operation.
      *
-     * @return DOMNode|boolean The new/updated child node or false if this
-     *                         failed.
-     *
-     * @throws Horde_Kolab_Format_Exception If converting the data to XML failed.
+     * @return mixed The value to be written.
      */
-    public function save($name, $attributes, $parent_node)
-    {
-        if (!($node = $this->findNodeRelativeTo('./' . $name, $parent_node))) {
-            return $this->storeNewNodeValue(
-                $parent_node,
-                $name,
-                $this->getProductId()
-            );
-        }
-        $this->replaceFirstNodeTextValue(
-            $node, 
-            $this->getProductId()
-        );
-        return $node;
-    }
-
-    /**
-     * Return the product ID.
-     *
-     * @return string The product ID for this library and the current handler.
-     */
-    public function getProductId()
+    protected function generateWriteValue($name, $attributes, $params)
     {
         return Horde_Kolab_Format_Xml::PRODUCT_ID . '-'
             . Horde_Kolab_Format::VERSION
-            . ' (type: ' . $this->getParam('type')
-            . ', format version: ' . $this->getParam('version')
-            . ', api version: ' .  $this->getParam('api_version')
+            . ' (api version: ' .  $params['api-version']
             . ')';
     }
 }
--- framework/Kolab_Format/test/Horde/Kolab/Format/Unit/Xml/Type/ProductIdTest.php
+++ framework/Kolab_Format/test/Horde/Kolab/Format/Unit/Xml/Type/ProductIdTest.php
@@ -33,96 +33,94 @@ require_once dirname(__FILE__) . '/../../../Autoload.php';
  * @link       http://pear.horde.org/index.php?package=Kolab_Format
  */
 class Horde_Kolab_Format_Unit_Xml_Type_ProductIdTest
-extends PHPUnit_Framework_TestCase
+extends Horde_Kolab_Format_TestCase
 {
     public function testLoadProductId()
     {
-        list($doc, $rootNode, $pid) = $this->_getDefaultProduct(
-            array(),
+        $attributes = $this->_load(
             '<?xml version="1.0" encoding="UTF-8"?>
 <kolab version="1.0" a="b"><product-id>SOMETHING</product-id>c</kolab>'
         );
-        $attributes = array();
-        $pid->load('product-id', $attributes, $rootNode);
         $this->assertEquals('SOMETHING', $attributes['product-id']);
     }
 
     public function testLoadStrangeProductId()
     {
-        list($doc, $rootNode, $pid) = $this->_getDefaultProduct(
-            array(),
+        $attributes = $this->_load(
             '<?xml version="1.0" encoding="UTF-8"?>
 <kolab version="1.0" a="b"><product-id type="strange"><b/>STRANGE<a/></product-id>c</kolab>'
         );
-        $attributes = array();
-        $pid->load('product-id', $attributes, $rootNode);
         $this->assertEquals('STRANGE', $attributes['product-id']);
     }
 
     public function testLoadMissingProductId()
     {
-        list($doc, $rootNode, $pid) = $this->_getDefaultProduct(
-            array(),
+        $attributes = $this->_load(
             '<?xml version="1.0" encoding="UTF-8"?>
 <kolab version="1.0"/>'
         );
-        $attributes = array();
-        $pid->load('product-id', $attributes, $rootNode);
         $this->assertEquals('', $attributes['product-id']);
     }
 
     public function testSave()
     {
-        list($doc, $rootNode, $pid) = $this->_getDefaultProduct(
-            array('type' => 'kolab', 'version' => '1.0', 'api_version' => 2)
-        );
+        list($params, $root_node, $pid) = $this->_getProductId();
+        $params['api-version'] = 2;
         $this->assertInstanceOf(
             'DOMNode', 
-            $pid->save('product-id', array(), $rootNode)
+            $pid->save('product-id', array(), $root_node, $params)
         );
     }
 
     public function testSaveXml()
     {
-        list($doc, $rootNode, $pid) = $this->_getDefaultProduct(
-            array('type' => 'kolab', 'version' => '1.0', 'api_version' => 2)
-        );
-        $pid->save('product-id', array(), $rootNode);
+        list($params, $root_node, $pid) = $this->_getProductId();
+        $params['api-version'] = 2;
+        $pid->save('product-id', array(), $root_node, $params);
         $this->assertEquals(
             '<?xml version="1.0" encoding="UTF-8"?>
-<kolab version="1.0"><product-id>Horde_Kolab_Format_Xml-@version@ (type: kolab, format version: 1.0, api version: 2)</product-id></kolab>
+<kolab version="1.0"><product-id>Horde_Kolab_Format_Xml-@version@ (api version: 2)</product-id></kolab>
 ',
-            $doc->saveXML()
+            (string)$params['helper']
         );
     }
 
     public function testSaveOverwritesOldValue()
     {
-        list($doc, $rootNode, $pid) = $this->_getDefaultProduct(
-            array('type' => 'kolab', 'version' => '1.0', 'api_version' => 2),
+        list($params, $root_node, $pid) = $this->_getProductId(
             '<?xml version="1.0" encoding="UTF-8"?>
 <kolab version="1.0" a="b"><product-id type="strange"><b/>STRANGE<a/></product-id>c</kolab>'
         );
-        $pid->save('product-id', array(), $rootNode);
+        $params['api-version'] = 2;
+        $pid->save('product-id', array(), $root_node, $params);
         $this->assertEquals(
             '<?xml version="1.0" encoding="UTF-8"?>
-<kolab version="1.0" a="b"><product-id type="strange">Horde_Kolab_Format_Xml-@version@ (type: kolab, format version: 1.0, api version: 2)<b/><a/></product-id>c</kolab>
+<kolab version="1.0" a="b"><product-id type="strange">Horde_Kolab_Format_Xml-@version@ (api version: 2)<b/><a/></product-id>c</kolab>
 ',
-            $doc->saveXML()
+            (string)$params['helper']
         );
     }
 
-    private function _getDefaultProduct($params = array(), $previous = null)
+    private function _load($previous)
     {
-        $doc = new DOMDocument('1.0', 'UTF-8');
-        if ($previous !== null) {
-            $doc->loadXML($previous);
-        }
-        $root = new Horde_Kolab_Format_Xml_Type_Root(
-            $doc, array('type' => 'kolab', 'version' => '1.0')
+        list($params, $root_node, $pid) = $this->_getProductId($previous);
+        $attributes = array();
+        $params['api-version'] = 2;
+        $pid->load('product-id', $attributes, $root_node, $params);
+        return $attributes;
+    }
+
+    private function _getProductId(
+        $previous = null,
+        $kolab_type = 'kolab',
+        $version = '1.0'
+    )
+    {
+        return $this->getXmlType(
+            'Horde_Kolab_Format_Xml_Type_ProductId',
+            $previous,
+            $kolab_type,
+            $version
         );
-        $rootNode = $root->save();
-        $mdate = new Horde_Kolab_Format_Xml_Type_ProductId($doc, $params);
-        return array($doc, $rootNode, $mdate);
     }
 }
