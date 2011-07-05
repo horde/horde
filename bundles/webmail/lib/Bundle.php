fd6c7f4b4733a7dc2a58201dc49043386a79a067
Jan Schneider <jan@horde.org>
2011-07-05 18:16:21 +0200

    Development mode for webmail-4.0.2

--- bundles/webmail/lib/Bundle.php
+++ bundles/webmail/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '4.0.1';
+    const VERSION = '4.0.2-git';
 
     /**
      * The bundle descriptive name.
