db81138f327efe6d3a2d491152a6b419616316db
Vilius Šumskas <vilius@lnk.lt>
2011-07-10 17:38:50 +0300

    Initial driver factory and exception class.
    Signed-off-by: Jan Schneider <jan@horde.org>

---
+++ agora/lib/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Base exception class for Agora.
+ *
+ * Copyright 2009-2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author  Vilius Šumskas <vilius@lnk.lt>
+ * @package Agora
+ */
+class Agora_Exception extends Horde_Exception_Wrapped
+{
+}
---
+++ agora/lib/Factory/Driver.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Horde_Injector factory to create Agora_Driver instances.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author  Michael J. Rubinsky <mrubinsk@horde.org>
+ * @author  Vilius Šumskas <vilius@lnk.lt>
+ * @package Agora
+ */
+class Agora_Factory_Driver
+{
+    /**
+     * Instances.
+     *
+     * @var array
+     */
+    private $_instances = array();
+
+    /**
+     * The injector.
+     *
+     * @var Horde_Injector
+     */
+    private $_injector;
+
+    /**
+     * Constructor.
+     *
+     * @param Horde_Injector $injector  The injector to use.
+     */
+    public function __construct(Horde_Injector $injector)
+    {
+        $this->_injector = $injector;
+    }
+
+    /**
+     * Return the Agora_Driver:: instance.
+     *
+     * @param mixed $scope  Instance scope
+     *
+     * @return Agora_Driver
+     * @throws Agora_Exception
+     */
+    public function create($scope = 'agora', $forum_id = 0)
+    {
+        if (!isset($this->_instances[$scope])) {
+            $driver = $GLOBALS['conf']['threads']['split'] ? 'SplitSql' : 'Sql';
+            $params = Horde::getDriverConfig('sql');
+
+            $class = 'Agora_Driver_' . $driver;
+            if (!class_exists($class)) {
+                throw new Agora_Exception(sprintf('Unable to load the definition of %s.', $class));
+            }
+
+            $params = array(
+                'db' => $this->_injector->getInstance('Horde_Db_Adapter'),
+                'charset' => $params['charset'],
+            );
+
+            $driver = new $class($scope, $params);
+            $this->_instances[$scope] = $driver;
+        }
+
+        if ($forum_id) {
+            /* Check if there was a valid forum object to get. */
+            try {
+                $forum = $this->_instances[$scope]->getForum($forum_id);
+            } catch (Horde_Exception $e) {
+                throw new Agora_Exception($e->getMessage());
+            }
+
+            /* Set current forum id and forum data */
+            $this->_instances[$scope]->_forum = $forum;
+            $this->_instances[$scope]->_forum_id = (int)$forum_id;
+        }
+
+        return $this->_instances[$scope];
+    }
+}
