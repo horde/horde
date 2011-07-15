04cdb8d79e8d925d9c8f1737ba17a31b804d25a7
Gunnar Wrobel <p@rdus.de>
2011-07-15 10:17:44 +0200

    Add installer and Bundle.php.
    Decided against extending the "webmail" package and will duplicate the
    code and configurations. Not that nice but I found no reasonable
    solution for allowing Bundle.php and the */config/hooks.php files to
    be stackable.

--- bundles/kolab_webmail/lib/Bundle.php
+++ bundles/kolab_webmail/lib/Bundle.php
@@ -25,4 +25,38 @@ class Horde_Bundle extends Horde_Core_Bundle
      * The bundle descriptive name.
      */
     const FULLNAME = 'Horde Kolab Edition';
+
+    /**
+     * Asks for the administrator settings.
+     *
+     * @return string  The administrator name.
+     */
+    protected function _configAuth(Horde_Variables $vars)
+    {
+        $vars->auth__driver = 'kolab';
+        $host = $this->_cli->prompt('Provide the host name of your Kolab server:');
+        $maildomain = $this->_cli->prompt('Provide the primary mail domain of your Kolab server:');
+        $basedn = $this->_cli->prompt('Provide the base DN of your Kolab server:');
+        $phppw = $this->_cli->prompt('Provide the PHP pw of your Kolab LDAP nobody user:');
+        $vars->problems__email = 'postmaster@' . $maildomain;
+        $vars->problems__maildomain = $maildomain;
+        $vars->kolab__enabled = 'true';
+        $vars->kolab__primary_domain = $maildomain;
+        $vars->kolab__ldap__server = 'ldap://' . $host . ':389';
+        $vars->kolab__ldap__hostname = $host;
+        $vars->kolab__ldap__port = '389';
+        $vars->kolab__ldap__basedn = $basedn;
+        $vars->kolab__ldap__phpdn = 'cn=nobody,cn=internal,' . $basedn;
+        $vars->kolab__ldap__phppw = $phppw;
+        $vars->kolab__imap__server = $host;
+        $vars->kolab__imap__port = '993';
+        $vars->kolab__imap__sieveport = '2000';
+        $vars->kolab__imap__maildomain = $maildomain;
+        $vars->mailer__type = 'smtp';
+        $vars->mailer__params__auth = 'true';
+        $vars->mailer__params__port = 587;
+        $vars->mailer__params__host = $host;
+        $vars->prefs__driver = 'KolabImap';
+        return 'manager';
+    }
 }
