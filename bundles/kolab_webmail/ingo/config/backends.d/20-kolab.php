8bfcc8e30f422e701fa5801b70fdda28641c857b
Gunnar Wrobel <p@rdus.de>
2011-07-15 10:19:02 +0200

    Ingo configuration for Kolab.

--- bundles/kolab_webmail/ingo/config/backends.d/20-kolab.php
+++ bundles/kolab_webmail/ingo/config/backends.d/20-kolab.php
@@ -3,14 +3,16 @@
 $backends = array();
 
 $backends['kolab'] = array(
+    'disabled' => false,
     'transport' => 'timsieved',
     'hordeauth' => 'full',
     'params' => array(
-        'hostspec' => 'localhost', //@todo: Kolab::getServer('imap'),
+        'hostspec' => $GLOBALS['injector']->getInstance('Horde_Kolab_Session')->getImapServer(),
         'logintype' => 'PLAIN',
         'usetls' => true,
         'port' => $GLOBALS['conf']['kolab']['imap']['sieveport'],
-        'scriptname' => 'kmail-vacation.siv'
+        'scriptname' => 'kmail-vacation.siv',
+        'debug' => false,
     ),
     'script' => 'sieve',
     'scriptparams' => array(),
