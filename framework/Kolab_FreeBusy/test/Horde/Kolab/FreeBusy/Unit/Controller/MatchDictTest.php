78565f478b0fab2be1128788bc52d9e2c05efabd
Gunnar Wrobel <p@rdus.de>
2011-07-23 22:04:46 +0200

    Test the route analysis.

---
+++ framework/Kolab_FreeBusy/test/Horde/Kolab/FreeBusy/Unit/Controller/MatchDictTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Test the route matching.
+ *
+ * PHP version 5
+ *
+ * @category   Kolab
+ * @package    Kolab_FreeBusy
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_FreeBusy
+ */
+
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/../../Autoload.php';
+
+/**
+ * Test the route matching.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category   Kolab
+ * @package    Kolab_FreeBusy
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link       http://pear.horde.org/index.php?package=Kolab_FreeBusy
+ */
+class Horde_Kolab_FreeBusy_Unit_Controller_MatchDictTest
+extends Horde_Kolab_FreeBusy_TestCase
+{
+
+    /**
+     * @dataProvider provideElements
+     */
+    public function testGetDict($key, $value)
+    {
+        $mapper = new Horde_Routes_Mapper();
+        $mapper->connect(
+            ':(callee).:(type)',
+            array(
+                'controller'   => 'freebusy',
+                'action'       => 'fetch',
+                'requirements' => array(
+                    'type'   => '(i|x|v)fb',
+                    'callee' => '[^/]+'),
+            )
+        );
+        $request = new Horde_Controller_Request_Mock();
+        $request->setPath('/owner@example.org.xfb');
+        $dict = new Horde_Kolab_FreeBusy_Controller_MatchDict(
+            $mapper, $request
+        );
+        $result = $dict->getMatchDict();
+        $this->assertEquals($value, $result[$key]);
+    }
+
+    public function provideElements()
+    {
+        return array(
+            array('controller', 'freebusy'),
+            array('action', 'fetch'),
+            array('type', 'xfb'),
+            array('callee', 'owner@example.org'),
+        );
+    }
+}
