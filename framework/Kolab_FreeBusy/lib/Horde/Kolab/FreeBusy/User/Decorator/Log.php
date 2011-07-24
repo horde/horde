230e6275b330808619f58bae948158247c0a3ccc
Gunnar Wrobel <p@rdus.de>
2011-07-24 21:34:19 +0200

    Corrected user log decorator.

--- framework/Kolab_FreeBusy/lib/Horde/Kolab/FreeBusy/User/Decorator/Log.php
+++ framework/Kolab_FreeBusy/lib/Horde/Kolab/FreeBusy/User/Decorator/Log.php
@@ -30,11 +30,12 @@
  * @since    Horde 3.2
  */
 class Horde_Kolab_FreeBusy_User_Decorator_Log
+implements Horde_Kolab_FreeBusy_User
 {
     /**
      * The decorated user.
      *
-     * @var Horde_Kolab_FreeBusy_User_Interface
+     * @var Horde_Kolab_FreeBusy_User
      */
     private $_user;
 
@@ -46,39 +47,109 @@ class Horde_Kolab_FreeBusy_User_Decorator_Log
     private $_logger;
 
     /**
+     * The remote .
+     *
+     * @var mixed
+     */
+    private $_remote;
+
+    /**
      * Constructor.
      *
-     * @param Horde_Kolab_FreeBusy_User_Interface $user   The decorated
-     *                                                    user.
-     * @param mixed                               $logger The log handler. The
-     *                                                    class must at least
-     *                                                    provide the notice()
-     *                                                    and err() methods.
+     * @param Horde_Kolab_FreeBusy_User $user    The decorated user.
+     * @param Horde_Controller_Request  $request The request.
+     * @param mixed                     $logger  The log handler. The class must
+     *                                           at least provide the notice()
+     *                                           and err() methods.
      */
     public function __construct(
-        Horde_Kolab_FreeBusy_User_Interface $user,
+        Horde_Kolab_FreeBusy_User $user,
+        Horde_Controller_Request $request,
         $logger
-    ) {
+    )
+    {
         $this->_user   = $user;
         $this->_logger = $logger;
+        $vars = $request->getServerVars();
+        $this->_remote = isset($vars['REMOTE_ADDR']) ? $vars['REMOTE_ADDR'] : 'unknown';
     }
+
+    /**
+     * Return the primary id of the user accessing the system.
+     *
+     * @return string The primary id.
+     */
+    public function getPrimaryId()
+    {
+        return $this->_user->getPrimaryId();
+    }
+
+    /**
+     * Return the password of the user accessing the system.
+     *
+     * @return string The password.
+     */
+    public function getPassword()
+    {
+        return $this->_user->getPassword();
+    }
+
+    /**
+     * Return the primary domain of the user accessing the system.
+     *
+     * @return string The primary domain.
+     */
+    public function getDomain()
+    {
+        return $this->_user->getDomain();
+    }
+
+    /**
+     * Return the groups this user is member of.
+     *
+     * @return array The groups for this user.
+     */
+    public function getGroups()
+    {
+        return $this->_user->getGroups();
+    }
+
 
     /**
      * Finds out if a set of login credentials are valid.
      *
-     * @param array $pass The password to check.
-     *
-     * @return boolean  Whether or not the password was correct.
+     * @return boolean Whether or not the password was correct.
      */
-    public function authenticate($pass)
+    public function isAuthenticated()
     {
-        $result = $this->_user->authenticate($pass);
-            if ($result) {
+        $result = $this->_user->isAuthenticated();
+        $id = $this->_user->getPrimaryId();
+        if ($result) {
+            $this->_logger->notice(
+                sprintf(
+                    'Login success for "%s" from "%s" to free/busy.',
+                    $id,
+                    $this->_remote
+                )
+            );
+        } else {
+            if (!empty($id)) {
+                $this->_logger->err(
+                    sprintf(
+                        'Failed login for "%s" from "%s" to free/busy',
+                        $id,
+                        $this->_remote
+                    )
+                );
+            } else {
                 $this->_logger->notice(
                     sprintf(
-                        'Login success for %s from %s to free/busy.', $this->user, $_SERVER['REMOTE_ADDR']));
-            } else {
-                $this->_logger->err(sprintf('Failed login for %s from %s to free/busy', $this->user, $_SERVER['REMOTE_ADDR']));
+                        'Anonymous access from "%s" to free/busy.',
+                        $this->_remote
+                    )
+                );
             }
+        }
+        return $result;
     }
 }
