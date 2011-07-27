a3b5e941635f77d7e0ac9e1caf4f5e41832cc5c4
Gunnar Wrobel <p@rdus.de>
2011-07-27 07:05:19 +0200

    Add exceptions.

---
+++ framework/Kolab_FreeBusy/lib/Horde/Kolab/FreeBusy/Exception/NotFound.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Indicates missing free/busy data.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_FreeBusy
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_FreeBusy
+ */
+
+/**
+ * Indicates missing free/busy data.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @category Kolab
+ * @package  Kolab_FreeBusy
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Kolab_FreeBusy
+ */
+class Horde_Kolab_FreeBusy_Exception_NotFound
+extends Horde_Kolab_FreeBusy_Exception
+{
+}
