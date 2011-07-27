1e9a61269a16ab6da88dc70954f015383bc01582
Gunnar Wrobel <p@rdus.de>
2011-07-27 07:09:04 +0200

    Adapt to the Provider interface.

--- framework/Kolab_FreeBusy/lib/Horde/Kolab/FreeBusy/Provider/Local.php
+++ framework/Kolab_FreeBusy/lib/Horde/Kolab/FreeBusy/Provider/Local.php
@@ -30,40 +30,75 @@ class Horde_Kolab_FreeBusy_Provider_Local
 implements Horde_Kolab_FreeBusy_Provider
 {
     /**
+     * The owner of the data.
+     *
+     * @var Horde_Kolab_FreeBusy_Owner
+     */
+    private $_owner;
+
+    /**
+     * The user accessing the data.
+     *
+     * @var Horde_Kolab_FreeBusy_User
+     */
+    private $_user;
+
+    /**
+     * Constructor
+     *
+     * @param Horde_Kolab_FreeBusy_Owner $owner  The owner of the data.
+     * @param Horde_Kolab_FreeBusy_User  $user   The user accessing the data.
+     */
+    public function __construct(
+        Horde_Kolab_FreeBusy_Owner $owner, Horde_Kolab_FreeBusy_User $user
+    )
+    {
+        $this->_owner  = $owner;
+        $this->_user   = $user;
+    }
+
+    /**
      * Trigger a resource.
      *
      * @param Horde_Controller_Response  $response The response handler.
-     * @param Horde_Kolab_FreeBusy_Owner $owner    The owner of the data.
-     * @param Horde_Kolab_FreeBusy_User  $user     The user accessing the data.
-     * @param string                     $resource The resource to trigger.
      * @param array                      $params   Additional parameters.
      *
      * @return NULL
      */
     public function trigger(
         Horde_Controller_Response $response,
-        Horde_Kolab_FreeBusy_Owner $owner,
-        Horde_Kolab_FreeBusy_User $user,
-        $resource,
         $params = array()
     )
     {
+        $this->logger->debug(sprintf("Starting generation of free/busy data for user %s",
+                                     $this->params->callee));
+
+        $params = array('extended' => $this->params->type == 'xfb');
+
+        // @todo: Reconsider this. We have been decoupled from the
+        // global context here but reinjecting this value seems
+        // extremely weird. Are there any other options?
+        $this->app->callee = $this->params->callee;
+        $this->data = $this->app->driver->fetch($this->params);
+
+        $this->logger->debug('Delivering complete free/busy data.');
+
+        /* Display the result to the user */
+        $this->render();
+
+        $this->logger->debug('Free/busy generation complete.');
     }
 
     /**
      * Fetch data for an owner.
      *
      * @param Horde_Controller_Response  $response The response handler.
-     * @param Horde_Kolab_FreeBusy_Owner $owner    The owner of the data.
-     * @param Horde_Kolab_FreeBusy_User  $user     The user accessing the data.
      * @param array                      $params   Additional parameters.
      *
      * @return NULL
      */
     public function fetch(
         Horde_Controller_Response $response,
-        Horde_Kolab_FreeBusy_Owner $owner,
-        Horde_Kolab_FreeBusy_User $user,
         $params = array()
     )
     {
@@ -73,16 +108,27 @@ implements Horde_Kolab_FreeBusy_Provider
      * Delete data of an owner.
      *
      * @param Horde_Controller_Response  $response The response handler.
-     * @param Horde_Kolab_FreeBusy_Owner $owner    The owner of the data.
-     * @param Horde_Kolab_FreeBusy_User  $user     The user accessing the data.
      * @param array                      $params   Additional parameters.
      *
      * @return NULL
      */
     public function delete(
         Horde_Controller_Response $response,
-        Horde_Kolab_FreeBusy_Owner $owner,
-        Horde_Kolab_FreeBusy_User $user,
+        $params = array()
+    )
+    {
+    }
+
+    /**
+     * Regenerate all data accessible to the current user.
+     *
+     * @param Horde_Controller_Response  $response The response handler.
+     * @param array                      $params   Additional parameters.
+     *
+     * @return NULL
+     */
+    public function regenerate(
+        Horde_Controller_Response $response,
         $params = array()
     )
     {
