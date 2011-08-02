62e2fc772b55ad5a8f1ab3b47425d278d54f2fec
Jan Schneider <jan@horde.org>
2011-08-02 15:44:49 +0200

    This no longer contains sync prefs.

--- kronolith/config/prefs.php
+++ kronolith/config/prefs.php
@@ -24,7 +24,7 @@ $prefGroups['view'] = array(
 $prefGroups['share'] = array(
     'column' => _("Calendars"),
     'label' => _("Default Calendar"),
-    'desc' => _("Choose your default calendar and calendars to use for syncing with external devices."),
+    'desc' => _("Choose your default calendar."),
     'members' => array('default_share'),
 );
 
