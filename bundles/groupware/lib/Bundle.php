1c7e6fd2b1ac80075be7af10f7f2382778edf8dd
Jan Schneider <jan@horde.org>
2011-08-02 16:33:38 +0200

    Development mode for groupware-4.0.3

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '4.0.2';
+    const VERSION = '4.0.4-git';
 
     /**
      * The bundle descriptive name.
