f41545686fddab38cf79df09133a1d3b252b11aa
Michael J Rubinsky <mrubinsk@horde.org>
2011-08-05 14:45:19 -0400

    Bug: 10411 Don't access protected property, sniff version instead.

--- kronolith/lib/Api.php
+++ kronolith/lib/Api.php
@@ -1103,7 +1103,7 @@ class Kronolith_Api extends Horde_Registry_Api
         $error = _("No attendees have been updated because none of the provided email addresses have been found in the event's attendees list.");
         $sender_lcase = Horde_String::lower($sender);
         foreach ($atnames as $index => $attendee) {
-            if ($response->oldFormat) {
+            if ($response->getAttribute('VERSION') < 2) {
                 $addresses = Horde_Mime_Address::parseAddressList($attendee);
                 if (!count($addresses)) {
                     continue;
