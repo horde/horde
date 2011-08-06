11b4697a4ca73cab7bad44b584f6d79787042c72
Michael J Rubinsky <mrubinsk@horde.org>
2011-08-06 16:39:32 -0400

    check for columns already.
    Admin may have added some of these after install (like I did), to customize their install.

--- turba/migration/6_turba_upgrade_updateschema.php
+++ turba/migration/6_turba_upgrade_updateschema.php
@@ -28,16 +28,36 @@ class TurbaUpgradeUpdateSchema extends Horde_Db_Migration_Base
      */
     public function up()
     {
-        $this->addColumn('turba_objects', 'object_anniversary', 'string', array('limit' => 10));
-        $this->addColumn('turba_objects', 'object_department', 'string', array('limit' => 255));
-        $this->addColumn('turba_objects', 'object_spouse', 'string', array('limit' => 255));
-        $this->addColumn('turba_objects', 'object_homefax', 'string', array('limit' => 25));
-        $this->addColumn('turba_objects', 'object_anniversary', 'string', array('limit' => 10));
-        $this->addColumn('turba_objects', 'object_nickname', 'string', array('limit' => 255));
-        $this->addColumn('turba_objects', 'object_assistantphone', 'string', array('limit' => 25));
-        $this->addColumn('turba_objects', 'object_imaddress', 'string', array('limit' => 255));
-        $this->addColumn('turba_objects', 'object_imaddress2', 'string', array('limit' => 255));
-        $this->addColumn('turba_objects', 'object_imaddress3', 'string', array('limit' => 255));
+        $t = $this->_connection->table('turba_objects');
+        $cols = array_keys($t->getColumns());
+
+        if (!in_array('object_anniversary', $cols)) {
+            $this->addColumn('turba_objects', 'object_anniversary', 'string', array('limit' => 10));
+        }
+        if (!in_array('object_department', $cols)) {
+            $this->addColumn('turba_objects', 'object_department', 'string', array('limit' => 255));
+        }
+        if (!in_array('object_spouse', $cols)) {
+            $this->addColumn('turba_objects', 'object_spouse', 'string', array('limit' => 255));
+        }
+        if (!in_array('object_homefax', $cols)) {
+            $this->addColumn('turba_objects', 'object_homefax', 'string', array('limit' => 25));
+        }
+        if (!in_array('object_nickname', $cols)) {
+            $this->addColumn('turba_objects', 'object_nickname', 'string', array('limit' => 255));
+        }
+        if (!in_array('object_assistantphone', $cols)) {
+            $this->addColumn('turba_objects', 'object_assistantphone', 'string', array('limit' => 25));
+        }
+        if (!in_array('object_imaddress', $cols)) {
+            $this->addColumn('turba_objects', 'object_imaddress', 'string', array('limit' => 255));
+        }
+        if (!in_array('object_imaddress2', $cols)) {
+            $this->addColumn('turba_objects', 'object_imaddress2', 'string', array('limit' => 255));
+        }
+        if (!in_array('object_imaddress3', $cols)) {
+            $this->addColumn('turba_objects', 'object_imaddress3', 'string', array('limit' => 255));
+        }
     }
 
     /**
