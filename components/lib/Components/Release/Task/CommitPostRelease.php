da4f5daf6adb97e8a8415d69b7a26a5b8da3ee7e
Michael J Rubinsky <mrubinsk@horde.org>
2011-08-11 01:20:09 -0400

    Revert "Fix version number"
    This reverts commit e26df9dcd703b2dd801f20445d778e48db43ccc6.

--- components/lib/Components/Release/Task/CommitPostRelease.php
+++ components/lib/Components/Release/Task/CommitPostRelease.php
@@ -41,7 +41,7 @@ extends Components_Release_Task_Base
     {
         if (empty($options['next_version'])) {
             $options['next_version'] = Components_Helper_Version::validatePear(
-                Components_Helper_Version::nextVersion($this->getComponent()->getVersion())
+                $this->getComponent()->getVersion()
             );
         }
         if (isset($options['commit'])) {
