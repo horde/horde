66fbe7059b5a8daaeabbd96467415b306b0df812
Gunnar Wrobel <p@rdus.de>
2011-08-11 08:40:15 +0200

    Add a failing test for the development version problem after release when in pretend mode.

--- components/test/Components/Unit/Components/Release/Task/NextSentinelTest.php
+++ components/test/Components/Unit/Components/Release/Task/NextSentinelTest.php
@@ -92,21 +92,52 @@ public $version = \'5.0-git\';
         );
     }
 
+    public function testPretendWithoutVersion()
+    {
+        $tmp_dir = $this->_prepareApplicationDirectory();
+        $tasks = $this->getReleaseTasks();
+        $package = $this->getComponent($tmp_dir);
+        $tasks->run(
+            array('NextVersion', 'NextSentinel', 'CommitPostRelease'),
+            $package,
+            array(
+                'next_note' => '',
+                'pretend' => true,
+                'commit' => new Components_Helper_Commit(
+                    $this->output,
+                    array('pretend' => true)
+                )
+            )
+        );
+        $this->assertEquals(
+            array(
+                sprintf('Would add next version "5.0.1" with the initial note "" to %s/package.xml now.', $tmp_dir),
+                sprintf('Would extend sentinel in %s/docs/CHANGES with "5.0.1-git" now.', $tmp_dir),
+                sprintf('Would replace sentinel in %s/lib/Application.php with "5.0.1-git" now.', $tmp_dir),
+                sprintf('Would run "git add %s/package.xml" now.', $tmp_dir),
+                sprintf('Would run "git add %s/docs/CHANGES" now.', $tmp_dir),
+                sprintf('Would run "git add %s/lib/Application.php" now.', $tmp_dir),
+                'Would run "git commit -m "Development mode for Horde-5.0.1"" now.'
+            ),
+            $this->output->getOutput()
+        );
+    }
+
     private function _prepareApplicationDirectory()
     {
         $tmp_dir = $this->getTemporaryDirectory();
         mkdir($tmp_dir . '/docs');
         file_put_contents($tmp_dir . '/docs/CHANGES', "---\nOLD\n---\nTEST");
         mkdir($tmp_dir . '/lib');
-        file_put_contents($tmp_dir . '/lib/Application.php', "class Application {\npublic \$version = '0.0.0';\n}\n");
+        file_put_contents($tmp_dir . '/lib/Application.php', "class Application {\npublic \$version = '5.0.0';\n}\n");
         file_put_contents(
             $tmp_dir . '/package.xml',
             '<?xml version="1.0" encoding="UTF-8"?>
 <package xmlns="http://pear.php.net/dtd/package-2.0">
  <name>Horde</name>
  <version>
-  <release>4.0.1rc1</release>
-  <api>4.0.0</api>
+  <release>5.0.0</release>
+  <api>5.0.0</api>
  </version>
 </package>'
         );
