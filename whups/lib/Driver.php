7fca62b1e9ae611869737209ee7dd896a43fa8d3
Jan Schneider <jan@horde.org>
2011-08-13 22:33:58 +0200

    Remove all attachments during each recipient iteration.

--- whups/lib/Driver.php
+++ whups/lib/Driver.php
@@ -453,6 +453,13 @@ class Whups_Driver
             $attachmentAdded = false;
             if (empty($GLOBALS['conf']['mail']['link_attach']) &&
                 $opts['ticket']) {
+                /* We don't know how many attachments exactly have been added
+                 * for the last recipient, but we need to remove all of them
+                 * because the attachment list is potentially limited by
+                 * permissions. */
+                for ($i = 0, $c = count($attachments); $i < $c; $i++) {
+                    $mail->removePart($i);
+                }
                 foreach ($mycomments as $comment) {
                     foreach ($comment['changes'] as $change) {
                         if ($change['type'] == 'attachment') {
