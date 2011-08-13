c8c3b2232ec44b81a8c553501a0c60a343fa7290
Ralf Lang <lang@b1-systems.de>
2011-08-13 11:06:48 +0200

    Revert "Add REASON_LOCKED message to login.php"
    This reverts commit 4e17c509374a6e04baedb89406684d19bd3b2585.

--- framework/Auth/lib/Horde/Auth/Sql.php
+++ framework/Auth/lib/Horde/Auth/Sql.php
@@ -333,7 +333,7 @@ class Horde_Auth_Sql extends Horde_Auth_Base
      *
      * @throws Horde_Auth_Exception
      */
-    public function isLocked($userId, $show_details = false)
+    public function isLocked($userId, $details = false)
     {
         $userId = trim($userId);
         if (!$this->hasCapability('lock')) {
@@ -365,7 +365,7 @@ class Horde_Auth_Sql extends Horde_Auth_Base
                 }
             }
         }
-        if ($show_details == true) {
+        if ($details == true) {
             return $details;
         } else {
             return $details['locked'];
