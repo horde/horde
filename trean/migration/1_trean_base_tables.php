f6ea9ca620a9e4a906077a40f65db8a1a9fb3d93
Chuck Hagenbuch <chuck@horde.org>
2011-08-23 12:25:49 -0400

    Migration for creating trean base tables

--- trean/migration/1_trean_base_tables.php
+++ trean/migration/1_trean_base_tables.php
@@ -1,23 +1,53 @@
-CREATE TABLE trean_bookmarks (
-    bookmark_id             INT UNSIGNED NOT NULL,
-    user_id                 INT UNSIGNED NOT NULL,
-    bookmark_url            VARCHAR(1024) NOT NULL,
-    bookmark_title          VARCHAR(255),
-    bookmark_description    VARCHAR(1024),
-    bookmark_clicks         INT DEFAULT 0,
-    bookmark_rating         INT,
-    favicon_id              INT,
-    bookmark_http_status    VARCHAR(5),
-    PRIMARY KEY (bookmark_id)
-);
-CREATE INDEX trean_bookmarks_user_idx ON trean_bookmarks (user_id);
-CREATE INDEX trean_bookmarks_clicks_idx ON trean_bookmarks (bookmark_clicks);
-CREATE INDEX trean_bookmarks_rating_idx ON trean_bookmarks (bookmark_rating);
+<?php
+/**
+ * Create Trean base tables (as of Trean 1.x).
+ *
+ * Copyright 2010-2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author   Chuck Hagenbuch <chuck@horde.org>
+ * @category Horde
+ * @license  http://www.fsf.org/copyleft/gpl.html GPL
+ * @package  Trean
+ */
+class TreanBaseTables extends Horde_Db_Migration_Base
+{
+    /**
+     * Upgrade.
+     */
+    public function up()
+    {
+        $tableList = $this->tables();
 
-CREATE TABLE trean_favicons (
-    favicon_id        INT NOT NULL,
-    favicon_url       TEXT NOT NULL,
-    favicon_updated   INT NOT NULL,
-    PRIMARY KEY (favicon_id)
-);
-CREATE INDEX trean_favicons_url_idx ON trean_favicons (favicon_url(255));
+        if (!in_array('trean_bookmarks', $tableList)) {
+            $t = $this->createTable('trean_bookmarks', array('autoincrementKey' => 'bookmark_id'));
+            $t->column('user_id', 'integer', array('null' => false, 'unsigned' => true));
+            $t->column('favicon_id', 'integer', array('unsigned' => true));
+            $t->column('bookmark_url', 'string', array('limit' => 1024, 'null' => false));
+            $t->column('bookmark_title', 'string', array('limit' => 255));
+            $t->column('bookmark_description', 'string', array('limit' => 1024));
+            $t->column('bookmark_clicks', 'integer', array('default' => 0));
+            $t->column('bookmark_rating', 'integer');
+            $t->column('bookmark_http_status', 'string', array('limit' => 5));
+            $t->end();
+            $this->addIndex('trean_bookmarks', array('user_id'));
+            $this->addIndex('trean_bookmarks', array('bookmark_clicks'));
+            $this->addIndex('trean_bookmarks', array('bookmark_rating'));
+        }
+
+        if (!in_array('trean_favicons', $tableList)) {
+            $t = $this->createTable('trean_favicons', array('autoincrementKey' => 'favicon_id'));
+            $t->column('favicon_url', 'text', array('null' => false));
+            $t->column('favicon_updated', 'integer', array('null' => false, 'unsigned' => true));
+            $t->end();
+        }
+    }
+
+    public function down()
+    {
+        $this->dropTable('trean_bookmarks');
+        $this->dropTable('trean_favicons');
+    }
+}
