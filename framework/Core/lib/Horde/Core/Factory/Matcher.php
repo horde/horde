3b0532a22e0b0ca5c4fcc6ce37b90ad0c44bed88
Gunnar Wrobel <p@rdus.de>
2011-08-29 22:33:48 +0200

    Add a factory for Horde_Routes_Matcher and require Horde_Routes-1.1.0.

---
+++ framework/Core/lib/Horde/Core/Factory/Matcher.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @category Horde
+ * @package  Core
+ */
+class Horde_Core_Factory_Matcher extends Horde_Core_Factory_Injector
+{
+    public function create(Horde_Injector $injector)
+    {
+        $matcher = new Horde_Routes_Matcher(
+            $injector->getInstance('Horde_Routes_Mapper'),
+            $injector->getInstance('Horde_Controller_Request')
+        );
+        return $matcher;
+    }
+}
