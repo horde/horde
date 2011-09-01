a9a725c21f1c70002dd2f2b4f1f688818bd6f52b
Gunnar Wrobel <p@rdus.de>
2011-09-01 05:42:07 +0200

    Add a new handler for task lists.
    This tasklist handler allows to deal with differences in how Kolab
    handles task lists as compared to the default situation. As a first
    step the new handler avoids including the owner name in the default
    share.

---
+++ nag/lib/Tasklists.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The tasklists handler.
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author  Gunnar Wrobel <wrobel@pardus.de>
+ * @package Nag
+ */
+interface Nag_Tasklists
+{
+}
