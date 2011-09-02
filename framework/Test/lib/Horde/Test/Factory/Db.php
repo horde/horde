b21f83c2f56e238a449ff4e6f7aaa9dcb762a8e5
Gunnar Wrobel <p@rdus.de>
2011-09-02 08:04:37 +0200

    Add a first set of test service factories.

---
+++ framework/Test/lib/Horde/Test/Factory/Db.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Generates test database connectors.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/components/Horde_Test
+ */
+
+/**
+ * Generates test database connectors.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @since Horde_Test 1.2.0
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/components/Horde_Test
+ */
+class Horde_Test_Factory_Db
+{
+    /**
+     * Create a connector to an in-memory sqlite DB.
+     *
+     * @params array $params Additional options.
+     * <pre>
+     * 'migrations' - (array) An list of migrations that should be run.
+     *                Each element must contain the keys 'migrationsPath'
+     *                and 'schemaTableName'.
+     *                DEFAULT: empty
+     * </pre>
+     *
+     * @return Horde_Db_Adapter_Pdo_Sqlite The DB adapter.
+     */
+    public function createInMemorySqlite($params = array())
+    {
+        if (!extension_loaded('pdo') ||
+            !in_array('sqlite', PDO::getAvailableDrivers())) {
+            throw new Horde_Test_Exception('No sqlite extension or no sqlite PDO driver');
+        }
+        if (!class_exists('Horde_Db_Adapter_Pdo_Sqlite')) {
+            throw new Horde_Test_Exception('The "Horde_Db_Adapter_Pdo_Sqlite" class is unavailable!');
+        }
+        $db = new Horde_Db_Adapter_Pdo_Sqlite(array('dbname' => ':memory:', 'charset' => 'utf-8'));
+        if (isset($params['migrations'])) {
+            if (isset($params['migrations']['migrationsPath'])) {
+                $migrations = array($params['migrations']);
+            } else {
+                $migrations = $params['migrations'];
+            }
+            foreach ($migrations as $migration) {
+                $migrator = new Horde_Db_Migration_Migrator(
+                    $db, null, $migration
+                );
+                $migrator->up();
+            }
+        }
+        return $db;
+    }
+}
---
+++ framework/Test/lib/Horde/Test/Stub/Registry.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * A test replacement for Horde_Registry.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/components/Horde_Test
+ */
+
+/**
+ * A test replacement for Horde_Registry.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @since Horde_Test 1.2.0
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/components/Horde_Test
+ */
+class Horde_Test_Stub_Registry
+{
+    /**
+     * The currrent user.
+     *
+     * @var string
+     */
+    private $_user;
+
+    /**
+     * The current application.
+     *
+     * @var string
+     */
+    private $_app;
+
+    /**
+     * Constructor.
+     *
+     * @param string $user The current user.
+     * @param string $app  The current application.
+     */
+    public function __construct($user, $app)
+    {
+        $this->_user = $user;
+        $this->_app = $app;
+    }
+
+    /**
+     * Returns the currently logged in user, if there is one.
+     *
+     * @param string $format  The return format, defaults to the unique Horde
+     *                        ID. Alternative formats:
+     * <pre>
+     * bare - Horde ID without any domain information.
+     *        EXAMPLE: foo@example.com would be returned as 'foo'.
+     * domain: Domain of the Horde ID.
+     *         EXAMPLE: foo@example.com would be returned as 'example.com'.
+     * original: The username used to originally login to Horde.
+     * </pre>
+     *
+     * @return mixed  The user ID or false if no user is logged in.
+     */
+    public function getAuth($format = null)
+    {
+        return $this->_user;
+    }
+
+    /**
+     * Is a user an administrator?
+     *
+     * @param array $options  Options:
+     * <pre>
+     * 'permission' - (string) Allow users with this permission admin access
+     *                in the current context.
+     * 'permlevel' - (integer) The level of permissions to check for.
+     *               Defaults to Horde_Perms::EDIT.
+     * 'user' - (string) The user to check.
+     *          Defaults to self::getAuth().
+     * </pre>
+     *
+     * @return boolean  Whether or not this is an admin user.
+     */
+    public function isAdmin(array $options = array())
+    {
+        return false;
+    }
+
+    /**
+     * Return the requested configuration parameter for the specified
+     * application. If no application is specified, the value of
+     * the current application is used. However, if the parameter is not
+     * present for that application, the Horde-wide value is used instead.
+     * If that is not present, we return null.
+     *
+     * @param string $parameter  The configuration value to retrieve.
+     * @param string $app        The application to get the value for.
+     *
+     * @return string  The requested parameter, or null if it is not set.
+     */
+    public function get($parameter, $app = null)
+    {
+        return '';
+    }
+
+    /**
+     * Return the current application - the app at the top of the application
+     * stack.
+     *
+     * @return string  The current application.
+     */
+    public function getApp()
+    {
+        return $this->_app;
+    }
+}
