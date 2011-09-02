25c965a86f60a01eb3bff7cc945d8ed8c9a8ade4
Gunnar Wrobel <p@rdus.de>
2011-09-02 08:03:20 +0200

    Add my idea of how we could prepare complex test setups for testing Horde applications.
    Right now the test suite for each application is responsible of
    generating its own test setup. A lot of the basic elements required by
    each application (injector, registry, preferences, permissions, db
    etc.) are being setup in each test suite. This obviously duplicates
    code and gets complex as some of those basic building blocks need
    extensive setup routines/factories.
    
    I think it is easiest to centralize the setup routines in
    Horde_Test. Horde_Test_Setup has been added for this. It is a
    Horde_Injector based approach. The class accepts a configuration which
    defines factories/methods/parameters that will be used to create the
    test services. These will then be registered to the injector and can
    also be exported to globals.
    
    While the factories are being defined in Horde_Test they should always
    check that the prerequisite package is actually available. If not the
    factory needs to error out so that the test suite can pick up the
    error and skip tests that don't meet the required prerequisites. This
    should allow to add a Horde_Test factory that deals with a
    hypothetical package Horde_Xyz without adding a dependency on
    Horde_Xyz to Horde_Test (not even an optional one).

---
+++ framework/Test/lib/Horde/Test/Setup.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * A test helper for generating complex test setups.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Test
+ */
+
+/**
+ * A test helper for generating complex test setups.
+ *
+ * @since Horde_Test 1.2.0
+ *
+ * @category Horde
+ * @package  Test
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @link     http://pear.horde.org/index.php?package=Test
+ */
+class Horde_Test_Setup
+{
+    /**
+     * The Horde_Injector instance which serves as our service container.
+     *
+     * @var Horde_Injector
+     */
+    private $_injector;
+
+    /**
+     * In case the setup turns out to be unfullfillable this should contain an
+     * appropriate message indicating the problem.
+     *
+     * @var string
+     */
+    private $_error;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        if (class_exists('Horde_Injector')) {
+            $this->_injector = new Horde_Injector(new Horde_Injector_TopLevel());
+            $this->_injector->setInstance('Horde_Injector', $this->_injector);
+        } else {
+            $this->_error = 'The Horde_Injector class is unavailable!';
+        }
+    }
+
+    /**
+     * Add a new set of elements to the service container.
+     *
+     * @param array $params All parameters necessary for creating the services.
+     *                      The keys of the array elements define the name that
+     *                      will be used for registering the test service with
+     *                      the injector. The element values are a
+     *                      configuration array with the following elements:
+     * <pre>
+     * 'factory' - (string) Name of the factory. Can be a full class name or an
+     *             abbreviated name that will get prepended with
+     *             'Horde_Test_Factory_'
+     * 'method' - (string) Method name that will be invoked on the above factory
+     *            to generate the test service.
+     * 'params' - (array) Any parameters the factory method might require for
+     *            generating the test service. See the various factories/methods
+     *            for details.
+     * </pre>
+     *
+     * @return NULL
+     */
+    public function setup($params)
+    {
+        foreach ($params as $interface => $setup) {
+            if (!empty($this->_error)) {
+                break;
+            }
+            $this->add(
+                $interface,
+                $setup['factory'],
+                $setup['method'],
+                isset($setup['params']) ? $setup['params'] : array()
+            );
+        }
+    }
+
+    /**
+     * Add a new element to the service container.
+     *
+     * @oaram string $interface The interface name to register the service with.
+     * @param string $factory   The (abbreviated) name of the factory.
+     * @param string $method    The factory method that will generate the
+     *                          service.
+     * @param array  $params    All parameters necessary for creating the
+     *                          service.
+     *
+     * @return NULL
+     */
+    public function add($interface, $factory, $method, $params)
+    {
+        if (!empty($this->_error)) {
+            return;
+        }
+        if (!class_exists('Horde_Test_Factory_' . $factory) &&
+            !class_exists($factory)) {
+            $this->_error = "Neither the class \"Horde_Test_Factory_$factory\" nor \"$factory\" exist. \"$interface\" cannot be created!";
+            return;
+        }
+        if (class_exists('Horde_Test_Factory_' . $factory)) {
+            $f = $this->_injector->getInstance('Horde_Test_Factory_' . $factory);
+        } else {
+            $f = $this->_injector->getInstance($factory);
+        }
+        if (!method_exists($f, $method) &&
+            !method_exists($f, 'create' . $method)) {
+            $this->_error = "The factory lacks the specified method \"$method\"!";
+            return;
+        }
+        if (method_exists($f, 'create' . $method)) {
+            $method = 'create' . $method;
+        }
+        try {
+            $this->_injector->setInstance($interface, $f->{$method}($params));
+        } catch (Horde_Test_Exception $e) {
+            $this->_error = $e->getMessage();
+        }
+    }
+
+    /**
+     * Export elements from the injector into global scope.
+     *
+     * @param array $elements The elements to export.
+     *
+     * @return NULL
+     */
+    public function makeGlobal($elements)
+    {
+        if (!empty($this->_error)) {
+            return;
+        }
+        foreach ($elements as $key => $interface) {
+            $GLOBALS[$key] = $this->_injector->getInstance($interface);
+        }
+    }
+
+    /**
+     * Return any potential setup error.
+     *
+     * @return string The error.
+     */
+    public function getError()
+    {
+        return $this->_error;
+    }
+
+    /**
+     * Return the service container.
+     *
+     * @return Horde_Injector The injector.
+     */
+    public function getInjector()
+    {
+        return $this->_injector;
+    }
+}
