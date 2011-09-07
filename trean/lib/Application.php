4868df1f7b7fca210bbaa3bddacf8f370fc972ad
Michael J Rubinsky <mrubinsk@horde.org>
2011-09-07 14:34:12 -0400

    Don't set $bodyClass unconditionally every time Trean is initialized.
    Setting this in Trean_Application#_init causes any other application that
    does not have this value explicitly set, on every page, to take on this
    bodyClass when displaying one of that application's pages.

--- trean/lib/Application.php
+++ trean/lib/Application.php
@@ -62,9 +62,6 @@ class Trean_Application extends Horde_Registry_Application
             $rss->add('f', Horde_Util::getFormData('f'));
         }
         $GLOBALS['linkTags'] = array('<link rel="alternate" type="application/rss+xml" title="' . htmlspecialchars(_("Bookmarks Feed")) . '" href="' . $rss . '" />');
-        if ($GLOBALS['prefs']->getValue('show_folder_actions')) {
-            $GLOBALS['bodyClass'] = 'folderActions';
-        }
     }
 
     /**
