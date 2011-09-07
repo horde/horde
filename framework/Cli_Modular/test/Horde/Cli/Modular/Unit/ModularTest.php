be66896304b4d6d6051f417d0c0b9291d93755b1
Ralf Lang <lang@b1-systems.de>
2011-09-07 16:32:40 +0200

    Attribution change to Horde LLC. License file was already present

--- framework/Cli_Modular/test/Horde/Cli/Modular/Unit/ModularTest.php
+++ framework/Cli_Modular/test/Horde/Cli/Modular/Unit/ModularTest.php
@@ -3,6 +3,10 @@
  * Test the module wrapper.
  *
  * PHP version 5
+ * Copyright 2010-2011 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
  * @category   Kolab
  * @package    Cli_Modular
@@ -19,18 +23,6 @@ require_once dirname(__FILE__) . '/../Autoload.php';
 
 /**
  * Test the module wrapper.
- *
- * Copyright 2010-2011 The Horde Project (http://www.horde.org/)
- *
- * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.horde.org/licenses/lgpl21.
- *
- * @category   Kolab
- * @package    Cli_Modular
- * @subpackage UnitTests
- * @author     Gunnar Wrobel <wrobel@pardus.de>
- * @license    http://www.horde.org/licenses/lgpl21 LGPL
- * @link       http://www.horde.org/components/Horde_Cli_Modular
  */
 class Horde_Cli_Modular_Unit_ModularTest
 extends Horde_Cli_Modular_TestCase
