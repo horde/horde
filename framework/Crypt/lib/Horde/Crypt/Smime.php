e0e3c20968badd5ece44058f4b8fc22b73cd1b02
Ralf Lang <lang@b1-systems.de>
2011-09-08 14:20:39 +0200

    added COPYING file to crypt, licensing related edits in comments

--- framework/Crypt/lib/Horde/Crypt/Smime.php
+++ framework/Crypt/lib/Horde/Crypt/Smime.php
@@ -3,15 +3,15 @@
  * Horde_Crypt_Smime:: provides a framework for Horde applications to
  * interact with the OpenSSL library and implement S/MIME.
  *
- * Copyright 2002-2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2002-2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
  * @author   Mike Cochrane <mike@graftonhall.co.nz>
  * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @package  Crypt
  */
 class Horde_Crypt_Smime extends Horde_Crypt
