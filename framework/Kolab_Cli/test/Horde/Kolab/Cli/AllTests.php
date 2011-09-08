6a68474159a4f1ca359630f38390ef9ec1605a51
Ralf Lang <lang@b1-systems.de>
2011-09-08 20:41:25 +0200

    Changed license file to LGPL-2.1 because code was full of LGPL and the only mentioning of GPL was in package.xmChanged license file to LGPL-2.1 because code was full of LGPL and the only mentioning of GPL was in package.xmll

--- framework/Kolab_Cli/test/Horde/Kolab/Cli/AllTests.php
+++ framework/Kolab_Cli/test/Horde/Kolab/Cli/AllTests.php
@@ -8,7 +8,7 @@
  * @package    Kolab_Cli
  * @subpackage UnitTests
  * @author     Gunnar Wrobel <wrobel@pardus.de>
- * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link       http://pear.horde.org/index.php?package=Kolab_Cli
  */
 
--- framework/Kolab_Cli/test/Horde/Kolab/Cli/Unit/Cli/Module/BaseTest.php
+++ framework/Kolab_Cli/test/Horde/Kolab/Cli/Unit/Cli/Module/BaseTest.php
@@ -8,7 +8,7 @@
  * @package    Kolab_Cli
  * @subpackage UnitTests
  * @author     Gunnar Wrobel <wrobel@pardus.de>
- * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link       http://pear.horde.org/index.php?package=Kolab_Cli
  */
 
@@ -20,16 +20,16 @@ require_once dirname(__FILE__) . '/../../../Autoload.php';
 /**
  * Test the base modules.
  *
- * Copyright 2010-2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2010-2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
  * @category   Kolab
  * @package    Kolab_Cli
  * @subpackage UnitTests
  * @author     Gunnar Wrobel <wrobel@pardus.de>
- * @license    http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link       http://pear.horde.org/index.php?package=Kolab_Cli
  */
 class Horde_Kolab_Cli_Unit_Cli_Module_BaseTest
