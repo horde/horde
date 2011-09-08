234607bf5c3d02bc3fa0698b1ec812f1e750349b
Ralf Lang <lang@b1-systems.de>
2011-09-08 21:08:43 +0200

    Moving COPYING file, copyright related edits

--- framework/Kolab_Format/lib/Horde/Kolab/Format.php
+++ framework/Kolab_Format/lib/Horde/Kolab/Format.php
@@ -7,7 +7,7 @@
  * @category Kolab
  * @package  Kolab_Format
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Format
  */
 
@@ -16,16 +16,16 @@
  * format handler.
  *
  * Copyright 2007-2010 Klarälvdalens Datakonsult AB
- * Copyright 2010-2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2010-2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you did not
  * receive this file, see
- * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ * http://www.horde.org/licenses/lgpl21.
  *
  * @category Kolab
  * @package  Kolab_Format
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Format
  */
 interface Horde_Kolab_Format
--- framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Distributionlist.php
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Distributionlist.php
@@ -8,7 +8,7 @@
  * @package  Kolab_Format
  * @author   Thomas Jarosch <thomas.jarosch@intra2net.com>
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Server
  */
 
@@ -19,13 +19,13 @@
  *
  * See the enclosed file COPYING for license information (LGPL). If you did not
  * receive this file, see
- * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ * http://www.horde.org/licenses/lgpl21.
  *
  * @category Kolab
  * @package  Kolab_Format
  * @author   Thomas Jarosch <thomas.jarosch@intra2net.com>
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Server
  */
 class Horde_Kolab_Format_Xml_Distributionlist extends Horde_Kolab_Format_Xml
