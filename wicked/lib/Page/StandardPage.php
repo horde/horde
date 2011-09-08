e220bad24d5ec0da8102d5cba688aa231e3c0455
Jan Schneider <jan@horde.org>
2011-09-08 17:53:51 +0200

    Move standard page contents into a sub-directory for the Default Text_Wiki processor.

--- wicked/lib/Page/StandardPage.php
+++ wicked/lib/Page/StandardPage.php
@@ -55,7 +55,9 @@ class Wicked_Page_StandardPage extends Wicked_Page {
             $page = $GLOBALS['wicked']->retrieveByName($pagename);
         } catch (Wicked_Exception $e) {
             // If we can't load $pagename, see if there's default data for it.
-            $pagefile = WICKED_BASE . '/data/' . basename($pagename);
+            $pagefile = WICKED_BASE . '/data/'
+                . $GLOBALS['conf']['wicked']['format'] . '/'
+                . basename($pagename);
             if ($pagename == basename($pagename) &&
                 substr($pagename, 0, 1) != '.' &&
                 file_exists($pagefile)) {
