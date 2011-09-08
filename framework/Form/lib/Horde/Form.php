f1cfdaf285184341f1f6b7a098ade3c7c1c48311
Ralf Lang <lang@b1-systems.de>
2011-09-08 19:56:59 +0200

    Adding license file, copyright related edits

--- framework/Form/lib/Horde/Form.php
+++ framework/Form/lib/Horde/Form.php
@@ -9,10 +9,10 @@ require_once 'Horde/Form/Type.php';
  * other functionality for the Horde Application Framework.
  *
  * Copyright 2001-2007 Robert E. Coyle <robertecoyle@hotmail.com>
- * Copyright 2001-2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2001-2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
  * @author  Robert E. Coyle <robertecoyle@hotmail.com>
  * @author  Chuck Hagenbuch <chuck@horde.org>
