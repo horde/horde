e625c21c278c72274f18145cf9f1091501160c1f
Ralf Lang <lang@b1-systems.de>
2011-09-09 17:22:51 +0200

    licensing-related edits

--- jonah/lib/Block/Cloud.php
+++ jonah/lib/Block/Cloud.php
@@ -2,10 +2,10 @@
 /**
  * Display Tag Cloud
  *
- * Copyright 2007-2009 The Horde Project (http://www.horde.org/)
+ * Copyright 2007-2009 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
  *
  * @author  Michael Rubinsky <mrubinsk@horde.org>
  */
--- jonah/lib/Test.php
+++ jonah/lib/Test.php
@@ -2,10 +2,10 @@
 /**
  * This class provides the Jonah configuration for the test script.
  *
- * Copyright 2010-2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2010-2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
  *
  * @author  Michael Slusarz <slusarz@horde.org>
  * @package Jonah
