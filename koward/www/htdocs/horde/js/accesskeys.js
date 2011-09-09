e90fc440c4a90adc115bfe4851e489cb6eea0aab
Ralf Lang <lang@b1-systems.de>
2011-09-09 17:24:37 +0200

    licensing-related edits, added COPYING file

--- koward/www/htdocs/horde/js/accesskeys.js
+++ koward/www/htdocs/horde/js/accesskeys.js
@@ -2,7 +2,7 @@
  * Accesskeys javascript file.
  *
  * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  */
 var AccessKeys = {
 
