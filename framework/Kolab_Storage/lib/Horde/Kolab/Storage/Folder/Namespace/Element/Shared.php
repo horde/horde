75790f708f5b98773c0a5c6922595fd5e7f53f10
Ralf Lang <lang@b1-systems.de>
2011-09-09 10:34:14 +0200

    Adding COPYING file, copyright related edits

--- framework/Kolab_Storage/lib/Horde/Kolab/Storage/Folder/Namespace/Element/Shared.php
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/Folder/Namespace/Element/Shared.php
@@ -8,7 +8,7 @@
  * @category Kolab
  * @package  Kolab_Storage
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Storage
  */
 
@@ -16,15 +16,15 @@
  * The Horde_Kolab_Storage_Folder_Namespace_Element_Shared:: class represents
  * the shared namespace.
  *
- * Copyright 2010-2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2010-2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
  * @category Kolab
  * @package  Kolab_Storage
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Storage
  */
 class Horde_Kolab_Storage_Folder_Namespace_Element_Shared
--- framework/Kolab_Storage/test/Horde/Kolab/Storage/Unit/Data/Query/Preferences/BaseTest.php
+++ framework/Kolab_Storage/test/Horde/Kolab/Storage/Unit/Data/Query/Preferences/BaseTest.php
@@ -7,7 +7,7 @@
  * @category Kolab
  * @package  Kolab_Storage
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Storage
  */
 
@@ -19,15 +19,15 @@ require_once dirname(__FILE__) . '/../../../../Autoload.php';
 /**
  * Test the handling of the preference data query.
  *
- * Copyright 2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
  * @category Kolab
  * @package  Kolab_Storage
  * @author   Gunnar Wrobel <wrobel@pardus.de>
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @link     http://pear.horde.org/index.php?package=Kolab_Storage
  */
 class Horde_Kolab_Storage_Unit_Data_Query_Preferences_BaseTest
