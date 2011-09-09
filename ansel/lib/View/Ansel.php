8678bf61529bce68629b0dcce7b7bfd84a8eedb1
Ralf Lang <lang@b1-systems.de>
2011-09-09 13:37:17 +0200

    The Horde Project is Horde LLC now

--- ansel/lib/View/Ansel.php
+++ ansel/lib/View/Ansel.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Abstract Ansel_View class for Ansel UI specific views.
- * Copyright 2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
--- ansel/migration/9_ansel_upgrade_sqlng.php
+++ ansel/migration/9_ansel_upgrade_sqlng.php
@@ -9,7 +9,7 @@
 /**
  * Adds tables for the Sqlng share driver.
  *
- * Copyright 2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
