510f56a04dfb06be7d3002e2558de7408185e1df
Ralf Lang <lang@b1-systems.de>
2011-09-09 17:51:11 +0200

    beatnik: missed FSF links

--- beatnik/lib/Driver/ldap2dns.php
+++ beatnik/lib/Driver/ldap2dns.php
@@ -3,7 +3,7 @@
  * Copyright 2005-2007 Alkaloid Networks <http://www.alkaloid.net>
  *
  * See the enclosed file COPYING for license information (GPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
  *
  * @author Ben Klang <ben@alkaloid.net>
  * @package Beatnik
--- beatnik/lib/Driver/pdnsgsql.php
+++ beatnik/lib/Driver/pdnsgsql.php
@@ -8,7 +8,7 @@
  * Copyright 2008 Horde LLC <http://www.horde.org>
  *
  * See the enclosed file COPYING for license information (GPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
  *
  * @author Ben Klang <ben@alkaloid.net>
  * @package Beatnik
