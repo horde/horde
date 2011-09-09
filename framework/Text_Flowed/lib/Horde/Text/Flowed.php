e7df4211afb5346ff52a8ae45339c74425093095
Ralf Lang <lang@b1-systems.de>
2011-09-09 13:14:44 +0200

    Adding COPYING file, copyright related edits

--- framework/Text_Flowed/lib/Horde/Text/Flowed.php
+++ framework/Text_Flowed/lib/Horde/Text/Flowed.php
@@ -8,14 +8,14 @@
  * license, which is compatible with the LGPL.
  *
  * Copyright 2002-2003 Philip Mak
- * Copyright 2004-2011 The Horde Project (http://www.horde.org/)
+ * Copyright 2004-2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
  * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
- * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @package  Text_Flowed
  */
 class Horde_Text_Flowed
