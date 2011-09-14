b49803aa084217056de9679fec76ca7cc903eef7
Ralf Lang <lang@b1-systems.de>
2011-09-14 19:27:12 +0200

    Remove the version.php file Add a basic Api.php

---
+++ sesha/lib/Api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Sesha external API interface.
+ *
+ * Copyright 2003-2011 Horde LLC (http://www.horde.org/)
+ *
+ * This file defines Sesha's external API interface. Other applications can
+ * interact with Sesha through this API.
+ *
+ * @author  Bo Daley <bo@darkwork.net>
+ * @package Sesha
+ */
+
+class Sesha_Api extends Horde_Registry_Api
+{
+}
