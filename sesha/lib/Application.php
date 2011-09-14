73d4b89c2851c69efcf9f538a06f203d15b79a9f
Ralf Lang <lang@b1-systems.de>
2011-09-14 19:23:49 +0200

    A basic Application.php

---
+++ sesha/lib/Application.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Sesha application API.
+ *
+ * This file defines Horde's core API interface. Other core Horde libraries
+ * can interact with Skeleton through this API.
+ *
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @package Sesha
+ */
+
+/* Determine the base directories. */
+if (!defined('SESHA_BASE')) {
+    define('SESHA_BASE', dirname(__FILE__) . '/..');
+}
+
+if (!defined('HORDE_BASE')) {
+    /* If Horde does not live directly under the app directory, the HORDE_BASE
+     * constant should be defined in config/horde.local.php. */
+    if (file_exists(SESHA_BASE . '/config/horde.local.php')) {
+        include SESHA_BASE . '/config/horde.local.php';
+    } else {
+        define('HORDE_BASE', SESHA_BASE . '/..');
+    }
+}
+
+/* Load the Horde Framework core (needed to autoload
+ * Horde_Registry_Application::). */
+require_once HORDE_BASE . '/lib/core.php';
+/*
+ * The sesha application class
+ * @package Sesha
+ */
+class Sesha_Application extends Horde_Registry_Application
+{
+    /**
+     * The application's version.
+     *
+     * @var string
+     */
+    public $version = 'H4 (1.0-git)';
+
+}
