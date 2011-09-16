81fafd82aa576d7274f78a0a86fa163ad41c37ef
Ralf Lang <lang@b1-systems.de>
2011-09-16 18:46:56 +0200

    Add an exception class

---
+++ sesha/lib/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Base exception class for Sesha.
+ *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @package Sesha
+ */
+class Sesha_Exception extends Horde_Exception_Wrapped
+{
+}
