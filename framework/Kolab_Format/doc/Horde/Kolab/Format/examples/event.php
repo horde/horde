7943b6f518da1c8db863052629f0e56652ab0772
Gunnar Wrobel <p@rdus.de>
2011-09-22 21:59:00 +0200

    Fix example.

--- framework/Kolab_Format/doc/Horde/Kolab/Format/examples/event.php
+++ framework/Kolab_Format/doc/Horde/Kolab/Format/examples/event.php
@@ -22,12 +22,17 @@ $factory = new Horde_Kolab_Format_Factory();
 /** Generate the format handler */
 $format = $factory->create('Xml', 'Event');
 
+$now = new DateTime();
 /** Prepare a test object */
 $object = array(
     'uid' => 1,
     'summary' => 'test event',
-    'start-date' => time(),
-    'end-date' => time() + 24 * 60 * 60,
+    'start-date' => array(
+        'date' => $now,
+    ),
+    'end-date' => array(
+        'date' => $now,
+    )
 );
 
 /** Save this test data array in Kolab XML format */
