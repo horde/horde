bc86c3996d0fe3e635d8cbeeae3d287a7cbe2c09
Jan Schneider <jan@horde.org>
2011-09-27 19:12:16 +0200

    Released whups-2.0.0alpha1

--- whups/lib/Application.php
+++ whups/lib/Application.php
@@ -36,7 +36,7 @@ class Whups_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H4 (2.0-git)';
+    public $version = 'H4 (2.0-ALPHA1)';
 
     /**
      * Global variables defined:
