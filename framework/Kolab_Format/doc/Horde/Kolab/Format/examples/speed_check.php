6804870f5098609134187c613feeb85c6375ec67
Gunnar Wrobel <p@rdus.de>
2011-09-30 20:56:37 +0200

    Add a simple speed check example.

---
+++ framework/Kolab_Format/doc/Horde/Kolab/Format/examples/speed_check.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * A sample script for a quick speed check for reading and writing the XML data.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @link     http://pear.horde.org/index.php?package=Kolab_Server
+ */
+
+/**
+ * The Autoloader allows us to omit "require/include" statements.
+ */
+require_once 'Horde/Autoloader/Default.php';
+
+/** Create the factory */
+$factory = new Horde_Kolab_Format_Factory();
+
+/** Generate the format handler */
+$format = $factory->create('Xml', 'Event');
+
+/** Prepare a test object */
+if (!method_exists($format, 'getVersion')) {
+    $object = array(
+        'uid' => 1,
+        'summary' => 'test event',
+        'start-date' => time(),
+        'end-date' => time() + 24 * 60 * 60,
+    );
+} else {
+    $now = new DateTime();
+    $object = array(
+        'uid' => 1,
+        'summary' => 'test event',
+        'start-date' => array(
+            'date' => $now,
+        ),
+        'end-date' => array(
+            'date' => $now,
+        )
+    );
+}
+
+$timer = new Horde_Support_Timer();
+$timer->push();
+for ($i = 0;$i < 1000;$i++) {
+    /** Save this test data array in Kolab XML format */
+    $xml = $format->save($object);
+    /** Reload the object from the XML format */
+    $read_object = $format->load($xml);
+}
+
+var_dump($timer->pop());
