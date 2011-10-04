7c66db52120c431358de6ab919e747add1098d11
Gunnar Wrobel <p@rdus.de>
2011-10-04 07:00:31 +0200

    Basic skeleton for Horde_Push.

---
+++ framework/Push/test/Horde/Push/Autoload.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Setup autoloading for the tests.
+ *
+ * PHP version 5
+ *
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category   Horde
+ * @package    Push
+ * @subpackage UnitTests
+ * @author     Gunnar Wrobel <wrobel@pardus.de>
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @link       http://www.horde.org/libraries/Horde_Push
+ */
+
+require_once 'Horde/Test/Autoload.php';
+
+/** Catch strict standards */
+error_reporting(E_ALL | E_STRICT);
+
+/** Load the basic test definition */
+require_once dirname(__FILE__) . '/TestCase.php';
+
+/** Load stub definitions */
+//require_once dirname(__FILE__) . '/Stub/SomeStub.php';
