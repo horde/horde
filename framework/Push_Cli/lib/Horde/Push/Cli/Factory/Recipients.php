9f80940872ef04c458f14705379a4a7b3fd7897b
Gunnar Wrobel <p@rdus.de>
2011-10-04 21:08:22 +0200

    Started a basic Horde_Push CLI client.

---
+++ framework/Push_Cli/lib/Horde/Push/Cli/Factory/Recipients.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Creates the Horde_Push recipients.
+ *
+ * PHP version 5
+ *
+ * @category Horde
+ * @package  Push_Cli
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/gpl GPL-2.0
+ * @link     http://www.horde.org/components/Horde_Push_Cli
+ */
+
+/**
+ * Creates the Horde_Push recipients.
+ *
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL-2.0). If you did
+ * not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @category Horde
+ * @package  Push_Cli
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/gpl GPL-2.0
+ * @link     http://www.horde.org/components/Horde_Push_Cli
+ */
+class Horde_Push_Cli_Factory_Recipients
+{
+    /**
+     * Create the Horde_Push content element.
+     *
+     * @param array $arguments The command line arguments.
+     * @param array $options   Command line options.
+     * @param array $conf      The configuration.
+     */
+    public function create($conf)
+    {
+        return array();
+    }
+}
