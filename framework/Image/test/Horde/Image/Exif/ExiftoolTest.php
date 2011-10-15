162f8aa7e4461673e5354a344ad793029b38f9e9
Michael J Rubinsky <mrubinsk@horde.org>
2011-10-14 23:03:42 -0400

    Add basic unit test structure for Horde_Image_Exif classes.
    Basic structure and config, plus a simple make-sure-it-works test.

---
+++ framework/Image/test/Horde/Image/Exif/ExiftoolTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/Base.php';
+
+/**
+ * @author     Michael J Rubinsky <mrubinsk@horde.org>
+ * @category   Horde
+ * @package    Image
+ * @subpackage UnitTests
+ * @copyright  2011 Horde LLC (http://www.horde.org/)
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+
+
+class Horde_Image_Exif_ExiftoolTest extends Horde_Image_Test_Exif_Base
+{
+    public static function setUpBeforeClass()
+    {
+        $config = self::getConfig('IMAGE_EXIF_TEST_CONFIG',
+                                  dirname(__FILE__) . '/..');
+        if ($config && !empty($config['image']['exiftool'])) {
+            self::$_exif = new Horde_Image_Exif_Exiftool(array('exiftool' => $config['image']['exiftool']));
+            parent::setUpBeforeClass();
+        }
+    }
+}
