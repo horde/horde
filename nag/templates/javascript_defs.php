7ec0c607eb47dff85edb50a3ee5b84e35f729d32
Chuck Hagenbuch <chuck@horde.org>
2011-10-22 22:04:03 -0400

    Revert "Revert "Port the Kronolith ajax date/time picker over to Nag""
    This reverts commit f00325182a844af41ae7ee1ce01d6e260edb145e.

---
+++ nag/templates/javascript_defs.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Variables used in core javascript files. */
+$code['conf'] = array(
+    'date_format' => str_replace(array('%e', '%d', '%a', '%A', '%m', '%h', '%b', '%B', '%y', '%Y'),
+                                 array('d', 'dd', 'ddd', 'dddd', 'MM', 'MMM', 'MMM', 'MMMM', 'yy', 'yyyy'),
+                                 Horde_Nls::getLangInfo(D_FMT)),
+    'time_format' => $GLOBALS['prefs']->getValue('twentyFour') ? 'HH:mm' : 'hh:mm tt',
+);
+
+echo Horde::addInlineJsVars(array(
+    'var Nag' => $code
+), array('top' => true));
