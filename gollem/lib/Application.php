d3badbce808bc56a55e2bcfd63d56dd4e1404560
Jan Schneider <jan@horde.org>
2011-10-27 23:47:05 +0200

    Development mode for gollem-2.0.1

--- gollem/lib/Application.php
+++ gollem/lib/Application.php
@@ -50,7 +50,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H4 (2.0)';
+    public $version = 'H4 (2.0.1-git)';
 
     /**
      * Cached values to add to the session after authentication.
