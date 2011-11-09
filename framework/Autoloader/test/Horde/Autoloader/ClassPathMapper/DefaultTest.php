07b6bfa1fba41830d8adff59a3352c422c0b6f1e
Jan Schneider <jan@horde.org>
2011-11-09 15:51:24 +0100

    [jan] Make unit tests Windows compatible.

--- framework/Autoloader/test/Horde/Autoloader/ClassPathMapper/DefaultTest.php
+++ framework/Autoloader/test/Horde/Autoloader/ClassPathMapper/DefaultTest.php
@@ -14,15 +14,18 @@ class Horde_Autoloader_ClassPathMapper_DefaultTest extends PHPUnit_Framework_Tes
 
     public function providerClassNames()
     {
-        return array(
-            array('Module_Action_Suffix', 'dir/Module/Action/Suffix.php'),
-            array('MyModule_Action_Suffix', 'dir/MyModule/Action/Suffix.php'),
-            array('Module_MyAction_Suffix', 'dir/Module/MyAction/Suffix.php'),
-            array('MyModule_MyAction_Suffix', 'dir/MyModule/MyAction/Suffix.php'),
-            array('Module\Action\Suffix', 'dir/Module/Action/Suffix.php'),
-            array('MyModule\Action\Suffix', 'dir/MyModule/Action/Suffix.php'),
-            array('Module\MyAction\Suffix', 'dir/Module/MyAction/Suffix.php'),
-            array('MyModule\MyAction\Suffix', 'dir/MyModule/MyAction/Suffix.php'),
+        return array_map(
+            create_function('$a', '$a[1] = str_replace("/", DIRECTORY_SEPARATOR, $a[1]); return $a;'),
+            array(
+                array('Module_Action_Suffix', 'dir/Module/Action/Suffix.php'),
+                array('MyModule_Action_Suffix', 'dir/MyModule/Action/Suffix.php'),
+                array('Module_MyAction_Suffix', 'dir/Module/MyAction/Suffix.php'),
+                array('MyModule_MyAction_Suffix', 'dir/MyModule/MyAction/Suffix.php'),
+                array('Module\Action\Suffix', 'dir/Module/Action/Suffix.php'),
+                array('MyModule\Action\Suffix', 'dir/MyModule/Action/Suffix.php'),
+                array('Module\MyAction\Suffix', 'dir/Module/MyAction/Suffix.php'),
+                array('MyModule\MyAction\Suffix', 'dir/MyModule/MyAction/Suffix.php'),
+            )
         );
     }
 
