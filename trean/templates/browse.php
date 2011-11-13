d3344f1e96458a2f1051c2a71cda0d67b76de53e
Chuck Hagenbuch <chuck@horde.org>
2011-11-13 14:44:37 -0500

    Centralize the bookmarklet and include it on the "no bookmarks yet" screen explicitly.

--- trean/templates/browse.php
+++ trean/templates/browse.php
@@ -4,7 +4,8 @@ if (count($bookmarks)) {
     $view = new Trean_View_BookmarkList($GLOBALS['bookmarks']);
     echo $view->render();
 } else {
-    echo '<p><em>' . _("No bookmarks. Drag the \"New Bookmark\" link to your browser's toolbar to add them easily!") . '</em></p>';
+    require TREAN_TEMPLATES . '/bookmarklet.php';
+    echo '<p><em>' . sprintf(_("No bookmarks. Drag the %s link to your browser's toolbar to add them easily!"), $bookmarklet_link) . '</em></p>';
 }
 ?>
 </div>
