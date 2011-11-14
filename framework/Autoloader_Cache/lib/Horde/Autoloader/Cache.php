351190d0cd4e7ae85690fab2b861b6263cda23d6
Jan Schneider <jan@horde.org>
2011-11-14 06:00:24 +0100

    Add Autoloader_Cache library to cache class maps from the Autoloader package (Request #10396).

---
+++ framework/Autoloader_Cache/lib/Horde/Autoloader/Cache.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Decorator for Horde_Autoloader that implements caching of class-file-maps.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @package  Autoloader_Cache
+ */
+require_once 'Horde/Autoloader/Default.php';
+
+class Horde_Autoloader_Cache extends Horde_Autoloader_Default
+{
+    protected $_cache;
+
+    public function __construct()
+    {
+        parent::__construct();
+        if (extension_loaded('apc')) {
+            $this->_cache = apc_fetch('horde_autoloader_cache');
+        } elseif (extension_loaded('xcache')) {
+            $this->_cache = xcache_get('horde_autoloader_cache');
+        } elseif (extension_loaded('eaccelerator')) {
+            $this->_cache = eaccelerator_get('horde_autoloader_cache');
+        } elseif (($tempdir = sys_get_temp_dir()) &&
+                  is_readable($tempdir . '/horde_autoloader_cache')) {
+            $this->_cache = @json_decode(file_get_contents($tempdir . '/horde_autoloader_cache'), true);
+        }
+    }
+
+    public function __destruct()
+    {
+        if (extension_loaded('apc')) {
+            apc_store('horde_autoloader_cache', $this->_cache);
+        } elseif (extension_loaded('xcache')) {
+            xcache_set('horde_autoloader_cache', $this->_cache);
+        } elseif (extension_loaded('eaccelerator')) {
+            eaccelerator_put('horde_autoloader_cache', $this->_cache);
+        } elseif (($tempdir = sys_get_temp_dir()) &&
+                  (is_writable($tempdir . '/horde_autoloader_cache') ||
+                   (!file_exists($tempdir . '/horde_autoloader_cache') &&
+                    is_writable($tempdir)))) {
+            file_put_contents($tempdir . '/horde_autoloader_cache', json_encode($this->_cache));
+        }
+    }
+
+    /**
+     * Search registered mappers in LIFO order.
+     */
+    public function mapToPath($className)
+    {
+        if (!isset($this->_cache[$className])) {
+            $this->_cache[$className] = parent::mapToPath($className);
+        }
+        return $this->_cache[$className];
+    }
+}
+
+spl_autoload_unregister(array($__autoloader, 'loadClass'));
+$__autoloader = new Horde_Autoloader_Cache();
+$__autoloader->registerAutoloader();
