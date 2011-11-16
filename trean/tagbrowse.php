cbcebeb3292b57627683c06bbd9defb1e9c236c1
Michael J Rubinsky <mrubinsk@horde.org>
2011-11-16 16:41:34 -0500

    remove old test code

--- trean/tagbrowse.php
+++ trean/tagbrowse.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Tag browsing.
+ *
  * Copyright 2011 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (BSD). If you did not
@@ -19,12 +21,4 @@ $notification->notify(array('listeners' => 'status'));
 $v = new Trean_View_Browse();
 $v->render();
 
-// $b = new Trean_TagBrowse($injector->getInstance('Trean_Tagger'));
-// $b->clearSearch();
-// $b->addTag('personal');
-// $results = $b->getSlice(0, 5);
-// var_dump($results);
-
-
 require_once $registry->get('templates', 'horde') . '/common-footer.inc';
-
