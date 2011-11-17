87ae158edbc580c2389c9ea18eada1f11689c6f7
Michael M Slusarz <slusarz@horde.org>
2011-11-17 12:58:34 -0700

    opts.background is no longer used

--- imp/js/dimpbase.js
+++ imp/js/dimpbase.js
@@ -380,35 +380,34 @@ var DimpBase = {
         }
     },
 
-    loadMailbox: function(f, opts)
+    loadMailbox: function(f)
     {
         var is_search, need_delete;
-        opts = opts || {};
 
         if (!this.viewport) {
             this._createViewPort();
         }
 
-        if (!opts.background) {
-            this.resetSelected();
-            this.quicksearchClear(true);
+        this.resetSelected();
+        this.quicksearchClear(true);
 
-            if (this.view != f) {
-                $('folderName').update(DIMP.text.loading);
-                $('msgHeader').update();
-                this.viewswitch = true;
+        if (this.view != f) {
+            $('folderName').update(DIMP.text.loading);
+            $('msgHeader').update();
+            this.viewswitch = true;
 
-                /* Don't cache results of search folders - since we will need
-                 * to grab new copy if we ever return to it. */
-                if (this.isSearch()) {
-                    need_delete = this.view;
-                }
-
-                this.view = f;
+            /* Don't cache results of search folders - since we will need to
+             * grab new copy if we ever return to it. */
+            if (this.isSearch()) {
+                need_delete = this.view;
             }
+
+            this.view = f;
         }
 
-        this.viewport.loadView(f, { search: (this.uid ? { uid: this.uid } : null), background: opts.background});
+        this.viewport.loadView(f, {
+            search: (this.uid ? { uid: this.uid } : null)
+        });
 
         if (need_delete) {
             this.viewport.deleteView(need_delete);
