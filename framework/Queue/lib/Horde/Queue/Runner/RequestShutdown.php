e6f9a2b401229e7bd7cf0314a7fa57862718dd26
Michael J Rubinsky <mrubinsk@horde.org>
2011-11-17 15:43:11 -0500

    Register a shutdown_function, don't rely on __destruct being called.

--- framework/Queue/lib/Horde/Queue/Runner/RequestShutdown.php
+++ framework/Queue/lib/Horde/Queue/Runner/RequestShutdown.php
@@ -1,7 +1,14 @@
 <?php
 class Horde_Queue_Runner_RequestShutdown extends Horde_Queue_Runner
 {
-    public function __destruct()
+
+    public function __construct(Horde_Queue_Storage $storage)
+    {
+        parent::__construct($storage);
+        register_shutdown_function(array($this, 'shutdown'));
+    }
+
+    public function shutdown()
     {
         foreach ($this->_storage->getMany() as $task) {
             $task->run();
