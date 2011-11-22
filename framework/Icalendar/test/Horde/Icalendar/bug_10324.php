65fc7fa1d90231565d6e1830fb0ba0cb88b0ef38
Jan Schneider <jan@horde.org>
2011-11-22 15:40:17 +0100

    Remove accidentally added files.

--- framework/Icalendar/test/Horde/Icalendar/bug_10324.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once dirname(__FILE__) . '/common.php';
-$ical = new Horde_Icalendar();
-$ical->parseVCalendar(file_get_contents(dirname(__FILE__) . '/fixtures/bug10324.ics'));
-                               var_dump($ical);
