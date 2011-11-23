a824139040785da60aa2b841eb65669b60d54dfe
Michael M Slusarz <slusarz@horde.org>
2011-11-23 00:08:51 -0700

    phpdoc

--- framework/Text_Filter/lib/Horde/Text/Filter/Linkurls.php
+++ framework/Text_Filter/lib/Horde/Text/Filter/Linkurls.php
@@ -5,31 +5,30 @@
  * http://daringfireball.net/2010/07/improved_regex_for_matching_urls
  *
  * Changes:
- *   - Require at least one slash after the protocol. Horde's other filters don't
- *     expect us to match mailto: as part of these filters, so don't.
+ *   - Require at least one slash after the protocol. Horde's other filters
+ *     don't expect us to match mailto: as part of these filters, so don't.
  *   - Limit the URL protocol to 20 characters to avoid PCRE problems.
  *   - Allow "+" characters in URL protocols (like svn+ssh://).
  *
  * Parameters:
- * <pre>
- * callback - (string) A callback function that the URL is passed through
- *            before being set as the href attribute.  Must be a string with
- *            the function name, the function must take the original URL as
- *            the first and only parameter.
- *            DEFAULT: No callback
- * class - (string) The CSS class of the generated links.
- *         DEFAULT: none
- * encode - (boolean)  Whether to escape special HTML characters in the URLs
- *          and finally "encode" the complete tag so that it can be decoded
- *          later with the decode() method. This is useful if you want to
- *          run htmlspecialchars() or similar *after* using this filter.
- *          DEFAULT: false
- * nofollow - (boolean) Whether to set the 'rel="nofollow"' attribute on
- *            links.
- *            DEFAULT: false
- * target - (string) The link target.
- *          DEFAULT: '_blank'
- * </pre>
+ *   - callback: (string) A callback function that the URL is passed through
+ *               before being set as the href attribute.  Must be a string
+ *               with the function name, the function must take the original
+ *               URL as the first and only parameter.
+ *               DEFAULT: No callback
+ *   - class: (string) The CSS class of the generated links.
+ *            DEFAULT: none
+ *   - encode: (boolean)  Whether to escape special HTML characters in the
+ *             URLs and finally "encode" the complete tag so that it can be
+ *             decoded later with the decode() method. This is useful if you
+ *             want to run htmlspecialchars() or similar *after* using this
+ *             filter.
+ *             DEFAULT: false
+ *   - nofollow: (boolean) Whether to set the 'rel="nofollow"' attribute on
+ *               links.
+ *               DEFAULT: false
+ *   - target: (string) The link target.
+ *             DEFAULT: '_blank'
  *
  * Copyright 2003-2011 Horde LLC (http://www.horde.org/)
  *
@@ -87,21 +86,23 @@ class Horde_Text_Filter_Linkurls extends Horde_Text_Filter_Base
 (?:\b|^)
 (  # Capture 1: entire matched URL
   (
-   (?:[a-z][\w-+]{0,19})?:/{1,3}  # URL protocol and colon followed by 1-3 slashes, or just colon and slashes (://)
-    |                             #   or
+   (?:[a-z][\w-+]{0,19})?:/{1,3}  # URL protocol and colon followed by 1-3
+                                  # slashes, or just colon and slashes (://)
+    |                             #  - or -
     www\d{0,3}[.]                 # "www.", "www1.", "www2." … "www999."
-    |                             #   or
+    |                             #  - or -
     [a-z0-9.\-]+[.][a-z]{2,4}/    # looks like domain name followed by a slash
   )
   (?:                           # One or more:
     [^\s()<>\[\]]+                      # Run of non-space, non-()<>
-    |                                   #   or
+    |                                   #  - or -
     \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
   )+
   (?:                           # End with:
     \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
-    |                                   #   or
-    [^\s`!()\[\]{};:\'".,<>?«»“”‘’]     # not a space or one of these punct chars
+    |                                   #  - or -
+    [^\s`!()\[\]{};:\'".,<>?«»“”‘’]     # not a space or one of these punct
+                                        # chars
   )
 )
 END_OF_REGEX;
