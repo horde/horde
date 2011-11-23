f2cea2dad967b03f3f27b33226fe25d6d74cb767
Michael M Slusarz <slusarz@horde.org>
2011-11-23 04:08:59 -0700

    Fix critical issues with Autoload Caching.
    First, we should only be writing cache data if the cache array has
    changed; otherwise, we are creating a 0% hit rate on the cache entry.
    
    Internally Cache temp file and extension_loaded() values - the latter
    is an especially expensive call (see CODING_STANDARDS).
    
    Let file_[get|put]_contents() do all the work regarding write
    permissions - no need to explicitly check.
    
    Fix critical logic error in mapToPath() - we can't use isset() because
    we may be looking null values (e.g. class names that don't map to a
    file). Need to use array_key_exists() instead.

--- framework/Autoloader_Cache/lib/Horde/Autoloader/Cache.php
+++ framework/Autoloader_Cache/lib/Horde/Autoloader/Cache.php
@@ -10,64 +10,116 @@ require_once 'Horde/Autoloader/Default.php';
 
 class Horde_Autoloader_Cache extends Horde_Autoloader_Default
 {
+    /* Cache key name. */
+    const C_KEY = 'horde_autoloader_cache';
+
+    /* Cache types. */
+    const APC = 1;
+    const XCACHE = 2;
+    const EACCELERATOR = 3;
+    const TEMPFILE = 4;
+
     /**
      * Map of all classes already looked up.
      *
      * @var array
      */
-    protected $_cache;
+    protected $_cache = array();
+
+    /**
+     * THe cache type.
+     *
+     * @var array
+     */
+    protected $_cachetype;
+
+    /**
+     * Has the cache changed since the last save?
+     *
+     * @var boolean
+     */
+    protected $_changed = false;
+
+    /**
+     * Cached value of the temporary directory.
+     *
+     * @var string
+     */
+    protected $_tempdir;
 
     /**
      * Constructor.
      *
-     * Tries all supported cache backends and tries to retrieved the
-     * cached class map.
+     * Tries all supported cache backends and tries to retrieved the cached
+     * class map.
      */
     public function __construct()
     {
         parent::__construct();
+
         if (extension_loaded('apc')) {
-            $this->_cache = apc_fetch('horde_autoloader_cache');
+            $this->_cache = apc_fetch(self::C_KEY);
+            $this->_cachetype = self::APC;
         } elseif (extension_loaded('xcache')) {
-            $this->_cache = xcache_get('horde_autoloader_cache');
+            $this->_cache = xcache_get(self::C_KEY);
+            $this->_cachetype = self::XCACHE;
         } elseif (extension_loaded('eaccelerator')) {
-            $this->_cache = eaccelerator_get('horde_autoloader_cache');
-        } elseif (($tempdir = sys_get_temp_dir()) &&
-                  is_readable($tempdir . '/horde_autoloader_cache')) {
-            $this->_cache = @json_decode(file_get_contents($tempdir . '/horde_autoloader_cache'), true);
+            $this->_cache = eaccelerator_get(self::C_KEY);
+            $this->_cachetype = self::EACCELERATOR;
+        } elseif (($this->_tempdir = sys_get_temp_dir()) &&
+                  is_readable($this->_tempdir)) {
+            if (($data = file_get_contents($this->_tempdir . '/' . self::C_KEY)) !== false) {
+                $this->_cache = @json_decode($data, true);
+            }
+            $this->_cachetype = self::TEMPFILE;
         }
     }
 
     /**
      * Destructor.
      *
-     * Tries all supported cache backends and tries to save the class
-     * map to the cache.
+     * Tries all supported cache backends and tries to save the class map to
+     * the cache.
      */
     public function __destruct()
     {
-        if (extension_loaded('apc')) {
-            apc_store('horde_autoloader_cache', $this->_cache);
-        } elseif (extension_loaded('xcache')) {
-            xcache_set('horde_autoloader_cache', $this->_cache);
-        } elseif (extension_loaded('eaccelerator')) {
-            eaccelerator_put('horde_autoloader_cache', $this->_cache);
-        } elseif (($tempdir = sys_get_temp_dir()) &&
-                  (is_writable($tempdir . '/horde_autoloader_cache') ||
-                   (!file_exists($tempdir . '/horde_autoloader_cache') &&
-                    is_writable($tempdir)))) {
-            file_put_contents($tempdir . '/horde_autoloader_cache', json_encode($this->_cache));
+        if (!$this->_changed) {
+            return;
+        }
+
+        switch ($this->_cachetype) {
+        case self::APC:
+            apc_store(self::C_KEY, $this->_cache);
+            break;
+
+        case self::XCACHE:
+            xcache_set(self::C_KEY, $this->_cache);
+            break;
+
+        case self::EACCELERATOR:
+            eaccelerator_put(self::C_KEY, $this->_cache);
+            break;
+
+        case self::TEMPFILE:
+            file_put_contents($this->_tempdir . '/' . self::C_KEY, json_encode($this->_cache));
+            break;
         }
     }
 
     /**
      * Search registered mappers in LIFO order.
+     *
+     * @param string $className  TODO.
+     *
+     * @return string  TODO
      */
     public function mapToPath($className)
     {
-        if (!isset($this->_cache[$className])) {
+        if (!array_key_exists($className, $this->_cache)) {
             $this->_cache[$className] = parent::mapToPath($className);
+            $this->_changed = true;
         }
+
         return $this->_cache[$className];
     }
 }
