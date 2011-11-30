6f435e07cd2a1419a35c1de8f73fa81cc3d22b9c
Michael M Slusarz <slusarz@horde.org>
2011-11-30 15:21:56 -0700

    Fix regressions in search mailboxes due to Ticket #9773
    Move UIDVALIDITY check before empty mailbox check, since a UIDVALIDITY
    failure should cause all viewport information to be reset, regardless of
    whether the mailbox contains messages.
    
    UIDVALIDITY will also fail when the day changes, so metadata creation
    needs to appear below this line since metadata will be reset in this
    instance.

--- imp/lib/Mailbox.php
+++ imp/lib/Mailbox.php
@@ -1467,7 +1467,7 @@ class IMP_Mailbox implements Serializable
             : '';
 
         if ($this->search) {
-            return '1' . $date;
+            return '1' . ($date ? '|' . $date : '');
         }
 
         $sortpref = $this->getSort(true);
