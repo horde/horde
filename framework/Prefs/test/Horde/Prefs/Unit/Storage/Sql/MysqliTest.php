414b795f6a5e4365d2911d10b1391b0859c42d31
Jan Schneider <jan@horde.org>
2011-12-06 21:38:24 +0100

    Add basic SQL tests.

---
+++ framework/Prefs/test/Horde/Prefs/Unit/Storage/Sql/MysqliTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/Base.php';
+
+/**
+ * @author     Jan Schneider <jan@horde.org>
+ * @category   Horde
+ * @package    Prefs
+ * @subpackage UnitTests
+ * @copyright  2011 Horde LLC (http://www.horde.org/)
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class Horde_Prefs_Unit_Storage_Sql_MysqliTest extends Horde_Prefs_Test_Sql_Base
+{
+    public static function setUpBeforeClass()
+    {
+        if (!extension_loaded('mysqli')) {
+            self::$reason = 'No mysqli extension';
+            return;
+        }
+        $config = self::getConfig('PREFS_SQL_MYSQLI_TEST_CONFIG',
+                                  dirname(__FILE__) . '/../../..');
+        if ($config && !empty($config['prefs']['sql']['mysqli'])) {
+            self::$db = new Horde_Db_Adapter_Mysqli($config['prefs']['sql']['mysqli']);
+            parent::setUpBeforeClass();
+        }
+    }
+}
