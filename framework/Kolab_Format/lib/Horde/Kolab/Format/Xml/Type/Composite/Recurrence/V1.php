d3f7bd8cbf9d48f42322231ccdb620e71495e60c
Gunnar Wrobel <p@rdus.de>
2011-12-11 15:47:25 +0100

    [gwr] Completed missing data API v1 support.

---
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Type/Composite/Recurrence/V1.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Handles  api version 1 of recurrence data.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/libraries/Horde_Kolab_Format
+ */
+
+/**
+ * Handles  api version 1 of recurrence data.
+ *
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you did not
+ * receive this file, see
+ * http://www.horde.org/licenses/lgpl21.
+ *
+ * @since Horde_Kolab_Format 1.1.0
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/libraries/Horde_Kolab_Format
+ */
+class Horde_Kolab_Format_Xml_Type_Composite_Recurrence_V1
+extends Horde_Kolab_Format_Xml_Type_Composite_Recurrence
+{
+    protected $elements = array(
+        'interval'  => 'Horde_Kolab_Format_Xml_Type_RecurrenceInterval',
+        'day'       => 'Horde_Kolab_Format_Xml_Type_Multiple_String',
+        'daynumber' => 'Horde_Kolab_Format_Xml_Type_Integer',
+        'month'     => 'Horde_Kolab_Format_Xml_Type_String_MaybeMissing',
+        'range'     => 'Horde_Kolab_Format_Xml_Type_RecurrenceRange',
+        'exclusion' => 'Horde_Kolab_Format_Xml_Type_Multiple_String',
+        'complete'  => 'Horde_Kolab_Format_Xml_Type_Multiple_String',
+    );
+}
---
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Type/Date/V1.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Handles api version 1 of date attributes.
+ *
+ * PHP version 5
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/libraries/Horde_Kolab_Format
+ */
+
+/**
+ * Handles api version 1 of date attributes.
+ *
+ * Copyright 2011 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you did not
+ * receive this file, see
+ * http://www.horde.org/licenses/lgpl21.
+ *
+ * @since Horde_Kolab_Format 1.1.0
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/libraries/Horde_Kolab_Format
+ */
+class Horde_Kolab_Format_Xml_Type_Date_V1
+extends Horde_Kolab_Format_Xml_Type_String
+{
+    /**
+     * Load the value of a node.
+     *
+     * @param DOMNode                       $node   Retrieve value for this node.
+     * @param Horde_Kolab_Format_Xml_Helper $helper A XML helper instance.
+     * @param array                         $params Additiona parameters for
+     *                                              this parse operation.
+     *
+     * @return mixed|null The value or null if no value was found.
+     */
+    public function loadNodeValue(
+        $node,
+        Horde_Kolab_Format_Xml_Helper $helper,
+        $params = array()
+    )
+    {
+        return Horde_Kolab_Format_Date::decodeDate(
+            $helper->fetchNodeValue($node)
+        );
+    }
+
+    /**
+     * Update the specified attribute.
+     *
+     * @param string                        $name        The name of the attribute
+     *                                                   to be updated.
+     * @param mixed                         $value       The value to store.
+     * @param DOMNode                       $parent_node The parent node of the
+     *                                                   node that should be
+     *                                                   updated.
+     * @param Horde_Kolab_Format_Xml_Helper $helper      A XML helper instance.
+     * @param array                         $params      The parameters for this
+     *                                                   write operation.
+     * @param DOMNode|NULL                  $old_node    The previous value (or
+     *                                                   null if there is none).
+     *
+     * @return DOMNode|boolean The new/updated child node or false if this
+     *                         failed.
+     *
+     * @throws Horde_Kolab_Format_Exception If converting the data to XML failed.
+     */
+    public function saveNodeValue(
+        $name,
+        $value,
+        $parent_node,
+        Horde_Kolab_Format_Xml_Helper $helper,
+        $params = array(),
+        $old_node = false
+    )
+    {
+        $date = Horde_Kolab_Format_Date::encodeDate($value);
+        $node = parent::saveNodeValue(
+            $name, $date, $parent_node, $helper, $params, $old_node
+        );
+        return $node;
+    }
+}
