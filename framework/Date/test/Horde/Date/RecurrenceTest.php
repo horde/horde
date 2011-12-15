3fa2bdadfcc372902eeaec15d7524f10b124cc77
Michael J Rubinsky <mrubinsk@horde.org>
2011-12-15 17:12:31 -0500

    Add test for creating Horde_Date_Recurrence object in a different timezone than the date_default_timezone_get timezone.

--- framework/Date/test/Horde/Date/RecurrenceTest.php
+++ framework/Date/test/Horde/Date/RecurrenceTest.php
@@ -1007,7 +1007,23 @@ class Horde_Date_RecurrenceTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Horde_Date', $next);
         $this->assertEquals($next->mday, 9);
         date_default_timezone_set('Europe/Berlin');
+    }
 
+    public function testRecurrenceObjectWithNonDefaultTimezones()
+    {
+        // date_default_timezone_get is Europe/Berlin
+        $date = new Horde_Date('2011-10-01T15:00:00', 'America/New_York');
+        $rrule = new Horde_Date_Recurrence($date);
+        $rrule->setRecurType(Horde_Date_Recurrence::RECUR_WEEKLY);
+        $rrule->setRecurOnDay(Horde_Date::MASK_SATURDAY);
+        $rrule->setRecurInterval(2);
+
+        $after = new Horde_Date('2011-10-16');
+        $next = $rrule->nextRecurrence($after);
+
+        $this->assertInstanceOf('Horde_Date', $next);
+        $this->assertEquals('2011-10-29 15:00:00', (string)$next);
+        $this->assertEquals('America/New_York', $next->timezone);
     }
 
 }
