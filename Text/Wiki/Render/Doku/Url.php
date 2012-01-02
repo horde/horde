16cc24f53f9662b651014352a53bf0f396161476
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-01-02 16:14:34 +1030

    Prevent E_NOTICE

--- Text/Wiki/Render/Doku/Url.php
+++ Text/Wiki/Render/Doku/Url.php
@@ -18,22 +18,30 @@ class Text_Wiki_Render_Doku_Url extends Text_Wiki_Render {
 
     function token($options)
     {
+        $page_text_options = false;
+        if (isset($options['page']) && isset($options['text'])) {
+            $page_text_options = $options['page'] == $options['text'];
+        } elseif (empty($options['page']) && empty($options['text'])) {
+            $page_text_options = true;
+        }
+
+
         if ($options['type'] == 'start') {
-            if (! strlen($options['text']) || $options['page'] == $options['text']) {
+            if (!strlen($options['text']) || $page_text_options) {
                 return $options['href'];
             } else {
                 return '[['.$options['href'].'|';
             }
         }
         else if ($options['type'] == 'end') {
-            if (! strlen($options['text']) || $options['page'] == $options['text']) {
+            if (! strlen($options['text']) || $page_text_options) {
                 return '';
             } else {
                 return ']]';
             }
         }
         else {
-            if (! strlen($options['text']) || $options['page'] == $options['text']) {
+            if (! strlen($options['text']) || $page_text_options) {
                 return $options['href'];
             } else {
                 return '[['.$options['href'].'|'.$options['text'].']]';
