180ac13176f9d665cfceb6966ff29d6afd94695b
Michael J Rubinsky <mrubinsk@horde.org>
2012-01-12 14:07:55 -0500

    Reset Repeat tab's fields properly.
    Allows resetting the state of these fields, while still honoring the
    intent of commit 6f030e689ba69f95c675c52114ffb267ef79ac1c

--- kronolith/js/kronolith.js
+++ kronolith/js/kronolith.js
@@ -5855,10 +5855,12 @@ KronolithCore = {
                 $('kronolithEventRecurCount').disable();
             }
         } else {
-            if (!$('kronolithEventRepeatType').visible()) {
-                $('kronolithEventTabRecur').select('div').invoke('hide');
-                $('kronolithEventRepeatType').show();
-            }
+            $('kronolithEventTabRecur').select('div').each(function(e) {
+                if (e.identify != 'kronolithEventRepeatType') {
+                    e.hide();
+                }
+            });
+            $('kronolithEventRepeatType').show();
         }
     },
 
