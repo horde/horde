1b56dc8d4ebfe7702b835996007225b731345e79
Ralf Lang <lang@b1-systems.de>
2012-01-16 10:20:34 +0000

    moved this inside the class - no need for a global function here

--- framework/Rdo/test/Horde/Rdo/Sql/Base.php
+++ framework/Rdo/test/Horde/Rdo/Sql/Base.php
@@ -20,10 +20,40 @@ class Horde_Rdo_Test_Sql_Base extends Horde_Rdo_Test_Base
     protected static $LazyBaseObjectMapper;
     protected static $RelatedThingMapper;
 
+    protected static function _migrate_sql_rdo($db)
+    {
+        $migration = new Horde_Db_Migration_Base($db);
+
+        /* Cleanup potential left-overs. */
+        try {
+            $migration->dropTable('test_someeagerbaseobjects');
+            $migration->dropTable('test_somelazybaseobjects');
+            $migration->dropTable('test_relatedthings');
+        } catch (Horde_Db_Exception $e) {
+        }
+
+        $t = $migration->createTable('test_someeagerbaseobjects', array('autoincrementKey' => 'baseobject_id'));
+        $t->column('relatedthing_id', 'integer');
+        $t->column('atextproperty', 'string');
+        $t->end();
+
+        $t = $migration->createTable('test_somelazybaseobjects', array('autoincrementKey' => 'baseobject_id'));
+        $t->column('relatedthing_id', 'integer');
+        $t->column('atextproperty', 'string');
+        $t->end();
+
+        $t = $migration->createTable('test_relatedthings', array('autoincrementKey' => 'relatedthing_id'));
+        $t->column('relatedthing_textproperty', 'string', array('limit' => 255, 'null' => false));
+        $t->column('relatedthing_intproperty', 'integer', array('null' => false));
+        $t->end();
+
+        $migration->migrate('up');
+    }
+
+
     public static function setUpBeforeClass()
     {
-        require_once dirname(__FILE__) . '/../migration/sql.php';
-        migrate_sql_rdo(self::$db);
+        self::_migrate_sql_rdo(self::$db);
         // read sql file for statements
         $statements = array();
         $current_stmt = '';
