cd92782a26b70be81dd6f3bc9c84e8a511e888f3
Michael M Slusarz <slusarz@horde.org>
2012-01-17 19:05:58 -0700

    Fix path

--- framework/Imap_Client/lib/Horde/Imap/Client/Translation.php
+++ framework/Imap_Client/lib/Horde/Imap/Client/Translation.php
@@ -32,7 +32,7 @@ class Horde_Imap_Client_Translation extends Horde_Translation
     static public function t($message)
     {
         self::$_domain = 'Horde_Imap_Client';
-        self::$_directory = '@data_dir@' == '@'.'data_dir'.'@' ? dirname(__FILE__) . '/../../../locale' : '@data_dir@/Horde_Imap_Client/locale';
+        self::$_directory = '@data_dir@' == '@'.'data_dir'.'@' ? dirname(__FILE__) . '/../../../../locale' : '@data_dir@/Horde_Imap_Client/locale';
         return parent::t($message);
     }
 
@@ -49,7 +49,7 @@ class Horde_Imap_Client_Translation extends Horde_Translation
     static public function ngettext($singular, $plural, $number)
     {
         self::$_domain = 'Horde_Imap_Client';
-        self::$_directory = '@data_dir@' == '@'.'data_dir'.'@' ? dirname(__FILE__) . '/../../../locale' : '@data_dir@/Horde_Imap_Client/locale';
+        self::$_directory = '@data_dir@' == '@'.'data_dir'.'@' ? dirname(__FILE__) . '/../../../../locale' : '@data_dir@/Horde_Imap_Client/locale';
         return parent::ngettext($singular, $plural, $number);
     }
 }
