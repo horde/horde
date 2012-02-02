117a076cbadb963e422d3e05f111c54dae5a9723
Michael M Slusarz <slusarz@horde.org>
2012-02-01 18:13:47 -0700

    [mms] Fix regression where passphrase prompt would not appear for S/MIME encrypted messages.

--- imp/lib/Mime/Viewer/Smime.php
+++ imp/lib/Mime/Viewer/Smime.php
@@ -192,8 +192,8 @@ class IMP_Mime_Viewer_Smime extends Horde_Mime_Viewer_Base
 
         /* Make sure we have a passphrase. */
         $passphrase = $this->_impsmime->getPassphrase();
-        if (is_null($passphrase)) {
-            $imple = $GLOBALS['registry']->getInstance('Horde_Core_Factory_Imple')->create(array('imp', 'PassphraseDialog'), array(
+        if ($passphrase === false) {
+            $imple = $GLOBALS['injector']->getInstance('Horde_Core_Factory_Imple')->create(array('imp', 'PassphraseDialog'), array(
                 'type' => 'smimePersonal'
             ));
             $status->addText(Horde::link('#', '', '', '', '', '', '', array('id' => $imple->getPassphraseId())) . _("You must enter the passphrase for your S/MIME private key to view this data.") . '</a>');
