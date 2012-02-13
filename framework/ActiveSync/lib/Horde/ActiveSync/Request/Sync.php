144b2c17b53f1ecfec3136f197269fc79dd2b6bd
Michael J Rubinsky <mrubinsk@horde.org>
2012-02-13 16:25:58 -0500

    Pass clientid to the importer

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/Sync.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/Sync.php
@@ -306,7 +306,7 @@ class Horde_ActiveSync_Request_Sync extends Horde_ActiveSync_Request_Base
                             // Currently, 'read' is only sent by the PDA when it
                             // is ONLY setting the read flag.
                             if (isset($appdata->read)) {
-                                $importer->importMessageReadFlag($serverid, $appdata->read);
+                                $importer->importMessageReadFlag($serverid, $appdata->read, $clientid);
                             } else {
                                 $importer->importMessageChange($serverid, $appdata, $this->_device, false);
                             }
