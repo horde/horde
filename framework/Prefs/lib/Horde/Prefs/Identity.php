ee151c63a2feef43396c16840e850fd42cbbcbcc
Michael M Slusarz <slusarz@horde.org>
2012-02-29 17:52:06 -0700

    Horde_Mail 2.0: Horde_Prefs

--- framework/Prefs/lib/Horde/Prefs/Identity.php
+++ framework/Prefs/lib/Horde/Prefs/Identity.php
@@ -329,8 +329,11 @@ class Horde_Prefs_Identity
         }
 
         try {
-            Horde_Mime_Address::parseAddressList($this->getValue($this->_prefnames['from_addr'], $identity), array('validate' => true));
-        } catch (Horde_Mime_Exception $e) {
+            $rfc822 = new Horde_Mail_Rfc822();
+            $rfc822->parseAddressList($this->getValue($this->_prefnames['from_addr'], $identity), array(
+                'validate' => true
+            ));
+        } catch (Horde_Mail_Exception $e) {
             throw new Horde_Prefs_Exception($e);
         }
     }
