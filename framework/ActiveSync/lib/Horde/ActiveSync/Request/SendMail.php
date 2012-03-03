bbbe46af563d6e09f561997d3a54b5eab0c5af60
Michael J Rubinsky <mrubinsk@horde.org>
2012-03-03 13:54:21 -0500

    Catch exception from the driver.

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/SendMail.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/SendMail.php
@@ -26,8 +26,12 @@ class Horde_ActiveSync_Request_SendMail extends Horde_ActiveSync_Request_Base
         // All that happens here is that we receive an rfc822 message on stdin
         // and just forward it to the backend. We provide no output except for
         // an OK http reply
-        $rfc822 = file_get_contents('php://input');//$this->_request->getBody();
-
-        return $this->_driver->sendMail($rfc822);
+        $rfc822 = file_get_contents('php://input');
+        try {
+            return $this->_driver->sendMail($rfc822, false, false, false, true);
+        } catch (Horde_Exception $e) {
+            $this->_logger->err($e->getMessage());
+            throw new Horde_ActiveSync_Exception_InvalidRequest($e->getMessage());
+        }
     }
 }
