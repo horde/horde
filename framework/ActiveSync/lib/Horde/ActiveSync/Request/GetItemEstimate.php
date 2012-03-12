4d8a55b9bfdeb520b04869db6c204b58e5650578
Michael J Rubinsky <mrubinsk@horde.org>
2012-03-12 19:05:38 -0400

    format

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/GetItemEstimate.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/GetItemEstimate.php
@@ -4,7 +4,7 @@
  *
  * Copyright 2009-2012 Horde LLC (http://www.horde.org/)
  *
- * @author Michael J. Rubinsky <mrubinsk@horde.org>
+ * @author Michael J Rubinsky <mrubinsk@horde.org>
  * @package ActiveSync
  */
 /**
@@ -15,22 +15,22 @@
 class Horde_ActiveSync_Request_GetItemEstimate extends Horde_ActiveSync_Request_Base
 {
     /** Status Codes **/
-    const STATUS_SUCCESS = 1;
+    const STATUS_SUCCESS    = 1;
     const STATUS_INVALIDCOL = 2;
-    const STATUS_NOTPRIMED = 3;
-    const STATUS_KEYMISM = 4;
+    const STATUS_NOTPRIMED  = 3;
+    const STATUS_KEYMISM    = 4;
 
     /* Request tag constants */
     const GETITEMESTIMATE = 'GetItemEstimate:GetItemEstimate';
-    const VERSION = 'GetItemEstimate:Version';
-    const FOLDERS = 'GetItemEstimate:Folders';
-    const FOLDER = 'GetItemEstimate:Folder';
-    const FOLDERTYPE = 'GetItemEstimate:FolderType';
-    const FOLDERID = 'GetItemEstimate:FolderId';
-    const DATETIME = 'GetItemEstimate:DateTime';
-    const ESTIMATE = 'GetItemEstimate:Estimate';
-    const RESPONSE = 'GetItemEstimate:Response';
-    const STATUS = 'GetItemEstimate:Status';
+    const VERSION         = 'GetItemEstimate:Version';
+    const FOLDERS         = 'GetItemEstimate:Folders';
+    const FOLDER          = 'GetItemEstimate:Folder';
+    const FOLDERTYPE      = 'GetItemEstimate:FolderType';
+    const FOLDERID        = 'GetItemEstimate:FolderId';
+    const DATETIME        = 'GetItemEstimate:DateTime';
+    const ESTIMATE        = 'GetItemEstimate:Estimate';
+    const RESPONSE        = 'GetItemEstimate:Response';
+    const STATUS          = 'GetItemEstimate:Status';
 
     /**
      * Handle the request
