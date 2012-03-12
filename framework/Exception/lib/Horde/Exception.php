3fbe29d0ce4a5b0a8d6e21368601ed96c6e184ab
Gunnar Wrobel <p@rdus.de>
2012-03-12 16:27:08 +0100

    Move to PHP >= 5.3.0

--- framework/Exception/lib/Horde/Exception.php
+++ framework/Exception/lib/Horde/Exception.php
@@ -1,104 +1,30 @@
 <?php
-if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+/**
+ * Horde base exception class.
+ *
+ * Copyright 2008-2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Horde
+ * @package  Exception
+ */
+class Horde_Exception extends Exception
+{
     /**
-     * Horde base exception class that supports prior exception for PHP < 5.3.0
+     * Error details that should not be part of the main exception message,
+     * e.g. any additional debugging information.
      *
-     * Originates from
-     * http://framework.zend.com/wiki/display/ZFPROP/previous+Exception+on+Zend_Exception+-+Marc+Bennewitz
-     *
-     * Copyright 2008-2012 Horde LLC (http://www.horde.org/)
-     *
-     * See the enclosed file COPYING for license information (LGPL). If you
-     * did not receive this file, see http://www.horde.org/licenses/lgpl21.
-     *
-     * @category Horde
-     * @package  Exception
+     * @var string
      */
-    class Horde_Exception extends Exception
-    {
-        private $_previous = null;
+    public $details;
 
-        /**
-         * Error details that should not be part of the main exception message,
-         * e.g. any additional debugging information.
-         *
-         * @var string
-         */
-        public $details;
-
-        /**
-         * Has this exception been logged?
-         *
-         * @var boolean
-         */
-        public $logged = false;
-
-        /**
-         * Construct the exception
-         *
-         * @param string $msg
-         * @param int $code
-         * @param Exception $previous
-         */
-        public function __construct($msg = '', $code = 0, Exception $previous = null)
-        {
-            parent::__construct($msg, $code);
-            $this->_previous = $previous;
-        }
-
-        /**
-         * Returns previous Exception
-         *
-         * @return Exception|null
-         */
-        final public function getPrevious()
-        {
-            return $this->_previous;
-        }
-
-        /**
-         * String representation of the exception
-         *
-         * @return string
-         */
-        public function __toString()
-        {
-            if ($this->getPrevious()) {
-                return $this->getPrevious()->__toString() . "\n\nNext " . parent::__toString();
-            } else {
-                return parent::__toString();
-            }
-        }
-
-    }
-} else {
     /**
-     * Horde base exception class.
+     * Has this exception been logged?
      *
-     * Copyright 2008-2012 Horde LLC (http://www.horde.org/)
-     *
-     * See the enclosed file COPYING for license information (LGPL). If you
-     * did not receive this file, see http://www.horde.org/licenses/lgpl21.
-     *
-     * @category Horde
-     * @package  Exception
+     * @var boolean
      */
-    class Horde_Exception extends Exception
-    {
-        /**
-         * Error details that should not be part of the main exception message,
-         * e.g. any additional debugging information.
-         *
-         * @var string
-         */
-        public $details;
+    public $logged = false;
 
-        /**
-         * Has this exception been logged?
-         *
-         * @var boolean
-         */
-        public $logged = false;
-
-    }
 }
