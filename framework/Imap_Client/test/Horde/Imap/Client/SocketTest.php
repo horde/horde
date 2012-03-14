ff8bb7a349cbeeaf88ae6b2ff54da815bf3d9680
Jan Schneider <jan@horde.org>
2012-03-14 10:31:20 +0100

    Empty parameters are filtered out.

--- framework/Imap_Client/test/Horde/Imap/Client/SocketTest.php
+++ framework/Imap_Client/test/Horde/Imap/Client/SocketTest.php
@@ -37,8 +37,8 @@ class Horde_Imap_Client_SocketTest extends PHPUnit_Framework_TestCase
         // Test file is base64 encoded to obfuscate the data.
         $fetch_data = base64_decode(file_get_contents(dirname(__FILE__) . '/fixtures/bug_10503.txt'));
         $imap_test_ob = new Horde_Imap_Client_Stub_Socket(array(
-            'password' => '',
-            'username' => ''
+            'password' => 'stub',
+            'username' => 'stub'
         ));
 
         $sorted = $imap_test_ob->getClientSort(
