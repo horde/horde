f9ddfebfcc0d351a1fe12c787b8618819e78f9be
Jan Schneider <jan@horde.org>
2012-03-26 21:22:26 +0200

    Horde_View clean, tweak output.

--- trean/templates/list.html.php
+++ trean/templates/list.html.php
@@ -28,28 +28,27 @@ function doPrefsUpdate(column, sortDown)
  </tr>
 </thead>
 <tbody id="BookmarkList-body">
-<?php
-// List bookmarks.
-foreach ($this->bookmarks as $bookmark) {
-    $bookmark_url = Horde_Util::addParameter($this->redirectUrl, 'b', $bookmark->id);
-    if ($bookmark->http_status == 'error') {
-        $status = 'error.png';
-    } elseif ($bookmark->http_status == '') {
-        $status = '';
-    } else {
-        $status = (int)substr($bookmark->http_status, 0, 1) . 'xx.png';
-    }
-?>
+ <?php foreach ($this->bookmarks as $bookmark): ?>
  <tr>
   <td>
    <div class="bl-title">
-    <?php echo Horde::img(Trean::getFavicon($bookmark), '', 'class="favicon"', '') ?>
-    <?php if ($status) echo Horde::img('http/' . $status) ?>
-    <?php echo Horde::link($bookmark_url, '', '', $this->target) . htmlspecialchars($bookmark->title ? $bookmark->title : $bookmark->url) ?></a>
-    <small> &#8230; <?php echo htmlspecialchars($bookmark->url) . ' &#8230; ' . htmlspecialchars(Horde_String::substr($bookmark->description, 0, 200)) ?></small>
+    <?php echo Horde::img(Trean::getFavicon($bookmark), '', array('class' => 'favicon')) ?>
+    <?php if ($bookmark->http_status == 'error'): ?>
+    <?php echo Horde::img('http/error.png') ?>
+    <?php elseif ($bookmark->http_status): ?>
+    <?php echo Horde::img('http/' . (int)substr($bookmark->http_status, 0, 1) . 'xx.png') ?>
+    <?php endif; ?>
+    <?php echo $this->redirectUrl->add('b', $bookmark->id)->link(array('target' => $this->target)) . $this->h($bookmark->title ? $bookmark->title : $bookmark->url) ?></a>
+    <small>
+      <?php echo $this->h($bookmark->url) ?>
+      <?php if (strlen($bookmark->description)): ?>
+      &mdash;
+      <?php echo $this->h(Horde_String::truncate($bookmark->description, 200)) ?>
+      <?php endif ?>
+    </small>
     <ul class="horde-tags">
      <?php foreach ($bookmark->tags as $tag): ?>
-     <li><a href="<?php echo Horde::selfUrl()->add('tag', $tag) ?>"><?php echo htmlspecialchars($tag) ?></a></li>
+     <li><a href="<?php echo Horde::selfUrl()->add('tag', $tag) ?>"><?php echo $this->h($tag) ?></a></li>
      <?php endforeach ?>
     </ul>
    </div>
@@ -58,15 +57,15 @@ foreach ($this->bookmarks as $bookmark) {
    <?php echo $bookmark->clicks ?>
   </td>
   <td class="bl-actions">
-   <a href="<?php echo Horde::url('edit.php?bookmark=' . (int)$bookmark->id) ?>"><?php echo Horde::img('edit.png', _("Edit")) ?></a>
+   <a href="<?php echo Horde::url('edit.php')->add('bookmark', (int)$bookmark->id) ?>"><?php echo Horde::img('edit.png', _("Edit")) ?></a>
    <form class="bl-delete" action="<?php echo Horde::url('b/delete') ?>" method="post">
     <input type="hidden" name="bookmark" value="<?php echo (int)$bookmark->id ?>">
-    <input type="hidden" name="url" value="<?php echo htmlspecialchars(Horde::selfUrl()) ?>">
+    <input type="hidden" name="url" value="<?php echo $this->h(Horde::selfUrl()) ?>">
     <input type="submit" class="button" value="<?php echo _("Delete") ?>">
    </form>
   </td>
  </tr>
-<?php } ?>
+ <?php endforeach ?>
 </tbody>
 </table>
 </div>
