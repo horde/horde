85eb362388c59ff30ac4848d067c041e3cd6cebf
Michael M Slusarz <slusarz@horde.org>
2012-04-01 23:03:13 -0600

    This should be passed through the local add() now.

--- framework/Core/lib/Horde/PageOutput.php
+++ framework/Core/lib/Horde/PageOutput.php
@@ -343,7 +343,7 @@ class Horde_PageOutput
     public function addPopupJs()
     {
         if (!$this->hsf->isIncluded('popup.js', 'horde')) {
-            $this->hsf->add('popup.js', 'horde');
+            $this->add('popup.js', 'horde');
             $this->addInlineJsVars(array(
                 'Horde.popup_block_text' => Horde_Core_Translation::t("A popup window could not be opened. Your browser may be blocking popups.")
             ), true);
