4c4ca6b0af7a4f3ad60f8a0572ba5af7583c7221
Ralf Lang <lang@b1-systems.de>
2012-04-03 16:39:56 +0200

    PHPDoc for addRelationship

--- framework/Rdo/lib/Horde/Rdo/Query.php
+++ framework/Rdo/lib/Horde/Rdo/Query.php
@@ -233,6 +233,15 @@ class Horde_Rdo_Query
     }
 
     /**
+     * Add a relationship type to a query
+     * @param string $relationship  The name of the relationship as defined in the mapper
+     * @param array $args           The parameter array as defined in the mapper
+     *                              'mapper'     The mapper object of the result class
+     *                              'table'      Optional name of the table to use
+     *                              'tableAlias' Optional alias name for the base table
+     *                              'join_type'  Optional explicitly control the type of join
+     *                              'type'       The type of relation, any of the Constants in Horde_Rdo
+     * @return Horde_Rdo_Query  this
      */
     public function addRelationship($relationship, $args)
     {
