bc85ef272db02f8d44e51f0c08e97f0df19d054a
Ralf Lang <lang@b1-systems.de>
2012-04-03 15:07:53 +0200

    Remove phpdoc link to missing page

--- framework/Rdo/lib/Horde/Rdo/Factory.php
+++ framework/Rdo/lib/Horde/Rdo/Factory.php
@@ -8,7 +8,6 @@
  * @package  Test
  * @author   Ralf Lang <lang@b1-systems.de>
  * @license  http://www.horde.org/licenses/lgpl21 LGPL
- * @link     http://www.horde.org/components/Horde_Factory
  */
 
 
