d08653cce66defa3e6f6971af780dc3699080ad7
Michael M Slusarz <slusarz@horde.org>
2012-04-04 17:12:59 -0600

    MDN checking should be done on message mailbox, not view mailbox (which may be a search mailbox)

--- imp/message.php
+++ imp/message.php
@@ -651,7 +651,7 @@ if (!$readonly && $strip_atc) {
 }
 
 /* Do MDN processing now. */
-$mdntext = $imp_ui->MDNCheck(IMP::$mailbox, $uid, $mime_headers, $vars->mdn_confirm)
+$mdntext = $imp_ui->MDNCheck($mailbox, $uid, $mime_headers, $vars->mdn_confirm)
     ? strval(new IMP_Mime_Status(array(
         _("The sender of this message is requesting a notification from you when you have read this message."),
         sprintf(_("Click %s to send the notification message."), Horde::link(htmlspecialchars($selfURL->copy()->add('mdn_confirm', 1))) . _("HERE") . '</a>')
