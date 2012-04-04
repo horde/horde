3126b5b029526aba099d85dceaa5149ed4edf33b
Michael J Rubinsky <mrubinsk@horde.org>
2012-04-04 11:01:53 -0400

    Return the vacation rule's disable status

--- ingo/lib/Api.php
+++ ingo/lib/Api.php
@@ -193,9 +193,12 @@ class Ingo_Api extends Horde_Registry_Api
             ->create();
         $filters = $ingo_storage->retrieve(Ingo_Storage::ACTION_FILTERS);
         $vacation_rule_id = $filters->findRuleId(Ingo_Storage::ACTION_VACATION);
+        $rule = $filters->getRule($vacation_rule_id);
         $vacation = $ingo_storage->retrieve(Ingo_Storage::ACTION_VACATION);
+        $res = $vacation->toArray();
+        $res['disabled'] = $rule['disable'];
 
-        return $vacation->toArray();
+        return $res;
     }
 
     /**
