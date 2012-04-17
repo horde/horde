9e890a5dbb3e1a232c331cd5a04bea8c7f8203b6
Jan Schneider <jan@horde.org>
2012-04-17 14:51:03 +0200

    Alignment

--- kronolith/lib/Driver/Holidays.php
+++ kronolith/lib/Driver/Holidays.php
@@ -26,25 +26,29 @@ class Kronolith_Driver_Holidays extends Kronolith_Driver
      * @param Horde_Date $startDate  The start of range date.
      * @param Horde_Date $endDate    The end of date range.
      * @param array $options         Additional options:
-     *   - show_recurrence: (boolean) Return every instance of a recurring event?
+     *   - show_recurrence: (boolean) Return every instance of a recurring
+     *                       event?
      *                      DEFAULT: false (Only return recurring events once
-     *                      inside $startDate - $endDate range).
-     *   - has_alarm: (boolean) Only return events with alarms.
-     *                DEFAULT: false (Return all events)
-     *   - json: (boolean) Store the results of the event's toJson() method?
-     *           DEFAULT: false
-     *   - cover_dates: (boolean) Add the events to all days that they cover?
-     *                  DEFAULT: true
+     *                      inside $startDate - $endDate range)
+     *   - has_alarm:       (boolean) Only return events with alarms.
+     *                      DEFAULT: false (Return all events)
+     *   - json:            (boolean) Store the results of the event's toJson()
+     *                      method?
+     *                      DEFAULT: false
+     *   - cover_dates:     (boolean) Add the events to all days that they
+     *                      cover?
+     *                      DEFAULT: true
      *   - hide_exceptions: (boolean) Hide events that represent exceptions to
      *                      a recurring event.
      *                      DEFAULT: false (Do not hide exception events)
-     *   - fetch_tags: (boolean) Fetch tags for all events.
-     *                 DEFAULT: false (Do not fetch event tags)
+     *   - fetch_tags:      (boolean) Fetch tags for all events.
+     *                      DEFAULT: false (Do not fetch event tags)
      *
      * @throws Kronolith_Exception
      */
-    protected function _listEvents(
-        Horde_Date $startDate = null, Horde_Date $endDate = null, array $options = array())
+    protected function _listEvents(Horde_Date $startDate = null,
+                                   Horde_Date $endDate = null,
+                                   array $options = array())
     {
         if (!class_exists('Date_Holidays')) {
             Horde::logMessage('Support for Date_Holidays has been enabled but the package seems to be missing.', 'ERR');
