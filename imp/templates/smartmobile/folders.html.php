1040a962492aca80e80285bf4700de828633f836
Michael M Slusarz <slusarz@horde.org>
2012-04-28 23:56:28 -0600

    template tweaks

--- imp/templates/smartmobile/folders.html.php
+++ imp/templates/smartmobile/folders.html.php
@@ -1,19 +1,21 @@
 <div id="folders" data-role="page">
-  <div data-role="header">
-    <a rel="external" href="<?php echo $this->portal ?>"><?php echo _("Applications")?></a>
-    <h1><?php echo _("Folders") ?></h1>
-    <?php if ($this->logout): ?>
-    <a href="<?php echo $this->logout ?>" rel="external" data-theme="e" data-icon="delete"><?php echo _("Log out") ?></a>
-    <?php endif ?>
-  </div>
-  <div data-role="content">
-    <ul data-role="listview" data-filter="true" id="imp-folders-list">
-      <?php echo $this->tree ?>
-    </ul>
-  </div>
-  <?php if ($this->canCompose): ?>
-  <div data-role="footer" class="ui-bar" data-position="fixed">
-    <a href="#compose" data-role="button" data-icon="plus"><?php echo _("New Message") ?></a>
-  </div>
-  <?php endif ?>
+ <div data-role="header">
+  <a rel="external" href="<?php echo $this->portal ?>"><?php echo _("Applications")?></a>
+  <h1><?php echo _("Folders") ?></h1>
+<?php if ($this->logout): ?>
+  <a href="<?php echo $this->logout ?>" rel="external" data-theme="e" data-icon="delete"><?php echo _("Log out") ?></a>
+<?php endif ?>
+ </div>
+
+ <div data-role="content">
+  <ul data-role="listview" data-filter="true" id="imp-folders-list">
+   <?php echo $this->tree ?>
+  </ul>
+ </div>
+
+<?php if ($this->canCompose): ?>
+ <div data-role="footer" class="ui-bar" data-position="fixed">
+  <a href="#compose" data-role="button" data-icon="plus"><?php echo _("New Message") ?></a>
+ </div>
+<?php endif ?>
 </div>
