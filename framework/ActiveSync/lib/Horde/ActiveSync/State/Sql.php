ca992f65ed85a68d725e917a5ba5c31fb196ab15
Michael J Rubinsky <mrubinsk@horde.org>
2012-05-02 12:11:44 -0400

    Add default hierarchy value in sync_cache

--- framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
@@ -1239,6 +1239,7 @@ class Horde_ActiveSync_State_Sql extends Horde_ActiveSync_State_Base
                 'wait' => false,
                 'hbinterval' => false,
                 'folders' => array(),
+                'hierarchy' => array(),
                 'collections' => array());
         } else {
             return $data;
