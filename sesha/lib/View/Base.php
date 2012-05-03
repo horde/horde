66b3b58758fa6b89012a480186a8f7720a727b1c
Ralf Lang <lang@b1-systems.de>
2012-05-03 12:48:20 +0200

    Implement a Sesha_View_List to supersede the Horde_Template based classic list view

---
+++ sesha/lib/View/Base.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The Sesha_View_Base class contains all functions shared among
+ * views in Sesha.
+ *
+ * Copyright 2012-2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @package  Sesha
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+
+/**
+ * The Sesha_View_Base class contains all functions shared among
+ * views in Sesha.
+ *
+ * Copyright 2012-2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @package  Sesha
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+
+class Sesha_View_Base extends Horde_View
+{
+    /**
+     * The page title
+     * @var string
+     * @access public
+     */
+    public $title = '';
+}
---
+++ sesha/lib/View/List.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * The Sesha_View_List class wraps the list view logic to keep the client pages simple
+ *
+ * Copyright 2012-2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @package  Sesha
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+
+/**
+ * The Sesha_View_List class wraps the list view logic to keep the client pages simple
+ *
+ * Copyright 2012-2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @package  Sesha
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+
+class Sesha_View_List extends Sesha_View_Base
+{
+
+    /**
+     * The header text related to the listing table
+     * @var string
+     * @access public
+     */
+    public $header;
+
+    /**
+     * The categories selected
+     * @var array of strings
+     * @access public
+     */
+    public $selectedCategories;
+
+    /**
+     * The columns structure for the result table
+     * @var array
+     * @access public
+     */
+    public $columnHeaders;
+
+    /**
+     * The list of Property objects to use
+     * @var array
+     * @access public
+     */
+    public $shownProperties;
+
+    /**
+     * The stock display matrix
+     * @var array
+     * @access public
+     */
+    public $shownStock;
+
+    public function __construct(array $config)
+    {
+        if (!is_null($what) && !is_null($where)) {
+            $title = _("Search Inventory");
+            $this->header = _("Matching Inventory");
+        } else {
+            $this->header = $category_id ?
+                sprintf(_("Available Inventory in %s"), $selectedCategory->category) : _("Available Inventory");
+        }
+
+        $this->title = _('Sesha List View');
+        $this->selectedCategories = (is_array($config['selectedCategories'])) ? $config['selectedCategories'] : array($config['selectedCategories']);
+        $this->shownProperties = $this->properties($config['propertyIds']);
+        $this->columnHeaders = $this->columnHeaders($config['sortDir'], $config['sortBy']);
+        $this->shownStock = $this->stock();
+        parent::__construct($config);
+    }
+    /**
+     * Retrieve all categories from driver
+     * returns array of Sesha_Entity_Category
+     */
+    public function allCategories()
+    {
+        return Sesha::listCategories();
+    }
+
+    /**
+     * Build column header array out of the list of properties and default attributes
+     */
+    protected function columnHeaders($sortDir, $sortBy)
+    {
+        $prefs_url = Horde::url($GLOBALS['registry']->get('webroot', 'horde') . '/services/prefs/', true);
+        $sortdirclass = $sortDir ? 'sortup' : 'sortdown';
+        $baseurl = Horde::url('list.php');
+        $column_headers = array(
+            array('id' => 's' . Sesha::SESHA_SORT_STOCKID,
+                'class' => $sortBy == Sesha::SESHA_SORT_STOCKID ? ' class="' . $sortdirclass . '"' : '',
+                'link' => Horde::link(Horde_Util::addParameter($baseurl, 'sortby', Sesha::SESHA_SORT_STOCKID), _("Sort by stock ID"), 'sortlink') . _("Stock ID") . '</a>',
+                'width' => ' width="5%"'),
+            array('id' => 's' . Sesha::SESHA_SORT_NAME,
+                'class' => $sortBy == Sesha::SESHA_SORT_NAME ? ' class="' . $sortdirclass . '"' : '',
+                'link' => Horde::link(Horde_Util::addParameter($baseurl, 'sortby', Sesha::SESHA_SORT_NAME), _("Sort by item name"), 'sortlink') . _("Item Name") . '</a>',
+                'width' => '')
+        );
+        foreach ($this->shownProperties as $property) {
+            $column_headers[] = array(
+                'id' => 'sp' . $property->property_id,
+                'class' => $sortBy == 'p' . $property->property_id ? ' class="' . $sortdirclass . '"' : '',
+                'link' => Horde::link(Horde_Util::addParameter($baseurl, 'sortby', 'p' . $property->property_id), sprintf(_("Sort by %s"), htmlspecialchars($property->property)), 'sortlink') . htmlspecialchars($property->property) . '</a>',
+                'width' => '',
+            );
+        }
+        $column_headers[] = array(
+            'id' => 's' . Sesha::SESHA_SORT_NOTE,
+            'class' => $sortby == Sesha::SESHA_SORT_NOTE ? ' class="' . $sortdirclass . '"' : '',
+            'link' => Horde::link(Horde_Util::addParameter($baseurl, 'sortby', Sesha::SESHA_SORT_NOTE), _("Sort by note"), 'sortlink') . _("Note") . '</a>',
+            'width' => '',
+        );
+        return $column_headers;
+    }
+
+    /**
+     * The list of property objects to display
+     */
+    protected function properties($propertyIds = array())
+    {
+        if (empty($propertyIds)) {
+            /* The driver understands an empty filter as "all" but if none are selected, we want none */
+            $properties = array();
+        } else {
+            try {
+                $properties = $GLOBALS['injector']->getInstance('Sesha_Factory_Driver')->create()->getProperties($propertyIds);
+            } catch (Sesha_Exception $e) {
+                $properties = array();
+            }
+        }
+        return $properties;
+    }
+    /**
+     * The items which match the category or search criteria
+     */
+    protected function stock($filters = array())
+    {
+        $driver = $GLOBALS['injector']->getInstance('Sesha_Factory_Driver')->create();
+        // Get the inventory
+        try {
+            $stock = $driver->findStock();
+        } catch (Sesha_Exception $e) {
+            throw new Horde_Exception($e);
+        }
+        $isAdminEdit = Sesha::isAdmin(Horde_Perms::EDIT);
+        $itemEditImg = Horde::img('edit.png', _("Edit Item"));
+        $isAdminDelete = Sesha::isAdmin(Horde_Perms::DELETE);
+        $adminDeleteImg = Horde::img('delete.png', _("Delete Item"));
+
+        foreach ($stock as $item) {
+            $url = Horde_Util::addParameter($stock_url, 'stock_id', $item->stock_id);
+            $columns = array();
+
+            // icons
+            $icons = '';
+            if ($isAdminEdit) {
+                $icons .= Horde::link(Horde_Util::addParameter($url, 'actionId', 'update_stock'), _("Edit Item")) . $itemEditImg . '</a>';
+            }
+            if ($isAdminDelete) {
+                $icons .= Horde::link(Horde_Util::addParameter($url, 'actionId', 'remove_stock'), _("Delete Item")) . $adminDeleteImg . '</a>';
+            }
+            $columns[] = array('class' => ' class="nowrap"', 'column' => $icons);
+
+            // stock_id
+            $columns[] = array('class' => '', 'column' => Horde::link(Horde_Util::addParameter($url, 'actionId', 'view_stock'), _("View Item")) . htmlspecialchars($item->stock_id) . '</a>');
+
+            // name
+            $columns[] = array('class' => '', 'column' => Horde::link(Horde_Util::addParameter($url, 'actionId', 'view_stock'), _("View Item")) . htmlspecialchars($item->stock_name) . '</a>');
+
+            // properties
+            foreach ($this->shownProperties as $property) {
+                $value = $item->getValue($property);
+                $columns[] = array('class' => '', 'column' => $value ? htmlspecialchars($value->getDataValue()) : '&nbsp;');
+            }
+
+            // note
+            $columns[] = array('class' => '', 'column' => $item->note ? htmlspecialchars($item->note) : '&nbsp;');
+
+            $items[] = array('columns' => $columns);
+        }
+        return $items;
+    }
+}
