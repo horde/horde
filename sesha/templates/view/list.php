611853fc6f7907cdacb9505729242800a6d70218
Ralf Lang <lang@b1-systems.de>
2012-05-03 12:51:43 +0200

    Use a basic template for the sesha list view because I have not figured how to use view helpers to generate tables and forms

---
+++ sesha/templates/view/list.php
@@ -0,0 +1,55 @@
+<div class="header">
+ <span class="smallheader" style="float:right">
+    <?php echo $this->count; ?>
+ </span>
+    <?php echo $this->header; ?>
+</div>
+<div id="category-select">
+<form action="" method="get" name="stockcategoriesmenu">
+ <tag:form_input />
+ <select name="category_id" onchange="document.stockcategoriesmenu.submit()">
+  <option value=""><?php echo _('Show Category:'); ?></option>
+    <?php   foreach ($this->allCategories() as $category) {
+                $selected = in_array($category->category_id, $this->selectedCategories) ? ' selected="selected" ' : '';
+                printf('<option value="%s"%s/>%s</option>',
+                            $category->category_id,
+                            $selected,
+                            $category->category
+                       );
+
+            } ?>
+ </select>
+</form>
+</div>
+
+<table id="stock" class="stock sortable striped" cellspacing="0">
+<thead>
+ <tr class="item leftAlign">
+  <th width="1%" class="nosort">&nbsp;</th>
+    <?php
+    foreach ($this->columnHeaders as $header) {
+        printf('  <th id="%s" %s %s>%s</th>',
+            $header['id'],
+            $header['class'],
+            $header['width'],
+            $header['link']
+        );
+    };
+    ?>
+ </tr>
+</thead>
+<tbody>
+<?php
+    foreach ($this->shownStock as $item) {
+        print '<tr>';
+        foreach ($item['columns'] as $column) {
+            printf('<td %s>%s</td>',
+                $column['class'],
+                $column['column']
+            );
+        }
+        print '</tr>';
+    }
+?>
+</tbody>
+</table>
