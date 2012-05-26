e38355b20a64700185afa307f7ef623ce3e4da8d
Michael J Rubinsky <mrubinsk@horde.org>
2012-05-26 18:13:22 -0400

    Fix toggling tag entry on kronolith panel

--- kronolith/js/panel.js
+++ kronolith/js/panel.js
@@ -1,5 +1,5 @@
 document.observe('click', function(e) {
-    if (!e.isRightClick() && e.element().match('SPAN.toggleTags')) {
-        e.element().up().select('.toggleTags').invoke('toggle');
+    if (!e.isRightClick() && e.element().up().match('SPAN.toggleTags')) {
+        e.element().up().up().select('.toggleTags').invoke('toggle');
     }
 });
