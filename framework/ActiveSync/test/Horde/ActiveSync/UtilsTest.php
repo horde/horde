2e1a81a8758baf3cfb582f05e398d10e373941d2
Michael J Rubinsky <mrubinsk@horde.org>
2012-05-27 17:18:10 -0400

    Test base64 encoded uri from a SmartForward request

--- framework/ActiveSync/test/Horde/ActiveSync/UtilsTest.php
+++ framework/ActiveSync/test/Horde/ActiveSync/UtilsTest.php
@@ -27,7 +27,28 @@ class Horde_ActiveSync_UtilsTest extends Horde_Test_Case
             'DevTypeLen' => 3,
             'DevType' => 'PPC'
         );
+        $this->assertEquals($fixture, $results);
 
+        /* Smart Forward */
+        $url = 'eQIJBBCuTs0Z9ZK6Vldwb/dM8JusBHVeHIQDUFBDBwEBAwYxMTkyODEBBUlOQk9Y';
+        $results = Horde_ActiveSync_Utils::decodeBase64($url);
+
+        // This is binary data, test it separately.
+        $this->assertEquals('01', bin2hex($results['Options']));
+        unset($results['Options']);
+        $fixture = array(
+            'ProtVer' => 121,
+            'Command' => 2,
+            'Locale' => 1033,
+            'DevIDLen' => 16,
+            'DevID' => 'ae4ecd19f592ba5657706ff74cf09bac',
+            'PolKeyLen' => 4,
+            'PolKey' => 2216451701,
+            'DevTypeLen' => 3,
+            'DevType' => 'PPC',
+            'ItemId' => 119281,
+            'CollectionId' => 'INBOX'
+        );
         $this->assertEquals($fixture, $results);
     }
 
