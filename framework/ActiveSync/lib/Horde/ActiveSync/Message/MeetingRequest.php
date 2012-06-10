34b842f1dba1677aa328b6694ee81a8450582087
Michael J Rubinsky <mrubinsk@horde.org>
2012-06-10 17:26:56 -0400

    These dates will already be in UTC

--- framework/ActiveSync/lib/Horde/ActiveSync/Message/MeetingRequest.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Message/MeetingRequest.php
@@ -113,9 +113,9 @@ class Horde_ActiveSync_Message_MeetingRequest extends Horde_ActiveSync_Message_B
             $this->organizer = $organizer['path'];
         } catch (Horde_Icalendar_Exception $e) {}
         $this->globalobjid = Horde_ActiveSync_Utils::createGoid($vevent->getAttribute('UID'));
-        $this->starttime = new Horde_Date($vevent->getAttribute('DTSTART'), 'utc');
-        $this->endtime = new Horde_Date($vevent->getAttribute('DTEND'), 'utc');
-        $this->dtstamp = new Horde_Date($vevent->getAttribute('DTSTAMP'), 'utc');
+        $this->starttime = new Horde_Date($vevent->getAttribute('DTSTART'));
+        $this->endtime = new Horde_Date($vevent->getAttribute('DTEND'));
+        $this->dtstamp = new Horde_Date($vevent->getAttribute('DTSTAMP'));
         $this->location = Horde_String::truncate($vevent->getAttribute('LOCATION'), 255);
 
         try {
