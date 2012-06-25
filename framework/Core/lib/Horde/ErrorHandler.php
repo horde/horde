efa7400ee0814f7709222d291449c81d71ac894a
Michael M Slusarz <slusarz@horde.org>
2012-06-25 16:56:27 -0600

    Only logout if auth failure occurs in main authentication application

--- framework/Core/lib/Horde/ErrorHandler.php
+++ framework/Core/lib/Horde/ErrorHandler.php
@@ -31,13 +31,14 @@ class Horde_ErrorHandler
         if (is_object($error)) {
             switch (get_class($error)) {
             case 'Horde_Exception_AuthenticationFailure':
-                if (!$registry->getAuth()) {
+                if (!$registry->getAuth() ||
+                    $registry->clearAuthApp($error->applicaton)) {
                     break;
                 }
 
                 if (Horde_Cli::runningFromCLI()) {
                     $cli = new Horde_Cli();
-                    $cli->fatal($error ? $error : Horde_Core_Translation::t("You are not authenticated."));
+                    $cli->fatal($error);
                 }
 
                 $params = array(
