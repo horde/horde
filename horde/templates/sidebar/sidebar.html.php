14f82a481d3d786b6da6c91e6ee0c80eeb8c0052
Jan Schneider <jan@horde.org>
2012-06-27 23:08:22 +0200

    Make these buttons easier clickable.

--- horde/templates/sidebar/sidebar.html.php
+++ horde/templates/sidebar/sidebar.html.php
@@ -2,13 +2,11 @@
 
 <?php if ($this->newLink): ?>
   <div class="horde-new">
-    <div class="horde-new-plus"><?php echo $this->newLink ?></a></div>
-    <div class="horde-new-str"><p class="p17 white bold relief"><?php echo $this->newText ?></p></div>
 <?php if ($this->newRefresh): ?>
-    <div class="horde-new-refresh"><?php echo $this->newRefresh ?></a></div>
+    <div class="horde-new-refresh"><?php echo $this->newRefresh ?>&nbsp;</a></div>
     <div class="horde-new-split"></div>
 <?php endif ?>
-    <div class="clear"></div>
+    <span class="horde-new-link"><?php echo $this->newLink ?><?php echo $this->newText ?></a></span>
   </div>
 <?php endif ?>
 
