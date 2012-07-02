e0530d86d6c20bd512c9d39676b84451a5eb0039
Jan Schneider <jan@horde.org>
2012-07-02 16:44:46 +0200

    Avoid undefined index.

--- horde/templates/sidebar/_container.html.php
+++ horde/templates/sidebar/_container.html.php
@@ -4,7 +4,7 @@
 <div<?php if (!empty($container['id']) && !is_int($container['id'])) echo ' id="' . $container['id'] . '"' ?> class="horde-sidebar-<?php if ($containerCounter == $this->containersCount - 1) echo 'sub' ?>folder">
 <?php if (isset($container['content'])): ?>
 <?php echo $container['content'] ?>
-<?php else: ?>
+<?php elseif (isset($container['rows'])): ?>
 <?php echo $this->renderPartial('row', array('collection' => $container['rows'])) ?>
 <?php endif ?>
 </div>
