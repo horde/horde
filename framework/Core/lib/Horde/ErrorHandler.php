68610b81c06d7168436780424198c85aea99366b
Michael M Slusarz <slusarz@horde.org>
2012-07-10 15:35:25 -0600

    Redirect to login screen if authentication error is thrown and application is not yet authenticated to (Bug #11258).

--- framework/Core/lib/Horde/ErrorHandler.php
+++ framework/Core/lib/Horde/ErrorHandler.php
@@ -22,6 +22,8 @@ class Horde_ErrorHandler
      */
     static public function fatal($error)
     {
+        global $registry;
+
         try {
             Horde::logMessage($error, 'EMERG');
         } catch (Exception $e) {}
@@ -29,7 +31,8 @@ class Horde_ErrorHandler
         if (is_object($error)) {
             switch (get_class($error)) {
             case 'Horde_Exception_AuthenticationFailure':
-                if ($GLOBALS['registry']->clearAuthApp($error->applicaton)) {
+                if ($registry->isAuthenticated(array('app' => $error->application, 'notransparent' => true)) &&
+                    $registry->clearAuthApp($error->applicaton)) {
                     break;
                 }
 
@@ -49,14 +52,14 @@ class Horde_ErrorHandler
                     break;
                 }
 
-                header('Location: ' . $GLOBALS['registry']->getLogoutUrl($params));
+                header('Location: ' . $registry->getLogoutUrl($params));
                 exit;
             }
         }
 
         header('Content-type: text/html; charset=UTF-8');
         try {
-            $admin = $GLOBALS['registry']->isAdmin();
+            $admin = $registry->isAdmin();
             $cli = Horde_Cli::runningFromCLI();
 
             $errortext = '<h1>' . Horde_Core_Translation::t("A fatal error has occurred") . '</h1>';
