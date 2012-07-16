c38430b9c193fce12dcbe90bcd6c011199971565
Michael J Rubinsky <mrubinsk@horde.org>
2012-07-15 20:35:53 -0400

    Only send FOLDER container if we have collection data.

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/Ping.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/Ping.php
@@ -326,7 +326,7 @@ class Horde_ActiveSync_Request_Ping extends Horde_ActiveSync_Request_Base
             $this->_encoder->startTag(self::HEARTBEATINTERVAL);
             $this->_encoder->content($lifetime);
             $this->_encoder->endTag();
-        } else {
+        } elseif (!empty($collections)) {
             $this->_encoder->startTag(self::FOLDERS);
             foreach ($collections as $collection) {
                 if (isset($changes[$collection['id']])) {
