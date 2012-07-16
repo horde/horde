9636696144c988c7d36e56fdfd87471c3fba125f
Jan Schneider <jan@horde.org>
2012-07-16 15:06:56 +0200

    Fix undefined errors.

--- horde/templates/tree/_row.html.php
+++ horde/templates/tree/_row.html.php
@@ -1,9 +1,17 @@
-    <div class="horde-subnavi<?php if ($selected) echo ' horde-subnavi-active' ?>">
+    <div class="horde-subnavi<?php if (!empty($selected)) echo ' horde-subnavi-active' ?>">
       <div class="horde-subnavi-icon-1" style="background-image:url('<?php echo $icon ?>')"><a class="icon" href=""></a></div>
-      <div class="horde-subnavi-point"><a href="<?php echo $url ?>"><?php echo $label ?></a></div>
+      <div class="horde-subnavi-point">
+<?php if (!empty($url)): ?>
+        <a href="<?php echo $url ?>">
+<?php endif ?>
+          <?php echo $label ?>
+<?php if (!empty($url)): ?>
+        </a>
+<?php endif ?>
+      </div>
       <div class="clear"></div>
     </div>
-<?php if ($children): ?>
+<?php if (!empty($children)): ?>
     <div class="subfolders">
 <?php foreach ($children as $child): ?>
 <?php echo $this->renderPartial('row', array('locals' => $this->items[$child])) ?>
