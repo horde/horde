a73904c70afb5e1a3637a0562133e1315f9756e6
Jan Schneider <jan@horde.org>
2012-07-19 15:32:10 +0200

    [jan] Add configuration example for Kolab/IMAP address books.

--- turba/config/backends.php
+++ turba/config/backends.php
@@ -568,6 +568,121 @@ $cfgSources['personal_ldap'] = array(
 );
 
 /**
+ * A local address book on a Kolab or IMAP server. This implements a private
+ * per-user address book. Sharing of this source with other users is
+ * accomplished by IMAP ACLs and by setting 'use_shares' => true.
+ */
+$cfgSources['kolab'] = array(
+    // DISABLED by default
+    'disabled' => true,
+    'title' => _("Shared Address Books"),
+    'type' => 'kolab',
+    'params' => array(
+    ),
+    'map' => array(
+        '__key' => 'uid',
+        '__uid' => 'uid',
+        '__type' => '__type',
+        '__members' => '__members',
+        /* Personal */
+        'firstname' => 'given-name',
+        'lastname' => 'last-name',
+        'middlenames' => 'middle-names',
+        'namePrefix' => 'prefix',
+        'nameSuffix' => 'suffix',
+        'name' => array('fields' => array('namePrefix', 'firstname',
+                                          'middlenames', 'lastname',
+                                          'nameSuffix'),
+                        'format' => '%s %s %s %s %s',
+                        'parse' => array(
+                            array('fields' => array('firstname', 'middlenames',
+                                                    'lastname'),
+                                  'format' => '%s %s %s'),
+                            array('fields' => array('lastname', 'firstname'),
+                                  'format' => '%s, %s'),
+                            array('fields' => array('firstname', 'lastname'),
+                                  'format' => '%s %s'))),
+        // This is a shorter version of a "name" composite field which only
+        // consists of the first name and last name.
+        // 'name' => array('fields' => array('firstname', 'lastname'),
+        //                 'format' => '%s %s'),
+        'initials'          => 'initials',
+        'nickname'          => 'nick-name',
+        'photo'             => 'photo',
+        'phototype'         => 'phototype',
+        'gender'            => 'gender',
+        'birthday'          => 'birthday',
+        'spouse'            => 'spouse-name',
+        'anniversary'       => 'anniversary',
+        'children'          => 'children',
+        /* Location */
+        'workStreet'        => 'addr-business-street',
+        'workCity'          => 'addr-business-locality',
+        'workProvince'      => 'addr-business-region',
+        'workPostalCode'    => 'addr-business-postal-code',
+        'workCountry'       => 'addr-business-country',
+        'homeStreet'        => 'addr-home-street',
+        'homeCity'          => 'addr-home-locality',
+        'homeProvince'      => 'addr-home-region',
+        'homePostalCode'    => 'addr-home-postal-code',
+        'homeCountry'       => 'addr-home-country',
+        /* Communications */
+        'emails'            => 'emails',
+        'homePhone'         => 'phone-home1',
+        'workPhone'         => 'phone-business1',
+        'cellPhone'         => 'phone-mobile',
+        'fax'               => 'phone-businessfax',
+        'instantMessenger'  => 'im-address',
+        /* Organization */
+        'title'             => 'job-title',
+        'role'              => 'profession',
+        'company'           => 'organization',
+        'department'        => 'department',
+        'office'            => 'office-location',
+        'manager'           => 'manager-name',
+        'assistant'         => 'assistant',
+        /* Other */
+        'category'          => 'categories',
+        'notes'             => 'body',
+        'website'           => 'web-page',
+        'freebusyUrl'       => 'free-busy-url',
+        'language'          => 'language',
+        'latitude'          => 'latitude',
+        'longitude'         => 'longitude',
+        /* Invisible */
+        'email'             => 'email',
+        'pgpPublicKey'      => 'pgp-publickey',
+    ),
+    'tabs' => array(
+        _("Personal") => array('firstname', 'lastname', 'middlenames',
+                               'namePrefix', 'nameSuffix', 'name', 'initials',
+                               'nickname', 'gender', 'birthday', 'spouse',
+                               'anniversary', 'children', 'photo'),
+        _("Location") => array('workStreet', 'workCity', 'workProvince',
+                               'workPostalCode', 'workCountry',
+                               'homeStreet', 'homeCity', 'homeProvince',
+                               'homePostalCode', 'homeCountry'),
+        _("Communications") => array('emails', 'homePhone', 'workPhone',
+                                     'cellPhone', 'fax', 'instantMessenger'),
+        _("Organization") => array('title', 'role', 'company', 'department',
+                                   'office', 'manager', 'assistant'),
+        _("Other") => array('category', 'notes', 'website', 'freebusyUrl',
+                            'language', 'latitude', 'longitude'),
+    ),
+    'search' => array(
+        'name',
+        'emails'
+    ),
+    'strict' => array(
+        'uid',
+    ),
+    'export' => true,
+    'browse' => true,
+    'use_shares' => true,
+    'list_name_field' => 'lastname',
+);
+
+/**
  * A preferences-based address book. This will always be private. You can add
  * any attributes you like to the map and it will just work; you can also
  * create multiple prefs-based address books by changing the 'name' parameter.
