80636b3c2635d67c2768efba9163f7d72b5baa27
Michael M Slusarz <slusarz@horde.org>
2012-07-30 14:03:10 -0600

    Bug #11316: Fix undefined error

--- imp/lib/Ajax/Application.php
+++ imp/lib/Ajax/Application.php
@@ -1195,7 +1195,7 @@ class IMP_Ajax_Application extends Horde_Core_Ajax_Application
             if ($this->_vars->dataonly) {
                 $result = $compose->ajax->getBaseResponse();
                 $result->body = $fwd_msg['body'];
-                $result->opts->atc = $compose->ajax->getAttachmentInfo();
+                $result->opts->atc = $compose->ajax->getAttachmentInfo($fwd_msg['type']);
             } else {
                 $result = $compose->ajax->getResponse($fwd_msg);
             }
