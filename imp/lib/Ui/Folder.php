136d566b5cf2e41ed121b7ef7abe87ba4bfe659f
Michael M Slusarz <slusarz@horde.org>
2012-07-30 13:15:05 -0600

    Bug #11294: generateMbox() should accept either a string or array

--- imp/lib/Ui/Folder.php
+++ imp/lib/Ui/Folder.php
@@ -23,8 +23,8 @@ class IMP_Ui_Folder
      *
      * @author Didi Rieder <adrieder@sbox.tugraz.at>
      *
-     * @param array $mboxes  A list of mailbox names (UTF-8) to generate a
-     *                       mbox file for.
+     * @param mixed $mboxes  A mailbox name (UTF-8), or list of mailbox names,
+     *                       to generate a mbox file for.
      *
      * @return resource  A stream resource containing the text of a mbox
      *                   format mailbox file.
@@ -32,6 +32,13 @@ class IMP_Ui_Folder
     public function generateMbox($mboxes)
     {
         $body = fopen('php://temp', 'r+');
+
+        if (!is_array($mboxes)) {
+            if (!strlen($mboxes)) {
+                return $body;
+            }
+            $mboxes = array($mboxes);
+        }
 
         if (empty($mboxes)) {
             return $body;
