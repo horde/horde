338914626ac41c64fa7653eac8ec3f05e35b0ad2
Ralf Lang <lang@b1-systems.de>
2012-08-02 13:51:44 +0000

    push version to alpha2 (doesn't any part of the release process handle this?)

--- sesha/lib/Application.php
+++ sesha/lib/Application.php
@@ -42,7 +42,7 @@ class Sesha_Application extends Horde_Registry_Application
      *
      * @var string
      */
-    public $version = 'H5 (1.0.0-git)';
+    public $version = 'H5 (1.0.0-alpha2)';
 
     public function perms()
     {
