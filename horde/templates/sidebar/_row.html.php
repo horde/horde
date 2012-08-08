9787acf14c361b8502a6a7c86190eef70d638a1e
Michael J Rubinsky <mrubinsk@horde.org>
2012-08-08 12:15:28 -0400

    This gets rid of the mysterious extra 3 pixels of padding.

--- horde/templates/sidebar/_row.html.php
+++ horde/templates/sidebar/_row.html.php
@@ -1,7 +1,6 @@
 <div class="horde-subnavi<?php if (!empty($row['selected'])) echo ' horde-subnavi-active' ?>"<?php if (!empty($row['style'])) echo ' style="' . $row['style'] . '"' ?>>
  <div class="horde-subnavi-icon <?php echo $row['cssClass'] ?>">
   <a class="icon"></a>
- </div>
- <div<?php if (!empty($row['id'])) echo ' id="' . $row['id'] . '"' ?> class="horde-subnavi-point"><?php echo $row['link'] ?>
+ </div><div<?php if (!empty($row['id'])) echo ' id="' . $row['id'] . '"' ?> class="horde-subnavi-point"><?php echo $row['link'] ?>
  </div>
 </div>
