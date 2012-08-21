2c8d8624c118b5e7ceff02753a01dd45e86a5d13
Michael M Slusarz <slusarz@horde.org>
2012-08-21 13:07:54 -0600

    Get rid of toggle graphic on message page; takes up too much room for a feature that isn't used often

--- imp/templates/smartmobile/message.html.php
+++ imp/templates/smartmobile/message.html.php
@@ -9,14 +9,12 @@
 
  <div id="imp-message-header-toggle">
   <div class="ui-body ui-body-c">
-   <a href="#" data-role="button" data-icon="arrow-d" data-iconpos="notext"><?php echo _("Show more") ?></a>
    <span id="imp-message-subject"></span>
    <span id="imp-message-subject-from">(<?php echo _("from") ?> <span id="imp-message-from"></span>)</span><br>
    <span id="imp-message-date"></span>
   </div>
 
   <div class="ui-body ui-body-c" style="display:none">
-   <a href="#" data-role="button" data-icon="arrow-u" data-iconpos="notext"><?php echo _("Show less") ?></a>
    <table id="imp-message-headers"><tbody></tbody></table>
   </div>
  </div>
