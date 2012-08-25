237cbad194015324d70015e1aafee236c418f801
Michael M Slusarz <slusarz@horde.org>
2012-08-24 23:27:11 -0600

    Move all smartmobile page generation logic to IMP_Smartmobile class

--- imp/smartmobile.php
+++ imp/smartmobile.php
@@ -18,59 +18,7 @@ Horde_Registry::appInit('imp', array(
     'impmode' => Horde_Registry::VIEW_SMARTMOBILE
 ));
 
-$view = new Horde_View(array(
-    'templatePath' => IMP_TEMPLATES . '/smartmobile'
-));
-$view->addHelper('Text');
-
-/* Initialize the IMP_Imap_Tree object. By default, only show INBOX, special
- * mailboxes, and polled mailboxes. */
-$imptree = $injector->getInstance('IMP_Imap_Tree');
-$imptree->setIteratorFilter(Imp_Imap_Tree::FLIST_POLLED);
-$view->tree = $imptree->createTree('smobile_folders', array(
-    'poll_info' => true,
-    'render_type' => 'IMP_Tree_Jquerymobile'
-))->getTree(true);
-
-$imp_imap = $injector->getInstance('IMP_Factory_Imap')->create();
-if ($view->allowFolders = $imp_imap->access(IMP_Imap::ACCESS_FOLDERS)) {
-    $view->options = IMP::flistSelect(array(
-        'heading' => _("This message to"),
-        'optgroup' => true,
-        'inc_tasklists' => true,
-        'inc_notepads' => true,
-        'new_mbox' => true
-    ));
-}
-
-$view->portal = $registry->getServiceLink('portal', 'horde')->setRaw(false);
-$view->logout = $registry->getServiceLink('logout')->setRaw(false);
-$view->canSearch = $imp_imap->access(IMP_Imap::ACCESS_SEARCH);
-$view->canSpam = !empty($conf['spam']['reporting']);
-$view->canInnocent = !empty($conf['notspam']['reporting']);
-
-if ($view->canCompose = IMP::canCompose()) {
-    /* Setting up identities. */
-    $identity = $injector->getInstance('IMP_Identity');
-    $view->identities = array();
-    foreach ($identity->getSelectList() as $id => $from) {
-        $view->identities[] = array(
-            'label' => $from,
-            'sel' => ($id == $identity->getDefault()),
-            'val' => $id
-        );
-    }
-
-    $view->composeCache = $injector->getInstance('IMP_Factory_Compose')->create()->getCacheId();
-    $view->composeLink = $registry->getServiceLink('ajax', 'imp');
-    $view->composeLink->pathInfo = 'addAttachment';
-}
-
-require IMP_TEMPLATES . '/smartmobile/javascript_defs.php';
-$notification->notify(array('listeners' => 'status'));
-
-$page_output->addScriptFile('smartmobile.js');
-$page_output->addScriptFile('json2.js', 'horde');
+$ob = new IMP_Smartmobile(Horde_Variables::getDefaultVariables());
 
 $page_output->header(array(
     'smartmobileinit' => array(
@@ -80,17 +28,6 @@ $page_output->header(array(
     'view' => $registry::VIEW_SMARTMOBILE
 ));
 
-echo $view->render('folders');
-echo $view->render('mailbox');
-echo $view->render('message');
-if (IMP::canCompose()) {
-    echo $view->render('compose');
-}
-if ($imp_imap->access(IMP_Imap::ACCESS_SEARCH)) {
-    echo $view->render('search');
-}
-echo $view->render('confirm');
-if ($imp_imap->access(IMP_Imap::ACCESS_FOLDERS)) {
-    echo $view->render('target');
-}
+$ob->render();
+
 $page_output->footer();
