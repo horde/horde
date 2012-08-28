1a3620ece044c42547db5bcca4e302e78eae1857
Michael J Rubinsky <mrubinsk@horde.org>
2012-08-27 21:27:13 -0400

    Need to have #kronolithMenuCalendars wrap all calendar sections.
    Unless I'm missing something this is needed for toggling calendars
    other than internal to work correctly.

--- kronolith/templates/dynamic/sidebar.html.php
+++ kronolith/templates/dynamic/sidebar.html.php
@@ -28,93 +28,94 @@
     </div>
 
     <div id="kronolithMenuCalendars" style="display:none">
-    <h3 id="kronolithCalendarsFirst">
-      <?php if ($this->newShares): ?>
-      <a href="#" id="kronolithAddinternal" class="kronolithAdd" title="<?php echo _("New Calendar") ?>">+</a>
-      <?php endif; ?>
-      <span class="kronolithToggleCollapse" title="<?php echo _("Collapse") ?>"><?php echo _("My Calendars") ?></span>
-    </h3>
+      <h3 id="kronolithCalendarsFirst">
+        <?php if ($this->newShares): ?>
+        <a href="#" id="kronolithAddinternal" class="kronolithAdd" title="<?php echo _("New Calendar") ?>">+</a>
+        <?php endif; ?>
+        <span class="kronolithToggleCollapse" title="<?php echo _("Collapse") ?>"><?php echo _("My Calendars") ?></span>
+      </h3>
 
-    <div id="kronolithMyCalendars" class="kronolithCalendars"></div>
-  </div>
+      <div id="kronolithMyCalendars" class="kronolithCalendars"></div>
 
-  <?php if (Kronolith::hasApiPermission('tasks')): ?>
-  <div class="horde-subnavi-split"></div>
-  <div class="horde-sidebar-folder">
-    <h3>
-      <?php if ($this->newShares): ?>
-      <a href="#" id="kronolithAddtasklists" class="kronolithAdd" title="<?php echo _("New Task List") ?>">+</a>
-      <?php endif; ?>
-      <span class="kronolithToggleCollapse" title="<?php echo _("Collapse") ?>"><?php echo _("My Task Lists") ?></span>
-    </h3>
 
-    <div id="kronolithMyTasklists" class="kronolithCalendars"></div>
-  </div>
-  <?php endif; ?>
+    <?php if (Kronolith::hasApiPermission('tasks')): ?>
+    <div class="horde-subnavi-split"></div>
+    <div class="horde-sidebar-folder">
+      <h3>
+        <?php if ($this->newShares): ?>
+        <a href="#" id="kronolithAddtasklists" class="kronolithAdd" title="<?php echo _("New Task List") ?>">+</a>
+        <?php endif; ?>
+        <span class="kronolithToggleCollapse" title="<?php echo _("Collapse") ?>"><?php echo _("My Task Lists") ?></span>
+      </h3>
 
-  <div class="horde-subnavi-split"></div>
-  <div class="horde-sidebar-folder">
-    <h3>
-      <!-- to be added when searching for shared calendars is implemented <a href="#" id="kronolithAddinternalshared" class="kronolithAdd">+</a>-->
-      <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Shared Calendars") ?></span>
-    </h3>
-
-    <div style="display:none">
-      <div class="kronolithDialogInfo"><?php echo _("No items to display") ?></div>
-      <div id="kronolithSharedCalendars" class="kronolithCalendars"></div>
+      <div id="kronolithMyTasklists" class="kronolithCalendars"></div>
     </div>
-  </div>
+    <?php endif; ?>
 
-  <?php if (Kronolith::hasApiPermission('tasks')): ?>
-  <div class="horde-subnavi-split"></div>
-  <div class="horde-sidebar-folder">
-    <h3>
-      <!-- to be added when searching for shared calendars is implemented <a href="#" id="kronolithAddtasklistsshared" class="kronolithAdd">+</a>-->
-      <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Shared Task Lists") ?></span>
-    </h3>
+    <div class="horde-subnavi-split"></div>
+    <div class="horde-sidebar-folder">
+      <h3>
+        <!-- to be added when searching for shared calendars is implemented <a href="#" id="kronolithAddinternalshared" class="kronolithAdd">+</a>-->
+        <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Shared Calendars") ?></span>
+      </h3>
 
-    <div style="display:none">
-      <div class="kronolithDialogInfo"><?php echo _("No items to display") ?></div>
-      <div id="kronolithSharedTasklists" class="kronolithCalendars"></div>
+      <div style="display:none">
+        <div class="kronolithDialogInfo"><?php echo _("No items to display") ?></div>
+        <div id="kronolithSharedCalendars" class="kronolithCalendars"></div>
+      </div>
     </div>
-  </div>
-  <?php endif ?>
 
-  <div class="horde-subnavi-split"></div>
-  <div class="horde-sidebar-folder">
-    <h3>
-      <?php if ($this->isAdmin): ?>
-      <a href="#" id="kronolithAddresource" class="kronolithAdd" title="<?php echo _("Add Resource") ?>">+</a>
-      <?php endif; ?>
-      <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Resources") ?></span>
-    </h3>
-    <div id="kronolithResourceCalendars" class="kronolithCalendars" style="display:none"></div>
-  </div>
+    <?php if (Kronolith::hasApiPermission('tasks')): ?>
+    <div class="horde-subnavi-split"></div>
+    <div class="horde-sidebar-folder">
+      <h3>
+        <!-- to be added when searching for shared calendars is implemented <a href="#" id="kronolithAddtasklistsshared" class="kronolithAdd">+</a>-->
+        <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Shared Task Lists") ?></span>
+      </h3>
 
-  <div id="kronolithExternalCalendars"></div>
-
-  <div class="horde-subnavi-split"></div>
-  <div class="horde-sidebar-folder">
-    <h3>
-      <a href="#" id="kronolithAddremote" class="kronolithAdd" title="<?php echo _("Add Remote Calendar") ?>">+</a>
-      <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Remote Calendars") ?></span>
-    </h3>
-
-    <div style="display:none">
-      <div class="kronolithDialogInfo"><?php echo _("No items to display") ?></div>
-      <div id="kronolithRemoteCalendars" class="kronolithCalendars"></div>
+      <div style="display:none">
+        <div class="kronolithDialogInfo"><?php echo _("No items to display") ?></div>
+        <div id="kronolithSharedTasklists" class="kronolithCalendars"></div>
+      </div>
     </div>
-  </div>
+    <?php endif ?>
 
-  <div class="horde-subnavi-split"></div>
-  <div class="horde-sidebar-folder">
-    <h3>
-      <a href="#" id="kronolithAddholiday" class="kronolithAdd" title="<?php echo _("Add Holidays") ?>">+</a>
-      <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Holidays") ?></span>
-    </h3>
+    <div class="horde-subnavi-split"></div>
+    <div class="horde-sidebar-folder">
+      <h3>
+        <?php if ($this->isAdmin): ?>
+        <a href="#" id="kronolithAddresource" class="kronolithAdd" title="<?php echo _("Add Resource") ?>">+</a>
+        <?php endif; ?>
+        <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Resources") ?></span>
+      </h3>
+      <div id="kronolithResourceCalendars" class="kronolithCalendars" style="display:none"></div>
+    </div>
 
-    <div id="kronolithHolidayCalendars" class="kronolithCalendars" style="display:none"></div>
+    <div id="kronolithExternalCalendars"></div>
 
+    <div class="horde-subnavi-split"></div>
+    <div class="horde-sidebar-folder">
+      <h3>
+        <a href="#" id="kronolithAddremote" class="kronolithAdd" title="<?php echo _("Add Remote Calendar") ?>">+</a>
+        <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Remote Calendars") ?></span>
+      </h3>
+
+      <div style="display:none">
+        <div class="kronolithDialogInfo"><?php echo _("No items to display") ?></div>
+        <div id="kronolithRemoteCalendars" class="kronolithCalendars"></div>
+      </div>
+    </div>
+
+    <div class="horde-subnavi-split"></div>
+    <div class="horde-sidebar-folder">
+      <h3>
+        <a href="#" id="kronolithAddholiday" class="kronolithAdd" title="<?php echo _("Add Holidays") ?>">+</a>
+        <span class="kronolithToggleExpand" title="<?php echo _("Expand") ?>"><?php echo _("Holidays") ?></span>
+      </h3>
+
+      <div id="kronolithHolidayCalendars" class="kronolithCalendars" style="display:none"></div>
+
+      </div>
     </div>
   </div>
 </div>
