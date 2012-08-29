88e4ca7a8970a8323d74d85d0a647debc587828a
Jan Schneider <jan@horde.org>
2012-08-29 16:04:41 +0200

    Development mode for turba-4.0.0

--- turba/lib/Application.php
+++ turba/lib/Application.php
@@ -38,7 +38,7 @@ class Turba_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (4.0.0beta2)';
+    public $version = 'H5 (4.0.0-git)';
 
     /**
      */
