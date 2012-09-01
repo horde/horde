41797bc0144de6ed6aa5ca7904f5667767a26f95
Jan Schneider <jan@horde.org>
2012-09-01 19:48:40 +0200

    Remove ShowView functionality.

--- kronolith/js/calendar-panel.js
+++ kronolith/js/calendar-panel.js
@@ -15,8 +15,8 @@ function sbarToggle()
 
 document.observe('dom:loaded', function() {
     $$('#pageControlsInner .checkbox').invoke('observe', 'click', function() {
-        Views.invalidate();
-        ShowView(KronolithView, { date: KronolithDate.getFullYear() + (KronolithDate.getMonth() + 1).toPaddedString(2) + KronolithDate.getDate().toPaddedString(2), toggle_calendar: this.value }, false);
+        //Views.invalidate();
+        //ShowView(KronolithView, { date: KronolithDate.getFullYear() + (KronolithDate.getMonth() + 1).toPaddedString(2) + KronolithDate.getDate().toPaddedString(2), toggle_calendar: this.value }, false);
     });
 
     $$('#pageControlsInner .calendar-info').invoke('observe', 'click', function() {
