f5822032abe6502714a67e69c994d43d6e265008
Michael J Rubinsky <mrubinsk@horde.org>
2012-09-03 19:21:47 -0400

    Fix scope

--- ansel/lib/Block/RecentlyAddedGeodata.php
+++ ansel/lib/Block/RecentlyAddedGeodata.php
@@ -43,7 +43,7 @@ class Ansel_Block_RecentlyAddedGeodata extends Horde_Core_Block
     protected function _content()
     {
         Horde::initMap();
-        $page_output->addScriptFile('map.js');
+        $GLOBALS['page_output']->addScriptFile('map.js');
         $GLOBALS['page_output']->addScriptFile('blocks/geotag.js');
 
         try {
