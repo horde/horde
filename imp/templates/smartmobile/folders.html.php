b567641ed3a7e114ba6fa0397911ac7470e111b4
Michael M Slusarz <slusarz@horde.org>
2012-09-03 22:36:50 -0600

    per documentation, empty hrefs should point to '#'

--- imp/templates/smartmobile/folders.html.php
+++ imp/templates/smartmobile/folders.html.php
@@ -8,9 +8,9 @@
  </div>
 
  <div data-role="footer" class="ui-bar" data-position="fixed">
-  <a href="" data-role="button" id="imp-folders-refresh" data-icon="refresh"><?php echo _("Refresh") ?></a>
-  <a href="" data-role="button" id="imp-folders-showall"><?php echo _("Show All") ?></a>
-  <a href="" data-role="button" id="imp-folders-showpoll" style="display:none"><?php echo _("Show Polled") ?></a>
+  <a href="#" data-role="button" id="imp-folders-refresh" data-icon="refresh"><?php echo _("Refresh") ?></a>
+  <a href="#" data-role="button" id="imp-folders-showall"><?php echo _("Show All") ?></a>
+  <a href="#" data-role="button" id="imp-folders-showpoll" style="display:none"><?php echo _("Show Polled") ?></a>
 <?php if ($this->canCompose): ?>
   <a href="#compose" data-role="button"><?php echo _("New Message") ?></a>
 <?php endif ?>
