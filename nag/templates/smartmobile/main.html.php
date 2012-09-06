ec0135521ec627b515041182cde20f61a8864075
Michael J Rubinsky <mrubinsk@horde.org>
2012-09-06 14:49:58 -0400

    Fix page id

--- nag/templates/smartmobile/main.html.php
+++ nag/templates/smartmobile/main.html.php
@@ -1,12 +1,13 @@
-<div data-role="page" id="list-view">
+<div data-role="page" id="nag-list">
  <?php echo $this->smartmobileHeader(array('backlink' => array('#lists', _("Lists")), 'logout' => true, 'title' => _("My Tasks"))) ?>
  <div data-role="content" class="ui-body">
  </div>
  <div data-role="footer" data-id="nag-footer" data-position="fixed">
   <div data-role="navbar">
    <ul>
-    <li><a href="#create" data-rel="dialog" data-transition="slideup" data-icon="plus"><?php echo _("New") ?></a></li>
+    <li><a href="#" data-rel="dialog" data-transition="slideup" data-icon="plus"><?php echo _("New") ?></a></li>
    </ul>
   </div>
  </div>
 </div>
+
