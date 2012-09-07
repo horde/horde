52cc87b050998de015a991699038b631de221d27
Michael M Slusarz <slusarz@horde.org>
2012-09-07 15:28:46 -0600

    Fix formatting in compose screen

--- imp/templates/smartmobile/compose.html.php
+++ imp/templates/smartmobile/compose.html.php
@@ -1,36 +1,35 @@
 <div id="compose" data-role="page">
  <?php echo $this->smartmobileHeader(array('logout' => true, 'title' => _("New Message"))) ?>
 
- <form id="imp-redirect-form" style="display:none">
-  <input type="hidden" id="imp-redirect-cache" name="composeCache" value="<?php echo $this->h($this->composeCache) ?>" />
-  <label for="imp-redirect-to"><?php echo _("To:") ?></label>
-  <input type="text" id="imp-redirect-to" name="redirect_to" />
- </form>
+ <div data-role="content">
+  <form id="imp-redirect-form" style="display:none">
+   <input type="hidden" id="imp-redirect-cache" name="composeCache" value="<?php echo $this->h($this->composeCache) ?>" />
+   <label for="imp-redirect-to"><?php echo _("To:") ?></label>
+   <input type="text" id="imp-redirect-to" name="redirect_to" />
+  </form>
 
- <form id="imp-compose-form">
-  <input type="hidden" id="imp-compose-cache" name="composeCache" value="<?php echo $this->h($this->composeCache) ?>" />
-  <div data-role="fieldcontain">
-   <label for="imp-compose-identity"><?php echo _("From:") ?></label>
-   <select id="imp-compose-identity" name="identity">
+  <form id="imp-compose-form">
+   <input type="hidden" id="imp-compose-cache" name="composeCache" value="<?php echo $this->h($this->composeCache) ?>" />
+   <div data-role="fieldcontain">
+    <label for="imp-compose-identity"><?php echo _("From:") ?></label>
+    <select id="imp-compose-identity" name="identity">
 <?php foreach ($this->identities as $identity): ?>
-    <option value="<?php echo $this->h($identity['val']) ?>"<?php if ($identity['sel']) echo ' selected="selected"' ?>><?php echo $this->h($identity['label']) ?></option>
+     <option value="<?php echo $this->h($identity['val']) ?>"<?php if ($identity['sel']) echo ' selected="selected"' ?>><?php echo $this->h($identity['label']) ?></option>
 <?php endforeach ?>
-   </select>
+    </select>
 
-   <label for="imp-compose-to"><?php echo _("To:") ?></label>
-   <input type="text" id="imp-compose-to" name="to" />
+    <label for="imp-compose-to"><?php echo _("To:") ?></label>
+    <input type="text" id="imp-compose-to" name="to" />
+    <ul id="imp-compose-suggestions" data-role="listview" data-inset="true"></ul>
 
-   <ul id="imp-compose-suggestions" data-role="listview" data-inset="true"></ul>
-  </div>
+    <label for="imp-compose-subject"><?php echo _("Subject:") ?></label>
+    <input type="text" id="imp-compose-subject" name="subject" />
 
-  <div data-role="fieldcontain">
-   <label for="imp-compose-subject"><?php echo _("Subject:") ?></label>
-   <input type="text" id="imp-compose-subject" name="subject" />
-
-   <label for="imp-compose-message"><?php echo _("Text:") ?></label>
-   <textarea id="imp-compose-message" name="message" rows="15" class="fixed"></textarea>
-  </div>
- </form>
+    <label for="imp-compose-message"><?php echo _("Text:") ?></label>
+    <textarea id="imp-compose-message" name="message" rows="15" class="fixed"></textarea>
+   </div>
+  </form>
+ </div>
 
  <div data-role="footer" class="ui-bar">
   <a href="#" id="imp-compose-submit"><?php echo _("Send Message") ?></a>
