5bd2ecdc16c4ca44dc021f4d4338e07e6b4a4f8f
Michael M Slusarz <slusarz@horde.org>
2012-09-07 13:07:47 -0600

    Add swipebutton plugin

---
+++ framework/Core/js/jquery.mobile/plugins/swipebutton.js
@@ -0,0 +1,94 @@
+/*
+* jquery.swipeButton.js - v1.2.0 - 2012-05-31
+* http://andymatthews.net/code/swipebutton/
+*
+* Altered by the Horde Project (c) 2012
+*
+* - Original released under MIT license -
+*
+* Copyright (c) 2012 andy matthews
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following
+* conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+(function($){
+
+    $.fn.swipeButton = function(o) {
+
+        o = $.extend({}, $.fn.swipeButton.defaults, o);
+
+        var buttonDiv = 'divSwipe',
+            removeButtonDiv = function(d) {
+                d.hide(200, function() { d.remove(); });
+            };
+
+        this.on('click', 'li', function(e) {
+            removeButtonDiv($(e.currentTarget).find('.' + buttonDiv));
+        });
+
+        return this.on(o.action, 'li', function(e) {
+            var div, h,
+                ob = { buttons: [] },
+                $li = $(e.currentTarget),
+                existing = $(e.delegateTarget).find('.' + buttonDiv),
+                add = !(existing.parent().is($li).length);
+
+            // Remove currently displayed buttons
+            removeButtonDiv(existing);
+
+            // Add buttons, if needed
+            if (add) {
+                $li.trigger('swipebutton', ob);
+
+                if (ob.buttons.length) {
+                    h = $li.height();
+
+                    div = $('<div></div>')
+                        .addClass(buttonDiv)
+                        .hide()
+                        .height(h)
+                        .css({
+                            background: '#ddd',
+                            position: 'absolute',
+                            textAlign: 'center',
+                            width: '100%',
+                            zIndex: 5
+                        });
+
+                    $.each(ob.buttons, function(k, v) {
+                        div.append(v);
+                    });
+
+                    $li.prepend(div);
+                    div.show(200);
+                }
+            }
+
+            return false;
+        });
+    };
+
+    $.fn.swipeButton.defaults = {
+        action: 'swiperight'
+    };
+
+}(jQuery));
