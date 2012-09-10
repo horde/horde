e9f1ddfd04d95ee1b772b01129cc837b0f705cb1
Michael J Rubinsky <mrubinsk@horde.org>
2012-09-10 14:58:11 -0400

    Remove unused template

--- nag/templates/smartmobile/create.html.php
+++
@@ -1,6 +0,0 @@
-<div data-role="page" id="create">
- <?php echo $this->smartmobileHeader(array('title' => $this->h($this->create_title))) ?>
- <div data-role="content">
-  <?php $this->create_form->renderActive(); ?>
- </div>
-</div>
