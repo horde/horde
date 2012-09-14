12518c6217057e145aae8a08390f28121d470133
Michael J Rubinsky <mrubinsk@horde.org>
2012-09-14 10:21:48 -0400

    Bug: 11421 Re-add this as a dialog page.
    Got lost/changed somewhere along the line.

--- kronolith/templates/smartmobile/event.html.php
+++ kronolith/templates/smartmobile/event.html.php
@@ -1,4 +1,4 @@
-<div data-role="page" data-theme="b" id="eventview">
+<div data-role="dialog" data-theme="b" id="eventview">
  <?php echo $this->smartmobileHeader(array('title' => _("Event"))) ?>
  <div data-role="content" class="ui-body"></div>
 </div>
