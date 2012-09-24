85d0bbee7d5a95da93ae4085e3ee1b2155691ecf
Michael J Rubinsky <mrubinsk@horde.org>
2012-09-24 10:03:24 -0400

    Development mode for ansel-2.0.3

--- ansel/lib/Application.php
+++ ansel/lib/Application.php
@@ -33,7 +33,7 @@ class Ansel_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H4 (2.0.2)';
+    public $version = 'H4 (2.0.3-git)';
 
     /**
      * Global variables defined:
