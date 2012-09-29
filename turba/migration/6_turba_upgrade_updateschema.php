683cbcd661e797a2afc42b7d097460e6fb4eee9a
Michael J Rubinsky <mrubinsk@horde.org>
2012-09-28 21:21:01 -0400

    Fix column name in down migration.

--- turba/migration/6_turba_upgrade_updateschema.php
+++ turba/migration/6_turba_upgrade_updateschema.php
@@ -69,10 +69,10 @@ class TurbaUpgradeUpdateSchema extends Horde_Db_Migration_Base
         $this->removeColumn('turba_objects', 'object_department');
         $this->removeColumn('turba_objects', 'object_spouse');
         $this->removeColumn('turba_objects', 'object_homefax');
-        $this->removeColumn('turba_objects', 'object_anniversary');
         $this->removeColumn('turba_objects', 'object_nickname');
         $this->removeColumn('turba_objects', 'object_assistantphone');
         $this->removeColumn('turba_objects', 'object_imaddress');
+        $this->removeColumn('turba_objects', 'object_imaddress2');
         $this->removeColumn('turba_objects', 'object_imaddress3');
     }
 
