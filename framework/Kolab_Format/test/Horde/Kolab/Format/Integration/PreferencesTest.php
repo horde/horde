18d407caef2dc4e28ecf38eefaa4c71343bcab8f
Jan Schneider <jan@horde.org>
2012-10-03 15:12:53 +0200

    We don't want a list when using <categories> in this object type.

--- framework/Kolab_Format/test/Horde/Kolab/Format/Integration/PreferencesTest.php
+++ framework/Kolab_Format/test/Horde/Kolab/Format/Integration/PreferencesTest.php
@@ -57,7 +57,7 @@ extends Horde_Kolab_Format_TestCase
         $object = array(
             'uid' => 1,
             'pref' => array('test'),
-            'categories' => array('Test')
+            'categories' => 'Test'
         );
         $xml = $preferences->save($object, array('previous' => $xml));
         $expect = file_get_contents(
@@ -76,7 +76,7 @@ extends Horde_Kolab_Format_TestCase
         $object = array(
             'uid' => 1,
             'pref' => array('test'),
-            'categories' => array('Test'),
+            'categories' => 'Test',
             'creation-date' => 1
         );
         $xml = $preferences->save($object);
