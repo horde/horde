086dd0cb24762c41f0442930680cbdffa714b855
Jan Schneider <jan@horde.org>
2012-10-04 19:42:27 +0200

    Make those notifications stick.

--- hermes/entry.php
+++ hermes/entry.php
@@ -49,10 +49,10 @@ case 'hermes_form_time_entry':
         $form->getInfo($vars, $info);
         try {
             if ($vars->exists('id')) {
-                $notification->push(_("Your time was successfully updated."), 'horde.success');
+                $notification->push(_("Your time was successfully updated."), 'horde.success', array('sticky'));
                 $GLOBALS['injector']->getInstance('Hermes_Driver')->updateTime(array($info));
             } else {
-                $notification->push(_("Your time was successfully entered."), 'horde.success');
+                $notification->push(_("Your time was successfully entered."), 'horde.success', array('sticky'));
                 $GLOBALS['injector']->getInstance('Hermes_Driver')->enterTime($GLOBALS['registry']->getAuth(), $info);
             }
         } catch (Exception $e) {
