92ab115922417ecdd2c5c6d4b35b887a6cbad7f5
Jan Schneider <jan@horde.org>
2012-10-12 23:08:17 +0200

    Released imp-6.0.0beta4

--- imp/lib/Application.php
+++ imp/lib/Application.php
@@ -59,7 +59,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (6.0.0-git)';
+    public $version = 'H5 (6.0.0beta4)';
 
     /**
      * Server key used in logged out session.
