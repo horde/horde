c768d68e5df4874a97e640d1cde0a655fc7fb3bb
Michael M Slusarz <slusarz@horde.org>
2012-10-15 16:13:16 -0600

    [mms] Update CKEditor to v2.6.5.

---
+++ framework/Editor/js/plugins/devtools/lang/ku.js
@@ -0,0 +1,1 @@
+CKEDITOR.plugins.setLang("devtools","ku",{devTools:{title:"زانیاری توخم",dialogName:"ناوی پهنجهرهی دیالۆگ",tabName:"ناوی بازدهر تاب",elementId:"ناسنامهی توخم",elementType:"جۆری توخم"}});
--- framework/Editor/js/plugins/placeholder/lang/da.js
+++ framework/Editor/js/plugins/placeholder/lang/da.js
@@ -1,1 +1,1 @@
-CKEDITOR.plugins.setLang("placeholder","da",{placeholder:{title:"Egenskaber for pladsholder",toolbar:"Opret pladsholder",text:"Tekst til pladsholder",edit:"Rediger pladsholder",textMissing:"Pladsholder skal indeholde tekst"}});
+CKEDITOR.plugins.setLang("placeholder","da",{placeholder:{title:"Egenskaber for pladsholder",toolbar:"Opret pladsholder",text:"Tekst til pladsholder",edit:"Redigér pladsholder",textMissing:"Pladsholder skal indeholde tekst"}});
