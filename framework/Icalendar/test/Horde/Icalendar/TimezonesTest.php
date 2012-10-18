25015103cbf4f2fe9c8fd3688745c843ef382f4f
Jan Schneider <jan@horde.org>
2012-10-18 16:09:41 +0200

    Make timezone tests work again.

---
+++ framework/Icalendar/test/Horde/Icalendar/TimezonesTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @category   Horde
+ * @package    Icalendar
+ * @subpackage UnitTests
+ */
+
+require_once __DIR__ . '/Autoload.php';
+
+/**
+ * @category   Horde
+ * @package    Icalendar
+ * @subpackage UnitTests
+ */
+class Horde_Icalendar_TimezonesTest extends Horde_Test_Case
+{
+    public function setUp()
+    {
+        date_default_timezone_set('UTC');
+    }
+
+    public function testFiles()
+    {
+        $test_files = glob(__DIR__ . '/fixtures/vTimezone/*.ics');
+        foreach ($test_files as $file) {
+            $result = '';
+            $ical = new Horde_Icalendar();
+            $ical->parsevCalendar(file_get_contents($file));
+            foreach ($ical->getComponents() as $component) {
+                if ($component->getType() != 'vEvent') {
+                    continue;
+                }
+                $date = $component->getAttribute('DTSTART');
+                if (is_array($date)) {
+                    continue;
+                }
+                $result .= str_replace("\r", '', $component->getAttribute('SUMMARY')) . "\n";
+                $d = new Horde_Date($date);
+                $result .= $d->format('H:i') . "\n";
+            }
+            $this->assertStringEqualsFile(
+                __DIR__ . '/fixtures/vTimezone/' . basename($file, 'ics') . 'txt',
+                $result,
+                'Failed parsing file ' . basename($file));
+        }
+    }
+}
