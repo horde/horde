e354b4d753fe20aea49108603742675c506b0816
Jan Schneider <jan@horde.org>
2012-10-18 11:03:11 +0200

    Fix loading js files (Bug #10795).

--- turba/lib/Prefs/Special/Columnselect.php
+++ turba/lib/Prefs/Special/Columnselect.php
@@ -18,18 +18,17 @@ class Turba_Prefs_Special_Columnselect implements Horde_Core_Prefs_Ui_Special
      */
     public function init(Horde_Core_Prefs_Ui $ui)
     {
-        global $page_output;
-
-        $page_output->addScriptFile('scriptaculous/effects.js', 'horde');
-        $page_output->addScriptFile('scriptaculous/dragdrop.js', 'horde');
-        $page_output->addScriptFile('columnprefs.js');
     }
 
     /**
      */
     public function display(Horde_Core_Prefs_Ui $ui)
     {
-        global $attributes, $cfgSources, $injector, $prefs;
+        global $attributes, $cfgSources, $injector, $page_output, $prefs;
+
+        $page_output->addScriptFile('scriptaculous/effects.js', 'horde');
+        $page_output->addScriptFile('scriptaculous/dragdrop.js', 'horde');
+        $page_output->addScriptFile('columnprefs.js');
 
         $sources = Turba::getColumns();
 
