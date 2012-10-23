e54bd48f30975414d16be6674d2481b7f968b750
Michael M Slusarz <slusarz@horde.org>
2012-10-23 02:31:43 -0600

    Add some tests for Ids object

--- framework/Imap_Client/test/Horde/Imap/Client/IdsTest.php
+++ framework/Imap_Client/test/Horde/Imap/Client/IdsTest.php
@@ -102,6 +102,61 @@ class Horde_Imap_Client_IdsTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function testRangeGeneration()
+    {
+        $ids = new Horde_Imap_Client_Ids('100,200,300');
+
+        $this->assertEquals(
+            '100:300',
+            $ids->range_string
+        );
+
+        $ids = new Horde_Imap_Client_Ids(Horde_Imap_Client_Ids::ALL);
+
+        $this->assertEquals(
+            '',
+            $ids->range_string
+        );
+    }
+
+    public function testSorting()
+    {
+        $ids = new Horde_Imap_Client_Ids('14,12,10');
+
+        $this->assertEquals(
+            '14,12,10',
+            $ids->tostring
+        );
+        $this->assertEquals(
+            '10,12,14',
+            $ids->tostring_sort
+        );
+    }
+
+    public function testSpecialIdValueStringRepresentations()
+    {
+        $ids = new Horde_Imap_Client_Ids(Horde_Imap_Client_Ids::ALL);
+
+        $this->assertEquals(
+            '1:*',
+            $ids->tostring
+        );
+
+        $ids = new Horde_Imap_Client_Ids(Horde_Imap_Client_Ids::SEARCH_RES);
+
+        $this->assertEquals(
+            '$',
+            $ids->tostring
+        );
+
+        $ids = new Horde_Imap_Client_Ids(Horde_Imap_Client_Ids::LARGEST);
+
+        $this->assertEquals(
+            '*',
+            $ids->tostring
+        );
+    }
+
     public function testPop3SequenceStringGenerate()
     {
         $this->assertEquals(
