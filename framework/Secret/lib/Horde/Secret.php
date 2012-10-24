467831f88724b48f8687d025000adf6886566bc4
Michael M Slusarz <slusarz@horde.org>
2012-10-24 10:09:20 -0600

    [mms] Limit decryption/encryption key to 56 bytes (Bug #11566).
    Conflicts:
    	framework/Secret/lib/Horde/Secret.php
    	framework/Secret/package.xml

--- framework/Secret/lib/Horde/Secret.php
+++ framework/Secret/lib/Horde/Secret.php
@@ -96,7 +96,8 @@ class Horde_Secret
     /**
      * Returns the cached crypt object.
      *
-     * @param string $key  The key to use for [de|en]cryption.
+     * @param string $key  The key to use for [de|en]cryption. Only the first
+     *                     56 bytes of this string is used.
      *
      * @return Crypt_Blowfish  The crypt object.
      * @throws Horde_Secret_Exception
@@ -107,9 +108,11 @@ class Horde_Secret
             throw new Horde_Secret_Exception('Key must be a string', 2);
         }
 
-        if (strlen($key) > 56) {
-            throw new Horde_Secret_Exception('Key must be less than 56 characters and non-zero. Supplied key length: ' . strlen($key), 3);
+        if (!strlen($key)) {
+            throw new Horde_Secret_Exception('Key must be non-zero.', 3);
         }
+
+        $key = substr($key, 0, 56);
 
         $idx = hash('md5', $key);
         if (!isset($this->_cipherCache[$idx])) {
