b5c79a148bbdda8c76321b8fd1d89ebc4f44d292
Michael M Slusarz <slusarz@horde.org>
2012-10-30 13:40:54 -0600

    6.1 version

--- imp/lib/Application.php
+++ imp/lib/Application.php
@@ -59,7 +59,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (6.0.1-git)';
+    public $version = 'H5 (6.1.0-git)';
 
     /**
      * Server key used in logged out session.
