b8d50825cb7bf9e6244159aa126057e36d5be707
Michael M Slusarz <slusarz@horde.org>
2012-11-02 02:09:32 -0600

    Move this method to Horde_Crypt_Pgp, where it belongs.

--- imp/lib/Crypt/Pgp.php
+++ imp/lib/Crypt/Pgp.php
@@ -754,33 +754,4 @@ class IMP_Crypt_Pgp extends Horde_Crypt_Pgp
         return $out;
     }
 
-    /**
-     * Generates a public key from a private key.
-     *
-     * @todo Move to Horde_Crypt
-     *
-     * @param string $data  Armor text of private key.
-     *
-     * @return string  Armor text of public key, or null if it could not be
-     *                 generated.
-     */
-    public function getPublicKeyFromPrivateKey($data)
-    {
-        $keyring = $this->_putInKeyring(array($data), 'private');
-        $fingerprints = $this->getFingerprintsFromKey($data);
-        reset($fingerprints);
-
-        $cmdline = array(
-            '--armor',
-            '--export',
-            key($fingerprints)
-        );
-
-        $result = $this->_callGpg($cmdline, 'r', array(), true, true);
-
-        return empty($result->output)
-            ? null
-            : $result->output;
-    }
-
 }
