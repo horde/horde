53dbe382846df93a0cda2e048702645d08b392f3
Michael M Slusarz <slusarz@horde.org>
2012-11-03 10:58:34 -0600

    [mms] Correctly show ACL entry for current user even if ACL response for mailbox is empty.
    It is possible ACL response will be empty, but we need to explicitly
    check via MYRIGHTS to see if the user really does have ACL rights to the
    mailbox.

--- imp/lib/Imap/Acl.php
+++ imp/lib/Imap/Acl.php
@@ -53,12 +53,27 @@ class IMP_Imap_Acl
     {
         try {
             $imp_imap = $GLOBALS['injector']->getInstance('IMP_Factory_Imap')->create();
-            return $user
-                ? $imp_imap->getMyACLRights($mbox)
-                : $imp_imap->getACL($mbox);
+            if ($user) {
+                $ret = $imp_imap->getMyACLRights($mbox);
+            } else {
+                $ret = $imp_imap->getACL($mbox);
+                $user = $imp_imap->getParam('username');
+                if (!isset($ret[$user]) &&
+                    ($acl = $this->getACL($mbox, true))) {
+                    $ret[$user] = $acl;
+                }
+            }
         } catch (IMP_Imap_Exception $e) {
-            throw new IMP_Exception(_("Could not retrieve ACL"));
+            switch ($e->getCode()) {
+            case $e::NOPERM:
+                throw new IMP_Exception(_("You do not have permission to view the ACLs on this mailbox."));
+
+            default:
+                throw new IMP_Exception(_("Could not retrieve ACL."));
+            }
         }
+
+        return $ret;
     }
 
     /**
