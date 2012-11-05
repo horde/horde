347a422293ce87dc1b43172ddc1aed8de587aa7c
Jan Schneider <jan@horde.org>
2012-11-05 19:16:14 +0100

    [jan] Fix notice when parsing Etc/UTC rules.

--- framework/Timezone/test/Horde/Timezone/IcalendarTest.php
+++ framework/Timezone/test/Horde/Timezone/IcalendarTest.php
@@ -14,24 +14,41 @@ require_once __DIR__ . '/Autoload.php';
 
 class Horde_Timezone_IcalendarTest extends Horde_Test_Case
 {
-    public function testSomething()
+    public function testEurope()
     {
-        $tz = new Horde_Timezone_Mock();
+        $tz = new Horde_Timezone_Mock('europe');
         $this->assertStringEqualsFile(
             __DIR__ . '/fixtures/europe.ics',
             $tz->getZone('Europe/Jersey')->toVtimezone()->exportVcalendar()
+        );
+    }
+
+    public function testEtc()
+    {
+        $tz = new Horde_Timezone_Mock('etcetera');
+        $this->assertStringEqualsFile(
+            __DIR__ . '/fixtures/etcetera.ics',
+            $tz->getZone('Etc/UTC')->toVtimezone()->exportVcalendar()
         );
     }
 }
 
 class Horde_Timezone_Mock extends Horde_Timezone
 {
+    protected $_zone;
+
+    public function __construct($zone)
+    {
+        parent::__construct();
+        $this->_zone = $zone;
+    }
+
     protected function _download()
     {
     }
 
     protected function _extractAndParse()
     {
-        $this->_parse(file_get_contents(__DIR__ . '/fixtures/europe'));
+        $this->_parse(file_get_contents(__DIR__ . '/fixtures/' . $this->_zone));
     }
 }
