ee18a08557c8123d2dad0849e0bd269084f469b1
Michael J Rubinsky <mrubinsk@horde.org>
2012-11-09 12:15:01 -0500

    Revert "QRESYNC -> CONDSTORE"
    This reverts commit 17cf850a888fdceda3cb02979ce12ff9533be1b5.
    
    Not yet ready for prime time.
    
    Conflicts:
    	framework/ActiveSync/lib/Horde/ActiveSync/Folder/Imap.php

--- framework/ActiveSync/lib/Horde/ActiveSync/Folder/Imap.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Folder/Imap.php
@@ -36,7 +36,10 @@ class Horde_ActiveSync_Folder_Imap extends Horde_ActiveSync_Folder_Base
     const COUNT       = 'messages';
 
     /**
-     * The folder's current message list.
+     * The folder's current message list. Only used for servers that do not
+     * support QRESYNC.
+     *
+     * An array of UIDs.
      *
      * @var array
      */
@@ -78,7 +81,7 @@ class Horde_ActiveSync_Folder_Imap extends Horde_ActiveSync_Folder_Base
 
     /**
      * Cache the known lowest UID we received during the initial SYNC request.
-     * Only available (or even needed) if server supports CONDSTORE. This value
+     * Only available (or even needed) if server supports QRESYNC. This value
      * will never change unless the syncstate is removed.
      *
      * @var integer
@@ -165,7 +168,7 @@ class Horde_ActiveSync_Folder_Imap extends Horde_ActiveSync_Folder_Base
      */
     public function updateState()
     {
-        // If we support CONDSTORE, do not bother keeping a cache of messages,
+        // If we support QRESYNC, do not bother keeping a cache of messages,
         // since we do not need them.
         if ($this->modseq() == 0) {
             $this->_messages = array_diff(array_keys($this->_messages), $this->_removed);
