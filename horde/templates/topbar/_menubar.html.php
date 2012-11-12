27ef3f883dd0e27d4c4ee1b59483ef12742efab3
Jan Schneider <jan@horde.org>
2012-11-12 18:56:10 +0100

    [jan] Allow to set custom search field label.

--- horde/templates/topbar/_menubar.html.php
+++ horde/templates/topbar/_menubar.html.php
@@ -18,7 +18,7 @@
         <input autocomplete="off" id="horde-search-input" type="text" />
       </div>
 <?php else: ?>
-      <input type="text" id="horde-search-input" name="searchfield" class="formGhost" title="<?php echo _("Search") ?>" />
+      <input type="text" id="horde-search-input" name="searchfield" class="formGhost" title="<?php echo $this->searchLabel ?>" />
 <?php endif ?>
       <input type="image" id="horde-search-icon" src="<?php echo $this->searchIcon ?>" />
     </form>
