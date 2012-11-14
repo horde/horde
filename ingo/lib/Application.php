2ea24511f5fa0672a03bd2f2cd982bda5c78bd67
Jan Schneider <jan@horde.org>
2012-11-14 18:00:05 +0100

    Released ingo-2.0.10

--- ingo/lib/Application.php
+++ ingo/lib/Application.php
@@ -40,7 +40,7 @@ class Ingo_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H4 (2.0.10-git)';
+    public $version = 'H4 (2.0.10)';
 
     /**
      * Global variables defined:
