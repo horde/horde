9096dd2d086c69ca9bfac5327fb4fe96f310d47e
Jan Schneider <jan@horde.org>
2012-11-14 18:02:11 +0100

    Released turba-3.0.16

--- turba/lib/Application.php
+++ turba/lib/Application.php
@@ -38,7 +38,7 @@ class Turba_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H4 (3.0.15)';
+    public $version = 'H4 (3.0.16)';
 
     /**
      * Global variables defined:
