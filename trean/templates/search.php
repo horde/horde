8db8ebf68b6d3e03d01d0b38fcdea187c6075ce0
Jan Schneider <jan@horde.org>
2012-11-20 15:23:01 +0100

    No longer used.

--- trean/templates/search.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!count($bookmarks)) {
-    echo '<p><em>' . _("No Bookmarks found") . '</em></p>';
-} else {
-    $view = new Trean_View_BookmarkList($bookmarks);
-    $view->showTagBrowser(false);
-    echo $view->render($search_title);
-}
