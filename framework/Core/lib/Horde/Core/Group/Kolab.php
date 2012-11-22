ac49a602394d8b6d3daad1ca38b74b3c0730ad6f
Gunnar Wrobel <p@rdus.de>
2012-11-22 21:16:23 +0100

    [gwr] Add missing core wrapper for Kolab group driver (Bug #11737).

---
+++ framework/Core/lib/Horde/Core/Group/Kolab.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This class provides Horde-specific code that extends the base Kolab driver.
+ *
+ * Copyright 2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Gunnar Wrobel <wrobel@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Core
+ */
+class Horde_Core_Group_Kolab extends Horde_Core_Group_Ldap
+{
+}
