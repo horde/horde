246e56e8a6c42f9fdf1ced9bd520fcd91b87b2b1
Chuck Hagenbuch <chuck@horde.org>
2012-11-26 00:30:20 -0500

    Put related tags back on this view

--- trean/app/controllers/BrowseByTag.php
+++ trean/app/controllers/BrowseByTag.php
@@ -9,7 +9,6 @@ class Trean_BrowseByTag_Controller extends Horde_Controller_Base
 
         $tagBrowser = new Trean_TagBrowser($this->getInjector()->getInstance('Trean_Tagger'), $tag);
         $view = new Trean_View_BookmarkList(null, $tagBrowser);
-        $view->showTagBrowser(false);
 
         $page_output = $this->getInjector()->getInstance('Horde_PageOutput');
         $notification = $this->getInjector()->getInstance('Horde_Notification');
