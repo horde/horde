3fc2d3e536c95552a57d30776fed08b93d6679ff
Jan Schneider <jan@horde.org>
2012-11-27 13:04:07 +0100

    Released nag-4.0.1

--- nag/lib/Application.php
+++ nag/lib/Application.php
@@ -40,7 +40,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.0.1-git)';
+    public $version = 'H5 (4.0.1)';
 
     /**
      * Global variables defined:
