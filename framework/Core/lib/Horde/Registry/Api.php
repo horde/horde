faeb4f51549eebdbd2f5acde224da5145dd69db9
Michael M Slusarz <slusarz@horde.org>
2012-11-29 01:14:54 -0700

    Cache methods list

--- framework/Core/lib/Horde/Registry/Api.php
+++ framework/Core/lib/Horde/Registry/Api.php
@@ -34,6 +34,13 @@ class Horde_Registry_Api
     protected $_links = array();
 
     /**
+     * Cached list of methods.
+     *
+     * @var array
+     */
+    protected $_methods = array();
+
+    /**
      * The listing of API calls that do not require permissions checking.
      *
      * @var array
@@ -75,18 +82,18 @@ class Horde_Registry_Api
      */
     final public function methods()
     {
-        $disabled = $this->disabled();
-        $methods = array();
-
-        $reflect = new ReflectionClass($this);
-        foreach ($reflect->getMethods(ReflectionMethod::IS_PUBLIC) as $val) {
-            if (($val->getDeclaringClass()->name != __CLASS__) &&
-                !in_array($val->name, $disabled)) {
-                $methods[] = $val->name;
+        if (empty($this->_methods)) {
+            $disabled = $this->disabled();
+            $reflect = new ReflectionClass($this);
+            foreach ($reflect->getMethods(ReflectionMethod::IS_PUBLIC) as $v) {
+                if (($v->getDeclaringClass()->name != __CLASS__) &&
+                    !in_array($v->name, $disabled)) {
+                    $this->_methods[] = $v->name;
+                }
             }
         }
 
-        return $methods;
+        return $this->_methods;
     }
 
     /**
