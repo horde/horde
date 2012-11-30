0c820cf2f6fff2a7dbbd366e2f1ab1f2f2e7384d
Michael M Slusarz <slusarz@horde.org>
2012-11-30 13:49:39 -0700

    [mms] Fix Openssl driver for PHP 5.3.0-5.3.2 (Bug #11797).

--- framework/Crypt_Blowfish/lib/Horde/Crypt/Blowfish/Openssl.php
+++ framework/Crypt_Blowfish/lib/Horde/Crypt/Blowfish/Openssl.php
@@ -33,16 +33,24 @@ class Horde_Crypt_Blowfish_Openssl extends Horde_Crypt_Blowfish_Base
      */
     public function encrypt($text)
     {
-        // Need to mask error output, since an invalid warning message was
-        // issued prior to 5.3.4 for empty IVs.
-        return @openssl_encrypt($text, 'bf-' . $this->cipher, $this->key, true, strval($this->iv));
+        if (PHP_VERSION_ID <= 50302) {
+            return @openssl_encrypt($text, 'bf-' . $this->cipher, $this->key, true);
+        } elseif (PHP_VERSION_ID == 50303) {
+            // Need to mask error output, since an invalid warning message was
+            // issued prior to 5.3.4 for empty IVs in ECB mode.
+            return @openssl_encrypt($text, 'bf-' . $this->cipher, $this->key, true, strval($this->iv));
+        }
+
+        return openssl_encrypt($text, 'bf-' . $this->cipher, $this->key, true, strval($this->iv));
     }
 
     /**
      */
     public function decrypt($text)
     {
-        return openssl_decrypt($text, 'bf-' . $this->cipher, $this->key, true, strval($this->iv));
+        return (PHP_VERSION_ID <= 50302)
+            ? openssl_decrypt($text, 'bf-' . $this->cipher, $this->key, true)
+            : openssl_decrypt($text, 'bf-' . $this->cipher, $this->key, true, strval($this->iv));
     }
 
 }
