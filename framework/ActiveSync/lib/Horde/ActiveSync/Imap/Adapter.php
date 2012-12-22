horde/horde

commit 1633b590a0a024f9a329e4aad5cff7b5f0d43f93

Bug: 11754 Server may advertise CONDSTORE but not support per mailbox MODSEQ.

framework/ActiveSync/lib/Horde/ActiveSync/Imap/Adapter.php
@@ -307,8 +307,8 @@ class Horde_ActiveSync_Imap_Adapter
                 }
                 $folder->setChanges($search_ret['match']->ids, $flags);
             }
-        } elseif (!$condstore) {
-            $this->_logger->debug('NO CONDSTORE: ' . $folder->minuid);
+        } else {
+            $this->_logger->debug('NO CONDSTORE or per mailbox MODSEQ: ' . $folder->minuid);
             $query = new Horde_Imap_Client_Search_Query();
             $search_ret = $imap->search(
                 $mbox,

commit 0fcebebcc8f30fe5a0332b3dcd366fe0bafc0590

Fix regression introduced with 1633b590a0a024f9a329e4aad5cff7b5f0d43f93:
If server *does* support per mailbox mod-sequences but there were no
changes with this SYNC request, we were fooled into thinking we
did not support mod-sequences. This prevents unnecessary changes being
sent to client.

framework/ActiveSync/lib/Horde/ActiveSync/Imap/Adapter.php
@@ -307,7 +307,7 @@ class Horde_ActiveSync_Imap_Adapter
                 }
                 $folder->setChanges($search_ret['match']->ids, $flags);
             }
-        } else {
+        } elseif (!$condstore || ($condstore && $modseq == 0)) {
             $this->_logger->debug('NO CONDSTORE or per mailbox MODSEQ: ' . $folder->minuid);
             $query = new Horde_Imap_Client_Search_Query();
             $search_ret = $imap->search(