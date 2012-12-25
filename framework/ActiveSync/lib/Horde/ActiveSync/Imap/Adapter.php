5468598e4a91b2e47b16f7a71a46b49e34c477e8
Michael J Rubinsky <mrubinsk@horde.org>
2012-12-25 16:54:07 -0500

    Revert "Don't use regex to avoid php segfaults here."
    This reverts commit 910bc6df306c40478ff2ae5d73607772872532fc.

--- framework/ActiveSync/lib/Horde/ActiveSync/Imap/Adapter.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Imap/Adapter.php
@@ -1218,18 +1218,7 @@ class Horde_ActiveSync_Imap_Adapter
      */
     protected function _stripNon7BitChars($text)
     {
-        // Don't use regex to avoid php bug that causes segfaults.
-        $text = strval($text);
-        $output = '';
-        for ($i = 0, $len = strlen($text); $i < $len; ++$i) {
-            if (ord($text[$i]) > 128) {
-                continue;
-            } else {
-                $output .= $text[$i];
-            }
-        }
-
-        return $output;
+        return preg_replace('/[^\x09\x0A\x0D\x20-\x7E]/', '', $text);
     }
 
 }
