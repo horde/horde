1f01d122f8d21e37a1cf966b10b3296c38ac5662
Jan Schneider <jan@horde.org>
2013-01-10 16:23:20 +0100

    Development mode for gollem-3.0.0RC3

--- gollem/lib/Application.php
+++ gollem/lib/Application.php
@@ -50,7 +50,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.0RC2)';
+    public $version = 'H5 (3.0.0-git)';
 
     /**
      * Server key used in logged out session.
