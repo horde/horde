730440e4922f7cd02a1e039727d312ee72c3df52
Michael J Rubinsky <mrubinsk@horde.org>
2013-01-10 20:00:55 -0500

    Can't mock objects with typehints that are not autoloadable.

--- framework/Core/test/Horde/Core/ActiveSyncTests.php
+++ framework/Core/test/Horde/Core/ActiveSyncTests.php
@@ -65,8 +65,8 @@ class Horde_Core_ActiveSyncTests extends Horde_Test_Case
 
     public function testGetFolders()
     {
-        $connector = $this->getMockSkipConstructor('Horde_Core_ActiveSync_Connector');
-        $connector->expects($this->once())->method('horde_listApis')->will($this->returnValue(array('mail')));
+        $connector = new MockConnector();
+
         $adapter = $this->getMockSkipConstructor('Horde_ActiveSync_Imap_Adapter');
         $adapter->expects($this->once())->method('getMailboxes')->will($this->returnValue($this->_mailboxes));
         $adapter->expects($this->any())->method('getSpecialMailboxes')->will($this->returnValue($this->_special));
@@ -104,6 +104,24 @@ class Horde_Core_ActiveSyncTests extends Horde_Test_Case
             }
         }
     }
+}
+
+/**
+ * Mock Connector. Can't mock it since it contain type hints for objects from
+ * other libraries (which causes PHPUnit to have a fit).
+ *
+ */
+class MockConnector extends Horde_Core_ActiveSync_Connector
+{
+    public function __construct()
+    {
+    }
+
+    public function horde_listApis()
+    {
+        return array('mail');
+    }
+
 }
 
 /**
