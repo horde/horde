910cf0b16d2b7b38455537a8aec6f3234c218a52
Michael M Slusarz <slusarz@horde.org>
2013-01-14 23:55:30 -0700

    coding style

--- imp/attachment.php
+++ imp/attachment.php
@@ -19,7 +19,10 @@
 // We do not need to be authenticated to get the file. Most users won't send
 // linked attachments just to other IMP users.
 require_once __DIR__ . '/lib/Application.php';
-Horde_Registry::appInit('imp', array('authentication' => 'none', 'session_control' => 'none'));
+Horde_Registry::appInit('imp', array(
+    'authentication' => 'none',
+    'session_control' => 'none'
+));
 
 // Lets see if we are even able to send the user an attachment.
 if (!$conf['compose']['link_attachments']) {
