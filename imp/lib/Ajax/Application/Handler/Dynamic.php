7b5c80229adf498b2d8a1f62374ffd7adca563c2
Michael M Slusarz <slusarz@horde.org>
2013-01-18 12:44:06 -0700

    [mms] Fix saving mailbox expansion state in basic folders view (Bug #11979).

--- imp/lib/Ajax/Application/Handler/Dynamic.php
+++ imp/lib/Ajax/Application/Handler/Dynamic.php
@@ -360,53 +360,6 @@ class IMP_Ajax_Application_Handler_Dynamic extends Horde_Core_Ajax_Application_H
     }
 
     /**
-     * AJAX action: Expand mailboxes (saves expanded state in prefs).
-     *
-     * Variables used:
-     *   - mboxes: (string) The list of mailboxes to process (JSON encoded
-     *             array; mailboxes are base64url encoded).
-     *
-     * @return boolean  True.
-     */
-    public function expandMailboxes()
-    {
-        if (!empty($this->vars->mboxes)) {
-            $imptree = $GLOBALS['injector']->getInstance('IMP_Imap_Tree');
-
-            foreach (Horde_Serialize::unserialize($this->vars->mboxes, Horde_Serialize::JSON) as $val) {
-                $imptree->expand(IMP_Mailbox::formFrom($val));
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * AJAX action: Collapse mailboxes.
-     *
-     * Variables used:
-     *   - all: (integer) 1 to show all mailboxes.
-     *   - mboxes: (string) The list of mailboxes to process (JSON encoded
-     *             array; mailboxes are base64url encoded) if 'all' is 0.
-     *
-     * @return boolean  True.
-     */
-    public function collapseMailboxes()
-    {
-        $imptree = $GLOBALS['injector']->getInstance('IMP_Imap_Tree');
-
-        if ($this->vars->all) {
-            $imptree->collapseAll();
-        } elseif (!empty($this->vars->mboxes)) {
-            foreach (Horde_Serialize::unserialize($this->vars->mboxes, Horde_Serialize::JSON) as $val) {
-                $imptree->collapse(IMP_Mailbox::formFrom($val));
-            }
-        }
-
-        return true;
-    }
-
-    /**
      * AJAX action: Modify list of polled mailboxes.
      *
      * Variables used:
