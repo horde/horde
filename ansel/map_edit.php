ac678a45b51b6a2d943eab0204ec46f8621b5588
Michael J Rubinsky <mrubinsk@horde.org>
2013-01-23 12:20:19 -0500

    Use Horde_View, tweak.

--- ansel/map_edit.php
+++ ansel/map_edit.php
@@ -25,115 +25,68 @@ if (empty($image_id)) {
     throw new Ansel_Exception(
         _("An error has occured retrieving the image. Details have been logged."));
 }
+
 $image = $injector
     ->getInstance('Ansel_Storage')
     ->getImage($image_id);
 $gallery = $injector->
     getInstance('Ansel_Storage')
     ->getGallery($image->gallery);
+
 if (!$gallery->hasPermission($GLOBALS['registry']->getAuth(), Horde_Perms::EDIT)) {
     throw new Horde_Exception_PermissionDenied(
         _("Not Authorized. Details have been logged for the server administrator."));
 }
 
+$view = $injector->getInstance('Horde_View');
+$view->image_id = $image_id;
+$view->loadingImg = Horde::img('loading.gif', _("Loading..."));
+$view->filename = $image->filename;
+
 // Determine if we already have a geotag or are we tagging it for the 1st time
 if (empty($image->lat)) {
-    $geodata = array('image_id' => $image->id,
-                     'image_latitude' => "20",
-                     'image_longitude' => "40",
-                     'image_location' => '',
-                     'icon' => Ansel::getImageUrl($image->id, 'mini', true),
-                     'markerOnly' => true,
-                     'draggable' => true);
+    $geodata = array(
+        'image_id' => $image->id,
+        'image_latitude' => '20',
+        'image_longitude' => '40',
+        'image_location' => '',
+        'icon' => Ansel::getImageUrl($image->id, 'mini', true),
+        'markerOnly' => true,
+        'draggable' => true);
     $isNew = 1;
 } else {
-    $geodata = array('image_id' => $image->id,
-                     'image_latitude' => $image->lat,
-                     'image_longitude' => $image->lng,
-                     'image_location' => $image->location,
-                     'icon' => Ansel::getImageUrl($image->id, 'mini', true),
-                     'markerOnly' => true,
-                     'draggable' => true);
+    $geodata = array(
+        'image_id' => $image->id,
+        'image_latitude' => $image->lat,
+        'image_longitude' => $image->lng,
+        'image_location' => $image->location,
+        'icon' => Ansel::getImageUrl($image->id, 'mini', true),
+        'markerOnly' => true,
+        'draggable' => true);
     $isNew = 0;
 }
 
 // JSON representation of the image's geotag
 $json = Horde_Serialize::serialize(array($geodata), Horde_Serialize::JSON);
 
-/* Gettext strings */
-$save = _("Save");
-$returnText = _("Return to Image View");
-$findText = _("Find");
-$fetchingText = _("Fetching location");
-$locateText = _("Locate image at:");
-$errorText = _("Unable to find location. Error code:");
-
-// Links, img src
-$returnLink = Ansel::getUrlFor(
-    'view', array(
-        'view' => 'Image',
-        'image' => $image_id,
-        'gallery' => $gallery->id));
-$image_tag = '<img src="' . Ansel::getImageUrl($image_id, 'thumb', true) . '" alt="[thumbnail]" />';
-
 // Url for geotag ajax helper
 $gtUrl = $registry->getServiceLink('ajax', 'ansel')->setRaw(true);
 $gtUrl->url .= 'imageSaveGeotag';
 
-$loadingImg = Horde::img('loading.gif', _("Loading..."));
-
 // Obtain other geotagged images to possibly locate this image at
-$imgs = $injector->getInstance('Ansel_Storage')
+$view->imgs = $injector
+    ->getInstance('Ansel_Storage')
     ->getRecentImagesGeodata($GLOBALS['registry']->getAuth());
-if (count($imgs) > 0) {
-    $other_images = '<div class="ansel_location_sameas">'
-        . _("Click on a thumbnail to locate at the same point.") . '<br />';
-    foreach ($imgs as $id => $data) {
+
+if (count($view->imgs) > 0) {
+    $js = array();
+    foreach ($view->imgs as $id => $data) {
         if ($id != $image_id) {
-            if (!empty($data['image_location'])) {
-                $iTitle = $data['image_location'];
-            } else {
-                $iTitle = _point2Deg($data['image_latitude'], true)
-                    . ' ' . _point2Deg($data['image_longitude']);
-            }
-            $tempurl = new Horde_Url('#');
-            $other_images .= $tempurl->link(array('title' => $iTitle, 'onclick' => "mapEdit.setLocation('" . $data['image_latitude'] . "', '" . $data['image_longitude'] . "', '" . $data['image_location'] . "');return false")) . '<img src="' . Ansel::getImageUrl($id, 'mini', true) . '" alt="[thumbnail]" /></a>';
+            $js[] = '$("geo_' . $id . '").observe("click", function() { mapEdit.setLocation("' . $data['image_latitude'] . '", "' . $data['image_longitude'] . '", "' . $data['image_location'] . '");return false; } )';
         }
     }
-    $other_images .= '</div>';
-} else {
-    $other_images = '';
+    $page_output->addInlineScript($js, true);
 }
-
-/* Build the HTML */
-$html = <<<EOT
-<div id="status">&nbsp;</div>
-<div style="width:450px;float:left;">
-  <div id="ansel_map" style="width:450px;height:450px;"></div>
-</div>
-<div style="float:left;width:250px;min-height:450px;">
- <div class="control">
-  <h4>{$locateText}</h4>
-  <form>
-   <input type="text" id="locationInput" name="locationInput" />
-   <span id="locationInput_loading_img" style="display:none;">{$loadingImg}</span>
-   <input id="locationAction" value="{$findText}" class="button" type="submit" />
-  </form>
- </div>
- {$other_images}
- <div class="control" style="vertical-align:bottom;">
-  <div style="text-align:center;margin-top:6px;">{$image_tag}</div>
-  <div class="ansel_geolocation">
-   <div id="ansel_locationtext">&nbsp;</div>
-   <div id="ansel_latlng">&nbsp;</div>
-  </div>
- </div>
-</div>
-<div class="clear"></div>
-<div class="control">
- <input class="button" id="saveButton" type="submit" value="{$save}" /><input class="button" type="submit" onclick="window.close();" value="{$returnText}" />
-</div>
-EOT;
 
 $page_output->addInlineScript(
     "var mapEdit = new AnselMapEdit({$json}, {
@@ -143,12 +96,10 @@ $page_output->addInlineScript(
     $('saveButton').observe('click', mapEdit.save.bind(mapEdit));
     $('locationAction').observe('click', function(e) { mapEdit.geocode($('locationInput').value); e.stop(); });",
     true);
-$page_output->topbar = $page_output->sidebar = false;
 
-// Start the output
+$page_output->topbar = $page_output->sidebar = false;
 $page_output->header(array(
     'title' => $title
 ));
-echo '<div class="header">' . sprintf(_("Update position of %s"), $image->filename) . '</div>';
-echo $html;
+echo $view->render('map_edit');
 $page_output->footer();
