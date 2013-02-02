928167c82de3f8a6d274b856b7134c89b1b6ff0d
Michael J Rubinsky <mrubinsk@horde.org>
2013-02-02 13:58:55 -0500

    Actually update the state object so we don't mirror this change back.

--- framework/ActiveSync/lib/Horde/ActiveSync/Connector/Importer.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Connector/Importer.php
@@ -261,6 +261,11 @@ class Horde_ActiveSync_Connector_Importer
             $change['id'] = $uid;
             $change['mod'] = time();
             $change['parent'] = $this->_folderId;
+            $this->_state->updateState(
+                Horde_ActiveSync::CHANGE_TYPE_DELETE,
+                $change,
+                Horde_ActiveSync::CHANGE_ORIGIN_PIM,
+                $this->_backend->getUser());
         }
 
         return array('results' => $results, 'missing' => $missing);
