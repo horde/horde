bf82d412ca0a52a0c9ee4914e338d055dae83b0d
Jan Schneider <jan@horde.org>
2013-02-14 16:36:52 +0100

    Revert "Remove owner from user permissions."
    This reverts commit e3b6a4247cfebf104f940ec95bbf06a2fec68922.

--- framework/Perms/lib/Horde/Perms/Permission/Kolab.php
+++ framework/Perms/lib/Horde/Perms/Permission/Kolab.php
@@ -90,7 +90,6 @@ extends Horde_Perms_Permission
             $element->toHorde($data);
         }
         $data['type'] = 'matrix';
-        unset($data['users'][$this->_storage->getOwner()]);
         return $data;
     }
 
