fbe519238a85e0f70a4239a0e6146e1260377ad9
Michael M Slusarz <slusarz@horde.org>
2013-03-03 16:35:42 -0700

    Better description of what lzf does

--- horde/lib/Test.php
+++ horde/lib/Test.php
@@ -129,7 +129,7 @@ class Horde_Test
         ),
         'lzf' => array(
             'descrip' => 'LZF Compression Support (PECL extension)',
-            'error' => 'If the lzf PECL module is available, Horde can compress some cached data in your session to make your session size smaller (highly recommended).'
+            'error' => 'If the lzf PECL module is available, Horde can perform real-time compression on cached data to reduce session storage load (highly recommended).'
         ),
         'mbstring' => array(
             'descrip' => 'Mbstring Support',
