2951ab196d1ca143b2a597a94c505b52e9114041
o <o@immerda.ch>
2013-03-07 00:05:27 +0100

    generate rsa/rsa keys instead of dsa
    in accordance with gnupg we should create rsa keys instead of
    deprecated dsa/el-gamal keys.
    
    see:
    http://lists.gnupg.org/pipermail/gnupg-devel/2009-May/025079.html

--- framework/Crypt/lib/Horde/Crypt/Pgp.php
+++ framework/Crypt/lib/Horde/Crypt/Pgp.php
@@ -192,7 +192,7 @@ class Horde_Crypt_Pgp extends Horde_Crypt
      * @throws Horde_Crypt_Exception
      */
     public function generateKey($realname, $email, $passphrase, $comment = '',
-                                $keylength = 1024, $expire = null)
+                                $keylength = 1024, $expire = null, $key_type = 'RSA', $subkey_type = 'RSA')
     {
         /* Create temp files to hold the generated keys. */
         $pub_file = $this->_createTempFile('horde-pgp');
@@ -207,9 +207,9 @@ class Horde_Crypt_Pgp extends Horde_Crypt
         $input = array(
             '%pubring ' . $pub_file,
             '%secring ' . $secret_file,
-            'Key-Type: DSA',
+            'Key-Type: ' . $key_type,
             'Key-Length: ' . $keylength,
-            'Subkey-Type: ELG-E',
+            'Subkey-Type: ' . $subkey_type,
             'Subkey-Length: ' . $keylength,
             'Name-Real: ' . $realname,
             'Name-Email: ' . $email,
