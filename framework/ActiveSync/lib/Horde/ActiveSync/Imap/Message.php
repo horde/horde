559496f98a92a0b4a94ec1b84cb3aff9f6d49117
Michael J Rubinsky <mrubinsk@horde.org>
2013-03-06 22:30:08 -0500

    Avoid warnings about modifying an overloaded property.

--- framework/ActiveSync/lib/Horde/ActiveSync/Imap/Message.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Imap/Message.php
@@ -651,7 +651,7 @@ class Horde_ActiveSync_Imap_Message
         if (empty($this->_envelope)) {
             $this->_fetchEnvelope();
         }
-        $r = array_pop($this->_envelope->reply_to->addresses);
+        $r = current($this->_envelope->reply_to->addresses);
         $a = new Horde_Mail_Rfc822_Address($r);
 
         return $a->writeAddress(false);
@@ -667,7 +667,7 @@ class Horde_ActiveSync_Imap_Message
         if (empty($this->_envelope)) {
             $this->_fetchEnvelope();
         }
-        $from = array_pop($this->_envelope->from->addresses);
+        $from = current($this->_envelope->from->addresses);
         $a = new Horde_Mail_Rfc822_Address($from);
 
         return $a->writeAddress(false);
