195fc2507401a8ae05b0020ba450c3c86ae52189
Michael J Rubinsky <mrubinsk@horde.org>
2013-03-12 11:58:53 -0400

    Parse (but don't do anything with yet) the incoming COMPOSEMAIL request.

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/SendMail.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/SendMail.php
@@ -36,6 +36,22 @@
  */
 class Horde_ActiveSync_Request_SendMail extends Horde_ActiveSync_Request_Base
 {
+    const COMPOSEMAIL_SENDMAIL        = 'ComposeMail:SendMail';
+    const COMPOSEMAIL_SMARTFORWARD    = 'ComposeMail:SmartForward';
+    const COMPOSEMAIL_SMARTREPLY      = 'ComposeMail:SmartReply';
+    const COMPOSEMAIL_SAVEINSENTITEMS = 'ComposeMail:SaveInSentItems';
+    const COMPOSEMAIL_REPLACEMIME     = 'ComposeMail:ReplaceMime';
+    const COMPOSEMAIL_TYPE            = 'ComposeMail:Type';
+    const COMPOSEMAIL_SOURCE          = 'ComposeMail:Source';
+    const COMPOSEMAIL_FOLDERID        = 'ComposeMail:FolderId';
+    const COMPOSEMAIL_ITEMID          = 'ComposeMail:ItemId';
+    const COMPOSEMAIL_LONGID          = 'ComposeMail:LongId';
+    const COMPOSEMAIL_INSTANCEID      = 'ComposeMail:InstanceId';
+    const COMPOSEMAIL_MIME            = 'ComposeMail:MIME';
+    const COMPOSEMAIL_CLIENTID        = 'ComposeMail:ClientId';
+    const COMPOSEMAIL_STATUS          = 'ComposeMail:Status';
+    const COMPOSEMAIL_ACCOUNTID       = 'ComposeMail:AccountId';
+
     /**
      * Handle the request
      *
@@ -60,7 +76,31 @@ class Horde_ActiveSync_Request_SendMail extends Horde_ActiveSync_Request_Base
                 throw new Horde_ActiveSync_Exception_InvalidRequest($e->getMessage());
             }
         } else {
-            $this->_logger->err('TODO');
+            // Get the first element and see what type of mail request we have.
+            $e = $this->_decoder->getElement();
+            if ($e[Horde_ActiveSync_Wbxml::EN_TYPE] != Horde_ActiveSync_Wbxml::EN_TYPE_STARTTAG) {
+                throw new Horde_ActiveSync_Exception('Protocol Error');
+            }
+
+            $sendmail = $smartreply = $smartforward = false;
+            switch ($e[Horde_ActiveSync_Wbxml::EN_TAG]) {
+            case self::COMPOSEMAIL_SENDMAIL:
+                $sendmail = true;
+                break;
+            case self::COMPOSEMAIL_SMARTREPLY:
+                $smartreply = true;
+                break;
+            case self::COMPOSEMAIL_SMARTFORWARD:
+                $smartforward = true;
+            }
+
+            if (!$sendmail && !$smartreply && !$smartforward) {
+                throw new Horde_ActiveSync_Exception('Protocol Error. Did not receive sendmail/smartreply/smartforward in wbxml request.');
+            }
+
+            $mail = new Horde_ActiveSync_Message_SendMail(array('logger' => $this->_logger, 'protocolversion' => $this->_version));
+            $mail->decodeStream($this->_decoder);
+
         }
     }
 
