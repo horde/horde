b6b787413417f328878054c8eb979c50fb479d76
Jan Schneider <jan@horde.org>
2013-03-14 11:36:10 +0100

    Bump version.

--- ingo/lib/Application.php
+++ ingo/lib/Application.php
@@ -49,7 +49,7 @@ class Ingo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.4-git)';
+    public $version = 'H5 (3.1.0-git)';
 
     /**
      * Global variables defined:
