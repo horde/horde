8da0e62198093493d73112475f6322a889739a42
Michael M Slusarz <slusarz@horde.org>
2013-03-19 01:03:41 -0600

    Tabs -> Spaces

--- framework/Css_Parser/lib/Sabberworm/CSS/Property/CSSNamespace.php
+++ framework/Css_Parser/lib/Sabberworm/CSS/Property/CSSNamespace.php
@@ -6,32 +6,32 @@ namespace Sabberworm\CSS\Property;
 * CSSNamespace represents an @namespace rule.
 */
 class CSSNamespace {
-	private $mUrl;
-	private $sPrefix;
+    private $mUrl;
+    private $sPrefix;
 
-	public function __construct($mUrl, $sPrefix = null) {
-		$this->mUrl = $mUrl;
-		$this->sPrefix = $sPrefix;
-	}
+    public function __construct($mUrl, $sPrefix = null) {
+        $this->mUrl = $mUrl;
+        $this->sPrefix = $sPrefix;
+    }
 
-	public function __toString() {
-		return '@namespace '.($this->sPrefix === null ? '' : $this->sPrefix.' ').$this->mUrl->__toString().';';
-	}
+    public function __toString() {
+        return '@namespace '.($this->sPrefix === null ? '' : $this->sPrefix.' ').$this->mUrl->__toString().';';
+    }
 
-	public function getUrl() {
-		return $this->mUrl;
-	}
+    public function getUrl() {
+        return $this->mUrl;
+    }
 
-	public function getPrefix() {
-		return $this->sPrefix;
-	}
+    public function getPrefix() {
+        return $this->sPrefix;
+    }
 
-	public function setUrl($mUrl) {
-		$this->mUrl = $mUrl;
-	}
+    public function setUrl($mUrl) {
+        $this->mUrl = $mUrl;
+    }
 
-	public function setPrefix($sPrefix) {
-		$this->sPrefix = $sPrefix;
-	}
+    public function setPrefix($sPrefix) {
+        $this->sPrefix = $sPrefix;
+    }
 
 }
