7cca1217e9d0249e2cb2c426820e6a30ce0febdd
Michael M Slusarz <slusarz@horde.org>
2013-03-20 17:25:26 -0600

    More fixes upstream (tweaks to patches previously applied here)

--- framework/Css_Parser/lib/Sabberworm/CSS/Value/Color.php
+++ framework/Css_Parser/lib/Sabberworm/CSS/Value/Color.php
@@ -22,20 +22,17 @@ class Color extends CSSFunction {
     }
 
     public function __toString() {
-        if (isset($this->aComponents['a'])) {
-            return parent::__toString();
+        // Shorthand RGB color values
+        // TODO: Include in output settings (once they’re done)
+        if(implode('', array_keys($this->aComponents)) === 'rgb') {
+            $sResult = sprintf(
+                '%02x%02x%02x',
+                $this->aComponents['r']->__toString(),
+                $this->aComponents['g']->__toString(),
+                $this->aComponents['b']->__toString()
+            );
+            return '#'.(($sResult[0] == $sResult[1]) && ($sResult[2] == $sResult[3]) && ($sResult[4] == $sResult[5]) ? "$sResult[0]$sResult[2]$sResult[4]" : $sResult);
         }
-
-        $out = sprintf(
-            '%02x%02x%02x',
-            $this->aComponents['r']->getSize(),
-            $this->aComponents['g']->getSize(),
-            $this->aComponents['b']->getSize()
-        );
-
-        return (($out[0] == $out[1]) && ($out[2] == $out[3]) && ($out[4] == $out[5]))
-            ? '#' . $out[0] . $out[2] . $out[4]
-            : '#' . $out;
+        return parent::__toString();
     }
-
 }
