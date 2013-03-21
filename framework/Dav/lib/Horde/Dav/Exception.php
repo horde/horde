cecf9df1f69c9b6650f4b0bf0d0b810ed96b005c
Jan Schneider <jan@horde.org>
2013-03-21 16:01:34 +0100

    Add Horde_Dav_Client.

---
+++ framework/Dav/lib/Horde/Dav/Exception.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2013 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Dav
+ */
+
+/**
+ * Base exception class for Horde_Dav.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Dav
+ */
+class Horde_Dav_Exception extends Horde_Exception_Wrapped
+{
+}
