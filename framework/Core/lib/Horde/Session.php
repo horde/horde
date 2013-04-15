b5895dcffe7623c69a6ad9ff01b5fd465028d48a
Jan Schneider <jan@horde.org>
2013-04-15 16:35:13 +0200

    phpdoc

--- framework/Core/lib/Horde/Session.php
+++ framework/Core/lib/Horde/Session.php
@@ -1,12 +1,22 @@
 <?php
 /**
- * The Horde_Session:: class provides a set of methods for handling the
- * administration and contents of the Horde session variable.
- *
  * Copyright 2010-2013 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Michael Slusarz <slusarz@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Core
+ *
+ * @property integer $begin  The timestamp when this session began (0 if
+ *                           session is not active).
+ */
+
+/**
+ * The Horde_Session class provides a set of methods for handling the
+ * administration and contents of the Horde session variable.
  *
  * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
