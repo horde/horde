307c2890949e2771e137b42827fe3b2a9bcd9363
Michael J Rubinsky <mrubinsk@horde.org>
2013-04-18 12:39:19 -0400

    Use a more likely delimter for the freeform children field.
    We will always export this as a single field instead of splitting
    on the delimiter since we can't guarentee what delimiter the user
    will use and a comma (the most likely choice) can also be used
    to seperate name suffixes (at least in some languages).

--- turba/lib/Driver.php
+++ turba/lib/Driver.php
@@ -2509,8 +2509,12 @@ class Turba_Driver implements Countable
                 break;
 
             case 'children':
-                // Children FROM horde are a simple string value, children going BACK to horde are an array with 'value' and 'new' keys
-                $message->children = explode(';', $value);
+                // Children FROM horde are a simple string value. Even though EAS
+                // uses an array stucture to pass them, we pass as a single
+                // string since we can't assure what delimter the user will
+                // use and (at least in some languages) a comma can be used
+                // within a full name.
+                $message->children = $value;
                 break;
 
             case 'notes':
@@ -2625,7 +2629,9 @@ class Turba_Driver implements Countable
 
         /* Children */
         if (is_array($message->children) && count($message->children)) {
-            $hash['children'] = implode(';', $message->children);
+            // We use a comma as incoming delimiter as it's the most
+            // common even though it might be used withing a name string.
+            $hash['children'] = implode(', ', $message->children);
         } elseif (!$message->isGhosted('children')) {
             $hash['children'] = '';
         }
