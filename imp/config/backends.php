ebd00b1eb2079a81fbadcaf8b4b6bea0374f15c7
Michael M Slusarz <slusarz@horde.org>
2013-04-27 05:54:06 -0600

    Remove old IMP Quota drivers
    There is really NO reason for anyone to use anything other than IMAP
    Quotas at this point.  Not only are most of these drivers unmaintained,
    but they are slllooooowwww, have complex setups, require advanced PHP
    features, and are not guaranteed to work with the current code.
    
    Simply put: there is absolutely no reason in this day and age to
    officially support anything other than IMAP quotas.  (In case anybody
    still does need this kind of functionality, it is still available via
    the hook driver, which will continue to be maintained).

--- imp/config/backends.php
+++ imp/config/backends.php
@@ -134,61 +134,12 @@
  *               - KB
  *
  *   These are the available drivers, along with their optional parameters:
- *     - command:  Use the UNIX quota command to handle quotas. Parameters:
- *       - quota_path: (string) [REQUIRED] Path to the quota binary. Command
- *                     line parameters can be specified in this value.
- *       - grep_path: (string) [REQUIRED] Path to the grep binary.
- *       - partition: (string) If all user mailboxes are on a single
- *                    partition, the partition label. By default, will
- *                    determine quota information using the user's home
- *                    directory value.
  *     - hook: Use the quota hook to handle quotas (see imp/config/hooks.php).
  *             All parameters defined for this driver will be passed to the
  *             quota hook function.
  *     - imap: Use the IMAP QUOTA extension to handle quotas. The IMAP server
  *             server must support the QUOTAROOT command to use this driver.
  *             This is the RECOMMENDED way of handling quotas.
- *     - maildir: Use Maildir++ quota files to handle quotas. Parameters:
- *         - msg_count: (boolean) Display information on the message limit
- *                      rather than the storage limit? The storage limit
- *                      information is displayed by default.
- *         - path: (string) The path to the user's Maildir directory. You may
- *                 use the two-character sequence "~U" to represent the user's
- *                 account name, and the actual username will be substituted
- *                 in that location. Example:
- *                   '/home/~U/Maildir/' or '/var/mail/~U/Maildir/'.
- *     - mdaemon: Use Mdaemon server to handle quotas. Parameters:
- *         - app_location: (string) Location of the application.
- *     - mercury32: Use Mercury/32 server to handle quotas. Parameters:
- *         - mail_user_folder: (string) The path to folder mail mercury.
- *     - sql: Use arbitrary SQL queries to handle quotas. This driver accepts
- *            these SQL connection parameters:
- *              - database
- *              - hostspec
- *              - password
- *              - phptype
- *              - username
- *            See horde/config/conf.php for further information on these
- *            parameters. If using the Horde DB, these parameters can be found
- *            in Horde's $GLOBALS['conf']['sql'] variable and may be merged
- *            into the parameter configuration like this:
- *              'params' => array_merge(
- *                  $GLOBALS['conf']['sql'],
- *                  array(
- *                      'query_quota' => [...],
- *                      'query_used' => [...],
- *                  )
- *              )
- *
- *            Additional SQL parameters:
- *              - query_quota: (string) SQL query which returns single
- *                             row/column with user quota (in bytes). %u is
- *                             replaced with current user name, %U with the
- *                             user name without the domain part, and %d
- *                             with the domain.
- *              - query_used: (string) SQL query which returns single
- *                            row/column with user used space (in bytes).
- *                            Placeholders are the same as in 'query_quota'.
  *
  * smtp: (array) If Horde is configured to use SMTP as the mailer, entries in
  *   this array will overwrite the default Horde SMTP parameters. The
