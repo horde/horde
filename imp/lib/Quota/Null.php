436642b17f2deea0dd2aafc1b74825839a9f3a27
Michael M Slusarz <slusarz@horde.org>
2013-04-27 05:51:43 -0600

    phpdoc

--- imp/lib/Quota/Null.php
+++ imp/lib/Quota/Null.php
@@ -1,16 +1,25 @@
 <?php
 /**
- * The IMP_Quota_Null:: is a null implementation of the quota driver.
- *
  * Copyright 2010-2013 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.horde.org/licenses/gpl.
  *
- * @author   Michael Slusarz <slusarz@horde.org>
- * @category Horde
- * @license  http://www.horde.org/licenses/gpl GPL
- * @package  IMP
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2010-2013 Horde LLC
+ * @license   http://www.horde.org/licenses/gpl GPL
+ * @package   IMP
+ */
+
+/**
+ * Null implementation of the quota driver.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2010-2013 Horde LLC
+ * @license   http://www.horde.org/licenses/gpl GPL
+ * @package   IMP
  */
 class IMP_Quota_Null extends IMP_Quota
 {
