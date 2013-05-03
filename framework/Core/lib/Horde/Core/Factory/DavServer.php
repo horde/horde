02c624116d0668abab9054bcc022d6b7e9eb76a8
Jan Schneider <jan@horde.org>
2013-05-03 14:27:09 +0200

    Move DAV server creation to factory.

---
+++ framework/Core/lib/Horde/Core/Factory/DavServer.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright 2013 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @package  Core
+ */
+
+/**
+ * Factory for the DAV server.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @package  Core
+ */
+class Horde_Core_Factory_DavStorage extends Horde_Core_Factory_Injector
+{
+    public function create(Horde_Injector $injector)
+    {
+        global $conf, $registry;
+
+        $principalBackend = new Horde_Dav_Principals(
+            $injector->getInstance('Horde_Core_Factory_Auth')->create(),
+            $injector->getInstance('Horde_Core_Factory_Identity')
+        );
+        $principals = new DAVACL\PrincipalCollection($principalBackend);
+        $principals->disableListing = $conf['auth']['list_users'] == 'input';
+
+        $calendarBackend = new Horde_Dav_Calendar_Backend($registry, $injector->getInstance('Horde_Dav_Storage'));
+        $caldav = new CalDAV\CalendarRootNode($principalBackend, $calendarBackend);
+        $contactsBackend = new Horde_Dav_Contacts_Backend($registry);
+        $carddav = new CardDAV\AddressBookRoot($principalBackend, $contactsBackend);
+
+        $server = new DAV\Server(
+            new Horde_Dav_RootCollection(
+                $registry,
+                array($principals, $caldav, $carddav),
+                isset($conf['mime']['magic_db']) ? $conf['mime']['magic_db'] : null
+            )
+        );
+        $server->setBaseUri(
+            $registry->get('webroot', 'horde')
+            . ($GLOBALS['conf']['urls']['pretty'] == 'rewrite' ? '/rpc/' : '/rpc.php/')
+        );
+        $server->addPlugin(
+            new DAV\Auth\Plugin(
+                new Horde_Dav_Auth(
+                    $injector->getInstance('Horde_Core_Factory_Auth')->create()
+                ),
+                'Horde DAV Server'
+            )
+        );
+        $server->addPlugin(
+            new CalDAV\Plugin()
+        );
+        $server->addPlugin(
+            new CardDAV\Plugin()
+        );
+        $server->addPlugin(
+            new DAV\Locks\Plugin(
+                new Horde_Dav_Locks($registry, $injector->getInstance('Horde_Lock'))
+            )
+        );
+        $server->addPlugin(new DAV\Browser\Plugin());
+
+        return $server;
+    }
+}
--- framework/Rpc/lib/Horde/Rpc/Webdav.php
+++ framework/Rpc/lib/Horde/Rpc/Webdav.php
@@ -47,53 +47,8 @@ class Horde_Rpc_Webdav extends Horde_Rpc
      */
     public function __construct($request, $params = array())
     {
-        global $conf, $injector, $registry;
-
         parent::__construct($request, $params);
-
-        $principalBackend = new Horde_Dav_Principals(
-            $injector->getInstance('Horde_Core_Factory_Auth')->create(),
-            $injector->getInstance('Horde_Core_Factory_Identity')
-        );
-        $principals = new DAVACL\PrincipalCollection($principalBackend);
-        $principals->disableListing = $conf['auth']['list_users'] == 'input';
-
-        $calendarBackend = new Horde_Dav_Calendar_Backend($registry, $injector->getInstance('Horde_Dav_Storage'));
-        $caldav = new CalDAV\CalendarRootNode($principalBackend, $calendarBackend);
-        $contactsBackend = new Horde_Dav_Contacts_Backend($registry);
-        $carddav = new CardDAV\AddressBookRoot($principalBackend, $contactsBackend);
-
-        $this->_server = new DAV\Server(
-            new Horde_Dav_RootCollection(
-                $registry,
-                array($principals, $caldav, $carddav),
-                isset($conf['mime']['magic_db']) ? $conf['mime']['magic_db'] : null
-            )
-        );
-        $this->_server->setBaseUri(
-            $registry->get('webroot', 'horde')
-            . ($GLOBALS['conf']['urls']['pretty'] == 'rewrite' ? '/rpc/' : '/rpc.php/')
-        );
-        $this->_server->addPlugin(
-            new DAV\Auth\Plugin(
-                new Horde_Dav_Auth(
-                    $injector->getInstance('Horde_Core_Factory_Auth')->create()
-                ),
-                'Horde DAV Server'
-            )
-        );
-        $this->_server->addPlugin(
-            new CalDAV\Plugin()
-        );
-        $this->_server->addPlugin(
-            new CardDAV\Plugin()
-        );
-        $this->_server->addPlugin(
-            new DAV\Locks\Plugin(
-                new Horde_Dav_Locks($registry, $injector->getInstance('Horde_Lock'))
-            )
-        );
-        $this->_server->addPlugin(new DAV\Browser\Plugin());
+        $this->_server = $GLOBALS['injector']->getInstance('Horde_Dav_Server');
     }
 
     /**
