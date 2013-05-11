cc608b567db1a44a69d3bea0546f2bdda0f3b709
Michael J Rubinsky <mrubinsk@horde.org>
2013-05-11 02:23:46 -0400

    A bit too greedy in keeping previous map entries.

--- framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
@@ -1449,7 +1449,7 @@ class Horde_ActiveSync_State_Sql extends Horde_ActiveSync_State_Base
         $guids = array($guid);
         foreach ($results as $oldkey) {
             if (preg_match('/^s{0,1}\{([0-9A-Za-z-]+)\}([0-9]+)$/', $oldkey['sync_key'], $matches)) {
-                if ($matches[1] == $guid && $matches[2] < $n) {
+                if ($matches[1] == $guid && $matches[2] < ($n - 1)) {
                     $remove[] = $oldkey['sync_key'];
                 }
             } else {
@@ -1489,7 +1489,7 @@ class Horde_ActiveSync_State_Sql extends Horde_ActiveSync_State_Base
             }
             foreach ($maps as $key) {
                 if (preg_match('/^s{0,1}\{([0-9A-Za-z-]+)\}([0-9]+)$/', $key, $matches)) {
-                    if ($matches[1] == $guid && $matches[2] < ($n - 2)) {
+                    if ($matches[1] == $guid && $matches[2] < ($n - 1)) {
                         $remove[] = $key;
                     }
                 }
