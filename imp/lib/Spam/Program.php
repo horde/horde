589a98e4015280d1849e4ec41d19ac89743d3736
Michael M Slusarz <slusarz@horde.org>
2013-05-17 14:29:16 -0600

    phpdoc

--- imp/lib/Spam/Program.php
+++ imp/lib/Spam/Program.php
@@ -14,11 +14,6 @@
 /**
  * Spam reporting driver utilizing a local binary.
  *
- * Copyright 2013 Horde LLC (http://www.horde.org/)
- *
- * See the enclosed file COPYING for license information (GPL). If you
- * did not receive this file, see http://www.horde.org/licenses/gpl.
- *
  * @author    Michael Slusarz <slusarz@horde.org>
  * @category  Horde
  * @copyright 2013 Horde LLC
