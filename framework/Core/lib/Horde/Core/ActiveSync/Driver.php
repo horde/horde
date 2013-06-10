9d5a6101a910e2808bd215b472f4d449618c7654
Michael J Rubinsky <mrubinsk@horde.org>
2013-06-09 23:44:53 -0400

    Don't validate the SyncStamp for Email folders.

--- framework/Core/lib/Horde/Core/ActiveSync/Driver.php
+++ framework/Core/lib/Horde/Core/ActiveSync/Driver.php
@@ -2254,6 +2254,11 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
      */
     public function getSyncStamp($collection, $last = null)
     {
+        // For Email collections, we don't care.
+        if (!in_array($collection, array(self::APPOINTMENTS_FOLDER_UID, self::NOTES_FOLDER_UID, self::CONTACTS_FOLDER_UID, self::TASKS_FOLDER_UID))) {
+            return time();
+        }
+
         if ($this->_connector->hasFeature('modseq', $collection)) {
             $modseq = $this->_connector->getHighestModSeq($collection);
             // Sanity check - if the last syncstamp is higher then the
