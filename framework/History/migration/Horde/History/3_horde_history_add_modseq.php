05752e920ad13329550034f3b250e8f84d78606e
Michael J Rubinsky <mrubinsk@horde.org>
2013-06-11 17:44:37 -0400

    Improve performance/scalability of migration.

--- framework/History/migration/Horde/History/3_horde_history_add_modseq.php
+++ framework/History/migration/Horde/History/3_horde_history_add_modseq.php
@@ -3,20 +3,28 @@ class HordeHistoryAddModSeq extends Horde_Db_Migration_Base
 {
     public function up()
     {
-        $t = $this->createTable('horde_histories_modseq', array('autoincrementKey' => 'history_modseq'));
+        $t = $this->createTable('horde_histories_modseq', array('autoincrementKey' => false));
+        $t->column('history_modseq', 'integer', array('null' => false, 'default' => 0));
         $t->column('history_modseqempty', 'integer', array('null' => false, 'default' => 0));
         $t->end();
         $this->addColumn('horde_histories', 'history_modseq', 'integer', array('default' => 0, 'null' => false));
-        $this->addIndex('horde_histories', array('history_modseq'));
 
         $rows= $this->selectAll('SELECT history_id FROM horde_histories ORDER BY history_ts ASC');
         $seq = 1;
+
+        $this->beginDbTransaction();
         foreach ($rows as $row) {
-            $this->insert('INSERT INTO horde_histories_modseq (history_modseqempty) VALUES(0)');
             $this->update('UPDATE horde_histories SET history_modseq = ? WHERE history_id = ?',
                 array($seq++, $row['history_id']));
         }
-        $this->delete('DELETE FROM horde_histories_modseq WHERE history_modseq <> ?', array($seq - 1));
+        $this->insert('INSERT INTO horde_histories_modseq (history_modseq) VALUES(?)', array($seq - 1));
+        $this->commitDbTransaction();
+
+        // Add the index after the new values are set for performance reasons.
+        $this->addIndex('horde_histories', array('history_modseq'));
+
+        // ...same with the autoincrement for this field.
+        $this->changeColumn('horde_histories_modseq', 'history_modseq', 'autoincrementKey');
     }
 
     public function down()
