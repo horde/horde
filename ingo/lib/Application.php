dcfbf0cf5c0e66d54cb9c8355ead28eb5adfd291
Jan Schneider <jan@horde.org>
2013-06-12 13:19:04 +0200

    Development mode for ingo-3.1.2

--- ingo/lib/Application.php
+++ ingo/lib/Application.php
@@ -49,7 +49,7 @@ class Ingo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.1.1)';
+    public $version = 'H5 (3.1.2-git)';
 
     /**
      * Global variables defined:
