1045680db736f3970ceee5ebbb887d079d01a6ea
Jan Schneider <jan@horde.org>
2013-06-12 13:32:59 +0200

    Development mode for trean-1.0.3

--- trean/lib/Application.php
+++ trean/lib/Application.php
@@ -33,7 +33,7 @@ class Trean_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (1.0.2)';
+    public $version = 'H5 (1.0.3-git)';
 
     /**
      * Global variables defined:
