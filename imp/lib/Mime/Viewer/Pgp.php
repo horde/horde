237cf602fb6f2846aa173eadfb88d201a389b12e
Michael M Slusarz <slusarz@horde.org>
2013-06-14 13:28:53 -0600

    Store raw PGP data in base embedded part

--- imp/lib/Mime/Viewer/Pgp.php
+++ imp/lib/Mime/Viewer/Pgp.php
@@ -333,6 +333,7 @@ class IMP_Mime_Viewer_Pgp extends Horde_Mime_Viewer_Base
         $new_part = Horde_Mime_Part::parseMessage($decrypted_data->message, array(
             'forcemime' => true
         ));
+        $new_part->setContents($decrypted_data->message);
 
         if ($new_part->getType() == 'multipart/signed') {
             $data = new Horde_Stream_Temp();
