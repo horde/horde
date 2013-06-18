ba4a96694a9d133b72bc6c0b552dd5f86317307b
Jan Schneider <jan@horde.org>
2013-06-18 23:54:38 +0200

    Development mode for ansel-3.0.0beta2

--- ansel/lib/Application.php
+++ ansel/lib/Application.php
@@ -39,7 +39,7 @@ class Ansel_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.0beta1)';
+    public $version = 'H5 (3.0.0-git)';
 
     /**
      * Global variables defined:
