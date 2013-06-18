a2c9d53b48f59b4c306c30952d8c83a3556112e8
Michael M Slusarz <slusarz@horde.org>
2013-06-18 00:51:34 -0600

    unused variable

--- framework/Imap_Client/lib/Horde/Imap/Client/Base/Connection.php
+++ framework/Imap_Client/lib/Horde/Imap/Client/Base/Connection.php
@@ -68,8 +68,7 @@ abstract class Horde_Imap_Client_Base_Connection
      */
     public function __construct(Horde_Imap_Client_Base $base, $debug)
     {
-        if (($secure = $base->getParam('secure')) &&
-            !extension_loaded('openssl')) {
+        if ($base->getParam('secure') && !extension_loaded('openssl')) {
             throw new InvalidArgumentException('Secure connections require the PHP openssl extension.');
         }
 
