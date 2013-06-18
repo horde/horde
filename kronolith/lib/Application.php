a768f057baf28e13d7df91e08463582f197fcf5b
Jan Schneider <jan@horde.org>
2013-06-18 18:38:54 +0200

    Released kronolith-4.1.1

--- kronolith/lib/Application.php
+++ kronolith/lib/Application.php
@@ -45,7 +45,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.1.1-git)';
+    public $version = 'H5 (4.1.1)';
 
     /**
      * Global variables defined:
