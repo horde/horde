9203ce3c95a5498755d61e58602fd26f95871e8a
Ralf Lang <lang@b1-systems.de>
2013-06-20 14:29:21 +0300

    minor cleanups as jan suggested

--- framework/Queue/migration/Horde/Queue/1_horde_queue_base_tables.php
+++ framework/Queue/migration/Horde/Queue/1_horde_queue_base_tables.php
@@ -3,12 +3,10 @@ class HordeQueueBaseTables extends Horde_Db_Migration_Base
 {
     public function up()
     {
-        if (!in_array('horde_queue_tasks', $this->tables())) {
-            $t = $this->createTable('horde_queue_tasks', array('autoincrementKey' => 'task_id'));
-            $t->column('task_queue', 'string', array('limit' => 255, 'null' => false));
-            $t->column('task_fields', 'text', array('null' => false, 'default' => ''));
-            $t->end();
-        }
+        $t = $this->createTable('horde_queue_tasks', array('autoincrementKey' => 'task_id'));
+        $t->column('task_queue', 'string', array('limit' => 255, 'null' => false));
+        $t->column('task_fields', 'text', array('null' => false, 'default' => ''));
+        $t->end();
     }
 
     public function down()
