f52bd874a1deefd33dd345242790ea3a12923fbc
Michael J Rubinsky <mrubinsk@horde.org>
2013-06-21 12:06:00 -0400

    Better description. Avoid confusing this with an update of the block content.

--- horde/lib/Block/FbStream.php
+++ horde/lib/Block/FbStream.php
@@ -139,7 +139,7 @@ EOT;
             $fbperms = $facebook->users->getAppPermissions();
             if (!empty($fbperms[Horde_Service_Facebook_Auth::EXTEND_PERMS_PUBLISHSTREAM])) {
                 $html .= '<input style="width:98%;margin-top:4px;margin-bottom:4px;" type="text" class="fbinput" id="' . $instance . '_newStatus" name="newStatus" />'
-                    . '<div><a class="horde-default" href="#" id="' . $instance . '_button">' . _("Update") . '</a></div>'
+                    . '<div><a class="horde-default" href="#" id="' . $instance . '_button">' . _("Post") . '</a></div>'
                     . Horde::img('loading.gif', '', array('id' => $instance. '_loading', 'style' => 'display:none;'));
             }
         } catch (Horde_Service_Facebook_Exception $e) {
