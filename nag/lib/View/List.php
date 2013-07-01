d23fd965976682c7d41c220137a0a0e106daeb63
Michael J Rubinsky <mrubinsk@horde.org>
2013-07-01 13:04:23 -0400

    Proper fix for Bug: 12420

--- nag/lib/View/List.php
+++ nag/lib/View/List.php
@@ -90,20 +90,20 @@ class Nag_View_List
             'title' => $this->_title
         ));
 
-        $tabs = new Horde_Core_Ui_Tabs('show_completed', $this->_vars);
+        $tabs = new Horde_Core_Ui_Tabs('tab_name', $this->_vars);
         if (!$GLOBALS['prefs']->isLocked('show_completed')) {
             $listurl = Horde::url('list.php');
-            $tabs->addTab(_("_All tasks"), $listurl, Nag::VIEW_ALL);
-            $tabs->addTab(_("Incom_plete tasks"), $listurl, Nag::VIEW_INCOMPLETE);
-            $tabs->addTab(_("_Future tasks"), $listurl, Nag::VIEW_FUTURE);
-            $tabs->addTab(_("_Completed tasks"), $listurl, Nag::VIEW_COMPLETE);
+            $tabs->addTab(_("_All tasks"), $listurl->add('show_completed', Nag::VIEW_ALL), 'all');
+            $tabs->addTab(_("Incom_plete tasks"), $listurl->add('show_completed', Nag::VIEW_INCOMPLETE), 'incomplete');
+            $tabs->addTab(_("_Future tasks"), $listurl->add('show_completed', Nag::VIEW_FUTURE), 'future');
+            $tabs->addTab(_("_Completed tasks"), $listurl->add('show_completed', Nag::VIEW_COMPLETE), 'complete');
         }
         foreach (Nag::listTasklists() as $list) {
             if ($list->get('issmart')) {
                 $tabs->addTab(
                     $list->get('name'),
                     $listurl->add(array('actionID' => 'smart', 'list' => $list->getName())),
-                    array('img' => 'search.png'));
+                    array('img' => 'search.png', 'tabname' => $list->getName()));
             }
         }
 
@@ -112,7 +112,7 @@ class Nag_View_List
         $view->addHelper(new Nag_View_Helper_List($view));
         $view->tasks = $this->_tasks;
         $view->tasks->reset();
-        $view->tabs = $tabs->render($this->_vars->get('show_completed'));
+        $view->tabs = $tabs->render($this->_vars->get('tab_name'));
         $view->browser = empty($this->_smartShare) && $this->_showTagBrowser ? $this->_getRelatedTags() . $this->_getTagTrail() : '';
         $view->title = $this->_title;
         $view->sortby = $prefs->getValue('sortby');
