d8ebda4b3e645f5054e5be431a8718881cd49fa1
Jan Schneider <jan@horde.org>
2013-07-16 23:46:06 +0200

    Released mnemo-4.1.1

--- mnemo/lib/Application.php
+++ mnemo/lib/Application.php
@@ -43,7 +43,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.1.1-git)';
+    public $version = 'H5 (4.1.1)';
 
     /**
      * Global variables defined:
