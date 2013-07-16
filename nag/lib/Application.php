33d0e040a731ef468a41faf9843b0bd5196adc3c
Jan Schneider <jan@horde.org>
2013-07-16 23:33:11 +0200

    Development mode for nag-4.1.2

--- nag/lib/Application.php
+++ nag/lib/Application.php
@@ -41,7 +41,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.1.1)';
+    public $version = 'H5 (4.1.2-git)';
 
     /**
      * Global variables defined:
