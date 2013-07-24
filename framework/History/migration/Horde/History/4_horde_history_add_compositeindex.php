eac70812abb650b71c1db6b401ef9a679a828579
Michael J Rubinsky <mrubinsk@horde.org>
2013-07-23 23:57:27 -0400

    Remove no longer needed index.

--- framework/History/migration/Horde/History/4_horde_history_add_compositeindex.php
+++ framework/History/migration/Horde/History/4_horde_history_add_compositeindex.php
@@ -3,7 +3,6 @@ class HordeHistoryAddCompositeIndex extends Horde_Db_Migration_Base
 {
     public function up()
     {
-        // Add the index after the new values are set for performance reasons.
         $this->addIndex('horde_histories', array('history_modseq', 'object_uid'));
     }
 
---
+++ framework/History/migration/Horde/History/5_horde_history_remove_compositeindex.php
@@ -0,0 +1,14 @@
+<?php
+class HordeHistoryRemoveCompositeIndex extends Horde_Db_Migration_Base
+{
+    public function up()
+    {
+        $this->dropIndex('horde_histories', array('history_modseq', 'object_uid'));
+    }
+
+    public function down()
+    {
+        $this->addIndex('horde_histories', array('history_modseq', 'object_uid'));
+    }
+
+}
