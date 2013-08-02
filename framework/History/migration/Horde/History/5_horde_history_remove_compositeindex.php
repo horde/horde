0e4a486e140a43a63adc9b9357b0f0c0a7d80283
Michael J Rubinsky <mrubinsk@horde.org>
2013-08-02 17:02:31 -0400

    Only remove index if we have the current index name.
    Bug: 12498

--- framework/History/migration/Horde/History/5_horde_history_remove_compositeindex.php
+++ framework/History/migration/Horde/History/5_horde_history_remove_compositeindex.php
@@ -3,8 +3,18 @@ class HordeHistoryRemoveCompositeIndex extends Horde_Db_Migration_Base
 {
     public function up()
     {
-        $this->removeIndex('horde_histories', 'history_modseq');
-        $this->removeIndex('horde_histories', 'object_uid');
+        // Older installs may have indexes differently named.
+        $indexes = $this->indexes('horde_histories');
+        $modseq_idx_name = $this->indexName('horde_histories', 'history_modseq');
+        $object_idx_name = $this->indexName('horde_histories', 'object_uid');
+        foreach ($indexes as $idx) {
+            switch ($idx->name) {
+            case $modseq_idx_name:
+            case $object_idx_name:
+                $this->removeIndex('horde_histories', array('name' => $idx->name));
+                break;
+            }
+        }
     }
 
     public function down()
--- framework/History/migration/Horde/History/6_horde_history_fix_botchedindexes.php
+++ framework/History/migration/Horde/History/6_horde_history_fix_botchedindexes.php
@@ -3,8 +3,13 @@ class HordeHistoryFixBotchedIndexes extends Horde_Db_Migration_Base
 {
     public function up()
     {
+        $indexes = $this->indexes('horde_histories');
         $idx = $this->indexName('horde_histories', array('column' => array('history_modseq', 'object_uid')));
-        $this->removeIndex('horde_histories', array('name' => $idx));
+        foreach ($indexes as $index) {
+            if ($index->name == $idx) {
+                $this->removeIndex('horde_histories', array('name' => $idx));
+            }
+        }
 
         // Re-add these.
         $this->addIndex('horde_histories', array('history_modseq'));
@@ -15,8 +20,18 @@ class HordeHistoryFixBotchedIndexes extends Horde_Db_Migration_Base
     public function down()
     {
         $this->addIndex('horde_histories', array('history_modseq', 'object_uid'));
-        $this->removeIndex('horde_histories', 'history_modseq');
-        $this->removeIndex('horde_histories', 'object_uid');
+        // Older installs may have indexes differently named.
+        $indexes = $this->indexes('horde_histories');
+        $modseq_idx_name = $this->indexName('horde_histories', 'history_modseq');
+        $object_idx_name = $this->indexName('horde_histories', 'object_uid');
+        foreach ($indexes as $idx) {
+            switch ($idx->name) {
+            case $modseq_idx_name:
+            case $object_idx_name:
+                $this->removeIndex('horde_histories', array('name' => $idx->name));
+                break;
+            }
+        }
     }
 
 }
