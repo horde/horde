24771eaa67769a02331173e5717a299c74fce370
Michael J Rubinsky <mrubinsk@horde.org>
2013-08-03 19:52:47 -0400

    BFN

--- framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
@@ -853,7 +853,7 @@ class Horde_ActiveSync_State_Sql extends Horde_ActiveSync_State_Base
     public function getChanges(array $options = array())
     {
         $this->_thisSyncStamp = $this->_backend->getSyncStamp(empty($this->_collection['id']) ? null : $this->_collection['id'], $this->_lastSyncStamp);
-        if (!$this->_thisSyncStamp) {
+        if ($this->_thisSyncStamp === false) {
             throw new Horde_ActiveSync_Exception_StaleState('Detecting a change in timestamp or modification sequence. Reseting state.');
         }
 
