745ae10437f5d51f0c65d2b905f5bd07db72f02a
Michael J Rubinsky <mrubinsk@horde.org>
2013-08-12 16:07:56 -0400

    Fix config key in test.

--- framework/History/test/Horde/History/Sql/MysqliTest.php
+++ framework/History/test/Horde/History/Sql/MysqliTest.php
@@ -23,7 +23,7 @@ class Horde_History_Sql_MysqliTest extends Horde_History_TestBase
         }
         $config = self::getConfig('HISTORY_SQL_MYSQLI_TEST_CONFIG', __DIR__ . '/..');
         if ($config && !empty($config['history']['sql']['mysqli'])) {
-            self::$db = new Horde_Db_Adapter_Mysqli($config['History']['sql']['mysqli']);
+            self::$db = new Horde_Db_Adapter_Mysqli($config['history']['sql']['mysqli']);
 
             $dir = dirname(__FILE__) . '/../../../../migration/Horde/History';
             if (!is_dir($dir)) {
