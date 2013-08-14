cb05e197bad5252e3c953a3f0a31670dc5ff6908
Jan Schneider <jan@horde.org>
2013-08-14 15:34:44 +0200

    [jan] Request CAPTCHA when creating pages too.

--- wicked/lib/Page/NewPage.php
+++ wicked/lib/Page/NewPage.php
@@ -91,6 +91,14 @@ class Wicked_Page_NewPage extends Wicked_Page
         $view->action = Wicked::url('NewPage');
         $view->formInput = Horde_Util::formInput();
         $view->referrer = $this->referrer();
+        if (!empty($GLOBALS['conf']['wicked']['captcha']) &&
+            !$GLOBALS['registry']->getAuth()) {
+            $figlet = new Text_Figlet();
+            Horde_Exception_Pear::catchError($figlet->loadFont(
+                $GLOBALS['conf']['wicked']['figlet_font']
+            ));
+            $view->captcha = $figlet->lineEcho(Wicked::getCAPTCHA(true));
+        }
         if ($this->_template) {
             $view->text = Wicked_Page::getPage($this->_template)->getText();
         }
@@ -120,6 +128,13 @@ class Wicked_Page_NewPage extends Wicked_Page
         if (!$this->allows(Wicked::MODE_EDIT)) {
             $notification->push(sprintf(_("You don't have permission to create \"%s\"."), $this->referrer()));
         } else {
+            if (!empty($GLOBALS['conf']['wicked']['captcha']) &&
+                !$GLOBALS['registry']->getAuth() &&
+                (Horde_String::lower(Horde_Util::getFormData('wicked_captcha')) != Horde_String::lower(Wicked::getCAPTCHA()))) {
+                $notification->push(_("Random string did not match."), 'horde.error');
+                return;
+            }
+
             $text = Horde_Util::getPost('page_text');
             if (empty($text)) {
                 $notification->push(_("Pages cannot be empty."), 'horde.error');
