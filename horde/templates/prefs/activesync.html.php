0acdf1ea0dbd202ff5086782e2a7b7254bb129e4
Jan Schneider <jan@horde.org>
2013-08-15 21:14:02 +0200

    Fix html, style.

--- horde/templates/prefs/activesync.html.php
+++ horde/templates/prefs/activesync.html.php
@@ -8,17 +8,16 @@
  </select>
 </div>
 <?php endif ?>
+
+<br class="spacer" />
+<h2 class="smallheader"><?php echo _("State Management") ?></h2>
 <p>
-<div class="smallheader">
- <?php echo _("State Management") ?>
-</div>
- <?php echo _("Reset all device state. This will cause your devices to resyncronize all items.") ?>
- <input class="horde-delete" type="submit" value="Reset" name="reset" />
+ <input class="horde-delete" type="submit" value="<?php echo _("Reset all device state") ?>" name="reset" />
+ <?php echo _("This will cause your devices to resyncronize all items.") ?>
 </p>
 
-<div class="smallheader">
- <?php echo _("Device Management") ?>
-</div>
+<br class="spacer" />
+<h2 class="smallheader"><?php echo _("Device Management") ?></h2>
 <?php if ($this->devices): ?>
 <input type="hidden" id="removedevice" name="removedevice" />
 <input type="hidden" name="wipeid" id="wipeid" />
