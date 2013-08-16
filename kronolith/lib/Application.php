747e317b2079d14b8258aea2c81687525ce7cfc3
Jan Schneider <jan@horde.org>
2013-08-16 15:43:01 +0200

    Bump version.

--- kronolith/lib/Application.php
+++ kronolith/lib/Application.php
@@ -45,7 +45,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.1.3-git)';
+    public $version = 'H5 (4.2.0-git)';
 
     /**
      * Global variables defined:
