f1b5c02bf36ba4b766fa70cadec736ce06345830
Michael J Rubinsky <mrubinsk@horde.org>
2013-08-22 14:38:47 -0400

    Improve logging options for ActiveSync.

--- framework/Core/lib/Horde/Core/Factory/ActiveSyncServer.php
+++ framework/Core/lib/Horde/Core/Factory/ActiveSyncServer.php
@@ -7,10 +7,16 @@ class Horde_Core_Factory_ActiveSyncServer extends Horde_Core_Factory_Injector
 {
     public function create(Horde_Injector $injector)
     {
+        if (empty($GLOBALS['conf']['activesync']['logging']['level'])) {
+            $level = Horde_ActiveSync_Wbxml::LOG_PROTOCOL;
+        } else {
+            $level = $GLOBALS['conf']['activesync']['logging']['level'];
+        }
+
         $server = new Horde_ActiveSync(
             $injector->getInstance('Horde_ActiveSyncBackend'),
-            new Horde_ActiveSync_Wbxml_Decoder(fopen('php://input', 'r')),
-            new Horde_ActiveSync_Wbxml_Encoder(fopen('php://output', 'w+')),
+            new Horde_ActiveSync_Wbxml_Decoder(fopen('php://input', 'r'), $level),
+            new Horde_ActiveSync_Wbxml_Encoder(fopen('php://output', 'w+'), $level),
             $injector->getInstance('Horde_ActiveSyncState'),
             $injector->getInstance('Horde_Controller_Request')
         );
