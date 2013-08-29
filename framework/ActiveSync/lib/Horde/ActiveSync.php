62f377356e0092307d0edad1d6e05153a23b0ee0
Michael Cramer <michael@bigmichi1.de>
2013-08-29 16:35:20 +0200

    Update ActiveSync.php
    static access

--- framework/ActiveSync/lib/Horde/ActiveSync.php
+++ framework/ActiveSync/lib/Horde/ActiveSync.php
@@ -795,7 +795,7 @@ class Horde_ActiveSync
                         'The device %s was disallowed for user %s per policy settings.',
                         $device->id,
                         $device->user);
-                    $this->logger->err($msg);
+                    self::$_logger->err($msg);
                     if ($version > self::VERSION_TWELVEONE) {
                         $this->_globalError = $callback_ret;
                     } else {
@@ -818,7 +818,7 @@ class Horde_ActiveSync
                         'The device %s was disallowed for user %s per policy settings.',
                         $device->id,
                         $device->user);
-                    $this->logger->err($msg);
+                    self::$_logger->err($msg);
                     if ($version > self::VERSION_TWELVEONE) {
                         $this->_globalError = $callback_ret;
                     } else {
