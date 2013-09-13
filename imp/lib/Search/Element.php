dd2e27d0710c5c85d12ba3127af844164233a067
Michael M Slusarz <slusarz@horde.org>
2013-09-12 23:45:36 -0600

    phpdoc

--- imp/lib/Search/Element.php
+++ imp/lib/Search/Element.php
@@ -1,16 +1,24 @@
 <?php
 /**
- * This class provides the framework for a search query element.
- *
  * Copyright 2010-2013 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.horde.org/licenses/gpl.
  *
- * @author   Michael Slusarz <slusarz@horde.org>
- * @category Horde
- * @license  http://www.horde.org/licenses/gpl GPL
- * @package  IMP
+ * @category  Horde
+ * @copyright 2010-2013 Horde LLC
+ * @license   http://www.horde.org/licenses/gpl GPL
+ * @package   IMP
+ */
+
+/**
+ * Abstract framework for a search query element.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2010-2013 Horde LLC
+ * @license   http://www.horde.org/licenses/gpl GPL
+ * @package   IMP
  */
 abstract class IMP_Search_Element implements Serializable
 {
