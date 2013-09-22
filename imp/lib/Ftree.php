8bc502e8509d9149cac2716722467d47e4be9b8c
Michael M Slusarz <slusarz@horde.org>
2013-09-22 07:10:30 -0600

    simplify

--- imp/lib/Ftree.php
+++ imp/lib/Ftree.php
@@ -175,13 +175,16 @@ class IMP_Ftree implements ArrayAccess, Countable, IteratorAggregate, Serializab
 
         if ($access_folders) {
             /* Add remote servers. */
-            $this->insert(iterator_to_array($injector->getInstance('IMP_Remote')));
+            $this->insert(iterator_to_array(
+                $injector->getInstance('IMP_Remote')
+            ));
 
             /* Add virtual folders to the tree. */
-            $iterator = IMP_Search_IteratorFilter::create(
-                IMP_Search_IteratorFilter::VFOLDER
-            );
-            array_map(array($this, 'insert'), iterator_to_array($iterator));
+            $this->insert(iterator_to_array(
+                IMP_Search_IteratorFilter::create(
+                    IMP_Search_IteratorFilter::VFOLDER
+                )
+            ));
         }
 
         if ($old_track) {
