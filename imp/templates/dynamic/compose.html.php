6044c73520483fa26f8b85bbf22ad90c078f2a94
Michael M Slusarz <slusarz@horde.org>
2013-09-22 08:59:59 -0600

    This isn't part of the label

--- imp/templates/dynamic/compose.html.php
+++ imp/templates/dynamic/compose.html.php
@@ -64,8 +64,8 @@
      <label>
       <?php echo $this->checkBoxTag('save_sent_mail', 1, false, array('class' => 'checkbox')) . _("Save in") ?>
       <span id="sent_mail_label"></span>
-      <span class="iconImg horde-popdown" id="save_sent_mail_load"></span>
      </label>
+     <span class="iconImg horde-popdown" id="save_sent_mail_load"></span>
      <?php echo $this->hiddenFieldTag('save_sent_mail_mbox') ?>
     </div>
 <?php else: ?>
