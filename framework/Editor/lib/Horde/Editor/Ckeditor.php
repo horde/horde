7eb78e14ab20574943bede123d55b1997871682c
Michael M Slusarz <slusarz@horde.org>
2013-09-25 11:56:04 -0600

    Changing the toolbar config can't be done this way

--- framework/Editor/lib/Horde/Editor/Ckeditor.php
+++ framework/Editor/lib/Horde/Editor/Ckeditor.php
@@ -63,8 +63,7 @@ class Horde_Editor_Ckeditor extends Horde_Editor
 
         if (isset($params['id'])) {
             $this->_js['script'] = array(
-                'CKEDITOR.replace("' . $params['id'] . '",' . $params['config'] . ')',
-                'CKEDITOR.config.toolbar_Full.push(["Code"])'
+                'CKEDITOR.replace("' . $params['id'] . '",' . $params['config'] . ')'
             );
         }
     }
