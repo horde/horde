617ceeb36bb69b458df58b0369507de9b4d8d2d8
Michael M Slusarz <slusarz@horde.org>
2013-09-27 11:02:05 -0600

    Revert "This seems to prevent a PHP bug from being triggered."
    This reverts commit 4faaf153875911d05a2329f693ff767cefdc2e7c.

--- framework/Mime/lib/Horde/Mime/Part.php
+++ framework/Mime/lib/Horde/Mime/Part.php
@@ -564,8 +564,7 @@ class Horde_Mime_Part implements ArrayAccess, Countable, Serializable
                 'filter' => array(
                     'convert.quoted-printable-encode' => array_filter(array(
                         'line-break-chars' => $stream->getEOL(),
-                        'line-length' => 76,
-                        'binary' => true
+                        'line-length' => 76
                     ))
                 )
             ));
