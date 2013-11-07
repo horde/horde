6f1ffaed44c6c4560602d82899297a328bc1a741
Michael M Slusarz <slusarz@horde.org>
2013-11-06 21:02:04 -0700

    Convert viewport mousewheel handler to use new DOM wheel event if available

--- imp/js/viewport.js
+++ imp/js/viewport.js
@@ -1354,7 +1354,8 @@ ViewPort_Scroller = Class.create({
             return;
         }
 
-        var c = this.vp.opts.content;
+        var c = this.vp.opts.content,
+            mw = this.mousewheelHandler.bindAsEventListener(this);
 
         // Create the outer div.
         this.scrollDiv = new Element('DIV', { className: 'vpScroll' }).setStyle({ cssFloat: 'left', overflow: 'hidden' }).hide();
@@ -1384,17 +1385,30 @@ ViewPort_Scroller = Class.create({
        });
 
         // Mouse wheel handler.
-        c.observe(Prototype.Browser.Gecko ? 'DOMMouseScroll' : 'mousewheel', function(e) {
-            var move_num = Math.min(this.vp.getPageSize(), 3);
-            this.moveScroll(this.currentOffset() + ((e.wheelDelta >= 0 || e.detail < 0) ? (-1 * move_num) : move_num));
-            /* Mozilla bug https://bugzilla.mozilla.org/show_bug.cgi?id=502818
-             * Need to stop or else multiple scroll events may be fired. We
-             * lose the ability to have the mousescroll bubble up, but that is
-             * more desirable than having the wrong scrolling behavior. */
-            if (Prototype.Browser.Gecko && !e.stop) {
-                Event.stop(e);
-            }
-        }.bindAsEventListener(this));
+        if ('onwheel' in document || (document.documentMode >= 9)) {
+            c.observe('wheel', mw);
+        } else {
+            c.observe('mousewheel', mw).observe('DomMouseScroll', mw);
+        }
+    },
+
+    mousewheelHandler: function(e)
+    {
+        var delta = e.wheelDelta || 0;
+
+        if (e.detail) {
+            delta = e.detail * -1;
+        }
+        if (e.deltaY) {
+            delta = e.deltaY * -1;
+        }
+        if (!Object.isUndefined(e.wheelDeltaY)) {
+            delta = e.wheelDeltaY;
+        }
+
+        if (delta) {
+            this.moveScroll(this.currentOffset() + (Math.min(this.vp.getPageSize(), 3) * (delta > 0 ? -1 : 1)));
+        }
     },
 
     setSize: function(viewsize, totalsize)
