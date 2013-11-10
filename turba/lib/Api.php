4a4155767a64b54f6242b142b8fa07ca02fc318b
Michael J Rubinsky <mrubinsk@horde.org>
2013-11-10 14:56:47 -0500

    Allow passing addressbook id to filter MODSEQ.

--- turba/lib/Api.php
+++ turba/lib/Api.php
@@ -621,12 +621,20 @@ class Turba_Api extends Horde_Registry_Api
     /**
      * Return the largest modification sequence from the history backend.
      *
+     * @param string $id  Addressbook id to return highest MODSEQ for. If
+     *                    null, the highest MODSEQ across all addressbooks is
+     *                    returned. @since 4.2.0
+     *
      * @return integer  The modseq.
      * @since 4.1.1
      */
-    public function getHighestModSeq()
+    public function getHighestModSeq($id = null)
     {
-        return $GLOBALS['injector']->getInstance('Horde_History')->getHighestModSeq('turba');
+        $parent = 'turba';
+        if (!empty($id)) {
+            $parent .= ':' . $id;
+        }
+        return $GLOBALS['injector']->getInstance('Horde_History')->getHighestModSeq($parent);
     }
 
     /**
