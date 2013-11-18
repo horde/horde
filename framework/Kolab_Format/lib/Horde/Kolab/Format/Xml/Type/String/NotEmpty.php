0aa07c81f46c6a626f8b8f69673e4fe9b3460592
Jan Schneider <jan@horde.org>
2013-11-18 16:21:12 +0100

    [jan] Correctly translate display-name attribute of distribution lists.

---
+++ framework/Kolab_Format/lib/Horde/Kolab/Format/Xml/Type/String/NotEmpty.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright 2011-2013 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/libraries/Horde_Kolab_Format
+ */
+
+/**
+ * Handles a string attribute that must not be empty.
+ *
+ * @category Kolab
+ * @package  Kolab_Format
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @link     http://www.horde.org/libraries/Horde_Kolab_Format
+ */
+class Horde_Kolab_Format_Xml_Type_String_NotEmpty
+extends Horde_Kolab_Format_Xml_Type_String
+{
+    /**
+     * Indicate which value type is expected.
+     *
+     * @var int
+     */
+    protected $value = Horde_Kolab_Format_Xml::VALUE_NOT_EMPTY;
+}
