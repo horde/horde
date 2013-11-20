083cac00bcdff081372bf7f1e27fe1e939f3e4ec
Michael J Rubinsky <mrubinsk@horde.org>
2013-11-20 12:35:17 -0500

    Match look with admin page.

--- horde/templates/prefs/activesync.html.php
+++ horde/templates/prefs/activesync.html.php
@@ -8,30 +8,30 @@
  </select>
 </div>
 <?php endif ?>
-
-<br class="spacer" />
-<h2 class="smallheader"><?php echo _("State Management") ?></h2>
-<p>
- <input class="horde-delete" type="submit" value="<?php echo _("Reset all device state") ?>" name="reset" />
- <?php echo _("This will cause your devices to resyncronize all items.") ?>
-</p>
-
 <br class="spacer" />
 <h2 class="smallheader"><?php echo _("Device Management") ?></h2>
 <?php if ($this->devices): ?>
 <input type="hidden" id="removedevice" name="removedevice" />
 <input type="hidden" name="wipeid" id="wipeid" />
 <input type="hidden" name="cancelwipe" id="cancelwipe" />
-<table class="horde-table striped">
+<table class="horde-table activesync-devices striped">
  <tr class="header">
-  <th></th>
   <th class="smallheader"><?php echo _("Device") ?></th>
   <th class="smallheader"><?php echo _("Last Sync Time") ?></th>
   <th class="smallheader"><?php echo _("Status") ?></th>
   <th class="smallheader"><?php echo _("Device Information") ?></th>
+  <th class="smallheader"><?php echo _("Actions")?></th>
  </tr>
 <?php foreach ($this->devices as $d): ?>
  <tr>
+  <td><?php echo $d->deviceType ?></td>
+  <td><?php echo $d->getLastSyncTimestamp() ? strftime($GLOBALS['prefs']->getValue('date_format') . ' %H:%M', $d->getLastSyncTimestamp()) : _("None") ?></td>
+  <td><?php echo $status ?></td>
+  <td>
+    <?php foreach ($d->getFormattedDeviceProperties() as $key => $value): ?>
+      <?php echo '<b>' . $key . '</b>: ' . $value . '<br />' ?>
+    <?php endforeach; ?>
+  </td>
   <td>
     <?php if ($d->policykey): ?>
       <input class="horde-delete" type="button" value="<?php echo _("Wipe") ?>" id="wipe_<?php echo $d->id ?>" />
@@ -46,14 +46,6 @@
     <?php endif; ?>
     <input class="horde-delete" type="button" value="<?php echo _("Remove") ?>" id="remove_<?php echo $d->id ?>" />
   </td>
-  <td><?php echo $d->deviceType ?></td>
-  <td><?php echo $d->getLastSyncTimestamp() ? strftime($GLOBALS['prefs']->getValue('date_format') . ' %H:%M', $d->getLastSyncTimestamp()) : _("None") ?></td>
-  <td><?php echo $status ?></td>
-  <td>
-    <?php foreach ($d->getFormattedDeviceProperties() as $key => $value): ?>
-      <?php echo '<b>' . $key . '</b>: ' . $value . '<br />' ?>
-    <?php endforeach; ?>
-  </td>
  </tr>
 <?php endforeach; ?>
 </table>
@@ -62,7 +54,10 @@
  <em><?php echo _("None") ?></em>
 </p>
 <?php endif; ?>
-
+<p>
+ <input class="horde-delete" type="submit" value="<?php echo _("Reset all device state") ?>" name="reset" />
+ <?php echo _("This will cause your devices to resyncronize all items.") ?>
+</p>
 <p>
  <strong>
   <?php echo _("NOTE: WIPING A DEVICE MAY RESET IT TO FACTORY DEFAULTS. PLEASE MAKE SURE YOU REALLY WANT TO DO THIS BEFORE REQUESTING A WIPE") ?>
