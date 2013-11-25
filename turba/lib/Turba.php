c58ff5db7a661df5211a796aeab4e728bd4b03dd
Jan Schneider <jan@horde.org>
2013-11-25 19:59:04 +0100

    Support displaying tag columns.

--- turba/lib/Turba.php
+++ turba/lib/Turba.php
@@ -177,7 +177,8 @@ class Turba
                     $source = array_splice($cols, 0, 1);
                     $columns[$source[0]] = array();
                     foreach ($cols as $col) {
-                        if (isset($GLOBALS['cfgSources'][$source[0]]['map'][$col])) {
+                        if ($col == '__tags' ||
+                            isset($GLOBALS['cfgSources'][$source[0]]['map'][$col])) {
                             $columns[$source[0]][] = $col;
                         }
                     }
