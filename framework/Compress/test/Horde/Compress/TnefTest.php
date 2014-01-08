ddeaee30c552f0007b93926bc96714f7258da48f
Michael M Slusarz <slusarz@horde.org>
2014-01-08 08:54:32 -0700

    Fix indents

--- framework/Compress/test/Horde/Compress/TnefTest.php
+++ framework/Compress/test/Horde/Compress/TnefTest.php
@@ -14,18 +14,18 @@ class Horde_Compress_TnefTest extends Horde_Test_Case
 {
     public $testdata;
 
-   public function testMeetingInvitation()
-   {
+    public function testMeetingInvitation()
+    {
         $tnef = Horde_Compress::factory('Tnef');
         $data = base64_decode(file_get_contents(__DIR__ . '/fixtures/TnefMeetingRequest.txt'));
         $tnef_data = $tnef->decompress($data);
         $this->assertEquals($tnef_data[0]['type'], 'text');
         $this->assertEquals($tnef_data[0]['subtype'], 'calendar');
         $this->assertEquals($tnef_data[0]['name'], 'Meeting');
-   }
+    }
 
-   public function testAttachments()
-   {
+    public function testAttachments()
+    {
         $data = base64_decode(file_get_contents(__DIR__ . '/fixtures/TnefAttachments.txt'));
         $tnef = Horde_Compress::factory('Tnef');
         $tnef_data = $tnef->decompress($data);
@@ -33,10 +33,10 @@ class Horde_Compress_TnefTest extends Horde_Test_Case
         $this->assertEquals($tnef_data[0]['subtype'], 'octet-stream');
         $this->assertEquals($tnef_data[0]['name'], 'hasselhoff_birthday.jpg');
         $this->assertEquals($tnef_data[0]['size'], 80051);
-   }
+    }
 
-   public function testMultipleAttachments()
-   {
+    public function testMultipleAttachments()
+    {
         $data = base64_decode(file_get_contents(__DIR__ . '/fixtures/TnefAttachmentsMultiple.txt'));
         $tnef = Horde_Compress::factory('Tnef');
         $tnef_data = $tnef->decompress($data);
@@ -46,6 +46,6 @@ class Horde_Compress_TnefTest extends Horde_Test_Case
         $this->assertEquals($tnef_data[1]['type'], 'application');
         $this->assertEquals($tnef_data[1]['subtype'], 'octet-stream');
         $this->assertEquals($tnef_data[1]['name'], 'Penguins.jpg');
-   }
+    }
 
 }
