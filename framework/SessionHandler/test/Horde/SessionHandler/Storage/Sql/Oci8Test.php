fd67d63c58b32eaf1b4fecb9e976b63fbdfc3735
Jan Schneider <jan@horde.org>
2014-01-16 14:54:50 +0100

    Add Oracle test.

---
+++ framework/SessionHandler/test/Horde/SessionHandler/Storage/Sql/Oci8Test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Prepare the test setup.
+ */
+require_once dirname(__FILE__) . '/Base.php';
+
+/**
+ * Copyright 2012-2013 Horde LLC (http://www.horde.org/)
+ *
+ * @author     Jan Schneider <jan@horde.org>
+ * @category   Horde
+ * @package    SessionHandler
+ * @subpackage UnitTests
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class Horde_SessionHandler_Storage_Sql_Oci8Test extends Horde_SessionHandler_Storage_Sql_Base
+{
+    public static function setUpBeforeClass()
+    {
+        if (!extension_loaded('oci8')) {
+            self::$reason = 'No oci8 extension';
+            return;
+        }
+        $config = self::getConfig('SESSIONHANDLER_SQL_OCI8_TEST_CONFIG',
+                                  dirname(__FILE__) . '/../..');
+        if (!$config || empty($config['sessionhandler']['sql']['oci8'])) {
+            self::$reason = 'No oci8 configuration';
+            return;
+        }
+        self::$db = new Horde_Db_Adapter_Oci8($config['sessionhandler']['sql']['oci8']);
+        parent::setUpBeforeClass();
+    }
+}
