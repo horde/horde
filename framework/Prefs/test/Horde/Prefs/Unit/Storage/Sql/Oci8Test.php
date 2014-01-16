5398ac32ea04a54ee65830eab122b580098a4c1f
Jan Schneider <jan@horde.org>
2014-01-16 14:48:56 +0100

    Add Oracle test.

---
+++ framework/Prefs/test/Horde/Prefs/Unit/Storage/Sql/Oci8Test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Prepare the test setup.
+ */
+require_once __DIR__ . '/Base.php';
+
+/**
+ * Copyright 2011-2013 Horde LLC (http://www.horde.org/)
+ *
+ * @author     Jan Schneider <jan@horde.org>
+ * @category   Horde
+ * @package    Prefs
+ * @subpackage UnitTests
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class Horde_Prefs_Unit_Storage_Sql_Oci8Test extends Horde_Prefs_Test_Sql_Base
+{
+    public static function setUpBeforeClass()
+    {
+        if (!extension_loaded('oci8')) {
+            self::$reason = 'No oci8 extension';
+            return;
+        }
+        $config = self::getConfig('PREFS_SQL_OCI8_TEST_CONFIG',
+                                  __DIR__ . '/../../..');
+        if ($config && !empty($config['prefs']['sql']['oci8'])) {
+            self::$db = new Horde_Db_Adapter_Oci8($config['prefs']['sql']['oci8']);
+            parent::setUpBeforeClass();
+        } else {
+            self::$reason = 'No oci8 configuration';
+        }
+    }
+}
