9ba9b15a29c2ad144e6536bc2a5925b9ac12c8b0
Jan Schneider <jan@horde.org>
2014-01-22 20:54:57 +0100

    Add test for Oracle.

---
+++ framework/Vfs/test/Horde/Vfs/Sql/Oci8Test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Prepare the test setup.
+ */
+require_once __DIR__ . '/Base.php';
+
+/**
+ * Copyright 2014 Horde LLC (http://www.horde.org/)
+ *
+ * @author     Jan Schneider <jan@horde.org>
+ * @category   Horde
+ * @package    Vfs
+ * @subpackage UnitTests
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class Horde_Vfs_Sql_Oci8Test extends Horde_Vfs_Test_Sql_Base
+{
+    public static function setUpBeforeClass()
+    {
+        if (!extension_loaded('oci8')) {
+            self::$reason = 'No oci8 extension';
+            return;
+        }
+        $config = self::getConfig('VFS_SQL_OCI8_TEST_CONFIG',
+                                  __DIR__ . '/..');
+        if ($config && !empty($config['vfs']['sql']['oci8'])) {
+            self::$db = new Horde_Db_Adapter_Oci8($config['vfs']['sql']['oci8']);
+            //self::$db->setLogger(new Horde_Log_Logger(new Horde_Log_Handler_Cli()));
+            parent::setUpBeforeClass();
+        } else {
+            self::$reason = 'No oci8 configuration';
+        }
+    }
+}
