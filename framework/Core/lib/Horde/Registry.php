3a6e1eb3656868a7a4f2048edddfd9707da702f2
Michael M Slusarz <slusarz@horde.org>
2014-01-22 21:41:34 -0700

    Offload registry config initialization code to a separate library
    The main registry library is getting a bit large/bloated, so this is a
    good candidate to move to a standalone library - about 7 KB of code that
    is only generally used once a session.

--- framework/Core/lib/Horde/Registry.php
+++ framework/Core/lib/Horde/Registry.php
@@ -88,6 +88,15 @@ class Horde_Registry implements Horde_Shutdown_Task
     public $nlsconfig;
 
     /**
+     * The current virtual host configuration file.
+     *
+     * @since 2.12.0
+     *
+     * @var string
+     */
+    public $vhost = null;
+
+    /**
      * The list of APIs.
      *
      * @var array
@@ -142,13 +151,6 @@ class Horde_Registry implements Horde_Shutdown_Task
      * @var integer
      */
     protected $_regmtime;
-
-    /**
-     * The current vhost configuration file.
-     *
-     * @var string
-     */
-    protected $_vhost = null;
 
     /**
      * Application bootstrap initialization.
@@ -502,11 +504,10 @@ class Horde_Registry implements Horde_Shutdown_Task
             $this->_regmtime = max($this->_regmtime, filemtime(HORDE_BASE . '/config/registry.local.php'));
         }
         if (!empty($conf['vhosts'])) {
-            $this->_vhost = HORDE_BASE . '/config/registry-' . $conf['server']['name'] . '.php';
-            if (file_exists($this->_vhost)) {
-                $this->_regmtime = max($this->_regmtime, filemtime($this->_vhost));
-            } else {
-                $this->_vhost = null;
+            $vhost = HORDE_BASE . '/config/registry-' . $conf['server']['name'] . '.php';
+            if (file_exists($vhost)) {
+                $this->vhost = $vhost;
+                $this->_regmtime = max($this->_regmtime, filemtime($vhost));
             }
         }
 
@@ -665,7 +666,7 @@ class Horde_Registry implements Horde_Shutdown_Task
         }
 
         /* First, try to load from cache. */
-        if (empty($this->_args['test'])) {
+        if (!$this->isTest()) {
             $lcache = $injector->getInstance('Horde_Core_Localcache');
 
             if (($cid = $this->_cacheId()) &&
@@ -680,118 +681,11 @@ class Horde_Registry implements Horde_Shutdown_Task
             }
         }
 
-        /* Read the registry configuration files. */
-        if (!file_exists(HORDE_BASE . '/config/registry.php')) {
-            throw new Horde_Exception('Missing registry.php configuration file');
-        }
+        $config = new Horde_Registry_Registryconfig($this);
+        $this->applications = $config->applications;
+        $this->_interfaces = $config->interfaces;
 
-        /* Set textdomain to Horde, so that we really only load translations
-         * from Horde. */
-        if ($this->getApp() != 'horde') {
-            textdomain('horde');
-        }
-
-        require HORDE_BASE . '/config/registry.php';
-        if ($files = glob(HORDE_BASE . '/config/registry.d/*.php')) {
-            foreach ($files as $r) {
-                include $r;
-            }
-        }
-        if (file_exists(HORDE_BASE . '/config/registry.local.php')) {
-            include HORDE_BASE . '/config/registry.local.php';
-        }
-        if ($this->_vhost) {
-            include $this->_vhost;
-        }
-
-        /* Reset textdomain. */
-        if ($this->getApp() != 'horde') {
-            textdomain($this->getApp());
-        }
-
-        if (!isset($this->applications['horde']['fileroot'])) {
-            $this->applications['horde']['fileroot'] = isset($app_fileroot)
-                ? $app_fileroot
-                : HORDE_BASE;
-        }
-        if (!isset($app_fileroot)) {
-            $app_fileroot = $this->applications['horde']['fileroot'];
-        }
-
-        /* Make sure the fileroot of Horde has a trailing slash to not trigger
-         * open_basedir restrictions that have that trailing slash too. */
-        $app_fileroot = rtrim($app_fileroot, '/') . '/';
-
-        if (!isset($this->applications['horde']['webroot'])) {
-            $this->applications['horde']['webroot'] = isset($app_webroot)
-                ? $app_webroot
-                : $this->_detectWebroot();
-        }
-        if (!isset($app_webroot)) {
-            $app_webroot = $this->applications['horde']['webroot'];
-        }
-
-        /* Scan for all APIs provided by each app, and set other common
-         * defaults like templates and graphics. */
-        foreach ($this->applications as $appName => &$app) {
-            if (!isset($app['status'])) {
-                $app['status'] = 'active';
-            } elseif ($app['status'] == 'heading' ||
-                      $app['status'] == 'topbar'  ||
-                      $app['status'] == 'link') {
-                continue;
-            }
-
-            $app['fileroot'] = isset($app['fileroot'])
-                ? rtrim($app['fileroot'], ' /')
-                : $app_fileroot . $appName;
-
-            if (!isset($app['name'])) {
-                $app['name'] = '';
-            }
-
-            if (!file_exists($app['fileroot']) ||
-                (empty($this->_args['test']) &&
-                 file_exists($app['fileroot'] . '/config/conf.xml') &&
-                 !file_exists($app['fileroot'] . '/config/conf.php'))) {
-                $app['status'] = 'inactive';
-                Horde::log('Setting ' . $appName . ' inactive because the fileroot does not exist or the application is not configured yet.', 'DEBUG');
-            }
-
-            $app['webroot'] = isset($app['webroot'])
-                ? rtrim($app['webroot'], ' /')
-                : $app_webroot . '/' . $appName;
-
-            if (($app['status'] != 'inactive') &&
-                isset($app['provides']) &&
-                (($app['status'] != 'admin') || $this->isAdmin())) {
-                if (is_array($app['provides'])) {
-                    foreach ($app['provides'] as $interface) {
-                        $this->_interfaces[$interface] = $appName;
-                    }
-                } else {
-                    $this->_interfaces[$app['provides']] = $appName;
-                }
-            }
-
-            if (!isset($app['templates']) && isset($app['fileroot'])) {
-                $app['templates'] = $app['fileroot'] . '/templates';
-            }
-            if (!isset($app['jsuri']) && isset($app['webroot'])) {
-                $app['jsuri'] = $app['webroot'] . '/js';
-            }
-            if (!isset($app['jsfs']) && isset($app['fileroot'])) {
-                $app['jsfs'] = $app['fileroot'] . '/js';
-            }
-            if (!isset($app['themesuri']) && isset($app['webroot'])) {
-                $app['themesuri'] = $app['webroot'] . '/themes';
-            }
-            if (!isset($app['themesfs']) && isset($app['fileroot'])) {
-                $app['themesfs'] = $app['fileroot'] . '/themes';
-            }
-        }
-
-        if (!empty($this->_args['test'])) {
+        if ($this->isTest()) {
             return;
         }
 
@@ -838,40 +732,6 @@ class Horde_Registry implements Horde_Shutdown_Task
             $this->_regmtime,
             $sha1
         ));
-    }
-
-    /**
-     * Attempt to auto-detect the Horde webroot.
-     *
-     * @return string  The webroot.
-     */
-    protected function _detectWebroot()
-    {
-        // Note for Windows: the below assumes the PHP_SELF variable uses
-        // forward slashes.
-        if (isset($_SERVER['SCRIPT_URL']) || isset($_SERVER['SCRIPT_NAME'])) {
-            $path = empty($_SERVER['SCRIPT_URL'])
-                ? $_SERVER['SCRIPT_NAME']
-                : $_SERVER['SCRIPT_URL'];
-            $hordedir = basename(str_replace(DIRECTORY_SEPARATOR, '/', realpath(HORDE_BASE)));
-            return (preg_match(';/' . $hordedir . ';', $path))
-                ? preg_replace(';/' . $hordedir . '.*;', '/' . $hordedir, $path)
-                : '';
-        }
-
-        if (!isset($_SERVER['PHP_SELF'])) {
-            return '/horde';
-        }
-
-        $webroot = preg_split(';/;', $_SERVER['PHP_SELF'], 2, PREG_SPLIT_NO_EMPTY);
-        $webroot = strstr(realpath(HORDE_BASE), DIRECTORY_SEPARATOR . array_shift($webroot));
-        if ($webroot !== false) {
-            return preg_replace(array('/\\\\/', ';/config$;'), array('/', ''), $webroot);
-        }
-
-        return ($webroot === false)
-            ? ''
-            : '/horde';
     }
 
     /**
@@ -2883,4 +2743,17 @@ class Horde_Registry implements Horde_Shutdown_Task
             ? $trans_lang
             : $this->nlsconfig->aliases[$trans_lang];
     }
+
+    /**
+     * Is the registry in 'test' mode?
+     *
+     * @since 2.12.0
+     *
+     * @return boolean  True if in testing mode.
+     */
+    public function isTest()
+    {
+        return !empty($this->_args['test']);
+    }
+
 }
