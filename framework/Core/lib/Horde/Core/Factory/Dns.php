8d580a2008c868e984a4423cc85e79f92b0e900e
Michael M Slusarz <slusarz@horde.org>
2014-02-04 11:19:01 -0700

    These issues have gone away in later versions of Net_DNS2

--- framework/Core/lib/Horde/Core/Factory/Dns.php
+++ framework/Core/lib/Horde/Core/Factory/Dns.php
@@ -18,14 +18,6 @@ class Horde_Core_Factory_Dns extends Horde_Core_Factory_Injector
             } catch (Net_DNS2_Exception $e) {}
         }
 
-        // TODO: Fixes for Net_DNS2 v1.0.0
-        if (!defined('SOCK_DGRAM')) {
-            define('SOCK_STREAM', 1);
-            define('SOCK_DGRAM', 2);
-        }
-
-        spl_autoload_unregister('Net_DNS2::autoload');
-
         return $resolver;
     }
 }
