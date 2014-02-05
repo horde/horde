7bf69c33d719df452744e8e018687a1a04c1ad30
Michael J Rubinsky <mrubinsk@horde.org>
2014-02-05 17:16:07 -0500

    One more place to fix the date.

--- framework/Core/lib/Horde/Core/HordeMap.php
+++ framework/Core/lib/Horde/Core/HordeMap.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2014 Horde LLC (http://www.horde.org/)
+ * Copyright 2009-2014 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
@@ -14,7 +14,7 @@
 /**
  * HordeMap.
  *
- * Copyright 2014 Horde LLC (http://www.horde.org/)
+ * Copyright 2009-2014 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
