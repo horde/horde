7da07ee4ecedaffee5c4d860d3ece2a959b81e69
Michael M Slusarz <slusarz@horde.org>
2014-02-07 01:08:00 -0700

    Improved formatting of the whitelist screen

--- ingo/templates/basic/whitelist/whitelist.html.php
+++ ingo/templates/basic/whitelist/whitelist.html.php
@@ -9,25 +9,16 @@
 <?php endif; ?>
  </h1>
 
- <table>
-  <tr>
-   <td colspan="2">
-    <em><?php echo $this->hordeLabel('whitelist', _("Wh_itelist addresses:")) ?></em>
-   </td>
-  </tr>
-  <tr>
-   <td>
-    <textarea name="whitelist" id="whitelist" rows="15" cols="80"><?php echo $this->h($this->whitelist) ?></textarea>
-   </td>
-   <td>
-    <?php echo $this->hordeHelp('ingo', 'whitelist-addresses') ?>
-   </td>
-  </tr>
-  <tr>
-   <td colspan="2" class="control">
-    <input class="horde-default" type="submit" value="<?php echo _("Save") ?>" />
-    <input type="button" id="whitelist_return" value="<?php echo _("Return to Rules List") ?>" />
-   </td>
-  </tr>
- </table>
+ <div>
+  <em><?php echo $this->hordeLabel('whitelist', _("Wh_itelist addresses:")) ?></em>
+ </div>
+
+ <div>
+  <textarea name="whitelist" id="whitelist" rows="15" cols="80"><?php echo $this->h($this->whitelist) ?></textarea>
+ </div>
+
+ <div class="control">
+  <input class="horde-default" type="submit" value="<?php echo _("Save") ?>" />
+  <input type="button" id="whitelist_return" value="<?php echo _("Return to Rules List") ?>" />
+ </div>
 </form>
