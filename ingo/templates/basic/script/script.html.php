611f043a31ad6c325d967d9b02becea37fe3bc56
Michael M Slusarz <slusarz@horde.org>
2014-02-07 01:43:59 -0700

    Improved formatting of the script screen

--- ingo/templates/basic/script/script.html.php
+++ ingo/templates/basic/script/script.html.php
@@ -1,38 +1,26 @@
 <h1 class="header"><?php echo _("Script") ?></h1>
 <?php if ($this->scriptexists): ?>
-<table>
+<table class="scriptHeader">
  <tr>
   <td align="center">
-   <table class="scriptHeader">
-    <tr>
 <?php if ($this->showactivate): ?>
-     <td>
-      <form method="post" name="activate_script" action="<?php echo $this->scripturl ?>">
-       <input type="hidden" name="actionID" value="action_activate" />
-       <input class="horde-default" type="submit" name="submit" value="<?php echo _("Activate Script") ?>" />
-      </form>
-     </td>
-     <td>
-      <form method="post" name="deactivate_script" action="<?php echo $this->scripturl ?>">
-       <input type="hidden" name="actionID" value="action_deactivate" />
-       <input type="submit" name="submit" value="<?php echo _("Deactivate Script") ?>" />
-      </form>
-     </td>
-     <td>
-      <form method="post" name="show_active_script" action="<?php echo $this->scripturl ?>">
-       <input type="hidden" name="actionID" value="show_active" />
-       <input type="submit" name="submit" value="<?php echo _("Show Active Script") ?>" />
-      </form>
-     </td>
+   <form method="post" name="activate_script" action="<?php echo $this->scripturl ?>">
+    <input type="hidden" name="actionID" value="action_activate" />
+    <input class="horde-default" type="submit" name="submit" value="<?php echo _("Activate Script") ?>" />
+   </form>
+   <form method="post" name="deactivate_script" action="<?php echo $this->scripturl ?>">
+    <input type="hidden" name="actionID" value="action_deactivate" />
+    <input type="submit" name="submit" value="<?php echo _("Deactivate Script") ?>" />
+   </form>
+   <form method="post" name="show_active_script" action="<?php echo $this->scripturl ?>">
+    <input type="hidden" name="actionID" value="show_active" />
+    <input type="submit" name="submit" value="<?php echo _("Show Active Script") ?>" />
+   </form>
 <?php else: ?>
-     <td>
-      <form method="post" name="show_current_script" action="<?php echo $this->scripturl ?>">
-       <input type="submit" name="submit" value="<?php echo _("Show Current Script") ?>" />
-      </form>
-     </td>
+   <form method="post" name="show_current_script" action="<?php echo $this->scripturl ?>">
+    <input type="submit" name="submit" value="<?php echo _("Show Current Script") ?>" />
+   </form>
 <?php endif; ?>
-    </tr>
-   </table>
   </td>
  </tr>
 </table>
@@ -40,5 +28,5 @@
 <?php echo $this->renderPartial('script', array('collection' => $this->scripts)) ?>
 
 <?php else: ?>
-    [<?php echo _("No script generated.") ?>]
+<div>[<?php echo _("No script generated.") ?>]</div>
 <?php endif; ?>
