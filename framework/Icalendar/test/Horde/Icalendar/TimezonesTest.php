9e26d10308c3676cf8e558f43b1e8237303a097d
Jan Schneider <jan@horde.org>
2014-02-11 18:17:40 +0100

    Revert "Move tests to timezonesTest."
    Bug is in Horde_Date.
    
    This reverts commit 76a135f2560e0aded3b4cfbf9dbfa7e11ccd70eb.

--- framework/Icalendar/test/Horde/Icalendar/TimezonesTest.php
+++ framework/Icalendar/test/Horde/Icalendar/TimezonesTest.php
@@ -42,41 +42,4 @@ class Horde_Icalendar_TimezonesTest extends Horde_Test_Case
                 'Failed parsing file ' . basename($file));
         }
     }
-
-    public function testBug12843LocalTimezoneNotTheSame()
-    {
-        $tz = date_default_timezone_get();
-        date_default_timezone_set('America/New_York');
-        $ical = new Horde_Icalendar();
-        $ical->parsevCalendar(file_get_contents(__DIR__ . '/fixtures/bug12843.ics'));
-        foreach ($ical->getComponents() as $component) {
-            if ($component->getType() != 'vEvent') {
-                continue;
-            }
-            $date_params = $component->getAttribute('DTSTART', true);
-            $date = $component->getAttribute('DTSTART');
-            $start = new Horde_Date($date, $date_params[0]['TZID']);
-            $this->assertEquals(18, $start->hour);
-        }
-        date_default_timezone_set($tz);
-    }
-
-    public function testBug12843LocalTimezoneTheSame()
-    {
-        $tz = date_default_timezone_get();
-        date_default_timezone_set('Europe/Berlin');
-        $ical = new Horde_Icalendar();
-        $ical->parsevCalendar(file_get_contents(__DIR__ . '/fixtures/bug12843.ics'));
-        foreach ($ical->getComponents() as $component) {
-            if ($component->getType() != 'vEvent') {
-                continue;
-            }
-            $date_params = $component->getAttribute('DTSTART', true);
-            $date = $component->getAttribute('DTSTART');
-            $start = new Horde_Date($date, $date_params[0]['TZID']);
-            $this->assertEquals(18, $start->hour);
-        }
-        date_default_timezone_set($tz);
-    }
-
 }
