f47fcc57d9bef1787549e5aab85b03c860125868
Michael J Rubinsky <mrubinsk@horde.org>
2014-02-11 11:54:49 -0500

    Fix setting the initially open tab.
    Fixes broken tab behavior when switching tabs for the first time
    in each page view.

--- turba/lib/View/Contact.php
+++ turba/lib/View/Contact.php
@@ -40,7 +40,7 @@ class Turba_View_Contact
 
         $vars = new Horde_Variables();
         $form = new Turba_Form_Contact($vars, $this->contact);
-        $form->setOpenSection(Horde_Util::getFormData('section'));
+        $form->setOpenSection(Horde_Util::getFormData('section', 0));
 
         /* Get the contact's history. */
         $history = $this->contact->getHistory();
