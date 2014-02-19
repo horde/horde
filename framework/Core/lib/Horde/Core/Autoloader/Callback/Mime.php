e05c6aefd5c0a342aae2489b37f805c0f710a3a8
Michael M Slusarz <slusarz@horde.org>
2014-02-18 23:00:11 -0700

    Send workaround to RFC 2231 by default always

--- framework/Core/lib/Horde/Core/Autoloader/Callback/Mime.php
+++ framework/Core/lib/Horde/Core/Autoloader/Callback/Mime.php
@@ -10,7 +10,7 @@ class Horde_Core_Autoloader_Callback_Mime
      */
     static public function callback()
     {
-        Horde_Mime::$brokenRFC2231 = !empty($GLOBALS['conf']['mailformat']['brokenrfc2231']);
+        Horde_Mime::$brokenRFC2231 = true;
     }
 
 }
