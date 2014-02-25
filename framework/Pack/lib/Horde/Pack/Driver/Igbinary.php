2b30d356b9cfab04f4a3faab11b86708aa74bee8
Michael M Slusarz <slusarz@horde.org>
2014-02-24 23:36:51 -0700

    [mms] Due to issues with igbinary 1.1.1, disable the igbinary driver.
    Unfortunately, there is a nasty bug with 1.1.1 that will cause PHP to
    terminate without error or segfault on certain data.  I can reliably
    trigger this when unserializing session data after logging into my test
    Cyrus 2.4 account.  This is obviously unacceptable.
    
    I have confirmed that the latest git release fixes this issue (from the
    github repo).  But I don't feel the need to create a Horde-specific
    release to remedy this at this time, so take the more conservative
    approach of disabling entirely.

--- framework/Pack/lib/Horde/Pack/Driver/Igbinary.php
+++ framework/Pack/lib/Horde/Pack/Driver/Igbinary.php
@@ -34,6 +34,9 @@ class Horde_Pack_Driver_Igbinary extends Horde_Pack_Driver
      */
     static public function supported()
     {
+        // @todo: Enable when igbinary 1.1.2 (or higher) is released
+        return false;
+
         return extension_loaded('igbinary');
     }
 
