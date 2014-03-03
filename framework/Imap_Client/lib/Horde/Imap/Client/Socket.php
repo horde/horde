bd95f925568c0a8ff4ac279f5a64534e551ef569
Michael M Slusarz <slusarz@horde.org>
2014-03-03 12:00:55 -0700

    Revert "Better variable name."
    This reverts commit f69f149b38cf8f2b0fcc82d249dfa289e0ecfc01.

--- framework/Imap_Client/lib/Horde/Imap/Client/Socket.php
+++ framework/Imap_Client/lib/Horde/Imap/Client/Socket.php
@@ -2707,8 +2707,8 @@ class Horde_Imap_Client_Socket extends Horde_Imap_Client_Base
 
             case Horde_Imap_Client::FETCH_BODYPARTSIZE:
                 if ($this->queryCapability('BINARY')) {
-                    foreach ($c_val as $mime_id) {
-                        $fetch->add('BINARY.SIZE[' . $mime_id . ']');
+                    foreach ($c_val as $key) {
+                        $fetch->add('BINARY.SIZE[' . $key . ']');
                     }
                 }
                 break;
