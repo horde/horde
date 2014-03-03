3a886979f211eab67bac24c28786f8d97555785f
Jan Schneider <jan@horde.org>
2014-03-03 17:27:21 +0100

    Update to SabreDAV 1.8.9.

--- framework/Dav/vendor/sabre/dav/lib/Sabre/DAV/URLUtil.php
+++ framework/Dav/vendor/sabre/dav/lib/Sabre/DAV/URLUtil.php
@@ -33,7 +33,7 @@ class URLUtil {
      */
     static function encodePath($path) {
 
-        return preg_replace_callback('/([^A-Za-z0-9_\-\.~\(\)\/:@])/',function($match) {
+        return preg_replace_callback('/([^A-Za-z0-9_\-\.~\(\)\/:])/',function($match) {
 
             return '%'.sprintf('%02x',ord($match[0]));
 
@@ -51,7 +51,7 @@ class URLUtil {
      */
     static function encodePathSegment($pathSegment) {
 
-        return preg_replace_callback('/([^A-Za-z0-9_\-\.~\(\):@])/',function($match) {
+        return preg_replace_callback('/([^A-Za-z0-9_\-\.~\(\):])/',function($match) {
 
             return '%'.sprintf('%02x',ord($match[0]));
 
