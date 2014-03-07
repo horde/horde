3359d76081f0e17e900acbca356ed8173453bfd8
Jan Schneider <jan@horde.org>
2014-03-07 12:52:15 +0100

    IMP_Mailbox needs mocking.

--- imp/test/Imp/Unit/Mime/Viewer/ItipTest.php
+++ imp/test/Imp/Unit/Mime/Viewer/ItipTest.php
@@ -40,6 +40,8 @@ extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        $this->markTestIncomplete('IMP_Mailbox needs to be mocked.');
+
         $this->_oldtz = date_default_timezone_get();
         date_default_timezone_set('UTC');
 
