58da5313037faa2c3ae30c52d5e620bb81cb09c7
Jan Schneider <jan@horde.org>
2014-03-10 13:20:07 +0100

    Released ingo-3.1.4

--- ingo/lib/Application.php
+++ ingo/lib/Application.php
@@ -49,7 +49,7 @@ class Ingo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.1.4-git)';
+    public $version = 'H5 (3.1.4)';
 
     /**
      * Global variables defined:
