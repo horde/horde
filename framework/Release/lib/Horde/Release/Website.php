0dbfcb999edf6ba6de927eeeab8cb87ddadc9700
Jan Schneider <jan@horde.org>
2014-03-10 23:23:55 +0100

    [jan] Add release task to update website version.

---
+++ framework/Release/lib/Horde/Release/Website.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Copyright 2014 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Horde
+ * @package  Release
+ * @author   Jan Schneider <jan@horde.org>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @link     http://www.horde.org/libraries/Horde_Release
+ */
+
+/**
+ * Class for interfacing with the version DB of the Horde website.
+ *
+ * @category Horde
+ * @package  Release
+ * @author   Jan Schneider <jan@horde.org>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @link     http://www.horde.org/libraries/Horde_Release
+ */
+class Horde_Release_Website
+{
+    /**
+     * Database handle.
+     *
+     * @var PDO
+     */
+    protected $_db;
+
+    /**
+     * Constructor.
+     *
+     * @param PDO $db  Database handle.
+     */
+    public function __construct(PDO $db)
+    {
+        $this->_db = $db;
+    }
+
+    /**
+     * Adds a new version to an application.
+     *
+     * @param array $info  Hash with the version information. Possible keys:
+     *                     - application: (string) The name of the application.
+     *                     - version: (string) The version string.
+     *                     - state: (string) The version state. One of
+     *                              "stable", "dev", "three". By default
+     *                              automatically detected from "version.
+     *                     - date: (DateTime) The release date. Defaults to
+     *                             today.
+     *                     - pear: (boolean) A PEAR release? Defaults to true.
+     *                     - dir: (string) Optional website directory, if not
+     *                            "application".
+     *
+     * @throws Horde_Exception
+     */
+    public function addNewVersion(array $info = array())
+    {
+        if (!isset($info['application']) || !isset($info['version'])) {
+            throw new LogicException('Missing parameter');
+        }
+        $info = array_merge(
+            array('state' => $this->_stateFromVersion($info['version']),
+                  'date' => new DateTime(),
+                  'pear' => true,
+                  'dir' => null),
+            $info
+        );
+        if (!in_array($info['state'], array('stable', 'dev', 'three'))) {
+            throw new LogicException('Invalid state ' . $info['state']);
+        }
+
+        $info['date'] = $info['date']->format('Y-m-d');
+        $bind = array();
+        foreach ($info as $key => $value) {
+            $bind[':' . $key] = $value;
+        }
+
+        try {
+            $stmt = $this->_db
+                ->prepare('SELECT 1 FROM versions WHERE application = :application AND state = :state');
+            $stmt->execute(array(':application' => $info['application'], ':state' => $info['state']));
+            $stmt = $stmt->fetchColumn()
+                ? $this->_db->prepare('UPDATE versions SET version = :version, date = :date, pear = :pear, dir = :dir WHERE application = :application AND state = :state')
+                : $this->_db->prepare('INSERT INTO versions (application, state, version, date, pear, dir) VALUES (:application, :state, :version, :date, :pear, :dir)');
+            if (!$stmt->execute($bind)) {
+                $error = $stmt->errorInfo();
+                throw new Horde_Exception($error[2], $error[1]);
+            }
+        } catch (PDOException $e) {
+            throw new Horde_Exception($e);
+        }
+    }
+
+    /**
+     * Returns the release stability of a version.
+     *
+     * @param string $version  A version string.
+     *
+     * @return string Release stability information.
+     * @throws Horde_Exception on invalid version string.
+     */
+    protected function _stateFromVersion($version)
+    {
+        if (!preg_match('/^(\d+\.\d+\.\d+)(alpha|beta|RC|dev)?\d*$/', $version, $match)) {
+            throw new Horde_Exception('Invalid version ' . $version);
+        }
+        return isset($match[2]) ? 'dev' : 'stable';
+    }
+}
---
+++ framework/Release/test/Horde/Release/Unit/Release/WebsiteTest.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Copyright 2014 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category   Horde
+ * @package    Release
+ * @subpackage UnitTests
+ * @author     Jan Schneider <jan@pardus.de>
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @link       http://pear.horde.org/index.php?package=Release
+ */
+
+/**
+ * Test the website udpate.
+ *
+ * @category   Horde
+ * @package    Release
+ * @subpackage UnitTests
+ * @author     Jan Schneider <jan@pardus.de>
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @link       http://pear.horde.org/index.php?package=Release
+ */
+class Horde_Release_Unit_Release_WebsiteTest extends Horde_Release_TestCase
+{
+    public function setUp()
+    {
+        $this->db = new PDO('sqlite::memory:');
+        $this->db->query('CREATE TABLE versions (application VARCHAR(255), state VARCHAR(255), version VARCHAR(255), date VARCHAR(10), pear BOOLEAN, dir VARCHAR(255), PRIMARY KEY (application, state))');
+    }
+
+    /**
+     * @expectedException LogicException
+     */
+    public function testFailWithMissingApplication()
+    {
+        $this->_createObject()->addNewVersion(array(
+            'version' => '1.0.1'
+        ));
+    }
+
+    /**
+     * @expectedException LogicException
+     */
+    public function testFailWithMissingVersion()
+    {
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'horde',
+        ));
+    }
+
+    /**
+     * @expectedException LogicException
+     */
+    public function testFailWithIncorrectState()
+    {
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'horde',
+            'state' => 'foo',
+            'version' => '1.0.1'
+        ));
+    }
+
+    public function testAddNewVersion()
+    {
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'horde',
+            'state' => 'stable',
+            'version' => '1.0.1'
+        ));
+        $this->assertEquals(
+            '1.0.1',
+            $this->db
+                ->query('SELECT version FROM versions WHERE application = \'horde\' AND state = \'stable\'')
+                ->fetchColumn()
+        );
+    }
+
+    public function testUpdateNewVersion()
+    {
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'horde',
+            'state' => 'dev',
+            'version' => '1.0.0RC1'
+        ));
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'horde',
+            'state' => 'dev',
+            'version' => '1.0.0RC2'
+        ));
+        $this->assertEquals(
+            1,
+            $this->db
+                ->query('SELECT count(*) FROM versions WHERE application = \'horde\' AND state = \'dev\'')
+                ->fetchColumn()
+        );
+        $this->assertEquals(
+            '1.0.0RC2',
+            $this->db
+                ->query('SELECT version FROM versions WHERE application = \'horde\' AND state = \'dev\'')
+                ->fetchColumn()
+        );
+    }
+
+    public function testDetectState()
+    {
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'foo',
+            'version' => '1.0.0beta1'
+        ));
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'foo',
+            'version' => '1.0.1'
+        ));
+        $this->assertEquals(
+            array(
+                array(
+                    0 => '1.0.0beta1',
+                    'version' => '1.0.0beta1',
+                    1 => 'dev',
+                    'state' => 'dev',
+                ),
+                array(
+                    0 => '1.0.1',
+                    'version' => '1.0.1',
+                    1 => 'stable',
+                    'state' => 'stable',
+                ),
+            ),
+            $this->db
+                ->query('SELECT version, state FROM versions WHERE application = \'foo\'')
+                ->fetchAll()
+        );
+    }
+
+    public function testNow()
+    {
+        $now = new DateTime('now');
+        $this->_createObject()->addNewVersion(array(
+            'application' => 'bar',
+            'state' => 'stable',
+            'version' => '1.0.1'
+        ));
+        $this->assertEquals(
+            $now->format('Y-m-d'),
+            $this->db
+                ->query('SELECT date FROM versions WHERE application = \'bar\' AND state = \'stable\'')
+                ->fetchColumn()
+        );
+    }
+
+    protected function _createObject()
+    {
+        return new Horde_Release_Website($this->db);
+    }
+}
