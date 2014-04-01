83225df269132a33de08d061bc2a3703376a2fa2
Michael M Slusarz <slusarz@horde.org>
2014-04-01 17:26:42 -0600

    Cleaner display

--- imp/templates/prefs/specialuse.html.php
+++ imp/templates/prefs/specialuse.html.php
@@ -1,5 +1,3 @@
 <?php foreach ($this->special_use as $v): ?>
-<option value="" disabled="disabled">- - - - - - - - - -</option>
 <option value="<?php echo $v['v'] ?>"><?php echo _("Server Suggestion") ?>: <?php echo $this->h($v['l']) ?></option>
-<option value="" disabled="disabled">- - - - - - - - - -</option>
 <?php endforeach; ?>
