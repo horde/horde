ba7eca1c9a15b238971dad5a61248767ee9acb08
Michael M Slusarz <slusarz@horde.org>
2014-04-13 17:29:31 -0600

    phpdoc

--- framework/Core/lib/Horde/Core/Prefs/Cache/Session.php
+++ framework/Core/lib/Horde/Core/Prefs/Cache/Session.php
@@ -1,16 +1,24 @@
 <?php
 /**
- * Cache storage implementation using Horde_Session.
- *
  * Copyright 2010-2014 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
- * @author   Michael Slusarz <slusarz@horde.org>
- * @category Horde
- * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
- * @package  Core
+ * @category  Horde
+ * @copyright 2010-2014 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Core
+ */
+
+/**
+ * Cache storage implementation using Horde_Session.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2010-2014 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Core
  */
 class Horde_Core_Prefs_Cache_Session extends Horde_Prefs_Cache_Base
 {
