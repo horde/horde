9c7c500dc6ab3de7c579cc0dd799982b0e34c1e4
Michael M Slusarz <slusarz@horde.org>
2014-04-22 17:26:03 -0600

    [mms] Fix scrolling through large search mailboxes in dynamic view (Bug #13138).

--- imp/lib/Ajax/Application/ListMessages.php
+++ imp/lib/Ajax/Application/ListMessages.php
@@ -131,10 +131,13 @@ class IMP_Ajax_Application_ListMessages
 
         if ($is_search) {
             /* For search mailboxes, we need to invalidate all browser data
-             * and repopulate, since BUIDs may have changed. */
-             $args['cache'] = array();
-             $args['change'] = true;
-             $result->data_reset = $result->rowlist_reset = true;
+             * and repopulate on force update, since BUIDs may have
+             * changed (TODO: only do this if search mailbox has changed?). */
+            if (!empty($args['change'])) {
+                 $args['cache'] = array();
+                 $args['change'] = true;
+                 $result->data_reset = $result->rowlist_reset = true;
+            }
         } elseif (!$args['initial'] && $args['cacheid'] && $args['cache']) {
             /* Check for UIDVALIDITY expiration. If it has changed, we need to
              * purge the cached items on the browser. */
