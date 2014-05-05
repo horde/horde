722eaf2e54a9c06b0b5b5facdb8c7aedb71efdd8
Jan Schneider <jan@horde.org>
2014-05-05 22:50:59 +0200

    Development mode for imp-6.2.0alpha2

--- imp/lib/Application.php
+++ imp/lib/Application.php
@@ -66,7 +66,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (6.2.0alpha1)';
+    public $version = 'H5 (6.2.0-git)';
 
     /**
      * Server key used in logged out session.
