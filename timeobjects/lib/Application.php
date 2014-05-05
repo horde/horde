05b797878037c145c46edf25b924a31791c6df84
Jan Schneider <jan@horde.org>
2014-05-05 15:11:34 +0200

    Released timeobjects-2.1.0

--- timeobjects/lib/Application.php
+++ timeobjects/lib/Application.php
@@ -29,5 +29,5 @@ require_once HORDE_BASE . '/lib/core.php';
 
 class Timeobjects_Application extends Horde_Registry_Application
 {
-    public $version = '2.0.5-git';
+    public $version = '2.1.0';
 }
