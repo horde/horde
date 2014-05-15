95818eb82d7f491f85b892b5ed5dd099f4ab9522
Thomas Jarosch <thomas.jarosch@intra2net.com>
2014-05-15 13:37:37 +0200

    Prevent endless loop in writeLiteral()
    writeLiteral() was stuck in an endless loop
    logging this message until the HDD was full:
    
    PHP ERROR: feof() expects parameter 1 to be resource, integer given [pid 24223 on line 106 of "/usr/share/pear/Horde/Imap/Client/Socket/Connection/Socket.php"]
    PHP ERROR: fread() expects parameter 1 to be resource, integer given [pid 24223 on line 107 of "/usr/share/pear/Horde/Imap/Client/Socket/Connection/Socket.php"]
    
    Prevent it by checking the return value of fread().
    
    While we are at it, check the return value of rewind(), too.
    
    Signed-off-by: Michael M Slusarz <slusarz@horde.org>

--- framework/Imap_Client/lib/Horde/Imap/Client/Socket/Connection/Socket.php
+++ framework/Imap_Client/lib/Horde/Imap/Client/Socket/Connection/Socket.php
@@ -80,9 +80,16 @@ extends Horde_Imap_Client_Socket_Connection_Base
             $data = $data->stream;
         }
 
-        rewind($data);
+        if (!rewind($data)) {
+            throw new Horde_Imap_Client_Exception(
+                Horde_Imap_Client_Translation::r("Server write error (stream rewind failed)."),
+                Horde_Imap_Client_Exception::SERVER_WRITEERROR
+            );
+        }
+
         while (!feof($data)) {
-            if (fwrite($this->_stream, fread($data, 8192)) === false) {
+            $read_data = fread($data, 8192);
+            if ($read_data === false || fwrite($this->_stream, $read_data) === false) {
                 throw new Horde_Imap_Client_Exception(
                     Horde_Imap_Client_Translation::r("Server write error."),
                     Horde_Imap_Client_Exception::SERVER_WRITEERROR
