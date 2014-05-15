fa9f3da4c718beefe084fb4b2c2786c2950544f6
Michael J Rubinsky <mrubinsk@horde.org>
2014-05-15 16:39:02 -0400

    Fix tracking bad login count and honoring the lock setting when using Application auth backend.

--- framework/Core/lib/Horde/Core/Auth/Application.php
+++ framework/Core/lib/Horde/Core/Auth/Application.php
@@ -407,8 +407,11 @@ class Horde_Core_Auth_Application extends Horde_Auth_Base
         if ($this->_base) {
             return $this->_base->hasCapability($capability);
         }
-
-        if (!isset($this->_appCapabilities)) {
+        // The follow capabilities are not determined by the Application,
+        // but by 'Horde'.
+        if (in_array(strtolower($capability), array('badlogincount', 'lock'))) {
+            return parent::hasCapability($capability);
+        } elseif (!isset($this->_appCapabilities)) {
             $this->_appCapabilities = $GLOBALS['registry']->getApiInstance($this->_app, 'application')->auth;
         }
 
