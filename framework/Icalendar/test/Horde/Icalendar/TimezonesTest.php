dcf574627145dd2c43c5bec92b3ab7026976c941
Jan Schneider <jan@horde.org>
2014-06-02 11:35:14 +0200

    Use @dataProvider.

--- framework/Icalendar/test/Horde/Icalendar/TimezonesTest.php
+++ framework/Icalendar/test/Horde/Icalendar/TimezonesTest.php
@@ -17,29 +17,39 @@ class Horde_Icalendar_TimezonesTest extends Horde_Test_Case
         date_default_timezone_set('UTC');
     }
 
-    public function testFiles()
+    /**
+     * @dataProvider timezones
+     */
+    public function testFile($file)
     {
-        $test_files = glob(__DIR__ . '/fixtures/vTimezone/*.ics');
-        foreach ($test_files as $file) {
-            $result = '';
-            $ical = new Horde_Icalendar();
-            $ical->parsevCalendar(file_get_contents($file));
-            foreach ($ical->getComponents() as $component) {
-                if ($component->getType() != 'vEvent') {
-                    continue;
-                }
-                $date = $component->getAttribute('DTSTART');
-                if (is_array($date)) {
-                    continue;
-                }
-                $result .= str_replace("\r", '', $component->getAttribute('SUMMARY')) . "\n";
-                $d = new Horde_Date($date);
-                $result .= $d->format('H:i') . "\n";
+        $result = '';
+        $ical = new Horde_Icalendar();
+        $ical->parsevCalendar(file_get_contents($file));
+        foreach ($ical->getComponents() as $component) {
+            if ($component->getType() != 'vEvent') {
+                continue;
             }
-            $this->assertStringEqualsFile(
-                __DIR__ . '/fixtures/vTimezone/' . basename($file, 'ics') . 'txt',
-                $result,
-                'Failed parsing file ' . basename($file));
+            $date = $component->getAttribute('DTSTART');
+            if (is_array($date)) {
+                continue;
+            }
+            $result .= str_replace("\r", '', $component->getAttribute('SUMMARY')) . "\n";
+            $d = new Horde_Date($date);
+            $result .= $d->format('H:i') . "\n";
         }
+        $this->assertStringEqualsFile(
+            __DIR__ . '/fixtures/vTimezone/' . basename($file, 'ics') . 'txt',
+            $result,
+            'Failed parsing file ' . basename($file));
+    }
+
+    public function timezones()
+    {
+        return array_map(
+            function($a) {
+                return array($a);
+            },
+            glob(__DIR__ . '/fixtures/vTimezone/*.ics')
+        );
     }
 }
