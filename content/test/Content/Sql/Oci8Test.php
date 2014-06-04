4b152e583951f10ec420fdf91c9f56ae29245318
Jan Schneider <jan@horde.org>
2014-06-04 11:08:15 +0200

    [jan] Add unit tests for Oracle.

---
+++ content/test/Content/Sql/Oci8Test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Prepare the test setup.
+ */
+require_once __DIR__ . '/Base.php';
+
+/**
+ * Copyright 2010-2014 Horde LLC (http://www.horde.org/)
+ *
+ * @author     Jan Schneider <jan@horde.org>
+ * @category   Horde
+ * @package    Content
+ * @subpackage UnitTests
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class Content_Sql_Oci8Test extends Content_Test_Sql_Base
+{
+    public static function setUpBeforeClass()
+    {
+        if (!extension_loaded('oci8')) {
+            self::$reason = 'No oci8 extension';
+            return;
+        }
+        $config = self::getConfig('CONTENT_SQL_OCI8_TEST_CONFIG',
+                                  __DIR__ . '/..');
+        if ($config && !empty($config['content']['sql']['oci8'])) {
+            self::$db = new Horde_Db_Adapter_Oci8($config['content']['sql']['oci8']);
+            parent::setUpBeforeClass();
+        }
+    }
+}
