22fa2e2d00759976c3f8e12189398fca3dc4778d
Thomas Jarosch <thomas.jarosch@intra2net.com>
2014-06-19 16:12:29 +0200

    Fix up broken add/modify operation in Kolab driver
    Signed-off-by: Jan Schneider <jan@horde.org>
    Bug: 13269

--- mnemo/lib/Driver/Kolab.php
+++ mnemo/lib/Driver/Kolab.php
@@ -132,13 +132,15 @@ class Mnemo_Driver_Kolab extends Mnemo_Driver
      */
     protected function _add($noteId, $desc, $body, $tags)
     {
+        $object = $this->_buildObject($noteId, $desc, $body, $tags);
+
         try {
-            $this->_getData()
-                ->create($this->_buildObject($noteId, $desc, $body, $tags));
+            $this->_getData()->create($object);
         } catch (Horde_Kolab_Storage_Exception $e) {
             throw new Mnemo_Exception($e);
         }
-        return $uid;
+
+        return $object['uid'];
     }
 
     /**
@@ -154,13 +156,16 @@ class Mnemo_Driver_Kolab extends Mnemo_Driver
      */
     protected function _modify($noteId, $desc, $body, $tags)
     {
+
+        $object = $this->_buildObject($noteId, $desc, $body, $tags);
+
         try {
-            $this->_getData()
-                ->modify($this->_buildObject($noteId, $desc, $body, $tags));
+            $this->_getData()->modify($object);
         } catch (Horde_Kolab_Storage_Exception $e) {
             throw new Mnemo_Exception($e);
         }
-        return $uid;
+
+        return $object['uid'];
     }
 
     /**
@@ -171,7 +176,7 @@ class Mnemo_Driver_Kolab extends Mnemo_Driver
      * @param string $body    The whole note body.
      * @param string $tags    The tags of the note.
      *
-     * @return string  The Kolab hash.
+     * @return object  The Kolab hash.
      */
     protected function _buildObject($noteId, $desc, $body, $tags)
     {
@@ -185,6 +190,8 @@ class Mnemo_Driver_Kolab extends Mnemo_Driver
             ->getInstance('Mnemo_Tagger')
             ->split($tags);
         usort($object['categories'], 'strcoll');
+
+        return $object;
     }
 
     /**
