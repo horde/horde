057ccdcfb2e929b69b08e9adca84448e5cb92154
Michael M Slusarz <slusarz@horde.org>
2014-06-30 22:53:15 -0600

    Don't allow keynavlist selection of empty value (Bug #13209)

--- framework/Core/js/keynavlist.js
+++ framework/Core/js/keynavlist.js
@@ -103,6 +103,9 @@ var KeyNavList = Class.create({
                 v.l = v.l.escapeHTML().gsub('  ', ' &nbsp;');
             }
             li = new Element('LI').insert(v.l).store('v', v.v);
+            if (v.v.empty()) {
+                li.addClassName('noselect');
+            }
             if (v.s) {
                 this.markSelected(li);
             }
@@ -233,6 +236,10 @@ var KeyNavList = Class.create({
 
             if (elt &&
                 (elt == this.div || elt.descendantOf(this.div))) {
+                if (elt.retrieve('v').empty()) {
+                    return;
+                }
+
                 this.markSelected(elt);
                 this.opts.onChoose(this.getCurrentEntry());
                 e.stop();
