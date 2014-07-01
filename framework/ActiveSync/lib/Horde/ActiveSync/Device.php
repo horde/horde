23427dd1957a50e1274d6af60525871df3e68490
Michael J Rubinsky <mrubinsk@horde.org>
2014-07-01 12:44:27 -0400

    Typo

--- framework/ActiveSync/lib/Horde/ActiveSync/Device.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Device.php
@@ -537,7 +537,7 @@ class Horde_ActiveSync_Device
                 Horde_ActiveSync_Device::MULTIPLEX_CALENDAR |
                 Horde_ActiveSync_Device::MULTIPLEX_NOTES |
                 Horde_ActiveSync_Device::MULTIPLEX_TASKS;
-        } else if ($strpos($this->userAgent, 'Outlook/15.0') !== false) {
+        } else if (strpos($this->userAgent, 'Outlook/15.0') !== false) {
             // OL2013 Doesn't support multiple contact lists.
             $this->_properties['properties'][self::MULTIPLEX] = Horde_ActiveSync_Device::MULTIPLEX_CONTACTS;
         } else {
