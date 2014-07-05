af251eeb963dbd24d9e21d2cd4890cc7fa79fed4
Michael J Rubinsky <mrubinsk@horde.org>
2014-07-04 20:32:35 -0400

    Bug: 13321 Issue a ITIP_CANCEL iTIP when event status changes to cancelled.

--- kronolith/lib/Ajax/Application/Handler.php
+++ kronolith/lib/Ajax/Application/Handler.php
@@ -318,7 +318,8 @@ class Kronolith_Ajax_Application_Handler extends Horde_Core_Ajax_Application_Han
         }
 
         if (($result !== true) && $this->vars->sendupdates) {
-            Kronolith::sendITipNotifications($event, $GLOBALS['notification'], Kronolith::ITIP_REQUEST);
+            $type = $event->status == Kronolith::STATUS_CANCELLED ? Kronolith::ITIP_CANCEL : Kronolith::ITIP_REQUEST;
+            Kronolith::sendITipNotifications($event, $GLOBALS['notification'], $type);
         }
         Kronolith::notifyOfResourceRejection($event);
 
