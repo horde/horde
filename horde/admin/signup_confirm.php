c4432f57f75ac3fc872ec4bab13b2468a4587bd6
Michael M Slusarz <slusarz@horde.org>
2014-07-11 11:03:31 -0600

    [mms] Fix fatal error in signup confirmation script.

--- horde/admin/signup_confirm.php
+++ horde/admin/signup_confirm.php
@@ -11,7 +11,7 @@
  * @package  Horde
  */
 
-require_once __DIR__ . '/../lib/base.php';
+require_once __DIR__ . '/../lib/Application.php';
 Horde_Registry::appInit('horde', array('authentication' => 'none'));
 
 $vars = $injector->getInstance('Horde_Variables');
