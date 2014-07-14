f67374ec247caaab16baf59109ee86d33c40a66c
Michael M Slusarz <slusarz@horde.org>
2014-07-14 16:52:52 -0600

    Better updating of blocks
    Constantly setting setTimeout() is bad, since it may result in memory
    leaks due to scoping issues

--- framework/Core/js/hordeblocks.js
+++ framework/Core/js/hordeblocks.js
@@ -7,26 +7,17 @@ var HordeBlocks = {
 
     addUpdateableBlock: function(app, blockid, uid, refresh, opts)
     {
-        var block = {
-            'app': app,
-            'blockid': blockid,
-            'refresh': refresh,
-            'uid': uid,
-            'options': opts
-        };
-        setTimeout(this.update.curry(block).bind(this), block.refresh);
-    },
+        uid = $(uid);
 
-    update: function(block)
-    {
-        HordeCore.doAction('blockAutoUpdate',
-            { app: block.app, blockid: block.blockid, options: block.options },
-            {
-                callback: function(r) {
-                    $(block.uid).update(r);
-                    setTimeout(this.update.curry(block).bind(this), block.refresh);
-                }.bind(this)
-            }
-        );
+        new PeriodicalExecuter(function() {
+            HordeCore.doAction('blockAutoUpdate', {
+                app: app,
+                blockid: blockid,
+                options: opts
+            }, {
+                callback: uid.update.bind(uid)
+            });
+        }, refresh / 1000);
     }
+
 };
