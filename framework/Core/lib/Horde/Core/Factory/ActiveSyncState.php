fe17f30d5a1585cc46d97763acb32ff14c6d2497
Michael M Slusarz <slusarz@horde.org>
2014-07-17 02:26:20 -0600

    Need to check validity of 'storage' parameter also

--- framework/Core/lib/Horde/Core/Factory/ActiveSyncState.php
+++ framework/Core/lib/Horde/Core/Factory/ActiveSyncState.php
@@ -9,22 +9,24 @@ class Horde_Core_Factory_ActiveSyncState extends Horde_Core_Factory_Injector
     {
         global $conf;
 
-        if (empty($conf['activesync']['enabled'])) {
-            throw new Horde_Exception('ActiveSync is disabled.');
+        if (!empty($conf['activesync']['enabled'])) {
+            $driver = $conf['activesync']['storage'];
+
+            switch (Horde_String::lower($driver)) {
+            case 'nosql':
+                $nosql = $injector->getInstance('Horde_Core_Factory_Nosql')->create('horde', 'activesync');
+                return new Horde_ActiveSync_State_Mongo(array(
+                    'connection' => $nosql
+                ));
+
+            case 'sql':
+                return new Horde_ActiveSync_State_Sql(array(
+                    'db' => $injector->getInstance('Horde_Core_Factory_Db')->create('horde', 'activesync')
+                ));
+            }
         }
 
-        $driver = $conf['activesync']['storage'];
-        switch (Horde_String::lower($driver)) {
-        case 'nosql':
-            $nosql = $injector->getInstance('Horde_Core_Factory_Nosql')->create('horde', 'activesync');
-            return new Horde_ActiveSync_State_Mongo(array(
-                'connection' => $nosql
-            ));
-        case 'sql':
-            return new Horde_ActiveSync_State_Sql(array(
-                'db' => $injector->getInstance('Horde_Core_Factory_Db')->create('horde', 'activesync')
-            ));
-        }
+        throw new Horde_Exception('ActiveSync is disabled.');
     }
 
 }
