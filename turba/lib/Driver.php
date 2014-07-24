15abd132862e7d3df9351f682187409288e0a33c
Michael J Rubinsky <mrubinsk@horde.org>
2014-07-24 16:13:17 -0400

    Fix exporting tags via ActiveSync

--- turba/lib/Driver.php
+++ turba/lib/Driver.php
@@ -2629,10 +2629,6 @@ class Turba_Driver implements Countable
                 }
                 break;
 
-            case '__tags':
-                $message->categories = $value;
-                break;
-
             case 'children':
                 // Children FROM horde are a simple string value. Even though EAS
                 // uses an array stucture to pass them, we pass as a single
@@ -2687,6 +2683,9 @@ class Turba_Driver implements Countable
                 break;
             }
         }
+
+        /* Get tags. */
+        $message->categories = $object->getValue('__tags');
 
         if (empty($this->fileas)) {
             $message->fileas = Turba::formatName($object);
