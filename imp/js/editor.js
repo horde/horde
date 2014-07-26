4bfb61984b68fa7fa635e176f4f94093ffc84c03
Michael M Slusarz <slusarz@horde.org>
2014-07-26 00:09:22 -0600

    Better default height setting for ckeditor; minimizes resizing effect

--- imp/js/editor.js
+++ imp/js/editor.js
@@ -41,6 +41,7 @@ var IMP_Editor = Class.create({
     start: function()
     {
         if (!this.editor) {
+            this.config.height = $(this.id).getHeight() - 75;
             this.editor = CKEDITOR.replace(this.id, this.config);
         }
     },
