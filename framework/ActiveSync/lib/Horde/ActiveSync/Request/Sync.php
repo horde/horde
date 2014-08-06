1f629bdaa83f2c1e325b76f568578a581101b0fe
Michael J Rubinsky <mrubinsk@horde.org>
2014-08-05 23:57:47 -0400

    No harm in allowing this even for EAS <= 12.1
    Also, protects against broken clients that may send this before
    EAS 12.1 as well. Possibly related to Bug: 13405

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/Sync.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/Sync.php
@@ -1108,8 +1108,7 @@ class Horde_ActiveSync_Request_Sync extends Horde_ActiveSync_Request_SyncBase
             }
 
             // EAS > 12.1 the Collection Class can be part of OPTIONS.
-            if ($this->_device->version > Horde_ActiveSync::VERSION_TWELVEONE &&
-                $this->_decoder->getElementStartTag(Horde_ActiveSync::SYNC_FOLDERTYPE)) {
+            if ($this->_decoder->getElementStartTag(Horde_ActiveSync::SYNC_FOLDERTYPE)) {
                 $options['class'] = $this->_decoder->getElementContent();
                 if (!$this->_decoder->getElementEndTag()) {
                     $this->_statusCode = self::STATUS_PROTERROR;
