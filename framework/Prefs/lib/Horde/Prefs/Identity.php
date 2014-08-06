104e19a1964489c0daa41b0a7a9329cf662eb3b8
Michael M Slusarz <slusarz@horde.org>
2014-08-06 13:37:44 -0600

    phpdoc

--- framework/Prefs/lib/Horde/Prefs/Identity.php
+++ framework/Prefs/lib/Horde/Prefs/Identity.php
@@ -5,19 +5,20 @@
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
- * @author   Jan Schneider <jan@horde.org>
- * @category Horde
- * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
- * @package  Prefs
+ * @category  Horde
+ * @copyright 2001-2014 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Prefs
  */
 
 /**
  * This class provides an interface to all identities a user might have.
  *
- * @author   Jan Schneider <jan@horde.org>
- * @category Horde
- * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
- * @package  Prefs
+ * @author    Jan Schneider <jan@horde.org>
+ * @category  Horde
+ * @copyright 2001-2014 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Prefs
  */
 class Horde_Prefs_Identity
 {
