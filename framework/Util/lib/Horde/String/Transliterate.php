60c6c3857f2e2b1647c504d55e4e495967f76bc1
Michael M Slusarz <slusarz@horde.org>
2014-08-06 12:16:17 -0600

    [mms] Fix regression in Horde_String_Transliterate for PHP 5.3 (Bug #13419).
    Sigh. phpdoc doesn't say on the doc page that this isn't supported - it
    is only a single line in the changelog that reveals this.

--- framework/Util/lib/Horde/String/Transliterate.php
+++ framework/Util/lib/Horde/String/Transliterate.php
@@ -59,7 +59,7 @@ class Horde_String_Transliterate
         );
 
         foreach ($methods as $val) {
-            if (($out = self::{$val}($str)) !== false) {
+            if (($out = call_user_func(array(self, $val), $str)) !== false) {
                 return $out;
             }
         }
