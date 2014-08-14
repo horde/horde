02bb6e3a94d59c193f40e59731b8b6ecb7e91797
Michael M Slusarz <slusarz@horde.org>
2014-08-13 18:04:45 -0600

    [mms] Add Horde_Mail_Mbox_Parse.

---
+++ framework/Mail/lib/Horde/Mail/Translation.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright 2014 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @category  Horde
+ * @copyright 2014 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd New BSD License
+ * @package   Mail
+ */
+
+/**
+ * Translation wrapper for the Horde_Mail package.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2014 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd New BSD License
+ * @package   Mail
+ * @since     2.5.0
+ */
+class Horde_Mail_Translation
+extends Horde_Translation
+{
+    /**
+     */
+    static public function t($message)
+    {
+        self::$_domain = 'Horde_Mail';
+        self::$_directory = '@data_dir@' == '@'.'data_dir'.'@' ? __DIR__ . '/../../../locale' : '@data_dir@/Horde_Mail/locale';
+        return parent::t($message);
+    }
+
+    /**
+     */
+    static public function ngettext($singular, $plural, $number)
+    {
+        self::$_domain = 'Horde_Mail';
+        self::$_directory = '@data_dir@' == '@'.'data_dir'.'@' ? __DIR__ . '/../../../locale' : '@data_dir@/Horde_Mail/locale';
+        return parent::ngettext($singular, $plural, $number);
+    }
+
+}
