0ecde5c8cb795c6b1af982a021c31b31f5839cd2
Michael M Slusarz <slusarz@horde.org>
2014-08-23 00:09:04 -0600

    Cleaner handling of cache setting in DB adapter object

--- framework/Core/lib/Horde/Core/Factory/Db.php
+++ framework/Core/lib/Horde/Core/Factory/Db.php
@@ -99,14 +99,7 @@ class Horde_Core_Factory_Db extends Horde_Core_Factory_Base
             $this->_instances[$sig] = $this->create();
         } elseif (!isset($this->_instances[$sig])) {
             try {
-                $this->_instances[$sig] = $this->_createDb($config);
-                if (!isset($config['cache'])) {
-                    /* Need to add cache ob here, instead of createDb, or else
-                     * we enter infinite bootstrapping loop. Bug #13439 */
-                    $this->_instances[$sig]->setCache(
-                        $this->_injector->getInstance('Horde_Cache')
-                    );
-                }
+                $this->_createDb($config, $sig);
             } catch (Horde_Exception $e) {}
         }
 
@@ -132,63 +125,71 @@ class Horde_Core_Factory_Db extends Horde_Core_Factory_Base
      */
     public function createDb($config)
     {
-        $ob = $this->_createDb($config);
-        if (!isset($config['cache'])) {
-            $ob->setCache($this->_injector->getInstance('Horde_Cache'));
-        }
-        return $ob;
+        return $this->_createDb($config);
     }
 
     /**
+     * @param string $sig     Save instance under this signature key.
+     * @param boolean $cache  Add default cache to driver?
      */
-    protected function _createDb($config)
+    protected function _createDb($config, $sig = null, $cache = true)
     {
         // Split read?
         if (!empty($config['splitread'])) {
             $read_config = $config['read'];
             unset($config['read'], $config['splitread']);
-            return new Horde_Db_Adapter_SplitRead(
-                $this->_createDb(array_merge($config, $read_config)),
-                $this->_createDb($config)
+            $ob = new Horde_Db_Adapter_SplitRead(
+                $this->_createDb(array_merge($config, $read_config), null, false),
+                $this->_createDb($config, null, false)
             );
-        }
-
-        if (isset($config['adapter'])) {
-            $class = $this->_getDriverName($config['adapter'], 'Horde_Db_Adapter');
-            unset($config['adapter']);
-        } elseif (empty($config['phptype'])) {
-            throw new Horde_Exception('The database configuration is missing.');
         } else {
-            switch ($config['phptype']) {
-            case 'mysqli':
-                $class = 'Horde_Db_Adapter_Mysqli';
-                break;
+            if (isset($config['adapter'])) {
+                $class = $this->_getDriverName($config['adapter'], 'Horde_Db_Adapter');
+                unset($config['adapter']);
+            } elseif (empty($config['phptype'])) {
+                throw new Horde_Exception('The database configuration is missing.');
+            } else {
+                switch ($config['phptype']) {
+                case 'mysqli':
+                    $class = 'Horde_Db_Adapter_Mysqli';
+                    break;
 
-            case 'mysql':
-                $class = extension_loaded('pdo_mysql')
-                    ? 'Horde_Db_Adapter_Pdo_Mysql'
-                    : 'Horde_Db_Adapter_Mysql';
-                break;
+                case 'mysql':
+                    $class = extension_loaded('pdo_mysql')
+                        ? 'Horde_Db_Adapter_Pdo_Mysql'
+                        : 'Horde_Db_Adapter_Mysql';
+                    break;
 
-            case 'oci8':
-                $class = 'Horde_Db_Adapter_Oci8';
-                break;
+                case 'oci8':
+                    $class = 'Horde_Db_Adapter_Oci8';
+                    break;
 
-            default:
-                $class = 'Horde_Db_Adapter_Pdo_' . ucfirst($config['phptype']);
-                break;
+                default:
+                    $class = 'Horde_Db_Adapter_Pdo_' . ucfirst($config['phptype']);
+                    break;
+                }
+            }
+
+            if (!empty($config['hostspec'])) {
+                $config['host'] = $config['hostspec'];
+                unset($config['hostspec']);
+            }
+
+            $ob = new $class($config);
+
+            if (!isset($config['logger'])) {
+                $ob->setLogger($this->_injector->getInstance('Horde_Log_Logger'));
             }
         }
 
-        if (!empty($config['hostspec'])) {
-            $config['host'] = $config['hostspec'];
-            unset($config['hostspec']);
+        if ($sig) {
+            $this->_instances[$sig] = $ob;
         }
 
-        $ob = new $class($config);
-
-        if (!isset($config['logger'])) {
-            $ob->setLogger($this->_injector->getInstance('Horde_Log_Logger'));
+        if ($cache && !isset($config['cache'])) {
+            /* Need to add cache ob here, after it is stored as an instance,
+             * or else we enter infinite bootstrapping loop. Bug #13439 */
+            $ob->setCache($this->_injector->getInstance('Horde_Cache'));
         }
 
         return $ob;
