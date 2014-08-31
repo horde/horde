cae96c2eaa680c1ce670283d6ccb8e83816c6072
Michael J Rubinsky <mrubinsk@horde.org>
2014-08-31 12:25:47 -0400

    Refresh resources when deleting an event also.

--- kronolith/js/kronolith.js
+++ kronolith/js/kronolith.js
@@ -4612,6 +4612,9 @@ KronolithCore = {
                                 this.view == 'workweek' ||
                                 this.view == 'day') {
                                 days = this.findEventDays(cal, eventid);
+                                days.each(function(day) {
+                                    this.refreshResources(day, cal, eventid);
+                                }.bind(this));
                             }
                             this.removeEvent(cal, eventid);
                             if (r.uid) {
