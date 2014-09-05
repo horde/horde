4a5a6a3ef05c6247a1e52744d8c77309a7b92dc9
Jan Schneider <jan@horde.org>
2014-09-05 17:48:53 +0200

    Development mode for passwd-5.0.3

--- passwd/lib/Application.php
+++ passwd/lib/Application.php
@@ -47,5 +47,5 @@ class Passwd_Application extends Horde_Registry_Application
     /**
      * The version of passwd as shown in the admin view
      */
-    public $version = 'H5 (5.0.2)';
+    public $version = 'H5 (5.0.3-git)';
 }
