5ab0db0259c7528ba7e915885ada66e1f1128502
Michael J Rubinsky <mrubinsk@horde.org>
2014-09-17 13:09:12 -0400

    Use the same template to render the preference version of EAS info.
    Bug: 13578
    
    Conflicts:
    	horde/templates/prefs/activesync.html.php

--- horde/templates/prefs/activesync.html.php
+++ horde/templates/prefs/activesync.html.php
@@ -22,42 +22,9 @@
 <input type="hidden" id="removedevice" name="removedevice" />
 <input type="hidden" name="wipeid" id="wipeid" />
 <input type="hidden" name="cancelwipe" id="cancelwipe" />
-<table class="horde-table striped">
- <tr class="header">
-  <th></th>
-  <th class="smallheader"><?php echo _("Device") ?></th>
-  <th class="smallheader"><?php echo _("Last Sync Time") ?></th>
-  <th class="smallheader"><?php echo _("Status") ?></th>
-  <th class="smallheader"><?php echo _("Device Information") ?></th>
- </tr>
-<?php foreach ($this->devices as $d): ?>
- <tr>
-  <td>
-    <?php if ($d->policykey): ?>
-      <input class="horde-delete" type="button" value="<?php echo _("Wipe") ?>" id="wipe_<?php echo $d->id ?>" />
-    <?php endif; ?>
-    <?php if ($d->rwstatus == Horde_ActiveSync::RWSTATUS_PENDING): ?>
-      <?php $status = $this->contentTag('span', _("Wipe Pending"), array('class' => 'notice')) ?>
-      <input type="button" value="<?php echo _("Cancel Wipe") ?>" id="cancel_<?php echo $d->id ?>" />
-    <?php elseif ($d->rwstatus == Horde_ActiveSync::RWSTATUS_WIPED): ?>
-      <?php $status = $this->contentTag('span', _("Device is Wiped"), array('class' => 'notice')) ?>
-    <?php else: ?>
-      <?php $status = $d->policykey ? _("Provisioned") : _("Not Provisioned") ?>
-    <?php endif; ?>
-    <input class="horde-delete" type="button" value="<?php echo _("Remove") ?>" id="remove_<?php echo $d->id ?>" />
-  </td>
-  <td><?php echo $d->deviceType ?></td>
-  <td><?php echo $d->getLastSyncTimestamp() ? strftime($GLOBALS['prefs']->getValue('date_format') . ' %H:%M', $d->getLastSyncTimestamp()) : _("None") ?></td>
-  <td><?php echo $status ?></td>
-  <td>
-    <?php foreach ($d->getFormattedDeviceProperties() as $key => $value): ?>
-      <?php echo '<b>' . $key . '</b>: ' . $value . '<br />' ?>
-    <?php endforeach; ?>
-    <b><?php echo _("Cached Heartbeat (seconds)")?></b>: <?php echo $d->hbinterval ?><br />
-  </td>
- </tr>
-<?php endforeach; ?>
-</table>
+
+<?php echo $this->render('device_table');?>
+
 <?php else: ?>
 <p>
  <em><?php echo _("None") ?></em>
