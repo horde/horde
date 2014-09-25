aa10ecf64e303fa9d76d59f87218890ee97a6cb8
Michael M Slusarz <slusarz@horde.org>
2014-09-25 12:50:08 -0600

    phpdoc

--- framework/Core/lib/Horde/PageOutput.php
+++ framework/Core/lib/Horde/PageOutput.php
@@ -1,17 +1,25 @@
 <?php
 /**
- * This object consolidates the elements needed to output a page to the
- * browser.
- *
  * Copyright 2012-2014 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
- * @author   Michael Slusarz <slusarz@horde.org>
- * @category Horde
- * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
- * @package  Core
+ * @category  Horde
+ * @copyright 2012-2014 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Core
+ */
+
+/**
+ * This object consolidates the elements needed to output a page to the
+ * browser.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2012-2014 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Core
  */
 class Horde_PageOutput
 {
