035beb3c939686c7262878a768d4364fa184d846
Michael M Slusarz <slusarz@horde.org>
2014-09-29 16:30:15 -0600

    Improved unit test for MIME encoding/decoding

--- framework/Mime/test/Horde/Mime/MimeTest.php
+++ framework/Mime/test/Horde/Mime/MimeTest.php
@@ -20,25 +20,79 @@
  */
 class Horde_Mime_MimeTest extends PHPUnit_Framework_TestCase
 {
-    public function testDecode()
+    /**
+     * @dataProvider decodeProvider
+     */
+    public function testDecode($data, $expected)
     {
         $this->assertEquals(
-            ' François Xavier. XXXXXX  <foo@example.com>',
-            Horde_Mime::decode('=?utf-8?Q?_Fran=C3=A7ois_Xavier=2E_XXXXXX_?= <foo@example.com>')
-        );
-
-        /* Not MIME encoded. */
-        $this->assertEquals(
-            '=? required=?',
-            Horde_Mime::decode('=? required=?')
+            $expected,
+            Horde_Mime::decode($data)
         );
     }
 
-    public function testNullCharacterInEncodeOutput()
+    public function decodeProvider()
+    {
+        return array(
+            array(
+                '=?utf-8?Q?_Fran=C3=A7ois_Xavier=2E_XXXXXX_?= <foo@example.com>',
+                ' François Xavier. XXXXXX  <foo@example.com>'
+            ),
+
+            /* Adapted from Dovecot's
+             * src/lib-mail/test-message-header-decode.c. */
+            array(
+                " \t=?utf-8?q?=c3=a4?=  =?utf-8?q?=c3=a4?=  b  \t\r\n ",
+                " \tää  b  \t\r\n "
+            ),
+            array(
+                "a =?utf-8?q?=c3=a4?= b",
+                "a ä b"
+            ),
+            array(
+                "a =?utf-8?q?=c3=a4?=\t\t\r\n =?utf-8?q?=c3=a4?= b",
+                "a ää b"
+            ),
+            array(
+                "a =?utf-8?q?=c3=a4?=  x  =?utf-8?q?=c3=a4?= b",
+                "a ä  x  ä b"
+            ),
+            array(
+                "a =?utf-8?b?w6TDpCDDpA==?= b",
+                "a ää ä b"
+            ), array(
+                "=?utf-8?b?w6Qgw6Q=?=",
+                "ä ä"
+            ),
+
+            /* Not MIME encoded. */
+            array(
+                '=? required=?',
+                '=? required=?'
+            )
+        );
+    }
+
+    /**
+     * @dataProvider encodeProvider
+     */
+    public function testEncode($data, $charset, $expected)
     {
         $this->assertEquals(
-            '=?utf-16le?b?AAA=?=',
-            Horde_Mime::encode("\x00", 'UTF-16LE')
+            $expected,
+            Horde_Mime::encode($data, $charset)
+        );
+    }
+
+    public function encodeProvider()
+    {
+        return array(
+            /* Null character in encode output. */
+            array(
+                "\x00",
+                'UTF-16LE',
+                '=?utf-16le?b?AAA=?='
+            )
         );
     }
 
