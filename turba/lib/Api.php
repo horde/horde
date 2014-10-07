2dbbf32db9a924ddde8ca6927164d6b61f03e53b
Michael J Rubinsky <mrubinsk@horde.org>
2014-10-07 12:06:05 -0400

    Fix defaulting to default 'search' fields.
    Conflicts:
    	turba/lib/Api.php

--- turba/lib/Api.php
+++ turba/lib/Api.php
@@ -1160,6 +1160,9 @@ class Turba_Api extends Horde_Registry_Api
                 continue;
             }
 
+            if (empty($opts['fields'][$source])) {
+                $opts['fields'][$source] = $GLOBALS['cfgSources'][$source]['search'];
+            }
             $sdriver = $driver->create($source);
 
             foreach ($names as $name) {
@@ -1172,9 +1175,6 @@ class Turba_Api extends Horde_Registry_Api
                         foreach ($opts['fields'][$source] as $field) {
                             $criteria[$field] = $trimname;
                         }
-                    }
-                    if (!count($criteria)) {
-                        $criteria['name'] = $trimname;
                     }
                 }
 
