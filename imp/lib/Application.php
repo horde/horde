d62209b6d9076720e2ac34c927185f84a7d1c24b
Michael M Slusarz <slusarz@horde.org>
2014-10-17 01:09:01 -0600

    Git master has long since had broken basic view behavior, so 6.3.0 isn't happening
    Only way it could happen would be to go back and branch from a commit
    right after 6.2.0 was released.  But what's the point?

--- imp/lib/Application.php
+++ imp/lib/Application.php
@@ -64,7 +64,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (6.3.0-git)';
+    public $version = 'H6 (7.0.0-git)';
 
     /**
      * Server key used in logged out session.
