0dc68e385fb25631e4a4d3d5b9ed61ea4f023a5d
Jan Schneider <jan@horde.org>
2014-10-25 18:02:48 +0200

    [jan] Update to PHPUnit 4.3.4 and dependencies.

--- framework/Test/bundle/vendor/sebastian/diff/src/Parser.php
+++ framework/Test/bundle/vendor/sebastian/diff/src/Parser.php
@@ -2,7 +2,7 @@
 /**
  * Diff
  *
- * Copyright (c) 2001-2013, Sebastian Bergmann <sebastian@phpunit.de>.
+ * Copyright (c) 2001-2014, Sebastian Bergmann <sebastian@phpunit.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,7 +37,7 @@
  * @package    Diff
  * @author     Sebastian Bergmann <sebastian@phpunit.de>
  * @author     Kore Nordmann <mail@kore-nordmann.de>
- * @copyright  2001-2013 Sebastian Bergmann <sebastian@phpunit.de>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://www.github.com/sebastianbergmann/diff
  */
@@ -50,12 +50,16 @@ namespace SebastianBergmann\Diff;
  * @package    Diff
  * @author     Sebastian Bergmann <sebastian@phpunit.de>
  * @author     Kore Nordmann <mail@kore-nordmann.de>
- * @copyright  2001-2013 Sebastian Bergmann <sebastian@phpunit.de>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://www.github.com/sebastianbergmann/diff
  */
 class Parser
 {
+    /**
+     * @param  string $string
+     * @return Diff[]
+     */
     public function parse($string)
     {
         $lines     = preg_split('(\r\n|\r|\n)', $string);
@@ -88,6 +92,10 @@ class Parser
         return $diffs;
     }
 
+    /**
+     * @param Diff  $diff
+     * @param array $lines
+     */
     private function parseFileDiff(Diff $diff, array $lines)
     {
         $chunks = array();
@@ -110,14 +118,14 @@ class Parser
             $last      = null;
 
             while (count($lines) &&
-                  (preg_match('(^(?P<type>[+ -])(?P<line>.*))', $last = array_shift($lines), $match) ||
-                  (strpos($last, '\\ No newline at end of file' ) === 0))) {
+                  (preg_match('(^(?P<type>[+ -])?(?P<line>.*))', $last = array_shift($lines), $match) ||
+                  (strpos($last, '\\ No newline at end of file') === 0))) {
                 if (count($match)) {
                     $type = Line::UNCHANGED;
 
                     if ($match['type'] == '+') {
                         $type = Line::ADDED;
-                    } else if ($match['type'] == '-') {
+                    } elseif ($match['type'] == '-') {
                         $type = Line::REMOVED;
                     }
 
