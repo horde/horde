73285efad50c5237b4638cd6f4014c0902e7ccbd
Jan Schneider <jan@horde.org>
2014-10-29 15:32:31 +0100

    Development mode for whups-3.0.2

--- whups/lib/Application.php
+++ whups/lib/Application.php
@@ -36,7 +36,7 @@ class Whups_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (3.0.1)';
+    public $version = 'H5 (3.0.2-git)';
 
     /**
      * Global variables defined:
