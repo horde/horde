3c40a433d9c8930db969fe75d43e52a8932616b1
Jan Schneider <jan@horde.org>
2014-10-29 17:10:13 +0100

    Released wicked-2.0.2

--- wicked/lib/Application.php
+++ wicked/lib/Application.php
@@ -43,7 +43,7 @@ class Wicked_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (2.0.2-git)';
+    public $version = 'H5 (2.0.2)';
 
     protected function _bootstrap()
     {
