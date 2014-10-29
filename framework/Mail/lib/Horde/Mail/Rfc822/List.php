93cc4f1ff99cf0ec7201e1d37aca66c305b53e21
Michael M Slusarz <slusarz@horde.org>
2014-10-28 23:45:06 -0600

    [mms] Added Horde_Mail_Rfc822_List#first() function.

--- framework/Mail/lib/Horde/Mail/Rfc822/List.php
+++ framework/Mail/lib/Horde/Mail/Rfc822/List.php
@@ -31,7 +31,9 @@
  * @property-read array $raw_addresses  The list of all addresses (Address
  *                                      objects).
  */
-class Horde_Mail_Rfc822_List extends Horde_Mail_Rfc822_Object implements ArrayAccess, Countable, SeekableIterator, Serializable
+class Horde_Mail_Rfc822_List
+    extends Horde_Mail_Rfc822_Object
+    implements ArrayAccess, Countable, SeekableIterator, Serializable
 {
     /** Filter masks. */
     const HIDE_GROUPS = 1;
@@ -273,6 +275,21 @@ class Horde_Mail_Rfc822_List extends Horde_Mail_Rfc822_Object implements ArrayAc
         }
 
         return false;
+    }
+
+    /**
+     * Convenience method to return the first element in a list.
+     *
+     * Useful since it allows chaining; older PHP versions did not allow array
+     * access dereferencing from the results of a function call.
+     *
+     * @since 2.5.0
+     *
+     * @return Horde_Mail_Rfc822_Object  Rfc822 object, or null if no object.
+     */
+    public function first()
+    {
+        return $this[0];
     }
 
     /**
