aeb5281ffb4c2f41435024b0379604c30a5cdde7
Jan Schneider <jan@horde.org>
2014-10-29 17:22:29 +0100

    Released webmail-5.2.3

--- bundles/webmail/lib/Bundle.php
+++ bundles/webmail/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '5.2.3-git';
+    const VERSION = '5.2.3';
 
     /**
      * The bundle descriptive name.
