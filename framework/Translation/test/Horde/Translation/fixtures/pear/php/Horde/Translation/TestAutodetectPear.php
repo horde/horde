f72091383fa4cd1433738b5581da0da5331dabae
Jan Schneider <jan@horde.org>
2014-11-04 18:26:51 +0100

    Actually test the different autodetect methods.

---
+++ framework/Translation/test/Horde/Translation/fixtures/pear/php/Horde/Translation/TestAutodetectPear.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author     Jan Schneider <jan@horde.org>
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @category   Horde
+ * @package    Translation
+ * @subpackage UnitTests
+ */
+class Horde_Translation_TestAutodetectPear extends Horde_Translation_Autodetect
+{
+    protected static $_domain = 'Horde_Translation';
+    protected static $_pearDirectory = '../../../data';
+}
---
+++ framework/Translation/test/Horde/Translation/fixtures/source/lib/Horde/Translation/TestAutodetectSource.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author     Jan Schneider <jan@horde.org>
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @category   Horde
+ * @package    Translation
+ * @subpackage UnitTests
+ */
+class Horde_Translation_TestAutodetectSource extends Horde_Translation_Autodetect
+{
+    protected static $_domain = 'Horde_Translation';
+    protected static $_pearDirectory = '@data_dir@';
+}
