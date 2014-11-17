870c6e784876030d9ac654303cd594cf942dd2cb
Jan Schneider <jan@horde.org>
2014-11-17 22:37:54 +0100

    [jan] Fix migration with Oracle.

--- framework/History/migration/Horde/History/4_horde_history_add_compositeindex.php
+++ framework/History/migration/Horde/History/4_horde_history_add_compositeindex.php
@@ -10,10 +10,12 @@ class HordeHistoryAddCompositeIndex extends Horde_Db_Migration_Base
     {
         // Older installs may have indexes differently named.
         $indexes = $this->indexes('horde_histories');
+        $idx_name = $this->indexName('horde_histories', array('column' => array('history_modseq', 'object_uid')));
         $modseq_idx_name = $this->indexName('horde_histories', 'history_modseq');
         $object_idx_name = $this->indexName('horde_histories', 'object_uid');
         foreach ($indexes as $idx) {
             switch ($idx->name) {
+            case $idx_name:
             case $modseq_idx_name:
             case $object_idx_name:
                 $this->removeIndex('horde_histories', array('name' => $idx->name));
