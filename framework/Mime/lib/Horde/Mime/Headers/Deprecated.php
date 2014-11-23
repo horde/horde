5a03355ee53e5e432e97be071eb5091c35f5a781
Michael M Slusarz <slusarz@horde.org>
2014-11-23 11:33:37 -0700

    [mms] Fix Horde_Mime_Headers_Deprecated#addReceivedHeader().

--- framework/Mime/lib/Horde/Mime/Headers/Deprecated.php
+++ framework/Mime/lib/Horde/Mime/Headers/Deprecated.php
@@ -73,9 +73,76 @@ class Horde_Mime_Headers_Deprecated
      */
     public function addReceivedHeader(array $opts = array())
     {
-        $this->_headers->addHeaderOb(
-            Horde_Mime_Headers_Received::createHordeHop($opts)
-        );
+        $old_error = error_reporting(0);
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            /* This indicates the user is connecting through a proxy. */
+            $remote_path = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            $remote_addr = $remote_path[0];
+            if (!empty($opts['dns'])) {
+                $remote = $remote_addr;
+                try {
+                    if ($response = $opts['dns']->query($remote_addr, 'PTR')) {
+                        foreach ($response->answer as $val) {
+                            if (isset($val->ptrdname)) {
+                                $remote = $val->ptrdname;
+                                break;
+                            }
+                        }
+                    }
+                } catch (Net_DNS2_Exception $e) {}
+            } else {
+                $remote = gethostbyaddr($remote_addr);
+            }
+        } else {
+            $remote_addr = $_SERVER['REMOTE_ADDR'];
+            if (empty($_SERVER['REMOTE_HOST'])) {
+                if (!empty($opts['dns'])) {
+                    $remote = $remote_addr;
+                    try {
+                        if ($response = $opts['dns']->query($remote_addr, 'PTR')) {
+                            foreach ($response->answer as $val) {
+                                if (isset($val->ptrdname)) {
+                                    $remote = $val->ptrdname;
+                                    break;
+                                }
+                            }
+                        }
+                    } catch (Net_DNS2_Exception $e) {}
+                } else {
+                    $remote = gethostbyaddr($remote_addr);
+                }
+            } else {
+                $remote = $_SERVER['REMOTE_HOST'];
+            }
+        }
+        error_reporting($old_error);
+
+        if (!empty($_SERVER['REMOTE_IDENT'])) {
+            $remote_ident = $_SERVER['REMOTE_IDENT'] . '@' . $remote . ' ';
+        } elseif ($remote != $_SERVER['REMOTE_ADDR']) {
+            $remote_ident = $remote . ' ';
+        } else {
+            $remote_ident = '';
+        }
+
+        if (!empty($opts['server'])) {
+            $server_name = $opts['server'];
+        } elseif (!empty($_SERVER['SERVER_NAME'])) {
+            $server_name = $_SERVER['SERVER_NAME'];
+        } elseif (!empty($_SERVER['HTTP_HOST'])) {
+            $server_name = $_SERVER['HTTP_HOST'];
+        } else {
+            $server_name = 'unknown';
+        }
+
+
+        $this->_headers->addHeaderOb(new Horde_Mime_Headers_Element_Multiple(
+            'Received',
+            'from ' . $remote . ' (' . $remote_ident .
+            '[' . $remote_addr . ']) ' .
+            'by ' . $server_name . ' (Horde Framework) with HTTP; ' .
+            date('r')
+        ));
     }
 
     /**
