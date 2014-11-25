6330ea6792be670673be199a43c6d03e1b99e514
Jan Schneider <jan@horde.org>
2014-11-25 11:43:20 +0100

    [jan] Add ReST rendering rule for horizontal rulers.
    Required in FW_5_2 because the missing rules causes a fatal error.

---
+++ wicked/lib/Text_Wiki/Render/Rst/Horiz.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2011-2014 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPLv2). If
+ * you did not receive this file, see
+ * http://www.horde.org/licenses/gpl
+ *
+ * @category Horde
+ * @package  Wicked
+ * @author   Jan Schneider <jan@horde.org>
+ * @link     http://www.horde.org/apps/wicked
+ * @license  http://www.horde.org/licenses/gpl GNU General Public License, version 2
+ */
+
+/**
+ * Renders a horizontal line for a Wiki page.
+ *
+ * @category Horde
+ * @package  Wicked
+ * @author   Jan Schneider <jan@horde.org>
+ * @link     http://www.horde.org/apps/wicked
+ * @license  http://www.horde.org/licenses/gpl GNU General Public License, version 2
+ */
+class Text_Wiki_Render_Rst_Horiz extends Text_Wiki_Render
+{
+    /**
+     * Renders a token into text matching the requested format.
+     * 
+     * @param array $options The "options" portion of the token (second
+     *                       element).
+     * 
+     * @return string The text rendered from the token options.
+     */
+    public function token($options)
+    {
+        return "----\n\n";
+    }
+}
