aaedc1ddf7779ea895ea11f853fc7e8b1d3c556b
Jan Schneider <jan@horde.org>
2014-12-03 17:42:22 +0100

    Development mode for wicked-2.0.4

--- wicked/lib/Application.php
+++ wicked/lib/Application.php
@@ -43,7 +43,7 @@ class Wicked_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (2.0.3)';
+    public $version = 'H5 (2.0.4-git)';
 
     protected function _bootstrap()
     {
