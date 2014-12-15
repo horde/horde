3bf1837e2cf4baf7d79b9163b651e9c2b3929a2f
Pascal Rigaux <pascal-rigaux@univ-paris1.fr>
2014-12-15 09:49:31 +0100

    fix nextFreebusy/prevFreebusy
    Signed-off-by: Jan Schneider <jan@horde.org>
    Bug: 13757

--- kronolith/js/kronolith.js
+++ kronolith/js/kronolith.js
@@ -1295,10 +1295,10 @@ KronolithCore = {
         }
         switch (tab.identify()) {
         case 'kronolithEventTabAttendees':
-            this.attendeeStartDateHandler($('kronolithFBDate').innerHTML);
+            this.attendeeStartDateHandler(this.getFBDate());
             break;
         case 'kronolithEventTabResources':
-            this.resourceStartDateHandler($('kronolithFBDate').innerHTML);
+            this.resourceStartDateHandler(this.getFBDate());
             break;
         }
     },
@@ -6150,7 +6150,7 @@ KronolithCore = {
                     }
                     if (!Object.isUndefined(r.fb)) {
                         this.freeBusy.get(attendee.l)[1] = r.fb;
-                        this.insertFreeBusy(attendee.l, $('kronolithFBDate').innerHTML);
+                        this.insertFreeBusy(attendee.l, this.getFBDate());
                     }
                 }.bind(this)
             });
@@ -6304,6 +6304,17 @@ KronolithCore = {
         }
     },
 
+    getFBDate: function () {
+   var startDate = $('kronolithFBDate').innerHTML;
+        startDate = startDate.split(' ');
+        if (startDate.length > 1) {
+            startDate = startDate[1];
+        } else {
+            startDate = startDate[0];
+        }
+        return Date.parseExact(startDate, Kronolith.conf.date_format);
+    },
+
     /**
      * Updates rows with free/busy information in the attendees table.
      *
@@ -6312,7 +6323,7 @@ KronolithCore = {
      * @param date   start     An optinal start date for f/b info. If omitted,
      *                         $('kronolithEventStartDate') is used.
      */
-    insertFreeBusy: function(attendee, startDate)
+    insertFreeBusy: function(attendee, start)
     {
         if (!$('kronolithEventDialog').visible() ||
             !this.freeBusy.get(attendee)) {
@@ -6327,7 +6338,7 @@ KronolithCore = {
         }
 
         if (!td.getWidth()) {
-            this.insertFreeBusy.bind(this, attendee, startDate).defer();
+            this.insertFreeBusy.bind(this, attendee, start).defer();
             return;
         }
 
@@ -6335,15 +6346,7 @@ KronolithCore = {
             div.purge();
             div.remove();
         }
-        if (startDate) {
-            startDate = startDate.split(' ');
-            if (startDate.length > 1) {
-                startDate = startDate[1];
-            } else {
-                startDate = startDate[0];
-            }
-            start = Date.parseExact(startDate, Kronolith.conf.date_format);
-        } else {
+        if (!start) {
             start = Date.parseExact($F('kronolithEventStartDate'), Kronolith.conf.date_format);
         }
         var end = start.clone().add(1).days(),
@@ -6393,7 +6396,7 @@ KronolithCore = {
 
     fbStartDateHandler: function(start)
     {
-        this.updateFBDate(Date.parseExact(start, Kronolith.conf.date_format));
+        this.updateFBDate(start);
         this.resetFBRows();
         // Need to check visisbility - multiple changes will break the display
         // due to the use of .defer() in insertFreeBusy().
@@ -6421,14 +6424,14 @@ KronolithCore = {
 
     nextFreebusy: function()
     {
-        var fbdate = Date.parse($('kronolithFBDate').innerHTML);
-        this.fbStartDateHandler(fbdate.addDays(1).toString(Kronolith.conf.date_format));
+        var fbdate = this.getFBDate();
+        this.fbStartDateHandler(fbdate.addDays(1));
     },
 
     prevFreebusy: function()
     {
-        var fbdate = Date.parse($('kronolithFBDate').innerHTML);
-        this.fbStartDateHandler(fbdate.addDays(-1).toString(Kronolith.conf.date_format));
+        var fbdate = this.getFBDate();
+        this.fbStartDateHandler(fbdate.addDays(-1));
     },
 
     /**
