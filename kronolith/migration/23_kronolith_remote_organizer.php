de542089b3d4ae345e1d8f37189ca09ff0824bbc
Ralf Lang <lang@b1-systems.de>
2015-01-10 09:27:03 +0100

    Database migration to add a remote organizer field to kronolith_events

---
+++ kronolith/migration/23_kronolith_remote_organizer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2014-2015 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/gpl GPL
+ * @package  Kronolith
+ */
+
+/**
+ * Fixes the type of the parents column.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/gpl GPL
+ * @package  Kronolith
+ */
+class KronolithRemoteOrganizer extends Horde_Db_Migration_Base
+{
+    /**
+     * Upgrade.
+     */
+    public function up()
+    {
+        $this->addColumn('kronolith_events', 'event_organizer', 'string', array('limit' => 250));
+    }
+
+    /**
+     * Downgrade
+     */
+    public function down()
+    {
+        $this->changeColumn('kronolith_events', 'event_organizer', 'text');
+    }
+}
