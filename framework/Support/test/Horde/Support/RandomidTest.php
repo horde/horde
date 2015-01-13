c9b2e584a380a01f335f6a97c22eefd8e762d791
Michael M Slusarz <slusarz@horde.org>
2015-01-13 04:29:26 -0700

    Better way of checking for duplicates

--- framework/Support/test/Horde/Support/RandomidTest.php
+++ framework/Support/test/Horde/Support/RandomidTest.php
@@ -24,17 +24,11 @@ class Horde_Support_RandomidTest extends PHPUnit_Framework_TestCase
     public function testDuplicates()
     {
         $values = array();
-        $cnt = 0;
 
         for ($i = 0; $i < 10000; ++$i) {
             $id = strval(new Horde_Support_Randomid());
-            if (isset($values[$id])) {
-                $cnt++;
-            } else {
-                $values[$id] = 1;
-            }
+            $this->assertArrayNotHasKey($id, $values);
+            $values[$id] = 1;
         }
-
-        $this->assertEquals(0, $cnt);
     }
 }
