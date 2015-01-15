bca4afa0f250523f14e66cf14da16667801d8a2a
Michael M Slusarz <slusarz@horde.org>
2015-01-15 12:55:28 -0700

    Re-add script name, with hacky CSS

--- ingo/templates/basic/script/_script.html.php
+++ ingo/templates/basic/script/_script.html.php
@@ -1,3 +1,4 @@
+<h2 class="smallheader rowOdd"><?php echo _("Script name") ?>: <?php echo $this->h($script['name']) ?></h2>
 <pre>
 <?php foreach ($script['lines'] as $i => $line): ?>
 <?php printf('%' . $script['width'] . "d: %s\n", $i + 1, $this->h($line)) ?>
