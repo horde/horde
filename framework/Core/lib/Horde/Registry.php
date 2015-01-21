9484ff6697b8e9878de91eb880b015a56748ced5
Michael M Slusarz <slusarz@horde.org>
2015-01-21 13:22:58 -0700

    Ensure that all cached config files are cleared when changing languages
    When changing prefs, the pref descriptions would remain in the old
    language for the current pageload only. This fixes that.
    
    See Bug #13818, although I cannot reproduce the "no change without
    relogin" part.

--- framework/Core/lib/Horde/Registry.php
+++ framework/Core/lib/Horde/Registry.php
@@ -2889,6 +2889,8 @@ class Horde_Registry implements Horde_Shutdown_Task
         if ($changed) {
             $this->rebuild();
 
+            $this->_cache['cfile'] = array();
+
             foreach ($this->listApps() as $app) {
                 if ($this->isAuthenticated(array('app' => $app, 'notransparent' => true))) {
                     $this->callAppMethod($app, 'changeLanguage');
