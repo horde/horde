66901fefc8dfe31144d1e90a13f536a564e4df2d
Michael J Rubinsky <mrubinsk@horde.org>
2015-01-21 14:15:56 -0500

    Revert "Temporary var_dump output to find out why Travis doesn't like the date."
    This reverts commit 608b4f0899555aaf7252989ccec04f025ea5d7b3.

--- framework/ActiveSync/lib/Horde/ActiveSync/Message/MeetingRequest.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Message/MeetingRequest.php
@@ -157,7 +157,6 @@ class Horde_ActiveSync_Message_MeetingRequest extends Horde_ActiveSync_Message_B
             $this->timezone = $tz->getSyncTZFromOffsets(
                 $tz->getOffsetsFromDate(new Horde_Date($this->_vEvent->getAttribute('DTSTART')))
             );
-            var_dump(new Horde_Date($this->_vEvent->getAttribute('DTSTART')));
         } catch (Horde_Icalendar_Exception $e) {}
         $this->alldayevent = (int)$this->_isAllDay();
     }
