7a540791b42929f46d0fc5d148190d1fb91c77bf
Jan Schneider <jan@horde.org>
2015-01-26 14:46:39 +0100

    Update to \Horde\ManageSieve.

--- framework/ManageSieve/test/Horde/ManageSieve/SieveTest.php
+++ framework/ManageSieve/test/Horde/ManageSieve/SieveTest.php
@@ -1,75 +1,57 @@
 <?php
 /**
- * This file contains the PHPUnit test case for Net_Sieve.
+ * Copyright 2006 Anish Mistry
+ * Copyright 2009-2015 Horde LLC (http://www.horde.org/)
  *
- * PHP version 4
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
  *
- * +-----------------------------------------------------------------------+
- * | All rights reserved.                                                  |
- * |                                                                       |
- * | Redistribution and use in source and binary forms, with or without    |
- * | modification, are permitted provided that the following conditions    |
- * | are met:                                                              |
- * |                                                                       |
- * | o Redistributions of source code must retain the above copyright      |
- * |   notice, this list of conditions and the following disclaimer.       |
- * | o Redistributions in binary form must reproduce the above copyright   |
- * |   notice, this list of conditions and the following disclaimer in the |
- * |   documentation and/or other materials provided with the distribution.|
- * |                                                                       |
- * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
- * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
- * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
- * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
- * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
- * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
- * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
- * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
- * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
- * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
- * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
- * +-----------------------------------------------------------------------+
- *
- * @category  Networking
- * @package   Net_Sieve
+ * @package   ManageSieve
  * @author    Anish Mistry <amistry@am-productions.biz>
- * @copyright 2006 Anish Mistry
- * @license   http://www.opensource.org/licenses/bsd-license.php BSD
- * @version   SVN: $Id$
- * @link      http://pear.php.net/package/Net_Sieve
+ * @author    Jan Schneider <jan@horde.org>
+ * @license   http://www.horde.org/licenses/bsd BSD
  */
 
-require_once dirname(__FILE__) . '/../Sieve.php';
+use \Horde, \Horde\ManageSieve, \Horde\ManageSieve\Exception;
 
 /**
- * PHPUnit test case for Net_Sieve.
+ * PHPUnit test case for Horde\ManageSieve.
  *
- * @category  Networking
- * @package   Net_Sieve
+ * @package   ManageSieve
  * @author    Anish Mistry <amistry@am-productions.biz>
+ * @author    Jan Schneider <jan@horde.org>
  * @copyright 2006 Anish Mistry
- * @license   http://www.opensource.org/licenses/bsd-license.php BSD
- * @version   Release: @package_version@
- * @link      http://pear.php.net/package/Net_Sieve
+ * @copyright 2009-2015 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd BSD
  */
-class SieveTest extends PHPUnit_Framework_TestCase
+class SieveTest extends Horde_Test_Case
 {
-    // contains the object handle of the string class
+    /**
+     * The ManageSieve client.
+     *
+     * @var \Horde\ManageSieve
+     */
     protected $fixture;
+
+    /**
+     * The tested scripts.
+     *
+     * @var array
+     */
+    protected $scripts;
 
     protected function setUp()
     {
-        if (!file_exists(dirname(__FILE__) . '/config.php')) {
-            $this->markTestSkipped('Test configuration incomplete. Copy config.php.dist to config.php.');
+        if (!file_exists(dirname(__FILE__) . '/conf.php')) {
+            $this->markTestSkipped('Test configuration incomplete. Copy conf.php.dist to conf.php.');
         }
-        require_once dirname(__FILE__) . '/config.php';
+        require_once dirname(__FILE__) . '/conf.php';
 
-        // Create a new instance of Net_Sieve.
-        $this->_pear = new PEAR();
-        $this->fixture = new Net_Sieve();
+        // Create a new instance of Horde\ManageSieve.
+        $this->fixture = new ManageSieve();
         $this->scripts = array(
-            'test script1' => "require \"fileinto\";\n\rif header :contains \"From\" \"@cnba.uba.ar\" \n\r{fileinto \"INBOX.Test1\";}\r\nelse \r\n{fileinto \"INBOX\";}",
-            'test script2' => "require \"fileinto\";\n\rif header :contains \"From\" \"@cnba.uba.ar\" \n\r{fileinto \"INBOX.Test\";}\r\nelse \r\n{fileinto \"INBOX\";}",
+            'test script1' => "require \"fileinto\";\r\nif header :contains \"From\" \"@cnba.uba.ar\" \r\n{fileinto \"INBOX.Test1\";}\r\nelse \r\n{fileinto \"INBOX\";}",
+            'test script2' => "require \"fileinto\";\r\nif header :contains \"From\" \"@cnba.uba.ar\" \r\n{fileinto \"INBOX.Test\";}\r\nelse \r\n{fileinto \"INBOX\";}",
             'test"scriptäöü3' => "require \"vacation\";\nvacation\n:days 7\n:addresses [\"matthew@de-construct.com\"]\n:subject \"This is a test\"\n\"I'm on my holiday!\nsadfafs\";",
             'test script4' => file_get_contents(dirname(__FILE__) . '/largescript.siv'));
     }
@@ -82,16 +64,13 @@ class SieveTest extends PHPUnit_Framework_TestCase
     
     protected function login()
     {
-        $result = $this->fixture->connect(HOST, PORT);
-        $this->assertTrue($this->check($result), 'Can not connect');
-        $result = $this->fixture->login(USERNAME, PASSWORD, null, '', false);
-        $this->assertTrue($this->check($result), 'Can not login');
+        $this->fixture->connect(HOST, PORT);
+        $this->fixture->login(USERNAME, PASSWORD, null, '', false);
     }
 
     protected function logout()
     {
-        $result = $this->fixture->disconnect();
-        $this->assertFalse($this->_pear->isError($result), 'Error on disconnect');
+        $this->fixture->disconnect();
     }
 
     protected function clear()
@@ -103,49 +82,37 @@ class SieveTest extends PHPUnit_Framework_TestCase
             $this->fixture->setActive(null);
         }
         foreach (array_keys($this->scripts) as $script) {
-            $this->fixture->removeScript($script);
+            try {
+                $this->fixture->removeScript($script);
+            } catch (Exception $e) {
+            }
         }
         $this->logout();
     }
 
-    protected function check($result)
-    {
-        if ($this->_pear->isError($result)) {
-            throw new Exception($result->getMessage());
-        }
-        return $result;
-    }
-
     public function testConnect()
     {
-        $result = $this->fixture->connect(HOST, PORT);
-        $this->assertTrue($this->check($result), 'Cannot connect');
+        $this->fixture->connect(HOST, PORT);
     }
     
     public function testLogin()
     {
-        $result = $this->fixture->connect(HOST, PORT);
-        $this->assertTrue($this->check($result), 'Cannot connect');
-        $result = $this->fixture->login(USERNAME, PASSWORD, null, '', false);
-        $this->assertTrue($this->check($result), 'Cannot login');
+        $this->fixture->connect(HOST, PORT);
+        $this->fixture->login(USERNAME, PASSWORD, null, '', false);
     }
 
     public function testDisconnect()
     {
-        $result = $this->fixture->connect(HOST, PORT);
-        $this->assertFalse($this->_pear->isError($result), 'Cannot connect');
-        $result = $this->fixture->login(USERNAME, PASSWORD, null, '', false);
-        $this->assertFalse($this->_pear->isError($result), 'Cannot login');
-        $result = $this->fixture->disconnect();
-        $this->assertFalse($this->_pear->isError($result), 'Error on disconnect');
+        $this->fixture->connect(HOST, PORT);
+        $this->fixture->login(USERNAME, PASSWORD, null, '', false);
+        $this->fixture->disconnect();
     }
 
     public function testListScripts()
     {
         $this->login();
-        $scripts = $this->fixture->listScripts();
+        $this->fixture->listScripts();
         $this->logout();
-        $this->assertFalse($this->_pear->isError($scripts), 'Can not list scripts');
     }
 
     public function testInstallScript()
@@ -156,8 +123,7 @@ class SieveTest extends PHPUnit_Framework_TestCase
         // First script.
         $scriptname = 'test script1';
         $before_scripts = $this->fixture->listScripts();
-        $result = $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
-        $this->assertFalse($this->_pear->isError($result), 'Can not install script ' . $scriptname);
+        $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
         $after_scripts = $this->fixture->listScripts();
         $diff_scripts = array_values(array_diff($after_scripts, $before_scripts));
         $this->assertTrue(count($diff_scripts) > 0, 'Script not installed');
@@ -166,8 +132,7 @@ class SieveTest extends PHPUnit_Framework_TestCase
         // Second script (install and activate)
         $scriptname = 'test script2';
         $before_scripts = $this->fixture->listScripts();
-        $result = $this->fixture->installScript($scriptname, $this->scripts[$scriptname], true);
-        $this->assertFalse($this->_pear->isError($result), 'Can not install script ' . $scriptname);
+        $this->fixture->installScript($scriptname, $this->scripts[$scriptname], true);
         $after_scripts = $this->fixture->listScripts();
         $diff_scripts = array_values(array_diff($after_scripts, $before_scripts));
         $this->assertTrue(count($diff_scripts) > 0, 'Script not installed');
@@ -180,6 +145,8 @@ class SieveTest extends PHPUnit_Framework_TestCase
     /**
      * There is a good chance that this test will fail since most servers have
      * a 32KB limit on uploaded scripts.
+     *
+     * @expectedException \Horde\ManageSieve\Exception
      */
     public function testInstallScriptLarge()
     {
@@ -187,8 +154,7 @@ class SieveTest extends PHPUnit_Framework_TestCase
         $this->login();
         $scriptname = 'test script4';
         $before_scripts = $this->fixture->listScripts();
-        $result = $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
-        $this->assertFalse($this->_pear->isError($result), 'Unable to upload large script (expected behavior for most servers)');
+        $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
         $after_scripts = $this->fixture->listScripts();
         $diff_scripts = array_diff($after_scripts, $before_scripts);
         $this->assertEquals($scriptname, reset($diff_scripts), 'Added script has a different name');
@@ -205,8 +171,7 @@ class SieveTest extends PHPUnit_Framework_TestCase
 
         $scriptname = 'test"scriptäöü3';
         $before_scripts = $this->fixture->listScripts();
-        $result = $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
-        $this->assertFalse($this->_pear->isError($result), 'Can not install script ' . $scriptname);
+        $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
         $after_scripts = $this->fixture->listScripts();
         $diff_scripts = array_values(array_diff($after_scripts, $before_scripts));
         $this->assertTrue(count($diff_scripts) > 0, 'Script not installed');
@@ -221,8 +186,7 @@ class SieveTest extends PHPUnit_Framework_TestCase
         $this->login();
         $scriptname = 'test script1';
         $before_scripts = $this->fixture->listScripts();
-        $result = $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
-        $this->assertFalse($this->_pear->isError($result), 'Can not install script ' . $scriptname);
+        $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
         $after_scripts = $this->fixture->listScripts();
         $diff_scripts = array_values(array_diff($after_scripts, $before_scripts));
         $this->assertTrue(count($diff_scripts) > 0);
@@ -237,7 +201,6 @@ class SieveTest extends PHPUnit_Framework_TestCase
         $this->clear();
         $this->login();
         $active_script = $this->fixture->getActive();
-        $this->assertFalse($this->_pear->isError($active_script), 'Error getting the active script');
         $this->logout();
     }
 
@@ -247,14 +210,16 @@ class SieveTest extends PHPUnit_Framework_TestCase
         $scriptname = 'test script1';
         $this->login();
         $result = $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
-        $result = $this->fixture->setActive($scriptname);
-        $this->assertFalse($this->_pear->isError($result), 'Can not set active script');
+        $this->fixture->setActive($scriptname);
         $active_script = $this->fixture->getActive();
         $this->assertEquals($scriptname, $active_script, 'Active script does not match');
 
         // Test for non-existant script.
-        $result = $this->fixture->setActive('non existant script');
-        $this->assertTrue($this->_pear->isError($result));
+        try {
+            $this->fixture->setActive('non existant script');
+            $this->fail('Exception expected');
+        } catch (Exception $e) {
+        }
         $this->logout();
     }
 
@@ -263,9 +228,8 @@ class SieveTest extends PHPUnit_Framework_TestCase
         $this->clear();
         $scriptname = 'test script1';
         $this->login();
-        $result = $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
-        $result = $this->fixture->removeScript($scriptname);
-        $this->assertFalse($this->_pear->isError($result), 'Error removing active script');
+        $this->fixture->installScript($scriptname, $this->scripts[$scriptname]);
+        $this->fixture->removeScript($scriptname);
         $this->logout();
     }
 }
