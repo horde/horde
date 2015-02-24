e4b00f0c7b1ac1a266648dfe539c773a2e827811
Michael M Slusarz <slusarz@horde.org>
2015-02-24 00:12:03 -0700

    Bump version

--- framework/lz4/horde_lz4.h
+++ framework/lz4/horde_lz4.h
@@ -4,7 +4,7 @@
 extern zend_module_entry horde_lz4_module_entry;
 #define phpext_horde_lz4_ptr &horde_lz4_module_entry
 
-#define HORDE_LZ4_EXT_VERSION "1.0.7"
+#define HORDE_LZ4_EXT_VERSION "1.0.8"
 
 #ifdef PHP_WIN32
 #   define PHP_HORDE_LZ4_API __declspec(dllexport)
