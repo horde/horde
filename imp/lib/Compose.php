a701ad0e6dc8421b591ed4e0e441acb7818ed8ec
Michael M Slusarz <slusarz@horde.org>
2015-02-24 20:49:04 -0700

    [mms] Don't honor linked attachment limit configuration options if linked attachments are disabled (Bug #13665).

--- imp/lib/Compose.php
+++ imp/lib/Compose.php
@@ -2923,6 +2923,10 @@ class IMP_Compose implements ArrayAccess, Countable, IteratorAggregate
     {
         global $conf;
 
+        if (empty($conf['compose']['link_attachments'])) {
+            return;
+        }
+
         $link_all = false;
         $linked = array();
 
