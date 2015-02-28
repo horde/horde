b81723de78483cebbde79a60bc7cfb7d4f07a63b
Michael J Rubinsky <mrubinsk@horde.org>
2015-02-28 16:19:14 -0500

    Add missing files.

---
+++ nag/lib/Ajax/Imple/ContactAutoCompleter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Attach the contact autocompleter to a HTML element.
+ *
+ * Copyright 2005-2015 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsdl.php.
+ *
+ * @author   Michael J Rubinsky <mrubinsk@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsdl.php BSD
+ * @package  Nag
+ */
+class Nag_Ajax_Imple_ContactAutoCompleter extends Horde_Core_Ajax_Imple_ContactAutoCompleter
+{
+    /**
+     */
+    protected function _getAddressbookSearchParams()
+    {
+        $ob = new stdClass;
+        $ob->fields = array();
+        $ob->sources = array();
+
+        return $ob;
+    }
+
+}
---
+++ nag/lib/Form/Type/NagContact.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Horde_Form
+ */
+class Nag_Form_Type_NagContact extends Horde_Form_Type
+{
+    public function isValid(&$var, &$vars, $value, &$message)
+    {
+        $email = new Horde_Mail_Rfc822_Address($value);
+        return $email->valid;
+    }
+
+    public function getTypeName()
+    {
+        return 'NagContact';
+    }
+
+    function getInfo(&$vars, &$var, &$info)
+    {
+        $value = $vars->get($var->getVarName());
+        $info = str_replace(',', '', $value);
+    }
+}
