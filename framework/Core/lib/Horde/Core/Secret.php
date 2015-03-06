9f3973a3d2f342e0e37fae85f18b22ffc83de1f1
Michael M Slusarz <slusarz@horde.org>
2015-03-05 21:08:16 -0700

    phpdoc

--- framework/Core/lib/Horde/Core/Secret.php
+++ framework/Core/lib/Horde/Core/Secret.php
@@ -1,17 +1,30 @@
 <?php
 /**
- * Wrap the base class in order to use a single secret key when authenticated
- * to Horde, to reduce complexity and minimze cookie size.
- *
  * Copyright 2012-2015 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
  *
- * @author   Michael Slusarz <slusarz@horde.org>
- * @category Horde
- * @license  http://www.horde.org/licenses/lgpl21 LGPL
- * @package  Core
+ * @category  Horde
+ * @copyright 2012-2015 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL
+ * @package   Core
+ */
+
+/**
+ * Wrap the base class in order to use a single secret key when authenticated
+ * to Horde, to reduce complexity and minimze cookie size.
+ *
+ * Horde_Secret should only be used to encrypt data within the current
+ * session. To encrypt data generally, directly use an encryption library
+ * since how data is stored in a session may change without warning between
+ * versions.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2012-2015 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL
+ * @package   Core
  */
 class Horde_Core_Secret extends Horde_Secret
 {
