2302a85d54c59736ead6cff04871b883549b7151
Jan Schneider <jan@horde.org>
2015-03-12 20:29:51 +0100

    [jan] Add 'ratio' parameter to Horde_Image_Effect_Imagick_LiquidResize.

--- framework/Image/lib/Horde/Image/Effect/Imagick/LiquidResize.php
+++ framework/Image/lib/Horde/Image/Effect/Imagick/LiquidResize.php
@@ -26,6 +26,7 @@ class Horde_Image_Effect_Imagick_LiquidResize extends Horde_Image_Effect
      * Valid parameters:
      *   - width: (integer) The target width.
      *   - height: (integer) The target height.
+     *   - ratio: (boolean) Keep aspect ratio.
      *   - delta_x: (integer) How much the seam may move on x axis (A value of
      *              0 causes the seam to be straight).
      *   - rigidity: (integer) Introduces a bias for non-straight seams.
@@ -40,20 +41,27 @@ class Horde_Image_Effect_Imagick_LiquidResize extends Horde_Image_Effect
      */
     public function apply()
     {
+        // Only supported if ImageMagick is compiled against lqr library.
+        if (!method_exists($this->_image->imagick, 'liquidRescaleImage')) {
+            throw new Horde_Image_Exception(
+                'Missing support for lqr in ImageMagick.'
+            );
+
+}
         $this->_params = new Horde_Support_Array($this->_params);
+        if ($this->_params->get('ratio', true)) {
+            $dim = $this->_image->getDimensions();
+            $this->_params->width = round(
+                $this->_params->height * $dim['width'] / $dim['height']
+            );
+        }
         try {
-            // Only supported if ImageMagick is compiled against lqr library.
-            if (method_exists($this->_image->imagick, 'liquidRescaleImage')) {
-                $this->_image->imagick->liquidRescaleImage(
-                    $this->_params->width, $this->_params->height,
-                    $this->_params->delta_x,
-                    $this->_params->rigidity
-                );
-            } else {
-                throw new Horde_Image_Exception(
-                    'Missing support for lqr in ImageMagick.'
-                );
-            }
+            $this->_image->imagick->liquidRescaleImage(
+                $this->_params->width,
+                $this->_params->height,
+                $this->_params->delta_x,
+                $this->_params->rigidity
+            );
         } catch (ImagickException $e) {
             throw new Horde_Image_Exception($e);
         }
