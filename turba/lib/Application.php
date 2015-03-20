f283c1a7a0ae5d53f1b2b0a49c3b7bdbcec289d3
Jan Schneider <jan@horde.org>
2015-03-20 15:02:32 +0100

    [jan] Don't try to load Content application if tags are disabled.
    Conflicts:
    	turba/docs/CHANGES
    	turba/package.xml

--- turba/lib/Application.php
+++ turba/lib/Application.php
@@ -85,17 +85,19 @@ class Turba_Application extends Horde_Registry_Application
      */
     protected function _init()
     {
-        global $injector, $registry, $session;
+        global $conf, $injector, $registry, $session;
 
-        /* For now, autoloading the Content_* classes depend on there being a
-         * registry entry for the 'content' application that contains at least
-         * the fileroot entry. */
-        $injector->getInstance('Horde_Autoloader')
-            ->addClassPathMapper(
-                new Horde_Autoloader_ClassPathMapper_Prefix('/^Content_/', $registry->get('fileroot', 'content') . '/lib/'));
+        if ($conf['tags']['enabled']) {
+            /* For now, autoloading the Content_* classes depend on there being
+             * a registry entry for the 'content' application that contains at
+             * least the fileroot entry. */
+            $injector->getInstance('Horde_Autoloader')
+                ->addClassPathMapper(
+                    new Horde_Autoloader_ClassPathMapper_Prefix('/^Content_/', $registry->get('fileroot', 'content') . '/lib/'));
 
-        if (!class_exists('Content_Tagger')) {
-            throw new Horde_Exception(_("The Content_Tagger class could not be found. Make sure the Content application is installed."));
+            if (!class_exists('Content_Tagger')) {
+                throw new Horde_Exception(_("The Content_Tagger class could not be found. Make sure the Content application is installed."));
+            }
         }
 
         // Turba source and attribute configuration.
