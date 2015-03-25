c12b889ca90a48ba57da8d5938ad73ca711da644
Michael M Slusarz <slusarz@horde.org>
2015-03-25 00:32:06 -0600

    [mms] Added PGP backend support for the gnupg PECL extension (Request #8093).
    For now, only a few features are implemented. Still need to add the bulk
    of the functionality.

---
+++ framework/Crypt/lib/Horde/Crypt/Pgp/Backend/Pecl.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Copyright 2015 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category  Horde
+ * @copyright 2015 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Crypt
+ */
+
+/**
+ * PGP backend that uses the PECL gnupg extension.
+ *
+ * NOTE: This class is NOT intended to be accessed outside of this package.
+ * There is NO guarantees that the API of this class will not change across
+ * versions.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2015 Horde LLC
+ * @internal
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package   Crypt
+ */
+class Horde_Crypt_Pgp_Backend_Pecl
+extends Horde_Crypt_Pgp_Backend
+{
+    /**
+     */
+    static public function supported()
+    {
+        return Horde_Util::extensionExists('gnupg');
+    }
+
+    /**
+     */
+    public function getSignersKeyId($text)
+    {
+        $gpg = new gnupg();
+
+        if (($info = $gpg->verify($text, false)) === false) {
+            throw new RuntimeException();
+        }
+
+        if (count($info)) {
+            $data = reset($info);
+            if (isset($data['fingerprint'])) {
+                return substr($data['fingerprint'], -8);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the fingerprints from a key block.
+     *
+     * @param string $pgpdata  The PGP data block.
+     *
+     * @return array  The fingerprints in $pgpdata indexed by key id.
+     */
+    public function getFingerprintsFromKey($pgpdata)
+    {
+        $gpg = new gnupg();
+
+        if (($info = $gpg->import($pgpdata)) === false) {
+            throw new RuntimeException();
+        }
+
+        $out = array();
+
+        if (strlen($info['fingerprint'])) {
+            $out['0x' . substr($info['fingerprint'], -8)] = $info['fingerprint'];
+        }
+
+        return $out;
+    }
+
+}
