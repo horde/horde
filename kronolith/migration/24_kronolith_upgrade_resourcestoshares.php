f97677693baadaac47e0219fba28266faf82be02
Michael J Rubinsky <mrubinsk@horde.org>
2015-04-07 21:52:58 -0400

    Start process of migrating existing resources.
    Still need to work out ownership and perms.

--- kronolith/migration/24_kronolith_upgrade_resourcestoshares.php
+++ kronolith/migration/24_kronolith_upgrade_resourcestoshares.php
@@ -28,11 +28,39 @@ class KronolithUpgradeResourcesToShares extends Horde_Db_Migration_Base
      */
     public function up()
     {
+        // shares ng
         $this->addColumn('kronolith_sharesng', 'attribute_email','text');
         $this->addColumn('kronolith_sharesng', 'attribute_members','text');
         $this->addColumn('kronolith_sharesng', 'attribute_response_type','integer');
         $this->addColumn('kronolith_sharesng', 'attribute_type', 'integer');
         $this->addColumn('kronolith_sharesng', 'attribute_isgroup', 'boolean', array('default' => false));
+
+        // legacy shares
+        $this->addColumn('kronolith_shares', 'attribute_email','text');
+        $this->addColumn('kronolith_shares', 'attribute_members','text');
+        $this->addColumn('kronolith_shares', 'attribute_response_type','integer');
+        $this->addColumn('kronolith_shares', 'attribute_type', 'integer');
+        $this->addColumn('kronolith_shares', 'attribute_isgroup', 'boolean', array('default' => false));
+
+        /** Migrate existing resources to shares */
+        $rows = $this->_connection->selectAll('SELECT * FROM kronolith_resources');
+        $shares = $GLOBALS['injector']
+             ->getInstance('Horde_Core_Factory_Share')
+             ->create('kronolith');
+        foreach ($rows as $row) {
+            $share = $shares->newShare(
+                null,
+                strval(new Horde_Support_Randomid()),
+                $row['resource_name']
+            );
+            $share->set('desc', $row['resource_description']);
+            $share->set('email', $row['resource_email']);
+            $share->set('response_type', $row['resource_response_type']);
+            $share->set('type', Kronolith::SHARE_TYPE_RESOURCE);
+            $share->set('isgroup', $row['resource_type'] == Kronolith_Resource::TYPE_GROUP);
+            $share->set('members', $row['resource_members']);
+            $share->save();
+        }
     }
 
     /**
@@ -40,12 +68,30 @@ class KronolithUpgradeResourcesToShares extends Horde_Db_Migration_Base
      */
     public function down()
     {
+        $shares = $GLOBALS['injector']
+             ->getInstance('Horde_Core_Factory_Share')
+             ->create('kronolith');
+
+        $resources = $shares->listShares(
+            null,
+            array('attributes' => array('type' => Kronolith::SHARE_TYPE_RESOURCE))
+        );
+
+        foreach ($resources as $resource) {
+            $shares->removeShare($resource);
+        }
+
         $this->removeColumn('kronolith_sharesng', 'attribute_email');
         $this->removeColumn('kronolith_sharesng', 'attribute_members');
         $this->removeColumn('kronolith_sharesng', 'attribute_response_type');
         $this->removeColumn('kronolith_sharesng', 'attribute_type');
         $this->removeColumn('kronolith_sharesng', 'attribute_isgroup');
 
+        $this->removeColumn('kronolith_shares', 'attribute_email');
+        $this->removeColumn('kronolith_shares', 'attribute_members');
+        $this->removeColumn('kronolith_shares', 'attribute_response_type');
+        $this->removeColumn('kronolith_shares', 'attribute_type');
+        $this->removeColumn('kronolith_shares', 'attribute_isgroup');
     }
 
 }
