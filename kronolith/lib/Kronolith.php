497e4fbc26e9d77e59f77d6e0b98167207cc1737
Jan Schneider <jan@horde.org>
2015-04-22 17:44:18 +0200

    Revert "Don't include resources in listInternalCalendars() result."
    This reverts commit 0e5a86f3b29d791eb42ee78410bb71c75aec618c.
    
    I added this while trying to find out why resources showed up where they
    shouldn't. And it looked like a good idea anyway. But it wasn't necessary after
    all.

--- kronolith/lib/Kronolith.php
+++ kronolith/lib/Kronolith.php
@@ -1011,18 +1011,12 @@ class Kronolith
         $kronolith_shares = $GLOBALS['injector']->getInstance('Kronolith_Shares');
 
         if ($owneronly || empty($GLOBALS['conf']['share']['hidden'])) {
-            $attributes = array('type' => Kronolith::SHARE_TYPE_USER);
-            if ($owneronly) {
-                $attributes['owner'] = $user;
-            }
             try {
                 $calendars = $kronolith_shares->listShares(
                     $user,
                     array('perm' => $permission,
-                          'attributes' => $attributes,
-                          'sort_by' => 'name'
-                    )
-                );
+                          'attributes' => $owneronly ? $user : null,
+                          'sort_by' => 'name'));
             } catch (Horde_Share_Exception $e) {
                 Horde::log($e);
                 return array();
@@ -1032,10 +1026,7 @@ class Kronolith
                 $calendars = $kronolith_shares->listShares(
                     $GLOBALS['registry']->getAuth(),
                     array('perm' => $permission,
-                          'attributes' => array(
-                              'owner' => $user,
-                              'type' => Kronolith::SHARE_TYPE_USER
-                          ),
+                          'attributes' => $user,
                           'sort_by' => 'name'));
             } catch (Horde_Share_Exception $e) {
                 Horde::log($e);
