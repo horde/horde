d1c0530dafc8667f4e3cc257a7aae8502f732ffb
Jan Schneider <jan@horde.org>
2015-04-23 15:54:47 +0200

    [jan] Fix searching with non-ASCII strings in AND/OR-combined searches.

--- framework/Imap_Client/lib/Horde/Imap/Client/Search/Query.php
+++ framework/Imap_Client/lib/Horde/Imap/Client/Search/Query.php
@@ -83,6 +83,14 @@ class Horde_Imap_Client_Search_Query implements Serializable
         $oldcharset = $this->_charset;
         $this->_charset = Horde_String::upper($charset);
 
+        foreach (array('and', 'or') as $item) {
+            if (isset($this->_search[$item])) {
+                foreach ($this->_search[$item] as &$val) {
+                    $val->charset($charset, $convert);
+                }
+            }
+        }
+
         if (!$convert || ($oldcharset == $this->_charset)) {
             return;
         }
