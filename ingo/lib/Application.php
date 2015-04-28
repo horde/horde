c712270d4eef978c9388d640d8350bb9cd113c77
Jan Schneider <jan@horde.org>
2015-04-28 21:35:08 +0200

    Released ingo-3.2.5

--- ingo/lib/Application.php
+++ ingo/lib/Application.php
@@ -49,7 +49,7 @@ class Ingo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.2.5-git)';
+    public $version = 'H5 (3.2.5)';
 
     /**
      * Cached list of all rulesets.
