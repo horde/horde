ceb9cebd2d65e5940ef2b6914043fea2b375e128
Jan Schneider <jan@horde.org>
2015-04-28 22:07:26 +0200

    Development mode for mnemo-4.2.7

--- mnemo/lib/Application.php
+++ mnemo/lib/Application.php
@@ -43,7 +43,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.2.6)';
+    public $version = 'H5 (4.2.7-git)';
 
     /**
      * Global variables defined:
