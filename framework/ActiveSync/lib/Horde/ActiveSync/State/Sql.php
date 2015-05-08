86f879d8bf3d558812e41d24725d837b387c9cd8
Michael J Rubinsky <mrubinsk@horde.org>
2015-05-08 12:02:44 -0400

    Remove commented out code.

--- framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/State/Sql.php
@@ -514,7 +514,6 @@ class Horde_ActiveSync_State_Sql extends Horde_ActiveSync_State_Base
                         }
                     }
                     unset($this->_changes[$key]);
-                    //$this->_changes = array_values($this->_changes);
                     break;
                 }
             }
