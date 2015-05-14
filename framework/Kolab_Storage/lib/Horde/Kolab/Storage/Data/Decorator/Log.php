8741996681f380cfbd74c16e06869882bc0dde45
Michael J Rubinsky <mrubinsk@horde.org>
2015-05-14 10:22:06 -0400

    Pass parameters to the decorated object.

--- framework/Kolab_Storage/lib/Horde/Kolab/Storage/Data/Decorator/Log.php
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/Data/Decorator/Log.php
@@ -523,7 +523,7 @@ implements Horde_Kolab_Storage_Data, Horde_Kolab_Storage_Data_Query
      */
     public function synchronize($params = array())
     {
-        $this->_data->synchronize();
+        $this->_data->synchronize($params);
         $this->_logger->debug(
             sprintf(
                 'Synchronized data cache for %s.',
