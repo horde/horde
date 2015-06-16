d19798e5ad535e5ae85685d846ab0c0adf5017be
Michael J Rubinsky <mrubinsk@horde.org>
2015-06-15 20:43:30 -0400

    Set the cache object for the imap client.

--- framework/Core/lib/Horde/Core/Factory/KolabStorage.php
+++ framework/Core/lib/Horde/Core/Factory/KolabStorage.php
@@ -88,6 +88,11 @@ class Horde_Core_Factory_KolabStorage extends Horde_Core_Factory_Base
                 'port'     => $configuration['port'],
                 'secure'   => $configuration['secure'],
                 'debug'    => isset($configuration['debug']) ? $configuration['debug'] : null,
+                'cache' =>  array('backend' => new Horde_Imap_Client_Cache_Backend_Cache(array(
+                    'cacheob' => new Horde_Cache(new Horde_Cache_Storage_Mock(), array(
+                        'compress' => true
+                    ))
+                )))
             ),
             'queries' => array(
                 'list' => array(
