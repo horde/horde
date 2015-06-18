65fbd30ef9f7cb54e35adddca3c0a833f1ee0e85
Jan Schneider <jan@horde.org>
2015-06-18 20:46:51 +0200

    Development mode for groupware-5.2.8

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '5.2.7';
+    const VERSION = '5.2.8-git';
 
     /**
      * The bundle descriptive name.
