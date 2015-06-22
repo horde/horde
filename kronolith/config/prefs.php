194c74f2770bf6cafffd938dba221053c0c0c0d7
Jan Schneider <jan@horde.org>
2015-06-22 12:04:40 +0200

    Use correct criteria for detecting the default sync_calendars.
    For the enum list we already consider permissions and sync_no_multiplex, but not for the default value.

--- kronolith/config/prefs.php
+++ kronolith/config/prefs.php
@@ -275,7 +275,7 @@ $_prefs['sync_calendars'] = array(
         $enum = array();
         $sync = @unserialize($GLOBALS['prefs']->getValue('sync_calendars'));
         if (empty($sync)) {
-            $GLOBALS['prefs']->setValue('sync_calendars', serialize(array(Kronolith::getDefaultCalendar())));
+            $GLOBALS['prefs']->setValue('sync_calendars', serialize(array(Kronolith::getDefaultCalendar(Horde_Perms::EDIT, !$GLOBALS['prefs']->getValue('activesync_no_multiplex')))));
         }
         foreach (Kronolith::listInternalCalendars(!$GLOBALS['prefs']->getValue('activesync_no_multiplex'), Horde_Perms::EDIT) as $key => $cal) {
             if ($cal->getName() != Kronolith::getDefaultCalendar(Horde_Perms::EDIT)) {
