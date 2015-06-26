1ab359aaab9a608495e71741387ae905b92df90c
Ralf Becker <RalfBecker@outdoor-training.de>
2015-06-26 22:53:51 +0200

    fixing Horde_Date_Exception: DateTimeZone::__construct(): Unknown or bad timezone (Z)

--- framework/Icalendar/test/Horde/Icalendar/ExportTest.php
+++ framework/Icalendar/test/Horde/Icalendar/ExportTest.php
@@ -224,24 +224,24 @@ EOT;
 
     public function testDuration0()
     {
-		$ical = new Horde_Icalendar;
-		$vevent = Horde_Icalendar::newComponent('VEVENT', $ical);
-		$vevent->setAttribute('SUMMARY', 'Testevent');
-		$vevent->setAttribute('UID', 'XXX');
-		$vevent->setAttribute('DTSTART', new DateTime('20150701T120000Z'));
-		$vevent->setAttribute('DTSTAMP', new DateTime('20150701T120000Z'));
-		$vevent->setAttribute('DURATION', 0);
-		$ical->addComponent($vevent);
-		$valarm = Horde_Icalendar::newComponent('VALARM', $vevent);
-		$valarm->setAttribute('TRIGGER', 0, array(
-			'VALUE' => 'DURATION',
-			'RELATED' => 'START',
-		));
-		$valarm->setAttribute('DESCRIPTION', 'Alarm at event-start');
-		$vevent->addComponent($valarm);
-		$this->assertStringEqualsFile(
+        $ical = new Horde_Icalendar;
+        $vevent = Horde_Icalendar::newComponent('VEVENT', $ical);
+        $vevent->setAttribute('SUMMARY', 'Testevent');
+        $vevent->setAttribute('UID', 'XXX');
+        $vevent->setAttribute('DTSTART', array('year' => 2015, 'month' => 7, 'mday' => 1), array('VALUE' => 'DATE'));
+        $vevent->setAttribute('DTSTAMP', array('year' => 2015, 'month' => 7, 'mday' => 1), array('VALUE' => 'DATE'));
+        $vevent->setAttribute('DURATION', 0);
+        $ical->addComponent($vevent);
+        $valarm = Horde_Icalendar::newComponent('VALARM', $vevent);
+        $valarm->setAttribute('TRIGGER', 0, array(
+            'VALUE' => 'DURATION',
+            'RELATED' => 'START',
+        ));
+        $valarm->setAttribute('DESCRIPTION', 'Alarm at event-start');
+        $vevent->addComponent($valarm);
+        $this->assertStringEqualsFile(
             __DIR__ . '/fixtures/duration0.ics',
             $ical->exportVCalendar()
         );
-	}
+    }
 }
