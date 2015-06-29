4e4637a2240138d282b6028f8275aeaea09f3513
Michael J Rubinsky <mrubinsk@horde.org>
2015-06-29 18:23:01 -0400

    Avoid reusing the Horde_Url object.
    Fixes issue where previous url parameters were being used on
    the wrong object.

--- turba/lib/Application.php
+++ turba/lib/Application.php
@@ -222,7 +222,6 @@ class Turba_Application extends Horde_Registry_Application
         }
 
         $user = $GLOBALS['registry']->getAuth();
-        $url = Horde::url('');
         $edit = Horde::url('addressbooks/edit.php');
 
         $sidebar->containers['my'] = array(
@@ -244,6 +243,7 @@ class Turba_Application extends Horde_Registry_Application
         $shares = array();
         $shared = array();
         foreach (Turba::listShares(false, Horde_Perms::SHOW) as $id => $abook) {
+            $url = Horde::url('');
             $row = array(
                 'selected' => $id == Turba::$source,
                 'url' => $url->add('source', $id),
@@ -289,6 +289,7 @@ class Turba_Application extends Horde_Registry_Application
             ),
         );
         foreach (Turba::getAddressBooks(Horde_Perms::SHOW) as $id => $abook) {
+            $url = Horde::url('');
             if (isset($shares[$id])) {
                 continue;
             }
