7574ff751e5c8840cd056edbffa1b322c30db6c3
Michael J Rubinsky <mrubinsk@horde.org>
2015-07-27 13:25:35 -0400

    Bug: 14064 and Bug: 13643  Fix reordering of rules.
    Rules were properly reordered in the backend, and the dom id of
    the rules was updated, but the existing links that contained the
    rule number was not. This should fix any remaining ordering issues.
    Note that this does not need to be merged up to Git master since that
    code was already heavily refactored to use rule uids instead of sort
    position.

--- ingo/js/filters.js
+++ ingo/js/filters.js
@@ -41,9 +41,14 @@ var IngoFilters = {
                                     rows = $('filterslist').select('DIV.filtersRow');
                                 rows.invoke('writeAttribute', 'id', null);
                                 rows.each(function(r) {
+                                    var a = r.select('a');
                                     r.writeAttribute('id', 'filtersrow_' + (i++));
+                                    a.each(function(l) {
+                                        var href_new = IngoFilters.replaceQueryParam('rulenumber', i - 1, l.href);
+                                        href_new = IngoFilters.replaceQueryParam('edit', i - 1, href_new);
+                                        l.setAttribute('href', href_new);
+                                   });
                                 });
-
                                 $('filtersSave').fade({ duration: 0.2 });
                             }
                         }
@@ -52,6 +57,13 @@ var IngoFilters = {
                 tag: 'div'
             });
         }
+    },
+
+    // Adapted from http://stackoverflow.com/questions/1090948/change-url-parameters
+    replaceQueryParam: function(param, newval, search) {
+        var regex = new RegExp("([?;&])" + param + "[^&;]*[;&]?");
+        var query = search.replace(regex, "$1").replace(/&$/, '');
+        return (query.length > 2 ? query + "&" : "?") + (param + "=" + newval);
     }
 
 };
