9310611e4f68e1abe9fc39927d2a977c48113842
Michael J Rubinsky <mrubinsk@horde.org>
2015-07-27 23:09:29 -0400

    Bug #14063 Override default email form field validation.
    Allow any valid rfc822 email list.

--- whups/lib/Form/Type/whupsemail.php
+++ whups/lib/Form/Type/whupsemail.php
@@ -2,4 +2,24 @@
 /**
  * @package Horde_Form
  */
-class Whups_Form_Type_whupsemail extends Horde_Form_Type_email {}
+class Whups_Form_Type_whupsemail extends Horde_Form_Type_email
+{
+
+    /**
+     * @param string $email An individual email address to validate.
+     *
+     * @return boolean
+     */
+    function validateEmailAddress($email)
+    {
+        $rfc = new Horde_Mail_Rfc822();
+        try {
+            $rfc->parseAddressList($email, array('validate' => true));
+        } catch (Horde_Mail_Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
