cf7b145dfcc8d093cfeb467a361f109ab54dbdc8
Michael J Rubinsky <mrubinsk@horde.org>
2015-07-31 23:13:25 -0400

    Released trean-1.1.2

--- trean/lib/Application.php
+++ trean/lib/Application.php
@@ -33,7 +33,7 @@ class Trean_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (1.1.2-git)';
+    public $version = 'H5 (1.1.2)';
 
     /**
      * Global variables defined:
