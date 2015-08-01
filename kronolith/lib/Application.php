33437842f4c3d02f6612de864a7a7d4737752fcd
Michael J Rubinsky <mrubinsk@horde.org>
2015-07-31 22:57:53 -0400

    Development mode for kronolith-4.2.10

--- kronolith/lib/Application.php
+++ kronolith/lib/Application.php
@@ -47,7 +47,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.2.9)';
+    public $version = 'H5 (4.2.10-git)';
 
     /**
      * Global variables defined:
