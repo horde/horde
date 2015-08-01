06680b5d7fff005bf6d2a5cd2c60abdd966f7e0a
Michael J Rubinsky <mrubinsk@horde.org>
2015-07-31 23:24:11 -0400

    Development mode for wicked-2.0.5

--- wicked/lib/Application.php
+++ wicked/lib/Application.php
@@ -43,7 +43,7 @@ class Wicked_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (2.0.4)';
+    public $version = 'H5 (2.0.5-git)';
 
     protected function _bootstrap()
     {
