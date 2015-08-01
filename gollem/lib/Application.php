87a69ceb8b91bda2ad02328c244e62a43a10ce53
Michael J Rubinsky <mrubinsk@horde.org>
2015-07-31 21:46:31 -0400

    Development mode for gollem-3.0.5

--- gollem/lib/Application.php
+++ gollem/lib/Application.php
@@ -50,7 +50,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.4)';
+    public $version = 'H5 (3.0.5-git)';
 
     /**
      * Server key used in logged out session.
