889629faf0c802f660083cd70151553ec32a47a8
Michael J Rubinsky <mrubinsk@horde.org>
2015-08-05 22:01:20 -0400

    Released gollem-3.0.5

--- gollem/lib/Application.php
+++ gollem/lib/Application.php
@@ -50,7 +50,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.5-git)';
+    public $version = 'H5 (3.0.5)';
 
     /**
      * Server key used in logged out session.
