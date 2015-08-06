e6c15617f4585b36f5d2bca7900523050ee4df75
Michael J Rubinsky <mrubinsk@horde.org>
2015-08-05 22:06:16 -0400

    Development mode for groupware-5.2.11

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '5.2.10';
+    const VERSION = '5.2.11-git';
 
     /**
      * The bundle descriptive name.
