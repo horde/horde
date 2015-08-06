3ec88b3832ceb6d9fdc9ffb96b3d4179e5c4aedc
Michael J Rubinsky <mrubinsk@horde.org>
2015-08-05 22:06:10 -0400

    Released groupware-5.2.10

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '5.2.10-git';
+    const VERSION = '5.2.10';
 
     /**
      * The bundle descriptive name.
