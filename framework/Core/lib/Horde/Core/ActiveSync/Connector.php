99403aa4d3d27981005726e9f15c862527688288
Michael J Rubinsky <mrubinsk@horde.org>
2015-08-15 09:45:27 -0400

    Pass the folder_id to the backend.
    Some backends like e.g. Kolab, require this to be set.

--- framework/Core/lib/Horde/Core/ActiveSync/Connector.php
+++ framework/Core/lib/Horde/Core/ActiveSync/Connector.php
@@ -581,9 +581,9 @@ class Horde_Core_ActiveSync_Connector
      *
      * @return Horde_ActiveSync_Message_Task  The task message object
      */
-    public function tasks_export($uid, array $options = array())
+    public function tasks_export($uid, array $options = array(), $folder_id = null)
     {
-        return $this->_registry->tasks->export($uid, 'activesync', $options);
+        return $this->_registry->tasks->export($uid, 'activesync', $options, $folder_id);
     }
 
     /**
