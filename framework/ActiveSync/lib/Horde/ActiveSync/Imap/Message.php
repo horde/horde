e29204d16c7ef71faf045c6abc36a5950baf81e7
Michael J Rubinsky <mrubinsk@horde.org>
2015-08-28 20:39:35 -0400

    Simplify. This is what 'canonical' => true does.

--- framework/ActiveSync/lib/Horde/ActiveSync/Imap/Message.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Imap/Message.php
@@ -402,9 +402,7 @@ class Horde_ActiveSync_Imap_Message
                 // Work around PHP bug 65776
                 if ($mpart->getPrimaryType() == 'text') {
                     $mpart->setContents(
-                        $mpart->replaceEOL(
-                            $mpart->getContents(array('stream' => true)), Horde_Mime_Part::RFC_EOL, true
-                        ),
+                        $mpart->getContents(array('stream' => true, 'canonical' => true)),
                         array('usestream' => true)
                     );
                 }
