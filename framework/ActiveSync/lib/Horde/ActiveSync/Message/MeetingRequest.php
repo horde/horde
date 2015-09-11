1c4b9948926c85de890636e196bcbefeac01f8b0
Michael J Rubinsky <mrubinsk@horde.org>
2015-09-11 09:59:33 -0400

    DTEND isn't required.

--- framework/ActiveSync/lib/Horde/ActiveSync/Message/MeetingRequest.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Message/MeetingRequest.php
@@ -199,7 +199,6 @@ class Horde_ActiveSync_Message_MeetingRequest extends Horde_ActiveSync_Message_B
         try {
             $this->globalobjid = Horde_Mapi::createGoid($vevent->getAttribute('UID'));
             $this->starttime = new Horde_Date($vevent->getAttribute('DTSTART'));
-            $this->endtime = new Horde_Date($vevent->getAttribute('DTEND'));
         } catch (Horde_Exception $e) {
             throw new Horde_ActiveSync_Exception($e);
         }
@@ -209,6 +208,7 @@ class Horde_ActiveSync_Message_MeetingRequest extends Horde_ActiveSync_Message_B
         } catch (Horde_Exception $e) {}
 
         try {
+            $this->endtime = new Horde_Date($vevent->getAttribute('DTEND'));
             $this->location = Horde_String::truncate($vevent->getAttribute('LOCATION'), 255);
         } catch (Horde_Icalendar_Exception $e) {}
 
