773bf246508a46a4de5b0438095f9aad30e92f92
Chuck Hagenbuch <chuck@horde.org>
2015-09-14 23:31:39 -0400

    Need to pass the current gallery_id to this View.

--- ansel/lib/View/GalleryProperties.php
+++ ansel/lib/View/GalleryProperties.php
@@ -455,7 +455,7 @@ class Ansel_View_GalleryProperties
         $dir = ANSEL_BASE . '/lib/View/GalleryRenderer';
         $files = scandir($dir);
         $views = array();
-        $galleryView = new Ansel_View_Gallery();
+        $galleryView = new Ansel_View_Gallery(array('gallery_id' => $this->_params['gallery']));
         foreach ($files as $file) {
             if ($file != 'Base.php' && strpos($file, '.') !== 0) {
                 $class = 'Ansel_View_GalleryRenderer_' . substr($file, 0, -4);
