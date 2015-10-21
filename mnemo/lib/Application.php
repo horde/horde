b41b951ea5b20e5d58e1db13cb682d256805c194
Michael J Rubinsky <mrubinsk@horde.org>
2015-10-20 22:51:07 -0400

    Development mode for mnemo-4.2.9

--- mnemo/lib/Application.php
+++ mnemo/lib/Application.php
@@ -43,7 +43,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.2.8)';
+    public $version = 'H5 (4.2.9-git)';
 
     /**
      * Global variables defined:
