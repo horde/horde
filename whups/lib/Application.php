7fbafd23a808b31b185c34e3ac60b47fc333ec19
Michael J Rubinsky <mrubinsk@horde.org>
2015-10-20 23:13:15 -0400

    Development mode for whups-3.0.6

--- whups/lib/Application.php
+++ whups/lib/Application.php
@@ -36,7 +36,7 @@ class Whups_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (3.0.5)';
+    public $version = 'H5 (3.0.6-git)';
 
     /**
      * Global variables defined:
