0cc89f785c14870ff91cc8cf4f11a6b3f53c006f
Michael J Rubinsky <mrubinsk@horde.org>
2015-12-14 11:34:27 -0500

    Place holder for section that will hold "Other Galleries" data.
    Similar to the "Other Galleries" widget in static view that
    shows other galleries owned by the same user as the currently
    displayed gallery.

--- ansel/templates/dynamic/sidebar.html.php
+++ ansel/templates/dynamic/sidebar.html.php
@@ -36,5 +36,9 @@
         <?php endforeach ?>
       </div>
     </div>
-
+  <div class="horde-sidebar-split"></div>
+  <div id="anselMenuOtherGalleries" style="display:none;">
+  <h3>
+    <span class="horde-expand" title="<?php echo _("Collapse") ?>"><?php echo _("User Galleries") ?></span>
+  </h3>
 </div>
