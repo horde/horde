689aa40b5bb47bf02e2fe05ad5f2fdf105d2c599
Michael J Rubinsky <mrubinsk@horde.org>
2016-01-15 08:51:11 -0500

    Remove test output.

--- framework/Timezone/test/Horde/Timezone/IcalendarTest.php
+++ framework/Timezone/test/Horde/Timezone/IcalendarTest.php
@@ -38,10 +38,10 @@ class Horde_Timezone_IcalendarTest extends Horde_Test_Case
     public function testBug14221()
     {
         $tz = new Horde_Timezone_Mock('budapest');
-        file_put_contents('/tmp/budapest.ics', $tz->getZone('Europe/Budapest')->toVtimezone()->exportVcalendar());
         $this->assertStringEqualsFile(
             __DIR__ . '/fixtures/budapest.ics',
             $tz->getZone('Europe/Budapest')->toVtimezone()->exportVcalendar()
         );
     }
+
 }
