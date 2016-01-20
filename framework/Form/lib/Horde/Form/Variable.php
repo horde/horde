2e1c8a63059b97410100b75820e694f4928521e9
Jan Schneider <jan@horde.org>
2016-01-20 12:53:54 +0100

    Don't assign by reference.

--- framework/Form/lib/Horde/Form/Variable.php
+++ framework/Form/lib/Horde/Form/Variable.php
@@ -130,7 +130,7 @@ class Horde_Form_Variable {
     {
         $this->humanName   = $humanName;
         $this->varName     = $varName;
-        $this->type        = &$type;
+        $this->type        = $type;
         $this->required    = $required;
         $this->readonly    = $readonly;
         $this->description = $description;
