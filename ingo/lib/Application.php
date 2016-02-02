0968494a193b0b9706d1e9e0790e74bf8cdd6e1b
Jan Schneider <jan@horde.org>
2016-02-02 12:59:22 +0100

    Development mode for ingo-3.2.9

--- ingo/lib/Application.php
+++ ingo/lib/Application.php
@@ -49,7 +49,7 @@ class Ingo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.2.8)';
+    public $version = 'H5 (3.2.9-git)';
 
     /**
      * Cached list of all rulesets.
