d363a867e9f5074c30eb6357df22e0260d78d584
Michael J Rubinsky <mrubinsk@horde.org>
2016-02-10 22:37:30 -0500

    Released kronolith-4.2.14

--- kronolith/lib/Application.php
+++ kronolith/lib/Application.php
@@ -47,7 +47,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.2.14-git)';
+    public $version = 'H5 (4.2.14)';
 
     /**
      * Global variables defined:
