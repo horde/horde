084c2b0f464fd23682cc80ed981509c1eb938715
Michael J Rubinsky <mrubinsk@horde.org>
2016-02-18 17:31:19 -0500

    The sql and sqlng drivers require a share_name.
    In fact, this is enforced when creating the share, so this should
    never happen.

--- framework/Share/lib/Horde/Share/Sql.php
+++ framework/Share/lib/Horde/Share/Sql.php
@@ -323,11 +323,7 @@ class Horde_Share_Sql extends Horde_Share_Base
         $sharelist = array();
         foreach ($shares as $data) {
             $this->_getSharePerms($data);
-            if (!empty($data['share_name'])) {
-                $sharelist[$data['share_name']] = $this->_createObject($data);
-            } else {
-                $sharelist[] = $this->_createObject($data);
-            }
+            $sharelist[$data['share_name']] = $this->_createObject($data);
         }
 
         return $sharelist;
