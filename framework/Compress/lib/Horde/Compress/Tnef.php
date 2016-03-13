f3ba59628fcc9d48596920457122f4605f9e22f1
Michael J Rubinsky <mrubinsk@horde.org>
2016-03-13 01:15:01 -0500

    Don't throw exception if the date cannot be parsed.
    Just ignore this value, and don't set it. This can happen if, e.g.,
    this is running on a 32bit machine with a timestamp that overflows
    Horde_Date (which, in turn attempts to create a PHP native DateTime
    object).

--- framework/Compress/lib/Horde/Compress/Tnef.php
+++ framework/Compress/lib/Horde/Compress/Tnef.php
@@ -664,7 +664,7 @@ class Horde_Compress_Tnef extends Horde_Compress_Base
             } catch (Horde_Mapi_Exception $e) {
                 throw new Horde_Compress_Exception($e);
             } catch (Horde_Date_Exception $e) {
-                throw new Horde_Compress_Exception($e);
+                $this->_logger->err(sprintf('TNEF: Unable to parse date string - %s', $e->getMessage()));
             }
             break;
         case self::AFROM:
