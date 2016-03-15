b34460ce5ab6a7a3afe8153be1f4455642ae8647
Jan Schneider <jan@horde.org>
2016-03-15 18:05:25 +0100

    Display ticket listeners to authenticated users.

--- whups/ticket/watch.php
+++ whups/ticket/watch.php
@@ -24,58 +24,81 @@ Whups::addTopbarSearch();
 $addform = new Whups_Form_AddListener($vars, _("Add Watcher"));
 $delform = new Whups_Form_DeleteListener($vars, _("Remove Watcher"));
 
-if ($vars->get('formname') == 'whups_form_addlistener') {
-    if ($addform->validate($vars)) {
-        $addform->getInfo($vars, $info);
-
-        try {
-            $whups_driver->addListener($id, '**' . $info['add_listener']);
-            $ticket->notify(
-                $info['add_listener'], false, array('**' . $info['add_listener'] => 'listener'));
-            $notification->push(
-                sprintf(_("%s will be notified when this ticket is updated."), $info['add_listener']),
-                'horde.success');
-            $ticket->show();
-        } catch (Whups_Exception $e) {
-            $notification->push($e, 'horde.error');
-        }
+if ($vars->get('formname') == 'whups_form_addlistener' &&
+    $addform->validate($vars)) {
+    $addform->getInfo($vars, $info);
+    try {
+        $whups_driver->addListener($id, '**' . $info['add_listener']);
+        $ticket->notify(
+            $info['add_listener'],
+            false,
+            array('**' . $info['add_listener'] => 'listener')
+        );
+        $notification->push(
+            sprintf(
+                _("%s will be notified when this ticket is updated."),
+                $info['add_listener']
+            ),
+            'horde.success');
+        $ticket->show();
+    } catch (Whups_Exception $e) {
+        $notification->push($e, 'horde.error');
     }
-} elseif ($vars->get('formname') == 'whups_form_deletelistener') {
-    if ($delform->validate($vars)) {
-        $delform->getInfo($vars, $info);
-        try {
-            $whups_driver->deleteListener($id, '**' . $info['del_listener']);
-            $notification->push(
-                sprintf(_("%s will no longer receive updates for this ticket."), $info['del_listener']),
-                'horde.success');
-            $ticket->show();
-        } catch (Whups_Exception $e) {
-            $notification->push($e, 'horde.error');
-        }
+} elseif ($listener = $vars->get('del_listener')) {
+    try {
+        $whups_driver->deleteListener($id, '**' . $listener);
+        $notification->push(
+            sprintf(
+                _("%s will no longer receive updates for this ticket."),
+                $listener
+            ),
+            'horde.success');
+        $ticket->show();
+    } catch (Whups_Exception $e) {
+        $notification->push($e, 'horde.error');
     }
 }
 
-$page_output->header(array(
-    'title' => sprintf(_("Watchers for %s"), '[#' . $id . '] ' . $ticket->get('summary'))
-));
-$notification->notify(array('listeners' => 'status'));
-require WHUPS_TEMPLATES . '/prevnext.inc';
+$form = new Whups_Form_TicketDetails(
+    $vars, $ticket, '[#' . $id . '] ' . $ticket->get('summary')
+);
+$ticket->setDetails($vars);
 
-$tabs = Whups::getTicketTabs($vars, $id);
-echo $tabs->render('watch');
-
-require WHUPS_TEMPLATES . '/ticket/watchers.inc';
+$listeners = array_keys($whups_driver->getListeners($id, false, false, false));
+array_walk(
+    $listeners,
+    function(&$listener)
+    {
+        $listener = preg_replace('/^\*\*/', '', $listener);
+    }
+);
+$owners = $whups_driver->getOwners($id);
+if ($owners) {
+    $owners = reset($owners);
+} else {
+    $owners = array();
+}
+$delurl = Horde::url('ticket/watch.php')->add('id', $id);
+$delimg = Horde::img('delete.png');
 
 $r = new Horde_Form_Renderer();
 
+// Output content.
+$page_output->header(array(
+    'title' => sprintf(
+        _("Watchers for %s"),
+        '[#' . $id . '] ' . $ticket->get('summary')
+    )
+));
+$notification->notify(array('listeners' => 'status'));
+require WHUPS_TEMPLATES . '/prevnext.inc';
+Whups::getTicketTabs($vars, $id)->render('watch');
+require WHUPS_TEMPLATES . '/ticket/watchers.inc';
 $addform->renderActive($r, $vars, Horde::url('ticket/watch.php'), 'post');
 echo '<br class="spacer" />';
-
-$delform->renderActive($r, $vars, Horde::url('ticket/watch.php'), 'post');
-echo '<br class="spacer" />';
-
-$form = new Whups_Form_TicketDetails($vars, $ticket, '[#' . $id . '] ' . $ticket->get('summary'));
-$ticket->setDetails($vars);
+if (!$registry->getAuth()) {
+    $delform->renderActive($r, $vars, Horde::url('ticket/watch.php'), 'post');
+    echo '<br class="spacer" />';
+}
 $form->renderInactive($form->getRenderer(), $vars);
-
 $page_output->footer();
