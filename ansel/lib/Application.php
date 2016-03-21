cb3b4f48012954769047243c74cfb8abc6f888c8
Michael J Rubinsky <mrubinsk@horde.org>
2016-03-21 12:21:16 -0400

    Released ansel-3.0.4

--- ansel/lib/Application.php
+++ ansel/lib/Application.php
@@ -39,7 +39,7 @@ class Ansel_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.4-git)';
+    public $version = 'H5 (3.0.4)';
 
     /**
      * Global variables defined:
