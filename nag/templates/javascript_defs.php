3f314cdd8888280ddb20c4bf8ca1d9fc54794baf
Jan Schneider <jan@horde.org>
2016-03-31 12:17:13 +0200

    Use Horde_Core_Script_Package_Datejs::translateFormat() to translate between JS and PHP date formats (Bug #14303).

--- nag/templates/javascript_defs.php
+++ nag/templates/javascript_defs.php
@@ -3,9 +3,9 @@
 /* Variables used in core javascript files. */
 $code['conf'] = array(
     'URI_AJAX' => (string)$GLOBALS['registry']->getServiceLink('ajax', 'nag'),
-    'date_format' => str_replace(array('%e', '%d', '%a', '%A', '%m', '%h', '%b', '%B', '%y', '%Y'),
-                                 array('d', 'dd', 'ddd', 'dddd', 'MM', 'MMM', 'MMM', 'MMMM', 'yy', 'yyyy'),
-                                 $GLOBALS['prefs']->getValue('date_format_mini')),
+    'date_format' => Horde_Core_Script_Package_Datejs::translateFormat(
+        $GLOBALS['prefs']->getValue('date_format_mini')
+    ),
     'tasklist_info_url' => (string)Horde::url('tasklists/info.php'),
     'time_format' => $GLOBALS['prefs']->getValue('twentyFour') ? 'HH:mm' : 'hh:mm tt',
 );
