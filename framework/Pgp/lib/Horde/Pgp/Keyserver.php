f35aa67498b6ce09c86cfc0277274c90a5ec8a3c
Jan Schneider <jan@horde.org>
2016-04-05 11:43:43 +0200

    I forgot to port the charset stripping from Horde_Crypt.

--- framework/Pgp/lib/Horde/Pgp/Keyserver.php
+++ framework/Pgp/lib/Horde/Pgp/Keyserver.php
@@ -164,7 +164,7 @@ class Horde_Pgp_Keyserver
             try {
                 $response = $this->_http->get($url);
                 // Some keyservers return HTML, try again.
-                if ($response->getHeader('Content-Type') != 'text/plain') {
+                if (strpos($response->getHeader('Content-Type'), 'text/plain') !== 0) {
                     continue;
                 }
                 $output = ltrim($response->getBody());
@@ -175,7 +175,7 @@ class Horde_Pgp_Keyserver
 
         if (!$output) {
             throw new Horde_Pgp_Exception(
-                Horde_Pgp_Translation::t("Could not obtain public key from the keyserver.") . ' Response: ' . var_export($response, true)
+                Horde_Pgp_Translation::t("Could not obtain public key from the keyserver.")
             );
         }
 
