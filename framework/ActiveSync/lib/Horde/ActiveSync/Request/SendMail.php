45151c3c975b46067bf4907f2e57aa132ff729cb
Michael J Rubinsky <mrubinsk@horde.org>
2016-05-16 14:35:07 -0400

    Wrapping.

--- framework/ActiveSync/lib/Horde/ActiveSync/Request/SendMail.php
+++ framework/ActiveSync/lib/Horde/ActiveSync/Request/SendMail.php
@@ -107,7 +107,9 @@ class Horde_ActiveSync_Request_SendMail extends Horde_ActiveSync_Request_Base
         $mail = Horde_ActiveSync::messageFactory('SendMail');
         $mail->decodeStream($this->_decoder);
         if ($smartreply || $smartforward) {
-            $mail->source->folderid = $this->_activeSync->getCollectionsObject()->getBackendIdForFolderUid($mail->source->folderid);
+            $mail->source->folderid =
+                $this->_activeSync->getCollectionsObject()
+                    ->getBackendIdForFolderUid($mail->source->folderid);
         }
 
         try {
