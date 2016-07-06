d15eaf86fdd6cb71ac192941c7e20b92fd4435bc
Michael J Rubinsky <mrubinsk@horde.org>
2016-07-06 15:29:11 -0400

    Bug: 14412 Don't include results outside of the query range.

--- kronolith/lib/Kronolith.php
+++ kronolith/lib/Kronolith.php
@@ -649,6 +649,11 @@ class Kronolith
                 }
             }
         } else {
+            // Don't include any results that are outside the query range.
+            if ((!empty($query->end) && $event->start->after($query->end)) ||
+                (!empty($query->start) && $event->start->before($query->start))) {
+                return;
+            }
             $eventStart = $event->start;
             $eventEnd = $event->end;
         }
