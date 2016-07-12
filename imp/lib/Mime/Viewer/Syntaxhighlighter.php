8ab04d3a387a1e77ef96caff67a1c2d3df67aed9
Jan Schneider <jan@horde.org>
2016-07-12 20:30:18 +0200

    phpdoc

--- imp/lib/Mime/Viewer/Syntaxhighlighter.php
+++ imp/lib/Mime/Viewer/Syntaxhighlighter.php
@@ -1,30 +1,24 @@
 <?php
 /**
- * Copyright 2003-2016 Horde LLC (http://www.horde.org/)
+ * Copyright 2016 Horde LLC (http://www.horde.org/)
  *
- * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
  *
  * @author   Michael J Rubinsky <mrubinsk@horde.org>
- * @author   Chuck Hagenbuch <chuck@horde.org>
  * @category Horde
- * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @license  http://www.horde.org/licenses/gpl GPL
  * @package  IMP
  */
+
 /**
- * The IMP_Mime_Viewer_Syntaxhighlighter class extends the
- * Horde_Core_Mime_Viewer_Syntaxhightler in order to catch when the
- * preview pane is updated.
- *
- * Copyright 2003-2016 Horde LLC (http://www.horde.org/)
- *
- * See the enclosed file COPYING for license information (LGPL). If you
- * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ * The IMP_Mime_Viewer_Syntaxhighlighter class extends
+ * Horde_Core_Mime_Viewer_Syntaxhighlighter in order to catch when the preview
+ * pane is updated.
  *
  * @author   Michael J Rubinsky <mrubinsk@horde.org>
- * @author   Chuck Hagenbuch <chuck@horde.org>
  * @category Horde
- * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @license  http://www.horde.org/licenses/gpl GPL
  * @package  IMP
  */
 class IMP_Mime_Viewer_Syntaxhighlighter extends Horde_Core_Mime_Viewer_Syntaxhighlighter
