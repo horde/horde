6fa76d745b4535c332276d21907ec48b835a1d8f
Jan Schneider <jan@horde.org>
2016-07-13 21:01:20 +0200

    phpdoc

--- framework/Core/lib/Horde/Core/Ajax/Imple/AutoCompleter.php
+++ framework/Core/lib/Horde/Core/Ajax/Imple/AutoCompleter.php
@@ -1,11 +1,18 @@
 <?php
 /**
- * Attach an auto completer to a HTML element.
- *
  * Copyright 2009-2016 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Michael Slusarz <slusarz@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Core
+ */
+
+/**
+ * Attach an auto completer to a HTML element.
  *
  * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
--- framework/Core/lib/Horde/Core/Ajax/Imple/ContactAutoCompleter.php
+++ framework/Core/lib/Horde/Core/Ajax/Imple/ContactAutoCompleter.php
@@ -1,11 +1,18 @@
 <?php
 /**
- * Javascript autocompleter for contacts.
- *
  * Copyright 2012-2016 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Michael Slusarz <slusarz@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Core
+ */
+
+/**
+ * JavaScript autocompleter for contacts.
  *
  * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
