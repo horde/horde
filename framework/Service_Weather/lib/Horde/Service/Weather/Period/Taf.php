e254170e8f3e30aa03051fa6ec3717c53727800d
Michael J Rubinsky <mrubinsk@horde.org>
2016-08-08 16:42:42 -0400

    Mapping for Taf periods.
    Note that it doesn't really make much sense to use this
    data source in place of other weather providers since
    the data is not structured the same (nor can it be).

--- framework/Service_Weather/lib/Horde/Service/Weather/Period/Taf.php
+++ framework/Service_Weather/lib/Horde/Service/Weather/Period/Taf.php
@@ -25,7 +25,14 @@ class Horde_Service_Weather_Period_Taf extends Horde_Service_Weather_Period_Base
      *
      * @var array
      */
-     protected $_map = array();
+     protected $_map = array(
+        'wind_speed' => 'wind',
+        'wind_direction' => 'windDirection',
+        'wind_degrees' => 'windDegrees',
+        'wind_gust' => 'windGust',
+        'high' => 'temperatureHigh',
+        'low' => 'temperatureLow'
+    );
 
     /**
      * Accessor so we can lazy-parse the results.
@@ -38,46 +45,32 @@ class Horde_Service_Weather_Period_Taf extends Horde_Service_Weather_Period_Base
     public function __get($property)
     {
         switch ($property) {
+        case 'is_pm':
+        case 'hour':
         case 'humidity':
         case 'precipitation_percent':
         case 'wind_gust':
         case 'snow_total':
         case 'rain_total':
+        case 'icon_url':
+        case 'icon':
             return false;
 
         case 'conditions':
-            return Horde_Service_Weather_Translation::t($this->_properties->weather[0]->main);
-
-        case 'is_pm':
-            return false;
-
-        case 'hour':
-            return false;
+            return 'foo ';//Horde_Service_Weather_Translation::t($this->_properties->weather[0]->main);
 
         case 'date':
-            return new Horde_Date($this->_properties->date);
-
-        case 'high':
-            return round($this->_properties->temp->day);
-
-        case 'low':
-            return round($this->_properties->temp->night);
-
-        case 'icon':
-            return $this->_forecast->weather->iconMap[
-                str_replace('.png', '', $this->_properties->weather[0]->icon)
-            ];
-
-        case 'wind_direction':
-            return Horde_Service_Weather::degToDirection($this->_properties->deg);
-
-        case 'wind_degrees':
-            return $this->_properties->deg;
-
-        case 'wind_speed':
-           return $this->_properties->speed;
+            return new Horde_Date($this->_forecast->validFrom);
 
         default:
+            if (!empty($this->_properties[$property])) {
+                return $this->_properties[$property];
+            }
+            if (!empty($this->_map[$property])) {
+                return !empty($this->_properties[$this->_map[$property]])
+                    ? $this->_properties[$this->_map[$property]]
+                    : false;
+            }
             throw new Horde_Service_Weather_Exception_InvalidProperty('This provider does not support the "' . $property . '" property');
         }
     }
