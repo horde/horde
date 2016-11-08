69adcfacdd93a086261c8de9b96d3f0a52896837
Michael J Rubinsky <mrubinsk@horde.org>
2016-11-08 17:37:19 -0500

    Development mode for nag-4.2.13

--- nag/lib/Application.php
+++ nag/lib/Application.php
@@ -43,7 +43,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.2.12)';
+    public $version = 'H5 (4.2.13-git)';
 
     /**
      * Global variables defined:
