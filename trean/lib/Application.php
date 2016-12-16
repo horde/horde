127dcbf1048e5c8235ea85470b112783d11b5176
Michael J Rubinsky <mrubinsk@horde.org>
2016-12-16 14:42:12 -0500

    Development mode for trean-1.1.8

--- trean/lib/Application.php
+++ trean/lib/Application.php
@@ -33,7 +33,7 @@ class Trean_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (1.1.7)';
+    public $version = 'H5 (1.1.8-git)';
 
     /**
      * Global variables defined:
