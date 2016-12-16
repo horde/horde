eb82f8b524cf683402c5644eb3735ef65393ab50
Michael J Rubinsky <mrubinsk@horde.org>
2016-12-16 14:56:36 -0500

    Released whups-3.0.9

--- whups/lib/Application.php
+++ whups/lib/Application.php
@@ -36,7 +36,7 @@ class Whups_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (3.0.9-git)';
+    public $version = 'H5 (3.0.9)';
 
     /**
      * Global variables defined:
