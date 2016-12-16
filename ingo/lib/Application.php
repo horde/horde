f672b2f638eb78f1e7b858d9dfeebbcd071e9e4d
Michael J Rubinsky <mrubinsk@horde.org>
2016-12-16 13:53:29 -0500

    Released ingo-3.2.13

--- ingo/lib/Application.php
+++ ingo/lib/Application.php
@@ -49,7 +49,7 @@ class Ingo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.2.13-git)';
+    public $version = 'H5 (3.2.13)';
 
     /**
      * Cached list of all rulesets.
