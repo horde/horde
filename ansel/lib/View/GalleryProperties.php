ddf62a0edca77c30f2b482ff1eb98512694d7087
Michael J Rubinsky <mrubinsk@horde.org>
2017-01-20 17:13:06 -0500

    Don't create an empty Ansel_View, we can pass null.

--- ansel/lib/View/GalleryProperties.php
+++ ansel/lib/View/GalleryProperties.php
@@ -457,11 +457,10 @@ class Ansel_View_GalleryProperties
         $dir = ANSEL_BASE . '/lib/View/GalleryRenderer';
         $files = scandir($dir);
         $views = array();
-        $galleryView = new Ansel_View_Gallery();
         foreach ($files as $file) {
             if ($file != 'Base.php' && strpos($file, '.') !== 0) {
                 $class = 'Ansel_View_GalleryRenderer_' . substr($file, 0, -4);
-                $view = new $class($galleryView);
+                $view = new $class(null);
                 $views[substr($file, 0, -4)] = $view->title;
             }
         }
