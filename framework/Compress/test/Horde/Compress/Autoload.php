f9229b154d0cba2f117169188d896535fcee2d66
Jan Schneider <jan@horde.org>
2017-02-13 15:00:52 +0100

    Autoload Math_BigInteger.

---
+++ framework/Compress/test/Horde/Compress/Autoload.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Setup autoloading for the tests.
+ *
+ * @category   Horde
+ * @package    Compress
+ * @subpackage UnitTests
+ * @author     Jan Schneider <jan@horde.org>
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+
+require_once 'Math/BigInteger.php';
