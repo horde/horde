a715e00adbc22b39d2a4c3f363afe7b3fbd89a07
Michael J Rubinsky <mrubinsk@horde.org>
2017-03-01 12:26:18 -0500

    Not sure why these were converted to autoincrementKeys
    these have always been string values and are manually inserted.

--- framework/ActiveSync/migration/Horde/ActiveSync/1_horde_activesync_base_tables.php
+++ framework/ActiveSync/migration/Horde/ActiveSync/1_horde_activesync_base_tables.php
@@ -4,13 +4,14 @@ class HordeActiveSyncBaseTables extends Horde_Db_Migration_Base
     public function up()
     {
         if (!in_array('horde_activesync_state', $this->tables())) {
-            $t = $this->createTable('horde_activesync_state', array('autoincrementKey' => array('sync_key')));
+            $t = $this->createTable('horde_activesync_state', array('autoincrementKey' => false));
             $t->column('sync_time', 'integer');
             $t->column('sync_key', 'string', array('limit' => 255, 'null' => false));
             $t->column('sync_data', 'text');
             $t->column('sync_devid', 'string', array('limit' => 255));
             $t->column('sync_folderid', 'string', array('limit' => 255));
             $t->column('sync_user', 'string', array('limit' => 255));
+            $t->primaryKey(array('sync_key'));
             $t->end();
 
             $this->addIndex('horde_activesync_state', array('sync_folderid'));
@@ -31,13 +32,14 @@ class HordeActiveSyncBaseTables extends Horde_Db_Migration_Base
             $this->addIndex('horde_activesync_map', array('sync_user'));
         }
         if (!in_array('horde_activesync_device', $this->tables())) {
-            $t = $this->createTable('horde_activesync_device', array('autoincrementKey' => array('device_id')));
+            $t = $this->createTable('horde_activesync_device', array('autoincrementKey' => false));
             $t->column('device_id', 'string', array('limit' => 255, 'null' => false));
             $t->column('device_type', 'string', array('limit' => 255, 'null' => false));
             $t->column('device_agent', 'string', array('limit' => 255, 'null' => false));
             $t->column('device_supported', 'text');
             $t->column('device_policykey', 'bigint', array('default' => 0));
             $t->column('device_rwstatus', 'integer');
+            $t->primaryKey(array('device_id'));
             $t->end();
         }
         if (!in_array('horde_activesync_device_users', $this->tables())) {
