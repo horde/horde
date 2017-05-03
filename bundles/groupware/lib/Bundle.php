4a8447bafdfe36676259cf01908d8036a6adda0d
Jan Schneider <jan@horde.org>
2017-05-03 16:51:03 +0200

    Released groupware-5.2.20

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '5.2.20-git';
+    const VERSION = '5.2.20';
 
     /**
      * The bundle descriptive name.
