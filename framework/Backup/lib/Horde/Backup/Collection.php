cd4bc9730c2d693848c9846f6613611a1aa2497d
Jan Schneider <jan@horde.org>
2017-05-05 13:02:08 +0200

    Pass a traversable directly.
    This allows applications to pass an existing iterator, and not necessarily having to implement the Iterator interface.

--- framework/Backup/lib/Horde/Backup/Collection.php
+++ framework/Backup/lib/Horde/Backup/Collection.php
@@ -13,12 +13,14 @@
 
 namespace Horde\Backup;
 
+use IteratorIterator;
+use Traversable;
+
 /**
  * A collection of backup objects of a certain application-specific type.
  *
  * Each application should extend this class for each type of backup data it
- * supports. The concrete class implementation must also implement the Iterator
- * methods so that the backup data can be returned on demand.
+ * supports.
  *
  * @author    Jan Schneider <jan@horde.org>
  * @category  Horde
@@ -26,7 +28,7 @@ namespace Horde\Backup;
  * @license   http://www.horde.org/licenses/bsd BSD
  * @package   Backup
  */
-abstract class Collection implements Iterator
+abstract class Collection extends IteratorIterator
 {
     /**
      * The collection's user.
@@ -40,8 +42,9 @@ abstract class Collection implements Iterator
      *
      * @param string $user  A user name.
      */
-    public function __construct($user)
+    public function __construct(Traversable $iterator, $user)
     {
+        parent::__construct($iterator);
         $this->_user = $user;
     }
 
