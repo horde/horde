5bc4a61fd8be9e377082d99635a447186232564f
Jan Schneider <jan@horde.org>
2017-05-17 10:40:16 +0200

    Forgot to add these.

---
+++ framework/Backup/lib/Horde/Backup/Reader/CompressIterator/Tar.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright 2017 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Backup
+ */
+
+namespace Horde\Backup\Reader\CompressIterator;
+
+use Horde\Backup\Reader\CompressIterator;
+
+/**
+ * Iterates over certain files in a TAR archive.
+ *
+ * @author    Jan Schneider <jan@horde.org>
+ * @category  Horde
+ * @copyright 2017 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd BSD
+ * @package   Backup
+ */
+class Tar extends CompressIterator
+{
+    /**
+     * Returns the unpacked backup data.
+     *
+     * @return mixed  Backup data.
+     */
+    public function current()
+    {
+        return $this->_packer->unpack($this->_info[$this->key()]['data']);
+    }
+}
---
+++ framework/Backup/lib/Horde/Backup/Reader/CompressIterator/Zip.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright 2017 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Backup
+ */
+
+namespace Horde\Backup\Reader\CompressIterator;
+
+use Horde_Compress_Zip as Compress;
+use Horde\Backup\Reader\CompressIterator;
+
+/**
+ * Iterates over certain files in a ZIP archive.
+ *
+ * @author    Jan Schneider <jan@horde.org>
+ * @category  Horde
+ * @copyright 2017 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd BSD
+ * @package   Backup
+ */
+class Zip extends CompressIterator
+{
+    /**
+     * The ZIP file contents.
+     *
+     * @var string
+     */
+    protected $_contents;
+
+    /**
+     * The ZIP uncompressor.
+     *
+     * @var Horde_Compress_Zip
+     */
+    protected $_compress;
+
+    /**
+     * Constructor.
+     *
+     * @param string $application  An application name.
+     * @param string $type         A collection type like "calendar" or
+     *                             "contact".
+     * @param array $info          ZIP archive info from Horde_Compress_Zip.
+     * @param string $contents     The ZIP file contents.
+     */
+    public function __construct($application, $type, $info, $contents)
+    {
+        $this->_contents = $contents;
+        $this->_compress = new Compress();
+        parent::__construct($application, $type, $info);
+    }
+
+    /**
+     * Returns the unpacked backup data.
+     *
+     * @return mixed  Backup data.
+     */
+    public function current()
+    {
+        return $this->_packer->unpack(
+            $this->_compress->decompress(
+                $this->_contents,
+                array(
+                    'action' => Compress::ZIP_DATA,
+                    'info' => $this->_info,
+                    'key' => $this->key()
+                )
+            )
+        );
+    }
+}
