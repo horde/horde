c6941908a8ad0de9cc283502d5434325a5ada6e4
Jan Schneider <jan@horde.org>
2017-08-01 15:57:04 +0200

    Released whups-3.0.11

--- whups/lib/Application.php
+++ whups/lib/Application.php
@@ -36,7 +36,7 @@ class Whups_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (3.0.11-git)';
+    public $version = 'H5 (3.0.11)';
 
     /**
      * Global variables defined:
