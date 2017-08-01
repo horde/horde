e95a1fe08c2ce481ddd5fb04847d4692fe3015bb
Jan Schneider <jan@horde.org>
2017-08-01 17:44:16 +0200

    Released groupware-5.2.21

--- bundles/groupware/lib/Bundle.php
+++ bundles/groupware/lib/Bundle.php
@@ -19,7 +19,7 @@ class Horde_Bundle extends Horde_Core_Bundle
     /**
      * The bundle version.
      */
-    const VERSION = '5.2.21-git';
+    const VERSION = '5.2.21';
 
     /**
      * The bundle descriptive name.
