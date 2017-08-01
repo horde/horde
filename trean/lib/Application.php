56393a5b6e3e9d73e2ac072da9f8c0f4ae8a5ab3
Jan Schneider <jan@horde.org>
2017-08-01 13:28:17 +0200

    Released trean-1.1.8

--- trean/lib/Application.php
+++ trean/lib/Application.php
@@ -33,7 +33,7 @@ class Trean_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (1.1.8-git)';
+    public $version = 'H5 (1.1.8)';
 
     /**
      * Global variables defined:
