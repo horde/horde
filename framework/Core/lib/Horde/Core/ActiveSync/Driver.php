1d98fbd5f57cf21932dac7dd61efd89dd802cfc0
Michael J Rubinsky <mrubinsk@horde.org>
2017-08-19 12:25:42 -0400

    Close output buffer before (re)throwing Exception_NotFound.
    Fixes breaking Sync when an object is not found.

--- framework/Core/lib/Horde/Core/ActiveSync/Driver.php
+++ framework/Core/lib/Horde/Core/ActiveSync/Driver.php
@@ -1326,6 +1326,8 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
                     $message->setUid($id);
                 }
             } catch (Horde_Exception_NotFound $e) {
+                $this->_logger->err($e->getMessage());
+                $this->_endBuffer();
                 throw $e;
             } catch (Horde_Exception $e) {
                 $this->_logger->err($e->getMessage());
@@ -1339,6 +1341,8 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
             try {
                 $recipients = $this->_connector->contacts_search($id[0], array('recipient_cache_search' => true));
             } catch (Horde_Exception_NotFound $e) {
+                $this->_logger->err($e->getMessage());
+                $this->_endBuffer();
                 throw $e;
             } catch (Horde_Exception $e) {
                 $this->_logger->err($e->getMessage());
@@ -1369,6 +1373,8 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
                     'mimesupport' => $collection['mimesupport'],
                     'device' => $this->_device));
             } catch (Horde_Exception_NotFound $e) {
+                $this->_logger->err($e->getMessage());
+                $this->_endBuffer();
                 throw $e;
             } catch (Horde_Exception $e) {
                 $this->_logger->err($e->getMessage());
@@ -1385,6 +1391,8 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
                     'bodyprefs' => $collection['bodyprefs'],
                     'mimesupport' => $collection['mimesupport']));
             } catch (Horde_Exception_NotFound $e) {
+                $this->_logger->err($e->getMessage());
+                $this->_endBuffer();
                 throw $e;
             } catch (Horde_Exception $e) {
                 $this->_logger->err($e->getMessage());
@@ -1401,6 +1409,8 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
                     'bodyprefs' => $collection['bodyprefs'],
                     'mimesupport' => $collection['mimesupport']));
             } catch (Horde_Exception_NotFound $e) {
+                $this->_logger->err($e->getMessage());
+                $this->_endBuffer();
                 throw $e;
             } catch (Horde_Exception $e) {
                 $this->_logger->err($e->getMessage());
