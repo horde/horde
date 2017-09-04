f8bbefdb445773d851083ede7e348a9f9507d431
Jan Schneider <jan@horde.org>
2017-09-04 17:59:17 +0200

    Re-merge Horde customizations and extensions.

--- wicked/lib/Text_Wiki/Text/Wiki/Render/Xhtml/Interwiki.php
+++ wicked/lib/Text_Wiki/Text/Wiki/Render/Xhtml/Interwiki.php
@@ -1,103 +1,56 @@
 <?php
-// vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 /**
- * Interwiki rule end renderer for Xhtml
- *
- * PHP versions 4 and 5
- *
- * @category   Text
- * @package    Text_Wiki
- * @author     Paul M. Jones <pmjones@php.net>
- * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
- * @version    CVS: $Id$
- * @link       http://pear.php.net/package/Text_Wiki
+ * @package Wicked
  */
-
-/**
- * This class renders inter wikis links in XHTML.
- *
- * @category   Text
- * @package    Text_Wiki
- * @author     Paul M. Jones <pmjones@php.net>
- * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
- * @version    Release: @package_version@
- * @link       http://pear.php.net/package/Text_Wiki
- */
-class Text_Wiki_Render_Xhtml_Interwiki extends Text_Wiki_Render {
-
-    var $conf = array(
+class Text_Wiki_Render_Xhtml_Interwiki extends Text_Wiki_Render
+{
+    public $conf = array(
         'sites' => array(
             'MeatBall' => 'http://www.usemod.com/cgi-bin/mb.pl?%s',
             'Advogato' => 'http://advogato.org/%s',
-            'Wiki'       => 'http://c2.com/cgi/wiki?%s'
+            'Wiki'     => 'http://c2.com/cgi/wiki?%s',
+            'Bookshelved' => 'http://bookshelved.org/cgi-bin/wiki.pl?%s'
         ),
-        'target' => '_blank',
-        'css' => null
+        'target' => '_blank'
     );
 
-
     /**
-    *
-    * Renders a token into text matching the requested format.
-    *
-    * @access public
-    *
-    * @param array $options The "options" portion of the token (second
-    * element).
-    *
-    * @return string The text rendered from the token options.
-    *
-    */
-
-    function token($options)
+     * Renders a token into text matching the requested format.
+     *
+     * @param array $options  The "options" portion of the token (second
+     *                        element).
+     *
+     * @return string  The text rendered from the token options.
+     */
+    public function token($options)
     {
+        $site = $options['site'];
+        $page = $options['page'];
         $text = $options['text'];
-        if (isset($options['url'])) {
-            // calculated by the parser (e.g. Mediawiki)
-            $href = $options['url'];
+
+        if (isset($this->conf['sites'][$site])) {
+            $href = $this->conf['sites'][$site];
         } else {
-            $site = $options['site'];
-            // toggg 2006/02/05 page name must be url encoded (e.g. may contain spaces)
-            $page = $this->urlEncode($options['page']);
+            return $text;
+        }
 
-            if (isset($this->conf['sites'][$site])) {
-                $href = $this->conf['sites'][$site];
-            } else {
-                return $text;
-            }
-
-            // old form where page is at end,
-            // or new form with %s placeholder for sprintf()?
-            if (strpos($href, '%s') === false) {
-                // use the old form
-                $href = $href . $page;
-            } else {
-                // use the new form
-                $href = sprintf($href, $page);
-            }
+        // old form where page is at end,
+        // or new form with %s placeholder for sprintf()?
+        if (strpos($href, '%s') === false) {
+            // use the old form
+            $href = $href . $page;
+        } else {
+            // use the new form
+            $href = sprintf($href, $page);
         }
 
         // allow for alternative targets
-        $target = $this->getConf('target');
+        $target = $this->getConf('target', '');
 
-        // build base link
-        $css = $this->formatConf(' class="%s"', 'css');
-        $text = $this->textEncode($text);
-        $output = "<a$css href=\"$href\"";
-
-        // are we targeting a specific window?
-        if ($target && $target != '_self') {
-            // this is XHTML compliant, suggested by Aaron Kalin.
-            // code tip is actually from youngpup.net, and it
-            // uses the $target as the new window name.
-            $target = $this->textEncode($target);
-            $output .= " onclick=\"window.open(this.href, '$target');";
-            $output .= " return false;\"";
+        if ($target && trim($target) != '') {
+            $target = " target=\"$target\"";
         }
 
-        $output .= ">$text</a>";
-
-        return $output;
+        return '<a' . $target . ' href="' . Horde::externalUrl($href) . '">' . $text . '</a>';
     }
 }
-?>
--- wicked/lib/Text_Wiki/Text/Wiki/Render/Xhtml/Url.php
+++ wicked/lib/Text_Wiki/Text/Wiki/Render/Xhtml/Url.php
@@ -1,131 +1,47 @@
 <?php
-// vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 /**
- * Url rule end renderer for Xhtml
- *
- * PHP versions 4 and 5
- *
- * @category   Text
- * @package    Text_Wiki
- * @author     Paul M. Jones <pmjones@php.net>
- * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
- * @version    CVS: $Id$
- * @link       http://pear.php.net/package/Text_Wiki
+ * @package Wicked
  */
-
-/**
- * This class renders URL links in XHTML.
- *
- * @category   Text
- * @package    Text_Wiki
- * @author     Paul M. Jones <pmjones@php.net>
- * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
- * @version    Release: @package_version@
- * @link       http://pear.php.net/package/Text_Wiki
- */
-class Text_Wiki_Render_Xhtml_Url extends Text_Wiki_Render {
-
-
-    var $conf = array(
-        'target' => '_blank',
-        'images' => true,
-        'img_ext' => array('jpg', 'jpeg', 'gif', 'png'),
-        'css_inline' => null,
-        'css_footnote' => null,
-        'css_descr' => null,
-        'css_img' => null
+class Text_Wiki_Render_Xhtml_Url extends Text_Wiki_Render
+{
+    public $conf = array(
+        'target' => '_blank'
     );
 
     /**
-    *
-    * Renders a token into text matching the requested format.
-    *
-    * @access public
-    *
-    * @param array $options The "options" portion of the token (second
-    * element).
-    *
-    * @return string The text rendered from the token options.
-    *
-    */
-
-    function token($options)
+     * Renders a token into text matching the requested format.
+     *
+     * @param array $options  The "options" portion of the token (second
+     *                        element).
+     *
+     * @return string  The text rendered from the token options.
+     */
+    public function token($options)
     {
-        // create local variables from the options array (text,
-        // href, type)
+        // Create local variables from the options array (text, href,
+        // type).
         extract($options);
 
-        // find the rightmost dot and determine the filename
+        // Find the rightmost dot and determine the filename
         // extension.
         $pos = strrpos($href, '.');
-        $ext = strtolower(substr($href, $pos + 1));
-        $href = $this->textEncode($href);
+        $ext = Horde_String::lower(substr($href, $pos + 1));
+        $href = htmlspecialchars($href);
 
-        // does the filename extension indicate an image file?
-        if ($this->getConf('images') &&
-            in_array($ext, $this->getConf('img_ext', array()))) {
-
-            // create alt text for the image
-            if (! isset($text) || $text == '') {
-                $text = basename($href);
-                $text = $this->textEncode($text);
-            }
-
-            // generate an image tag
-            $css = $this->formatConf(' class="%s"', 'css_img');
-            $start = "<img$css src=\"$href\" alt=\"$text\" title=\"$text\" /><!-- ";
-            $end = " -->";
-
+        // Allow for alternative targets on non-anchor HREFs.
+        if ($href[0] == '#') {
+            $target = '';
         } else {
-
-            // should we build a target clause?
-            if ($href{0} == '#' ||
-              strtolower(substr($href, 0, 7)) == 'mailto:') {
-              // targets not allowed for on-page anchors
-              // and mailto: links.
-                $target = '';
-            } else {
-        // allow targets on non-anchor non-mailto links
-                $target = $this->getConf('target');
-            }
-
-            // generate a regular link (not an image)
-            $text = $this->textEncode($text);
-            $css = $this->formatConf(' class="%s"', "css_$type");
-            $start = "<a$css href=\"$href\"";
-
-            if ($target && $target != '_self') {
-                // use a "popup" window.  this is XHTML compliant, suggested by
-                // Aaron Kalin.  uses the $target as the new window name.
-                $target = $this->textEncode($target);
-                $start .= " onclick=\"window.open(this.href, '$target');";
-                $start .= " return false;\"";
-            }
-            
-            if (isset($name)) {
-                $start .= " id=\"$name\"";
-            }
-
-            // finish up output
-            $start .= ">";
-            $end = "</a>";
-
-            // make numbered references look like footnotes when no
-            // CSS class specified, make them superscript by default
-            if ($type == 'footnote' && ! $css) {
-                $start = '<sup>' . $start;
-                $end = $end . '</sup>';
-            }
+            $target = $this->getConf('target', '');
         }
 
-        if ($options['type'] == 'start') {
-            $output = $start;
-        } else if ($options['type'] == 'end') {
-            $output = $end;
-        } else {
-            $output = $start . $text . $end;
+        $output = Horde::link(Horde::externalUrl($href), $href, 'external', htmlspecialchars($target)) . htmlspecialchars($text) . '</a>';
+
+        // Make numbered references look like footnotes.
+        if ($type == 'footnote') {
+            $output = '<sup>' . $output . '</sup>';
         }
+
         return $output;
     }
 }
-?>
