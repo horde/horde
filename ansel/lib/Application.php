68661ec46f25444ff86f69c9db9eb5c3b8c5bdbb
Jan Schneider <jan@horde.org>
2017-09-19 14:31:09 +0200

    Released ansel-3.0.8

--- ansel/lib/Application.php
+++ ansel/lib/Application.php
@@ -39,7 +39,7 @@ class Ansel_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.8-git)';
+    public $version = 'H5 (3.0.8)';
 
     /**
      * Global variables defined:
