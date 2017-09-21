cb34dc12e370a263ecbc708e5ad6797fa0066c07
Jan Schneider <jan@horde.org>
2017-09-21 14:18:39 +0200

    Released nag-4.2.17

--- nag/lib/Application.php
+++ nag/lib/Application.php
@@ -43,7 +43,7 @@ class Nag_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.2.17-git)';
+    public $version = 'H5 (4.2.17)';
 
     /**
      * Global variables defined:
