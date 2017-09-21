4e8642d754cac676ac3b6949cd2d94ce751d82ad
Jan Schneider <jan@horde.org>
2017-09-21 14:16:52 +0200

    Released gollem-3.0.12

--- gollem/lib/Application.php
+++ gollem/lib/Application.php
@@ -50,7 +50,7 @@ class Gollem_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (3.0.12-git)';
+    public $version = 'H5 (3.0.12)';
 
     /**
      * Server key used in logged out session.
